8842ca9089abbf36bd1dcb5fb5860e31e8c72096
Martin Edenhofer <martin.edenhofer@otrs.com>
2006-03-12 20:26:37 +0000

    fixed typos

--- Kernel/Language/de_PublicSurvey.pm
+++ Kernel/Language/de_PublicSurvey.pm
@@ -12,6 +12,6 @@ sub Data {
     $Self->{Translation}->{'Finish'} = 'Fertigstellen';
     $Self->{Translation}->{'finished'} = 'fertiggestellt';
     $Self->{Translation}->{'This Survey-Key is invalid!'} = 'Dieser Umfrage-Schlüssel ist nicht (mehr) gültig!';
-    $Self->{Translation}->{'Thank you for your feedback.'} = 'Sie haben diese Umfrage abegschlossen. Vielen Dank.';
+    $Self->{Translation}->{'Thank you for your feedback.'} = 'Sie haben diese Umfrage abgeschlossen. Vielen Dank.';
 }
 1;
