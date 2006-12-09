d5ef64add3d35fa63d24fab8ca33a07fbbcb1463
Manuel Hecht <manuel.hecht@otrs.com>
2006-12-09 00:36:32 +0000

    use OptionStrgHashRef() for pulldown

--- Kernel/Modules/AgentSurvey.pm
+++ Kernel/Modules/AgentSurvey.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentSurvey.pm - a survey module
 # Copyright (C) 2003-2006 OTRS GmbH, http://otrs.com/
 # --
-# $Id: AgentSurvey.pm,v 1.27 2006-11-21 23:23:43 mh Exp $
+# $Id: AgentSurvey.pm,v 1.28 2006-12-09 00:36:32 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use Kernel::System::Survey;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.27 $';
+$VERSION = '$Revision: 1.28 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 sub new {
@@ -204,45 +204,29 @@ sub Run {
             }
         }
         # output the possible status
+        my %NewStatus;
         if ($Survey{Status} eq 'New' || $Survey{Status} eq 'Invalid') {
-            $Survey{NewStatus} = 'Master';
-            $Self->{LayoutObject}->Block(
-                Name => 'SurveyStatus',
-                Data => {%Survey},
-            );
-
-            $Survey{NewStatus} = 'Valid';
-            $Self->{LayoutObject}->Block(
-                Name => 'SurveyStatus',
-                Data => {%Survey},
-            );
+            $NewStatus{'Master'} = 'Master';
+            $NewStatus{'Valid'} = 'Valid';
         }
         elsif ($Survey{Status} eq 'Valid') {
-            $Survey{NewStatus} = 'Master';
-            $Self->{LayoutObject}->Block(
-                Name => 'SurveyStatus',
-                Data => {%Survey},
-            );
-
-            $Survey{NewStatus} = 'Invalid';
-            $Self->{LayoutObject}->Block(
-                Name => 'SurveyStatus',
-                Data => {%Survey},
-            );
+            $NewStatus{'Master'} = 'Master';
+            $NewStatus{'Invalid'} = 'Invalid';
         }
         elsif ($Survey{Status} eq 'Master') {
-            $Survey{NewStatus} = 'Valid';
-            $Self->{LayoutObject}->Block(
-                Name => 'SurveyStatus',
-                Data => {%Survey},
-            );
-
-            $Survey{NewStatus} = 'Invalid';
-            $Self->{LayoutObject}->Block(
-                Name => 'SurveyStatus',
-                Data => {%Survey},
-            );
+            $NewStatus{'Valid'} = 'Valid';
+            $NewStatus{'Invalid'} = 'Invalid';
         }
+        my $NewStatusStr = $Self->{LayoutObject}->OptionStrgHashRef(
+            Name => 'NewStatus',
+            Data => \%NewStatus,
+        );
+        $Self->{LayoutObject}->Block(
+            Name => 'SurveyStatus',
+            Data => {
+                NewStatusStr => $NewStatusStr,
+            },
+        );
 
         $Output .= $Self->{LayoutObject}->Output(
             TemplateFile => 'AgentSurvey',
