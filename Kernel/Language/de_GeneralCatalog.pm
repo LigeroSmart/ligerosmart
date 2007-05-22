702ae7aed5a401be0ac342ac16afbcd205ba2ac6
Manuel Hecht <manuel.hecht@otrs.com>
2007-05-22 07:41:07 +0000

    Update translation.

--- Kernel/Language/de_GeneralCatalog.pm
+++ Kernel/Language/de_GeneralCatalog.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de_GeneralCatalog.pm - the german translation of GeneralCatalog
 # Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
 # --
-# $Id: de_GeneralCatalog.pm,v 1.4 2007-05-21 15:37:51 mh Exp $
+# $Id: de_GeneralCatalog.pm,v 1.5 2007-05-22 07:41:07 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -20,10 +20,9 @@ sub Data {
     $Self->{Translation}->{'General Catalog Management'} = 'Allgemeiner Katalog Verwaltung';
     $Self->{Translation}->{'Catalog Class'} = 'Katalog Klasse';
     $Self->{Translation}->{'Add a new Catalog Class.'} = 'Eine neue Katalog Klasse hinzufügen.';
+    $Self->{Translation}->{'Add Catalog Item'} = 'Katalog Eintrag hinzufügen';
+    $Self->{Translation}->{'Add Catalog Class'} = 'Katalog Klasse hinzufügen';
     $Self->{Translation}->{'Functionality'} = 'Funktionalität';
-    $Self->{Translation}->{'Add new item failed! See System Log for details.'} = 'Hinzufügen des neuen Eintrags fehlgeschlagen! Im System Log finden Sie weitere Informationen.';
-    $Self->{Translation}->{'Add new class failed! Class name not valid.'} = 'Hinzufügen der neuen Klasse fehlgeschlagen! Name der Klasse ungültig.';
-    $Self->{Translation}->{'Update item faild! See System Log for details.'} = 'Updaten des Eintrags fehlgeschlagen! Im System Log finden Sie weitere Informationen.';
 }
 
 1;
