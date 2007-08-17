b94360e2342a89ce58093233f060fb7da38c143a
Manuel Hecht <manuel.hecht@otrs.com>
2007-08-17 10:44:26 +0000

    Fixed bug# 2186 - Survey requests are send twice.

--- Kernel/System/Ticket/Event/SurveySendRequest.pm
+++ Kernel/System/Ticket/Event/SurveySendRequest.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Ticket/Event/SurveySendRequest.pm - send survey requests
 # Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
 # --
-# $Id: SurveySendRequest.pm,v 1.7 2007-08-17 10:12:33 mh Exp $
+# $Id: SurveySendRequest.pm,v 1.8 2007-08-17 10:44:26 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::Survey;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.7 $';
+$VERSION = '$Revision: 1.8 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 sub new {
@@ -47,7 +47,7 @@ sub Run {
             return;
         }
     }
-    if ($Param{Event} eq 'StateSet' || $Param{Event} eq 'TicketStateUpdate') {
+    if ($Param{Event} eq 'TicketStateUpdate') {
         my %Ticket = $Self->{TicketObject}->TicketGet(
             TicketID => $Param{TicketID},
         );
