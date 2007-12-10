acc54fc8035c7bd8d339e82d4d4b75c1ef7ad901
Manuel Hecht <manuel.hecht@otrs.com>
2007-12-10 14:18:34 +0000

    Add czech translation files.

---
+++ Kernel/Language/cz_GeneralCatalog.pm
@@ -0,0 +1,35 @@
+# --
+# Kernel/Language/cz_GeneralCatalog.pm - the czech translation of GeneralCatalog
+# Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
+# --
+# $Id: cz_GeneralCatalog.pm,v 1.2 2007-12-10 14:18:34 mh Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+
+package Kernel::Language::cz_GeneralCatalog;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.2 $) [1];
+
+sub Data {
+    my ($Self) = @_;
+
+    $Self->{Translation}->{'General Catalog'} = 'Všeobecný katalog';
+    $Self->{Translation}->{'General Catalog Management'}
+        = 'Řízení všeobecného katalogu';
+    $Self->{Translation}->{'Catalog Class'}            = 'Třídy v katalogu';
+    $Self->{Translation}->{'Add a new Catalog Class.'} = 'Přidat novou třídu do katalogu.';
+    $Self->{Translation}->{'Add Catalog Item'}         = 'Přidat prvek do katalogu';
+    $Self->{Translation}->{'Add Catalog Class'}        = 'Přidat třídu do katalogu';
+    $Self->{Translation}->{'Functionality'}            = 'Funkcionalita';
+
+    return 1;
+}
+
+1;
