b37a49bda38e0b2bc75ddea0e9b158a3f249a4cf
Manuel Hecht <manuel.hecht@otrs.com>
2008-02-06 17:46:58 +0000

    Init.

---
+++ Kernel/Output/HTML/ImportExportLayoutDTL.pm
@@ -0,0 +1,111 @@
+# --
+# Kernel/Output/HTML/ImportExportLayoutDTL.pm - layout backend module
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: ImportExportLayoutDTL.pm,v 1.1 2008-02-06 17:46:58 mh Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package Kernel::Output::HTML::ImportExportLayoutDTL;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::Output::HTML::ImportExportLayoutDTL - layout backend module
+
+=head1 SYNOPSIS
+
+All layout functions for display DTL code
+
+=over 4
+
+=cut
+
+=item new()
+
+create a object
+
+    $BackendObject = Kernel::Output::HTML::ImportExportLayoutDTL->new(
+        %Param,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (qw(ConfigObject LogObject MainObject ParamObject LayoutObject)) {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    return $Self;
+}
+
+=item FormInputCreate()
+
+create a input string
+
+    my $Value = $BackendObject->FormInputCreate(
+        Item   => $ItemRef,
+    );
+
+=cut
+
+sub FormInputCreate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{Item} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => 'Need Item!' );
+        return;
+    }
+
+    return $Param{Item}->{Input}->{Data};
+}
+
+=item FormDataGet()
+
+get form data
+
+    my $FormData = $BackendObject->FormDataGet();
+
+=cut
+
+sub FormDataGet {
+    my ( $Self, %Param ) = @_;
+
+    return;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (http://otrs.org/).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2008-02-06 17:46:58 $
+
+=cut
