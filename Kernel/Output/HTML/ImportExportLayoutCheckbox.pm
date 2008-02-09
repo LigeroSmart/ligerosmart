7e2d177c4f01fffc645b41ca767fe86a4535e6a1
Manuel Hecht <manuel.hecht@otrs.com>
2008-02-09 20:06:57 +0000

    Init.

---
+++ Kernel/Output/HTML/ImportExportLayoutCheckbox.pm
@@ -0,0 +1,139 @@
+# --
+# Kernel/Output/HTML/ImportExportLayoutCheckbox.pm - layout backend module
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: ImportExportLayoutCheckbox.pm,v 1.1 2008-02-09 20:06:57 mh Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package Kernel::Output::HTML::ImportExportLayoutCheckbox;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::Output::HTML::ImportExportLayoutCheckbox - layout backend module
+
+=head1 SYNOPSIS
+
+All layout functions for checkbox elements
+
+=over 4
+
+=cut
+
+=item new()
+
+create a object
+
+    $BackendObject = Kernel::Output::HTML::ImportExportLayoutCheckbox->new(
+        %Param,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (qw(ConfigObject LogObject MainObject ParamObject LayoutObject)) {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    return $Self;
+}
+
+=item FormInputCreate()
+
+create a input string
+
+    my $Value = $BackendObject->FormInputCreate(
+        Item   => $ItemRef,
+        Prefix => 'Prefix::',  # (optional)
+        Value  => 'Value',     # (optional)
+    );
+
+=cut
+
+sub FormInputCreate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{Item} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => 'Need Item!' );
+        return;
+    }
+
+    $Param{Prefix} ||= '';
+
+    my $String = "<input type=\"checkbox\" name=\"$Param{Prefix}$Param{Item}->{Key}\" ";
+
+    if ($Param{Value}) {
+        $String .= "checked ";
+    }
+
+    $String .= "> ";
+
+    return $String;
+}
+
+=item FormDataGet()
+
+get form data
+
+    my $FormData = $BackendObject->FormDataGet(
+        Item   => $ItemRef,
+        Prefix => 'Prefix::',  # (optional)
+    );
+
+=cut
+
+sub FormDataGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{Item} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => 'Need Item!' );
+        return;
+    }
+
+    $Param{Prefix} ||= '';
+
+    # get form data
+    my $FormData = $Self->{ParamObject}->GetParam(
+        Param => $Param{Prefix} . $Param{Item}->{Key},
+    );
+
+    return $FormData;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (http://otrs.org/).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2008-02-09 20:06:57 $
+
+=cut
