9fc6b3d5886971e95e00bac459e9ccaf18ef3cb9
Manuel Hecht <manuel.hecht@otrs.com>
2008-04-04 11:47:38 +0000

    Improved file read mode.

--- bin/ImportExport.pl
+++ bin/ImportExport.pl
@@ -3,7 +3,7 @@
 # ImportExport.pl - import/export script
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: ImportExport.pl,v 1.4 2008-04-04 10:18:25 mh Exp $
+# $Id: ImportExport.pl,v 1.5 2008-04-04 11:47:38 mh Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -35,7 +35,7 @@ use Kernel::System::Log;
 use Kernel::System::Main;
 
 use vars qw($VERSION $RealBin);
-$VERSION = qw($Revision: 1.4 $) [1];
+$VERSION = qw($Revision: 1.5 $) [1];
 
 # get options
 my %Opts;
@@ -111,6 +111,7 @@ if ( lc $Opts{a} eq 'import' ) {
         $SourceContent = $CommonObject{MainObject}->FileRead(
             Location => $Opts{i},
             Result   => 'SCALAR',
+            Mode     => 'binmode',
         );
 
         die "Can't read file $Opts{i}.\nImport aborted.\n" if !$SourceContent;
@@ -184,6 +185,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =head1 VERSION
 
-$Revision: 1.4 $ $Date: 2008-04-04 10:18:25 $
+$Revision: 1.5 $ $Date: 2008-04-04 11:47:38 $
 
 =cut
