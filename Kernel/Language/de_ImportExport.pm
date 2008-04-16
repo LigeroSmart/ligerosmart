69a1af4e43e7b24f36236c82ad1270b8635d6e75
Manuel Hecht <manuel.hecht@otrs.com>
2008-04-16 14:29:57 +0000

    Updated translation.

--- Kernel/Language/de_ImportExport.pm
+++ Kernel/Language/de_ImportExport.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de_ImportExport.pm - the german translation of ImportExport
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: de_ImportExport.pm,v 1.15 2008-04-04 11:47:08 mh Exp $
+# $Id: de_ImportExport.pm,v 1.16 2008-04-16 14:29:57 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.15 $) [1];
+$VERSION = qw($Revision: 1.16 $) [1];
 
 sub Data {
     my ($Self) = @_;
@@ -23,30 +23,27 @@ sub Data {
 
     return if ref $Lang ne 'HASH';
 
-    $Lang->{'Import/Export'}                 = 'Import/Export';
-    $Lang->{'Import/Export Management'}      = 'Import/Export Verwaltung';
-    $Lang->{'Add mapping template'}          = 'Mapping-Template hinzufügen';
-    $Lang->{'Start Import'}                  = 'Import starten';
-    $Lang->{'Start Export'}                  = 'Export starten';
-    $Lang->{'Step'}                          = 'Schritt';
-    $Lang->{'Edit common information'}       = 'Allgemeine Informationen bearbeiten';
-    $Lang->{'Edit object information'}       = 'Objekt-Informationen bearbeiten';
-    $Lang->{'Edit format information'}       = 'Format-Informationen bearbeiten';
-    $Lang->{'Edit mapping information'}      = 'Mapping-Informationen bearbeiten';
-    $Lang->{'Edit search information'}       = 'Such-Informationen bearbeiten';
-    $Lang->{'Import information'}            = 'Import Informationen';
-    $Lang->{'Column'}                        = 'Spalte';
-    $Lang->{'Column Seperator'}              = 'Spaltentrenner';
-    $Lang->{'Charset'}                       = 'Zeichensatz';
-    $Lang->{'Maximum number of one element'} = 'Maximale Anzahl eines Elements';
-    $Lang->{'Only import changed datasets'}  = 'Nur geänderte Datensätze importieren';
-    $Lang->{'Tabulator (TAB)'}               = 'Tabulator (TAB)';
-    $Lang->{'Semicolon (;)'}                 = 'Semicolon (;)';
-    $Lang->{'Colon (:)'}                     = 'Doppelpunkt (:)';
-    $Lang->{'Dot (.)'}                       = 'Punkt (.)';
-    $Lang->{'Identifier'}                    = 'Identifikator';
-    $Lang->{'Restrict export per search'}    = 'Export per Suche eischränken';
-    $Lang->{'Source File'}                   = 'Quell-Datei';
+    $Lang->{'Import/Export'}              = 'Import/Export';
+    $Lang->{'Import/Export Management'}   = 'Import/Export Verwaltung';
+    $Lang->{'Add mapping template'}       = 'Mapping-Template hinzufügen';
+    $Lang->{'Start Import'}               = 'Import starten';
+    $Lang->{'Start Export'}               = 'Export starten';
+    $Lang->{'Step'}                       = 'Schritt';
+    $Lang->{'Edit common information'}    = 'Allgemeine Informationen bearbeiten';
+    $Lang->{'Edit object information'}    = 'Objekt-Informationen bearbeiten';
+    $Lang->{'Edit format information'}    = 'Format-Informationen bearbeiten';
+    $Lang->{'Edit mapping information'}   = 'Mapping-Informationen bearbeiten';
+    $Lang->{'Edit search information'}    = 'Such-Informationen bearbeiten';
+    $Lang->{'Import information'}         = 'Import Informationen';
+    $Lang->{'Column'}                     = 'Spalte';
+    $Lang->{'Restrict export per search'} = 'Export per Suche eischränken';
+    $Lang->{'Source File'}                = 'Quell-Datei';
+    $Lang->{'Column Seperator'}           = 'Spaltentrenner';
+    $Lang->{'Tabulator (TAB)'}            = 'Tabulator (TAB)';
+    $Lang->{'Semicolon (;)'}              = 'Semicolon (;)';
+    $Lang->{'Colon (:)'}                  = 'Doppelpunkt (:)';
+    $Lang->{'Dot (.)'}                    = 'Punkt (.)';
+    $Lang->{'Charset'}                    = 'Zeichensatz';
 
     return 1;
 }
