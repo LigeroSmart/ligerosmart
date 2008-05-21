5ed238979a7c57914df42292be68cbf99e362d61
Manuel Hecht <manuel.hecht@otrs.com>
2008-05-21 09:13:15 +0000

    Improved methode to unpack @_

--- Kernel/Language/bg_ImportExport.pm
+++ Kernel/Language/bg_ImportExport.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/bg_ImportExport.pm - the bulgarian translation of ImportExport
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: bg_ImportExport.pm,v 1.3 2008-03-19 15:10:04 mh Exp $
+# $Id: bg_ImportExport.pm,v 1.4 2008-05-21 09:13:15 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,10 +15,11 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub Data {
-    my ($Self) = @_;
+    my $Self = shift;
+
     my $Lang = $Self->{Translation};
 
     return if ref $Lang ne 'HASH';
--- Kernel/Language/de_ImportExport.pm
+++ Kernel/Language/de_ImportExport.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de_ImportExport.pm - the german translation of ImportExport
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: de_ImportExport.pm,v 1.16 2008-04-16 14:29:57 mh Exp $
+# $Id: de_ImportExport.pm,v 1.17 2008-05-21 09:13:15 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,10 +15,11 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.16 $) [1];
+$VERSION = qw($Revision: 1.17 $) [1];
 
 sub Data {
-    my ($Self) = @_;
+    my $Self = shift;
+
     my $Lang = $Self->{Translation};
 
     return if ref $Lang ne 'HASH';
