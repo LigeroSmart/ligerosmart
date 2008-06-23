3f2b3985b3f242d51dd028b75b34dadd0c28cf33
Udo Bretz <udo.bretz@otrs.com>
2008-06-23 21:51:35 +0000

    removed old LinkObject2 code.

--- Kernel/Modules/AgentITSMLocationZoom.pm
+++ Kernel/Modules/AgentITSMLocationZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMLocationZoom.pm - the OTRS::ITSM Location zoom module
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentITSMLocationZoom.pm,v 1.1 2008-06-21 16:50:14 ub Exp $
+# $Id: AgentITSMLocationZoom.pm,v 1.2 2008-06-23 21:51:35 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::ITSMLocation;
 use Kernel::System::LinkObject;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -104,105 +104,6 @@ sub Run {
         }
     }
 
-#    # get partner link data list
-#    my $PartnerLinkDataList = $Self->{LinkObject}->PartnerLinkDataList(
-#        Class => 'ITSMLocation',
-#        Key   => $LocationID,
-#    );
-#
-#    # generate block list
-#    my $BlockList = {};
-#    for my $Item ( @{$PartnerLinkDataList} ) {
-#        $BlockList->{ $Item->{Blockname1} }->{ $Item->{Blockname2} }->{ $Item->{LinkID} } = $Item;
-#    }
-#
-#    # start partner output
-#    my $OutputHorizontalRuler = 1;
-#    for my $Blockname1 ( sort keys %{$BlockList} ) {
-#        for my $Blockname2 ( sort keys %{ $BlockList->{$Blockname1} } ) {
-#            my $ItemList = $BlockList->{$Blockname1}->{$Blockname2};
-#
-#            # get partner table columns
-#            my @LinkIDs            = keys %{$ItemList};
-#            my $PartnerTableColumn = $Self->{LinkObject}->PartnerTableColumnGet(
-#                Class          => $ItemList->{ $LinkIDs[0] }->{PartnerClass},
-#                Item           => $ItemList->{ $LinkIDs[0] },
-#                LinkTypeColumn => 1,
-#            );
-#
-#            # count columns
-#            my $ColumnCount = @{$PartnerTableColumn};
-#
-#            # output link block
-#            $Self->{LayoutObject}->Block(
-#                Name => 'Link',
-#                Data => {
-#                    Blockname1 => $Blockname1,
-#                    Blockname2 => $Blockname2,
-#                    Colspan    => $ColumnCount,
-#                },
-#            );
-#
-#            # output horizontal ruler
-#            if ($OutputHorizontalRuler) {
-#                $Self->{LayoutObject}->Block( Name => 'LinkHorizontalRuler' );
-#                $OutputHorizontalRuler = 0;
-#            }
-#
-#            # output column width block
-#            for my $Column ( @{$PartnerTableColumn} ) {
-#
-#                # output link header column block
-#                $Self->{LayoutObject}->Block(
-#                    Name => 'LinkColumnWidth',
-#                    Data => {
-#                        Width => $Column->{HTMLOutput}->{TableWidth} || '*',
-#                    },
-#                );
-#            }
-#
-#            # output headline column block
-#            for my $Column ( @{$PartnerTableColumn} ) {
-#
-#                # output link header column block
-#                $Self->{LayoutObject}->Block(
-#                    Name => 'LinkHeaderColumn',
-#                    Data => {
-#                        Content => $Column->{Headline},
-#                    },
-#                );
-#            }
-#            my $CssClass = '';
-#            for my $LinkData ( sort keys %{ $BlockList->{$Blockname1}->{$Blockname2} } ) {
-#
-#                # set output object
-#                $CssClass = $CssClass eq 'searchpassive' ? 'searchactive' : 'searchpassive';
-#
-#                # output link block
-#                $Self->{LayoutObject}->Block( Name => 'LinkRow' );
-#                for my $ColumnData ( @{$PartnerTableColumn} ) {
-#
-#                    # lookup content
-#                    my $ContentString = $Self->{LayoutObject}->LinkObjectContentStringCreate(
-#                        Class =>
-#                            $BlockList->{$Blockname1}->{$Blockname2}->{$LinkData}->{PartnerClass},
-#                        ColumnData => $ColumnData,
-#                        LinkData   => $BlockList->{$Blockname1}->{$Blockname2}->{$LinkData},
-#                    );
-#
-#                    # output link row column block
-#                    $Self->{LayoutObject}->Block(
-#                        Name => 'LinkRowColumn',
-#                        Data => {
-#                            Content  => $ContentString,
-#                            CssClass => $CssClass,
-#                        },
-#                    );
-#                }
-#            }
-#        }
-#    }
-
     # get linked objects
     my $LinkListWithData = $Self->{LinkObject}->LinkListWithData(
         Object => 'ITSMLocation',
@@ -251,7 +152,7 @@ sub Run {
     # set last screen view (ITSMLocation)
     $Self->{SessionObject}->UpdateSessionID(
         SessionID => $Self->{SessionID},
-        Key       => 'ITSMConfigItemLastScreenView',
+        Key       => 'ITSMLocationLastScreenView',
         Value     => "Action=$Self->{Action}&LocationID=$LocationID",
     );
 
