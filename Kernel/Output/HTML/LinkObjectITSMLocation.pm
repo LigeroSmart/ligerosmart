8b4e7952f10772d8360f8db3b6102dfe12ad8e12
Udo Bretz <udo.bretz@otrs.com>
2008-06-23 21:46:09 +0000

    Small layout change in TableCreateSimple().

--- Kernel/Output/HTML/LinkObjectITSMLocation.pm
+++ Kernel/Output/HTML/LinkObjectITSMLocation.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/LinkObjectITSMLocation.pm - layout backend module
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: LinkObjectITSMLocation.pm,v 1.2 2008-06-23 17:55:18 ub Exp $
+# $Id: LinkObjectITSMLocation.pm,v 1.3 2008-06-23 21:46:09 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::State;
 use Kernel::System::GeneralCatalog;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 =head1 NAME
 
@@ -330,7 +330,7 @@ sub TableCreateSimple {
                 my %Item = (
                     Type    => 'Link',
                     Content => 'L:' . $Location->{Name},
-                    Title   => "Location $Location->{Name}:",
+                    Title   => "Location: $Location->{Name}",
                     Link    => '$Env{"Baselink"}Action=AgentITSMLocationZoom&LocationID=' . $LocationID,
                 );
 
@@ -542,6 +542,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =head1 VERSION
 
-$Revision: 1.2 $ $Date: 2008-06-23 17:55:18 $
+$Revision: 1.3 $ $Date: 2008-06-23 21:46:09 $
 
 =cut
