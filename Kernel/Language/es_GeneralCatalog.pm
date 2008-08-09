f1e132b79779debbdf1b2507d380389ef641ef02
Manuel Hecht <manuel.hecht@otrs.com>
2008-08-09 13:42:29 +0000

    Added spanish translation. Many thanks to Aquiles Cohen.

---
+++ Kernel/Language/es_GeneralCatalog.pm
@@ -0,0 +1,38 @@
+# --
+# Kernel/Language/es_GeneralCatalog.pm - the spanish translation of GeneralCatalog
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: es_GeneralCatalog.pm,v 1.1 2008-08-09 13:42:29 mh Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package Kernel::Language::es_GeneralCatalog;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub Data {
+    my $Self = shift;
+
+    my $Lang = $Self->{Translation};
+
+    return if ref $Lang ne 'HASH';
+
+    $Lang->{'General Catalog'}            = 'Catalogo General';
+    $Lang->{'General Catalog Management'} = 'Administración de Catalogo General';
+    $Lang->{'Catalog Class'}              = 'Clase de Catalogo';
+    $Lang->{'Add a new Catalog Class.'}   = 'Añadir una nueva Clase al Catalogo';
+    $Lang->{'Add Catalog Item'}           = 'Añadir Elemento de Catalogo';
+    $Lang->{'Add Catalog Class'}          = 'Añadir Clase de Catalogo';
+    $Lang->{'Functionality'}              = 'Funcionalidad';
+
+    return 1;
+}
+
+1;
