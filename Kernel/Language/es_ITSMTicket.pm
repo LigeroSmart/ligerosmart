db0665ab4daaf63404f3cd9165e658c01ad1d7f0
Manuel Hecht <manuel.hecht@otrs.com>
2008-08-20 11:04:58 +0000

    Updated spanish translation. Thanks to Aquiles Cohen.

--- Kernel/Language/es_ITSMTicket.pm
+++ Kernel/Language/es_ITSMTicket.pm
@@ -3,7 +3,7 @@
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # Copyright (C) 2008 Aquiles Cohen
 # --
-# $Id: es_ITSMTicket.pm,v 1.3 2008-08-14 11:49:53 mh Exp $
+# $Id: es_ITSMTicket.pm,v 1.4 2008-08-20 11:04:58 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -16,7 +16,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -29,12 +29,12 @@ sub Data {
     $Lang->{'Decision'}                     = 'Desición';
     $Lang->{'Reason'}                       = 'Motivo';
     $Lang->{'Decision Date'}                = 'Fecha de Desición';
-    $Lang->{'Add decision to ticket'}       = 'Añadir desición a ticket';
+    $Lang->{'Add decision to ticket'}       = 'Añadir desición al ticket';
     $Lang->{'Decision Result'}              = 'Resultado de Desición';
     $Lang->{'Review Required'}              = 'Revisión requerida';
     $Lang->{'closed with workaround'}       = 'Cerrado con solución provisional';
     $Lang->{'Additional ITSM Fields'}       = 'Campos ITSM adicionales';
-    $Lang->{'Change ITSM fields of ticket'} = 'Cambiar cambos ITSM de ticket';
+    $Lang->{'Change ITSM fields of ticket'} = 'Modificar campos ITSM del ticket';
     $Lang->{'Repair Start Time'}            = 'Fecha inicial de reparación';
     $Lang->{'Recovery Start Time'}          = 'Fecha inicial de recuperación';
 
