aafd23d43f33e69ad4d9939072c624d2cb3ee49e
Manuel Hecht <manuel.hecht@otrs.com>
2008-08-20 10:58:32 +0000

    Updated spanish translation. Thanks to Aquiles Cohen.

--- Kernel/Language/es_GeneralCatalog.pm
+++ Kernel/Language/es_GeneralCatalog.pm
@@ -3,7 +3,7 @@
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # Copyright (C) 2008 Aquiles Cohen
 # --
-# $Id: es_GeneralCatalog.pm,v 1.3 2008-08-14 11:00:47 mh Exp $
+# $Id: es_GeneralCatalog.pm,v 1.4 2008-08-20 10:58:32 mh Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -16,7 +16,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -26,11 +26,11 @@ sub Data {
     return if ref $Lang ne 'HASH';
 
     $Lang->{'General Catalog'}            = 'Catalogo General';
-    $Lang->{'General Catalog Management'} = 'Administración de Catalogo General';
+    $Lang->{'General Catalog Management'} = 'Gestión del Catalogo General';
     $Lang->{'Catalog Class'}              = 'Clase de Catalogo';
     $Lang->{'Add a new Catalog Class.'}   = 'Añadir una nueva Clase al Catalogo';
-    $Lang->{'Add Catalog Item'}           = 'Añadir Elemento de Catalogo';
-    $Lang->{'Add Catalog Class'}          = 'Añadir Clase de Catalogo';
+    $Lang->{'Add Catalog Item'}           = 'Añadir Elemento al Catalogo';
+    $Lang->{'Add Catalog Class'}          = 'Añadir Clase al Catalogo';
     $Lang->{'Functionality'}              = 'Funcionalidad';
 
     return 1;
