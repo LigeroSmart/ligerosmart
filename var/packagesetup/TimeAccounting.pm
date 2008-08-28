a3039c4fe25f1cf39896b867543d20be088c8645
Thomas Raith <thomas.raith@otrs.com>
2008-08-28 14:18:25 +0000

    Improve the install handling.

---
+++ var/packagesetup/TimeAccounting.pm
@@ -0,0 +1,274 @@
+# --
+# TimeAccounting.pm - code to excecute during package installation
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: TimeAccounting.pm,v 1.1 2008-08-28 14:18:25 tr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package var::packagesetup::TimeAccounting;
+
+use strict;
+use warnings;
+
+use Kernel::System::Group;
+
+use vars qw(@ISA $VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+TimeAccounting.pm - code to excecute during package installation
+
+=head1 SYNOPSIS
+
+All functions
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+    use Kernel::Config;
+    use Kernel::System::Log;
+    use Kernel::System::Main;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $LogObject    = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $MainObject = Kernel::System::Main->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+    );
+    my $CodeObject = var::packagesetup::TimeAccounting->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (qw(ConfigObject LogObject MainObject)) {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+    $Self->{GroupObject}          = Kernel::System::Group->new( %{$Self} );
+
+    return $Self;
+}
+
+=item CodeInstall()
+
+run the code install part
+
+    my $Result = $CodeObject->CodeInstall();
+
+=cut
+
+sub CodeInstall {
+    my ( $Self, %Param ) = @_;
+
+    # add the group itsm-service
+    $Self->_GroupAdd(
+        Name        => 'time_accounting',
+        Description => 'Group for all time accounting user.',
+    );
+
+    return 1;
+}
+
+=item CodeUninstall()
+
+run the code uninstall part
+
+    my $Result = $CodeObject->CodeUninstall();
+
+=cut
+
+sub CodeUninstall {
+    my ( $Self, %Param ) = @_;
+
+    # deactivate the group itsm-service
+    $Self->_GroupDeactivate(
+        Name => 'time_accounting',
+    );
+
+    return 1;
+}
+
+=item _GroupAdd()
+
+add a group
+
+    my $Result = $CodeObject->_GroupAdd(
+        Name        => 'the-group-name',
+        Description => 'The group description.',
+    );
+
+=cut
+
+sub _GroupAdd {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Name Description)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    # get valid list
+    my %ValidList = $Self->{ValidObject}->ValidList(
+        UserID => 1,
+    );
+    my %ValidListReverse = reverse %ValidList;
+
+    # check if group already exists
+    my $GroupID = $Self->{GroupObject}->GroupLookup(
+        Group  => $Param{Name},
+        UserID => 1,
+    );
+
+    # reactivate the group
+    if ($GroupID) {
+
+        # get current group data
+        my %GroupData = $Self->{GroupObject}->GroupGet(
+            ID     => $GroupID,
+            UserID => 1,
+        );
+
+        # reactivate group
+        $Self->{GroupObject}->GroupUpdate(
+            %GroupData,
+            ValidID => $ValidListReverse{valid},
+            UserID  => 1,
+        );
+
+        return 1;
+    }
+
+    # add the group
+    else {
+        return if !$Self->{GroupObject}->GroupAdd(
+            Name    => $Param{Name},
+            Comment => $Param{Description},
+            ValidID => $ValidListReverse{valid},
+            UserID  => 1,
+        );
+    }
+
+    # lookup the new group id
+    my $NewGroupID = $Self->{GroupObject}->GroupLookup(
+        Group  => $Param{Name},
+        UserID => 1,
+    );
+
+    # add user root to the group
+    $Self->{GroupObject}->GroupMemberAdd(
+        GID        => $NewGroupID,
+        UID        => 1,
+        Permission => {
+            ro        => 1,
+            move_into => 1,
+            create    => 1,
+            owner     => 1,
+            priority  => 1,
+            rw        => 1,
+        },
+        UserID => 1,
+    );
+
+    return 1;
+}
+
+=item _GroupDeactivate()
+
+deactivate a group
+
+    my $Result = $CodeObject->_GroupDeactivate(
+        Name => 'the-group-name',
+    );
+
+=cut
+
+sub _GroupDeactivate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{Name} ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => 'Need Name!',
+        );
+        return;
+    }
+
+    # lookup group id
+    my $GroupID = $Self->{GroupObject}->GroupLookup(
+        Group => $Param{Name},
+    );
+
+    return if !$GroupID;
+
+    # get valid list
+    my %ValidList = $Self->{ValidObject}->ValidList(
+        UserID => 1,
+    );
+    my %ValidListReverse = reverse %ValidList;
+
+    # get current group data
+    my %GroupData = $Self->{GroupObject}->GroupGet(
+        ID     => $GroupID,
+        UserID => 1,
+    );
+
+    # deactivate group
+    $Self->{GroupObject}->GroupUpdate(
+        %GroupData,
+        ValidID => $ValidListReverse{invalid},
+        UserID  => 1,
+    );
+
+    return 1;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This Software is part of the OTRS project (http://otrs.org/).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2008-08-28 14:18:25 $
+
+=cut
