187811b021c5893b181ff4e365c2a242547a8c98
Martin Edenhofer <martin.edenhofer@otrs.com>
2008-09-08 20:15:29 +0000

    Added nagios acknowledge feature.

---
+++ Kernel/System/Ticket/Event/NagiosAcknowledge.pm
@@ -0,0 +1,97 @@
+# --
+# Kernel/System/Ticket/Event/NagiosAcknowledge.pm - acknowlege nagios tickets
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: NagiosAcknowledge.pm,v 1.1 2008-09-08 20:15:29 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package Kernel::System::Ticket::Event::NagiosAcknowledge;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (
+        qw(ConfigObject TicketObject LogObject UserObject CustomerUserObject SendmailObject TimeObject EncodeObject)
+        )
+    {
+        $Self->{$_} = $Param{$_} || die "Got no $_!";
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw(TicketID Event Config)) {
+        if ( !$Param{$_} ) {
+            $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
+            return;
+        }
+    }
+
+    # check if it's a Nagios related ticket
+    my %Ticket = $Self->{TicketObject}->TicketGet( TicketID => $Param{TicketID} );
+    if ( !$Ticket{TicketFreeText1} ) {
+        $Self->{LogObject}->Log( Priority => 'debug', Message => "No Nagios Ticket!" );
+        return 1;
+    }
+
+    # check if it's an acknowledge
+    return 1 if $Ticket{Lock} ne 'lock';
+
+    # send acknowledge to nagios
+    my $CMD = $Self->{ConfigObject}->Get( 'Nagios::Acknowledge::CMD' );
+    my $Data;
+    if ( $Ticket{TicketFreeText2} ) {
+        $Data = $Self->{ConfigObject}->Get( 'Nagios::Acknowledge::NamedPipe::Service' );
+    }
+    else {
+        $Data = $Self->{ConfigObject}->Get( 'Nagios::Acknowledge::NamedPipe::Host' );
+    }
+
+    # replace ticket tags
+    for my $Key ( keys %Ticket ) {
+        next if defined !$Ticket{$Key};
+
+        # strip not allowd chars
+        $Ticket{$Key} =~ s/'//g;
+        $Data =~ s/<$Key>/$Ticket{$Key}/g;
+    }
+
+    # replace time stamp
+    my $Time = time();
+    $Data =~ s/<UNIXTIME>/$Time/g;
+
+    # replace OUTPUTSTRING
+    $CMD =~ s/<OUTPUTSTRING>/$Data/g;
+
+    system ( $Data );
+
+    $Self->{TicketObject}->HistoryAdd(
+        TicketID     => $Param{TicketID},
+        HistoryType  => 'Misc',
+        Name         => "Send Acknowledge to Nagios",
+        CreateUserID => 1 || $Param{UserID},
+    );
+
+    return 1;
+}
+
+1;
---
+++ bin/otrs.NagiosCheck.pl
@@ -0,0 +1,133 @@
+#!/usr/bin/perl -w
+# --
+# otrs.NagiosCheck.pl - OTRS Nagios checker
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: otrs.NagiosCheck.pl,v 1.1 2008-09-08 20:15:29 martin Exp $
+# --
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# --
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+use File::Basename;
+use FindBin qw($RealBin);
+use lib dirname($RealBin);
+use lib dirname($RealBin) . '/Kernel/cpan-lib';
+
+use Getopt::Std;
+my %opts;
+getopt( 'c', \%opts );
+if ( $opts{h} ) {
+    print "Usage: $FindBin::Script [-N (runs as Nagioschecker)] [-v (verbose)] [-c /path/to/config_file]\n";
+    print "\n";
+    exit;
+}
+
+if ( !$opts{c} ) {
+    print STDERR "ERROR: Need -c CONFIGFILE\n";
+    exit 1;
+}
+elsif ( !-e $opts{c} ) {
+    print STDERR "ERROR: No such file $opts{c}\n";
+    exit 1;
+}
+
+# read config file
+my %Config;
+open (my $IN, '<', $opts{c} ) || die "ERROR: Can't open $opts{c}: $!\n";
+my $Content = '';
+while (<$IN>) {
+    $Content .= $_;
+}
+if ( !eval $Content ) {
+    print STDERR "ERROR: Invalid config file $opts{c}: $@\n";
+    exit 1;
+}
+
+use Kernel::Config;
+use Kernel::System::Log;
+use Kernel::System::Time;
+use Kernel::System::Main;
+use Kernel::System::DB;
+use Kernel::System::Ticket;
+
+# create common objects
+my %CommonObject = ();
+$CommonObject{ConfigObject} = Kernel::Config->new(%CommonObject);
+$CommonObject{LogObject}    = Kernel::System::Log->new( %CommonObject, LogPrefix => 'otrs.NagiosCheck' );
+$CommonObject{TimeObject}   = Kernel::System::Time->new(%CommonObject);
+$CommonObject{MainObject}   = Kernel::System::Main->new(%CommonObject);
+$CommonObject{DBObject}     = Kernel::System::DB->new(%CommonObject);
+$CommonObject{TicketObject} = Kernel::System::Ticket->new(%CommonObject);
+
+# search tickets
+my @TicketIDs = $CommonObject{TicketObject}->TicketSearch(
+    %{ $Config{Search} },
+    Limit  => 100_000,
+    Result => 'ARRAY',
+    UserID => 1,
+);
+my $TicketCount = scalar @TicketIDs;
+
+# verbose mode
+if ( $opts{v} ) {
+    for my $TicketID (@TicketIDs) {
+        my %Ticket = $CommonObject{TicketObject}->TicketGet( TicketID => $TicketID );
+        print STDERR "$Ticket{TicketID}:$Ticket{TicketNumber}\n";
+    }
+}
+
+# no checker mode
+if ( !$opts{N} ) {
+    print "$TicketCount\n";
+    exit 0;
+}
+
+# do critical and warning check
+for my $Type (qw(crit_treshhold warn_treshhold)) {
+    if ( defined $Config{ 'min_' . $Type } ) {
+        if ( $Config{ 'min_' . $Type } >= $TicketCount ) {
+            if ( $Type =~ /^crit_/ ) {
+                print "$Config{checkname} CRITICAL $Config{CRIT_TXT} $TicketCount\n";
+                exit 2;
+            }
+            elsif ( $Type =~ /^warn_/ ) {
+                print "$Config{checkname} WARNING $Config{WARN_TXT} $TicketCount\n";
+                exit 1;
+            }
+        }
+    }
+    if ( defined $Config{ 'max_' . $Type } ) {
+        if ( $Config{ 'max_' . $Type } <= $TicketCount ) {
+            if ( $Type =~ /^crit_/ ) {
+                print "$Config{checkname} CRITICAL $Config{CRIT_TXT} $TicketCount\n";
+                exit 2;
+            }
+            elsif ( $Type =~ /^warn_/ ) {
+                print "$Config{checkname} WARNING $Config{WARN_TXT} $TicketCount\n";
+                exit 1;
+            }
+        }
+    }
+}
+
+# return ok
+print "$Config{checkname} OK $Config{OK_TXT} $TicketCount\n";
+exit 0;
