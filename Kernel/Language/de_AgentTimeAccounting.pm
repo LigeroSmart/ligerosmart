588320577697cd8fb2349a919da80f8428c2f09e
Shawn Beasley <shawn.beasley@otrs.com>
2008-09-22 19:21:47 +0000

    Made changes to english wording - Changed button lable from New user setting -> Add time period

--- Kernel/Language/de_AgentTimeAccounting.pm
+++ Kernel/Language/de_AgentTimeAccounting.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de_AgentTimeAccounting.pm - the de language for AgentTimeAccounting
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: de_AgentTimeAccounting.pm,v 1.15 2008-09-22 19:13:48 shb Exp $
+# $Id: de_AgentTimeAccounting.pm,v 1.16 2008-09-22 19:21:47 shb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -59,7 +59,7 @@ sub Data {
         'User settings'                            => 'Nutzereinstellungen',
         'Show Overtime'                            => 'Überstunden anzeigen',
         'Create Project'                           => 'Projekt erstellen',
-        'New user setting'                         => 'Neue Nutzereinstellung',
+        'Add time period'                          => 'Neue Nutzereinstellung',
         'Remark'                                   => 'Anmerkung',
         'Starttime'                                => 'Beginn',
         'Endtime'                                  => 'Ende',
