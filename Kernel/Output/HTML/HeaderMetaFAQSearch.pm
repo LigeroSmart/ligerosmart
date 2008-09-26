9a877b927084a8f6037381320afd9a4e6110affc
Udo Bretz <udo.bretz@otrs.com>
2008-09-26 22:08:26 +0000

    Fixed problem with not needed parameters.

--- Kernel/Output/HTML/HeaderMetaFAQSearch.pm
+++ Kernel/Output/HTML/HeaderMetaFAQSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/HeaderMetaFAQSearch.pm
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: HeaderMetaFAQSearch.pm,v 1.1 2008-07-03 21:44:12 martin Exp $
+# $Id: HeaderMetaFAQSearch.pm,v 1.2 2008-09-26 22:08:26 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -25,7 +25,7 @@ sub new {
     bless( $Self, $Type );
 
     # get needed objects
-    for (qw(ConfigObject LogObject DBObject LayoutObject TimeObject UserID)) {
+    for (qw(ConfigObject LogObject LayoutObject TimeObject)) {
         $Self->{$_} = $Param{$_} || die "Got no $_!";
     }
 
