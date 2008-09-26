74e12605243f63decdd465b8f5d58bb52904065e
Shawn Beasley <shawn.beasley@otrs.com>
2008-09-26 14:10:28 +0000

    Made changes to English wording - Correted references to Overtime

--- Kernel/Language/de_AgentTimeAccounting.pm
+++ Kernel/Language/de_AgentTimeAccounting.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de_AgentTimeAccounting.pm - the de language for AgentTimeAccounting
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: de_AgentTimeAccounting.pm,v 1.28 2008-09-26 12:19:58 shb Exp $
+# $Id: de_AgentTimeAccounting.pm,v 1.29 2008-09-26 14:10:27 shb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -76,7 +76,8 @@ sub Data {
         'On sick leave'                            => 'Erkrankt',
         'Task'                                     => 'Tätigkeit',
         'Authorized overtime'                      => 'autorisierte Überstunden',
-        'Overtime'                                 => 'Überstunden',
+        'On overtime leave'                                 => 'Überstunden',
+        'Overtime leave'                                 => 'Überstunden',
         'Total'                                    => 'Summe',
         'Overview of '                             => 'Übersicht - ',
         'TimeAccounting of'                        => 'Zeiterfassung vom',
