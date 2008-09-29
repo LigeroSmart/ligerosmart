923baf02a8c2fd3b2986a994fab2f28545cc2c0f
Udo Bretz <udo.bretz@otrs.com>
2008-09-29 14:07:11 +0000

    Fixed possible problem with reserved sql word.

--- Kernel/System/FAQ.pm
+++ Kernel/System/FAQ.pm
@@ -2,7 +2,7 @@
 # Kernel/System/FAQ.pm - all faq funktions
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: FAQ.pm,v 1.46 2008-09-29 14:02:22 ub Exp $
+# $Id: FAQ.pm,v 1.47 2008-09-29 14:07:11 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -24,7 +24,7 @@ use Kernel::System::Ticket;
 use Kernel::System::Web::UploadCache;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.46 $) [1];
+$VERSION = qw($Revision: 1.47 $) [1];
 
 =head1 NAME
 
@@ -487,7 +487,7 @@ sub AttachmentAdd {
     # write attachment to db
     return $Self->{DBObject}->Do(
         SQL => "INSERT INTO faq_attachment " .
-            " (faq_id, filename, content_type, content_size, content, inline, " .
+            " (faq_id, filename, content_type, content_size, content, inlineattachment, " .
             " created, created_by, changed, changed_by) VALUES " .
             " (?, ?, ?, ?, ?, ?, current_timestamp, ?, current_timestamp, ?)",
         Bind => [
@@ -597,7 +597,7 @@ sub AttachmentIndex {
     }
 
     $Self->{DBObject}->Prepare(
-        SQL => 'SELECT id, filename, content_type, content_size, inline FROM '
+        SQL => 'SELECT id, filename, content_type, content_size, inlineattachment FROM '
             . 'faq_attachment WHERE faq_id = ? '
             . 'ORDER BY created',
         Bind => [ \$Param{ItemID} ],
@@ -3065,6 +3065,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =head1 VERSION
 
-$Revision: 1.46 $ $Date: 2008-09-29 14:02:22 $
+$Revision: 1.47 $ $Date: 2008-09-29 14:07:11 $
 
 =cut
