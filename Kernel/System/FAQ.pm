c19f18f3c8225a3f2dc8d6c8d41935d59fa1e38a
Udo Bretz <udo.bretz@otrs.com>
2008-10-09 07:40:46 +0000

    Removed unnecessary quoting.

--- Kernel/System/FAQ.pm
+++ Kernel/System/FAQ.pm
@@ -2,7 +2,7 @@
 # Kernel/System/FAQ.pm - all faq funktions
 # Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: FAQ.pm,v 1.49 2008-10-07 09:34:46 ub Exp $
+# $Id: FAQ.pm,v 1.50 2008-10-09 07:40:46 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -24,7 +24,7 @@ use Kernel::System::Ticket;
 use Kernel::System::Web::UploadCache;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.49 $) [1];
+$VERSION = qw($Revision: 1.50 $) [1];
 
 =head1 NAME
 
@@ -3011,8 +3011,8 @@ sub FAQPictureUploadAdd {
             . "Filename=$Attachment->{Filename}";
 
         # picture url in faq atttachment
-        my $Replace = "Action=AgentFAQ&amp;amp;Subaction=Download&amp;amp;"
-            . "ItemID=$Param{ItemID}&amp;amp;FileID=$Attachment->{FileID}";
+        my $Replace = "Action=AgentFAQ&Subaction=Download&"
+            . "ItemID=$Param{ItemID}&FileID=$Attachment->{FileID}";
 
         # rewrite picture urls
         FIELD:
@@ -3061,6 +3061,6 @@ did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =head1 VERSION
 
-$Revision: 1.49 $ $Date: 2008-10-07 09:34:46 $
+$Revision: 1.50 $ $Date: 2008-10-09 07:40:46 $
 
 =cut
