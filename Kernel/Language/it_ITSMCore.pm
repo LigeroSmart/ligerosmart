afed32ce8260519e865f74b39a844bf9cee23c44
Manuel Hecht <manuel.hecht@otrs.com>
2009-05-18 09:43:40 +0000

    Added italian translation. Thanks to Cristiano Caruso.

---
+++ Kernel/Language/it_ITSMCore.pm
@@ -0,0 +1,69 @@
+# --
+# Kernel/Language/it_ITSMCore.pm - the italian translation of ITSMCore
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# --
+# $Id: it_ITSMCore.pm,v 1.1 2009-05-18 09:43:40 mh Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::it_ITSMCore;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub Data {
+    my $Self = shift;
+
+    my $Lang = $Self->{Translation};
+
+    return if ref $Lang ne 'HASH';
+
+    $Lang->{'Criticality'}                         = 'Urgenzia';
+    $Lang->{'Impact'}                              = 'Impatto';
+    $Lang->{'Criticality <-> Impact <-> Priority'} = 'Urgenza <-> Impatto <-> Priorità';
+    $Lang->{'allocate'}                            = 'assegnare';
+    $Lang->{'Relevant to'}                         = 'Rilevante per';
+    $Lang->{'Includes'}                            = 'Include';
+    $Lang->{'Part of'}                             = 'Parte di';
+    $Lang->{'Depends on'}                          = 'Depende da';
+    $Lang->{'Required for'}                        = 'Richiesto per';
+    $Lang->{'Connected to'}                        = 'Connesso a';
+    $Lang->{'Alternative to'}                      = 'Alternativo a';
+    $Lang->{'Incident State'}                      = 'Stato dell\'Incidente';
+    $Lang->{'Current Incident State'}              = 'Stato Attuale dell\'Incidente';
+    $Lang->{'Current State'}                       = 'Stato Attuale';
+    $Lang->{'Service-Area'}                        = 'Area-di-Servizio';
+    $Lang->{'Minimum Time Between Incidents'}      = 'Minimo Tempo Tra Incidenti';
+    $Lang->{'Service Overview'}                    = 'Descrizione del Servizio';
+    $Lang->{'SLA Overview'}                        = 'Descrizione dello SLA';
+    $Lang->{'Associated Services'}                 = 'Servizi Associati';
+    $Lang->{'Associated SLAs'}                     = 'SLAs Associati';
+    $Lang->{'Back End'}                            = 'Back End';
+    $Lang->{'Demonstration'}                       = 'Dimostrazione';
+    $Lang->{'End User Service'}                    = 'Servizio Utente Finale';
+    $Lang->{'Front End'}                           = 'Front End';
+    $Lang->{'IT Management'}                       = 'IT Management';
+    $Lang->{'IT Operational'}                      = 'IT Operational';
+    $Lang->{'Other'}                               = 'Altro';
+    $Lang->{'Project'}                             = 'Progetto';
+    $Lang->{'Reporting'}                           = 'Rapporti';
+    $Lang->{'Training'}                            = 'Formazione';
+    $Lang->{'Underpinning Contract'}               = 'Underpinning Contract';
+    $Lang->{'Availability'}                        = 'Disponibilità';
+    $Lang->{'Errors'}                              = 'Errori';
+    $Lang->{'Other'}                               = 'Altro';
+    $Lang->{'Recovery Time'}                       = 'Tempo di Recupero';
+    $Lang->{'Resolution Rate'}                     = 'Tasso di Risoluzione';
+    $Lang->{'Response Time'}                       = 'Tempo di Risposta';
+    $Lang->{'Transactions'}                        = 'Transazioni';
+
+    return 1;
+}
+
+1;
