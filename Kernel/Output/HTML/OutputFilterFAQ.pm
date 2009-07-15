3680e8c05d73e8f502cdbd41ccd9d289cd1e3a2a
Udo Bretz <udo.bretz@otrs.com>
2009-07-15 13:34:47 +0000

    Fixed bug# 3986 - Inserting an FAQ Article to AgentTicketPhone does not work.

--- Kernel/Output/HTML/OutputFilterFAQ.pm
+++ Kernel/Output/HTML/OutputFilterFAQ.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Output/HTML/OutputFilterFAQ.pm - Output filter for FAQ module
-# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: OutputFilterFAQ.pm,v 1.3 2008-10-29 19:16:28 ub Exp $
+# $Id: OutputFilterFAQ.pm,v 1.4 2009-07-15 13:34:47 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -14,7 +14,7 @@ package Kernel::Output::HTML::OutputFilterFAQ;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '$Revision: 1.3 $';
+$VERSION = '$Revision: 1.4 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 sub new {
@@ -44,6 +44,41 @@ sub Run {
     # check permission
     return if !$Self->{LayoutObject}->{EnvRef}->{'UserIsGroupRo[faq]'};
 
+    # Template is RichTextEditor
+    if ( $Param{TemplateFile} eq 'RichTextEditor' ) {
+
+        # add custom event handler
+        my $Search = '\(function\(\) [ ] \{ \s+ var [ ] EditorConfig [ ] = [ ] \{';
+        my $Replace = <<'END';
+
+// define custom event for inserting faq articles
+var onInsertFAQText = new YAHOO.util.CustomEvent("onInsertFAQText");
+
+END
+        ${$Param{Data}} =~ s{ ($Search) }{$Replace$1}xms;
+
+        # add functionallity to insert an faq article to custom event handler
+        $Search = q{var [ ] myEditor [ ] = [ ] new [ ] YAHOO\.widget\.Editor \( 'RichText' ,[ ] EditorConfig \);};
+        $Replace = <<'END';
+
+    // subscribe event handler to the custom event
+    onInsertFAQText.subscribe( function(type, args) {
+
+        // get faq body
+        var FAQBody = args[0];
+
+        // insert FAQ body at cursor position
+        myEditor.execCommand('inserthtml', FAQBody);
+
+        // save new editor content
+        myEditor.saveHTML();
+    });
+END
+        ${$Param{Data}} =~ s{ ($Search) }{$1$Replace}xms;
+
+        return 1;
+    }
+
     # get allowed template names
     my $ValidTemplates = $Self->{ConfigObject}->Get('Frontend::Output::FilterElementPre')
         ->{FAQ}
@@ -55,6 +90,7 @@ sub Run {
     # add javascript function
     my $Search  = '</script>';
     my $Replace = <<'END';
+
 </script>
 
 <script language="JavaScript" type="text/javascript">
@@ -66,6 +102,7 @@ function FAQSubmit() {
 // -->
 </script>
 END
+
     ${$Param{Data}} =~ s{ $Search }{$Replace}ixms;
 
     # add FAQ link
@@ -75,6 +112,7 @@ END
     $Env{"Box0"}<a href="" onclick="FAQSubmit(); return false;" onmouseover="window.status='$JSText{"FAQ"}'; return true;" onmouseout="window.status='';"><img border="0" src="$Env{"Images"}help-small.png">$Text{"FAQ"}</a>$Env{"Box1"}
                   </td>
 END
+
     ${$Param{Data}} =~ s{ ($Search1) $Search2 }{$1$Replace}ixms;
 
     # add FAQ form
@@ -89,6 +127,7 @@ END
         <input type="hidden" name="What" value=""/>
       </form>
 END
+
     ${$Param{Data}} =~ s{ ($Search) }{$1$Replace}ixms;
 
     return 1;
