7f0263a9c78bca2aaf3a0b6fcc8cd652f7fcd361
Udo Bretz <udo.bretz@otrs.com>
2009-08-04 22:16:35 +0000

    Added french translation, thanks to Jonathan Peyrot!

---
+++ Kernel/Language/fr_FAQ.pm
@@ -0,0 +1,68 @@
+# --
+# Kernel/Language/fr_FAQ.pm - the french translation of FAQ
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2009 Jonathan Peyrot
+# --
+# $Id: fr_FAQ.pm,v 1.1 2009-08-04 22:16:35 ub Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package Kernel::Language::fr_FAQ;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub Data {
+    my $Self = shift;
+
+    my $Lang = $Self->{Translation};
+
+    return if ref $Lang ne 'HASH';
+
+    $Lang->{'You have already voted!'}           = 'Vous avez déjà voté !';
+    $Lang->{'No rate selected!'}                 = 'Pas de sélection !';
+    $Lang->{'Thanks for your vote!'}             = 'Merci pour votre vote !';
+    $Lang->{'Votes'}                             = 'Votes';
+    $Lang->{'LatestChangedItems'}                = 'Dernières questions modifiées';
+    $Lang->{'LatestCreatedItems'}                = 'Dernières questions créées';
+    $Lang->{'Top10Items'}                        = 'Top 10 des questions';
+    $Lang->{'ArticleVotingQuestion'}             = 'Voulez vous voter pour cette question ?';
+    $Lang->{'SubCategoryOf'}                     = 'Sous catégorie de';
+    $Lang->{'QuickSearch'}                       = 'Recherche rapide';
+    $Lang->{'DetailSearch'}                      = 'Détails de la recherche';
+    $Lang->{'Categories'}                        = 'Catégories';
+    $Lang->{'SubCategories'}                     = 'Sous-catégories';
+    $Lang->{'New FAQ Article'}                   = 'Nouvelle question dans la FAQ';
+    $Lang->{'FAQ Category'}                      = 'Catégorie FAQ';
+    $Lang->{'A category should have a name!'}    = 'Une catégorie doit avoir un nom !';
+    $Lang->{'A category should have a comment!'} = 'Une catégorie doit posséder un commentaire';
+    $Lang->{'FAQ News (new created)'}            = 'FAQ News (nouvelles questions)';
+    $Lang->{'FAQ News (recently changed)'}       = 'FAQ News (derniers changements)';
+    $Lang->{'FAQ News (Top 10)'}                 = 'FAQ News (Top 10)';
+    $Lang->{'StartDay'}                          = 'Jour Début';
+    $Lang->{'StartMonth'}                        = 'Mois Début';
+    $Lang->{'StartYear'}                         = 'Année Début';
+    $Lang->{'EndDay'}                            = 'Jour Fin';
+    $Lang->{'EndMonth'}                          = 'Mois Fin';
+    $Lang->{'EndYear'}                           = 'Année Fin';
+    $Lang->{'Approval'}                          = 'Autorisation';
+    $Lang->{
+        'No category accessible. To create an article you need access to at least one category. Please check your group/category permission under -category menu-!'
+        }
+        = 'Aucun catégorie accessible. Pour créer une question, vous devez avoir accès à au moins une catégorie. SVP vérifiez les permissions de votre groupe/catégorie via le menu -catégorie- !';
+    $Lang->{'Agent groups which can access this category.'}
+        = 'Groupes d\'Agents pouvant accéder à cette catégorie';
+    $Lang->{'A category needs min. one permission group!'}
+        = 'Une catégorie nécessite au minimum une permission de groupe !';
+    $Lang->{'Will be shown as comment in Explorer.'} = 'Sera affiché comme un commentaire dans l\'Explorer.';
+
+    return 1;
+}
+
+1;
