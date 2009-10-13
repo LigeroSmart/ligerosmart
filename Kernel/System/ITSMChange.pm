b3f168bcd9a6ae058bb427c33335d2f9c58c753a
Udo Bretz <udo.bretz@otrs.com>
2009-10-13 12:13:30 +0000

    Added maximum length checks for title, description, justification.

--- Kernel/System/ITSMChange.pm
+++ Kernel/System/ITSMChange.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMChange.pm - all change functions
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: ITSMChange.pm,v 1.35 2009-10-13 12:11:08 ub Exp $
+# $Id: ITSMChange.pm,v 1.36 2009-10-13 12:13:30 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::LinkObject;
 use Kernel::System::ITSMChange::WorkOrder;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.35 $) [1];
+$VERSION = qw($Revision: 1.36 $) [1];
 
 =head1 NAME
 
@@ -1182,6 +1182,7 @@ sub _CheckChangeParams {
                 Priority => 'error',
                 Message  => "The parameter $Argument must be shorter than 250 characters!",
             );
+            return;
         }
 
         # check the maximum length of description and justification
@@ -1191,6 +1192,7 @@ sub _CheckChangeParams {
                     Priority => 'error',
                     Message  => "The parameter $Argument must be shorter than 3800 characters!",
                 );
+                return;
             }
         }
     }
@@ -1291,6 +1293,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.35 $ $Date: 2009-10-13 12:11:08 $
+$Revision: 1.36 $ $Date: 2009-10-13 12:13:30 $
 
 =cut
