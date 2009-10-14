ee529b864e8a72651feff9197b6683c65c85b703
Udo Bretz <udo.bretz@otrs.com>
2009-10-14 20:48:47 +0000

    Added Chinese Simple translation, thanks to Never Min!

---
+++ Kernel/Language/zh_CN_ImportExport.pm
@@ -0,0 +1,54 @@
+# --
+# Kernel/Language/zh_CN_ImportExport.pm - the Chinese simple translation of ImportExport
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# --
+# $Id: zh_CN_ImportExport.pm,v 1.2 2009-10-14 20:48:47 ub Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::zh_CN_ImportExport;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.2 $) [1];
+
+sub Data {
+    my $Self = shift;
+
+    my $Lang = $Self->{Translation};
+
+    return if ref $Lang ne 'HASH';
+
+    $Lang->{'Import/Export'}              = '导入/导出';
+    $Lang->{'Import/Export Management'}   = '导入/导出管理';
+    $Lang->{'Add mapping template'}       = '增加映射模版';
+    $Lang->{'Start Import'}               = '开始导入';
+    $Lang->{'Start Export'}               = '开始导出';
+    $Lang->{'Step'}                       = '步骤';
+    $Lang->{'Edit common information'}    = '编辑共用信息';
+    $Lang->{'Edit object information'}    = '编辑对像信息';
+    $Lang->{'Edit format information'}    = '编辑格式信息';
+    $Lang->{'Edit mapping information'}   = '编辑映射信息';
+    $Lang->{'Edit search information'}    = '编辑搜索信息';
+    $Lang->{'Import information'}         = '导入信息';
+    $Lang->{'Column'}                     = '列';
+    $Lang->{'Restrict export per search'} = '限制导出每个搜寻';
+    $Lang->{'Source File'}                = '源文件';
+    $Lang->{'Column Seperator'}           = '列分隔符';
+    $Lang->{'Tabulator (TAB)'}            = '制表键 (TAB)';
+    $Lang->{'Semicolon (;)'}              = '分号 (;)';
+    $Lang->{'Colon (:)'}                  = '冒号 (:)';
+    $Lang->{'Dot (.)'}                    = '句号 (.)';
+    $Lang->{'Charset'}                    = '字符集';
+# add by Never
+    $Lang->{'Template'}                   = '模版';
+
+    return 1;
+}
+
+1;
