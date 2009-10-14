01be114ea2db04b2b9924998932de94696fc5b24
Bernhard Schmalhofer <bernhard.schmalhofer@otrs.com>
2009-10-14 09:43:02 +0000

    Simplified the generation of the SQL-Query in ChangeSearch().

--- Kernel/System/ITSMChange.pm
+++ Kernel/System/ITSMChange.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMChange.pm - all change functions
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: ITSMChange.pm,v 1.48 2009-10-14 09:10:12 bes Exp $
+# $Id: ITSMChange.pm,v 1.49 2009-10-14 09:43:02 bes Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::LinkObject;
 use Kernel::System::ITSMChange::WorkOrder;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.48 $) [1];
+$VERSION = qw($Revision: 1.49 $) [1];
 
 =head1 NAME
 
@@ -948,8 +948,8 @@ sub ChangeSearch {
     # conditions for CAB searches
     CABPARAM:
     for my $ArrRef (
-        [ 'CABAgent',    'user_id',          q{},  'cab_agent' ],
-        [ 'CABCustomer', 'customer_user_id', q{'}, 'cab_customer' ],
+        [ 'CABAgent',    'user_id',          q{},  'cab1' ],
+        [ 'CABCustomer', 'customer_user_id', q{'}, 'cab2' ],
         )
     {
         my ( $SearchField, $TableAttribute, $Delimiter, $JoinTable ) = @{$ArrRef};
@@ -990,14 +990,20 @@ sub ChangeSearch {
         # create string
         my $InString = join q{, }, @{ $Param{WorkOrderAgentID} };
 
-        push @SQLWhere, "wo2.workorder_agent_id IN ( $InString )";
-        push @JoinTables, 'wo1', 'wo2';
+        push @SQLWhere,   "wo2.workorder_agent_id IN ( $InString )";
+        push @JoinTables, 'wo2';
     }
 
     # assemble the SQL query
     my $SQL = " SELECT c.id FROM change_item c\n";
 
     # add the joins
+    my %LongTableName = (
+        wo1  => 'change_workorder',
+        wo2  => 'change_workorder',
+        cab1 => 'change_cab',
+        cab2 => 'change_cab',
+    );
     my %TableSeen;
     TABLE:
     for my $Table (@JoinTables) {
@@ -1007,22 +1013,15 @@ sub ChangeSearch {
 
         $TableSeen{$Table} = 1;
 
-        if ( $Table eq 'wo1' ) {
-            $SQL .= " INNER JOIN change_workorder $Table ON $Table.change_id = c.id \n";
-        }
-        elsif ( $Table eq 'wo2' ) {
-            $SQL .= " INNER JOIN change_workorder $Table ON $Table.change_id = wo1.change_id \n";
-        }
-        elsif ( $Table eq 'cab_agent' || $Table eq 'cab_customer' ) {
-            $SQL .= " INNER JOIN change_cab $Table ON $Table.change_id = c.id \n";
-        }
-        else {
+        if ( !$LongTableName{$Table} ) {
             $Self->{LogObject}->Log(
                 Priority => 'error',
-                Message  => "Encountered unknown join table '$Table'!",
+                Message  => "Encountered invalid join table '$Table'!",
             );
             return;
         }
+
+        $SQL .= " INNER JOIN $LongTableName{$Table} $Table ON $Table.change_id = c.id \n";
     }
 
     # add the WHERE clause
@@ -1586,6 +1585,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.48 $ $Date: 2009-10-14 09:10:12 $
+$Revision: 1.49 $ $Date: 2009-10-14 09:43:02 $
 
 =cut
