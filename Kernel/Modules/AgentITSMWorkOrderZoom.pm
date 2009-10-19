f0129f407fbfd33a936c468319ec6ee119626348
Renée Bäcker <renee.baecker@otrs.com>
2009-10-19 17:57:40 +0000

    first draft for ITSMWorkOrderZoom

--- Kernel/Modules/AgentITSMWorkOrderZoom.pm
+++ Kernel/Modules/AgentITSMWorkOrderZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMWorkOrderZoom.pm - the OTRS::ITSM::ChangeManagement work order zoom module
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMWorkOrderZoom.pm,v 1.1 2009-10-11 23:15:11 ub Exp $
+# $Id: AgentITSMWorkOrderZoom.pm,v 1.2 2009-10-19 17:57:40 reb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,9 +15,11 @@ use strict;
 use warnings;
 
 use Kernel::System::ITSMChange::WorkOrder;
+use Kernel::System::ITSMChange;
+use Kernel::System::GeneralCatalog;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -32,7 +34,10 @@ sub new {
             $Self->{LayoutObject}->FatalError( Message => "Got no $Object!" );
         }
     }
-    $Self->{WorkOrderObject} = Kernel::System::ITSMChange::WorkOrder->new(%Param);
+
+    $Self->{ChangeObject}         = Kernel::System::ITSMChange->new(%Param);
+    $Self->{WorkOrderObject}      = Kernel::System::ITSMChange::WorkOrder->new(%Param);
+    $Self->{GeneralCatalogObject} = Kernel::System::GeneralCatalog->new(%Param);
 
     # get config of frontend module
     $Self->{Config} = $Self->{ConfigObject}->Get("ITSMChangeManagement::Frontend::$Self->{Action}");
@@ -43,21 +48,111 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
 
+    my $WorkOrderID = $Self->{ParamObject}->GetParam( Param => 'WorkOrderID' );
+
+    # check needed stuff
+    if ( !$WorkOrderID ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => 'No WorkOrderID is given!',
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # get workorder data
+    my $WorkOrder = $Self->{WorkOrderObject}->WorkOrderGet(
+        WorkOrderID => $WorkOrderID,
+        UserID      => $Self->{UserID},
+    );
+
+    if ( !$WorkOrder ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "WorkOrder $WorkOrder not found in database!",
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # strip header on max 80 chars
+    $WorkOrder->{Title} =~ s{ \A (.{80}) .* \z }{ $1 }xms;
+
+    # break words after 80 chars
+    $WorkOrder->{Instruction} =~ s{ (\S{80}) }{ $1\n }xmsg;
+    $WorkOrder->{Report}      =~ s{ (\S{80}) }{ $1\n }xmsg;
+
+    # get the change that workorder belongs to
+    my $Change = $Self->{ChangeObject}->ChangeGet(
+        ChangeID => $WorkOrder->{ChangeID},
+        UserID   => $Self->{UserID},
+    );
+
     # output header
     my $Output = $Self->{LayoutObject}->Header(
-        Title =>    # ...,
+        Title => $WorkOrder->{Title},
     );
     $Output .= $Self->{LayoutObject}->NavigationBar();
+
+    # get create user data
+    my %CreateUser = $Self->{UserObject}->GetUserData(
+        UserID => $WorkOrder->{CreateBy},
+        Cached => 1,
+    );
+    for my $Postfix (qw(UserLogin UserFirstname UserLastname)) {
+        $WorkOrder->{ 'Create' . $Postfix } = $CreateUser{$Postfix};
+    }
+
+    # get change user data
+    my %ChangeUser = $Self->{UserObject}->GetUserData(
+        UserID => $WorkOrder->{ChangeBy},
+        Cached => 1,
+    );
+    for my $Postfix (qw(UserLogin UserFirstname UserLastname)) {
+        $WorkOrder->{ 'Change' . $Postfix } = $ChangeUser{$Postfix};
+    }
+
+    # get change builder user
+    my %ChangeBuilderUser = $Self->{UserObject}->GetUserData(
+        UserID => $Change->{ChangeBuilderID},
+        Cached => 1,
+    );
+    for my $Postfix (qw(UserLogin UserFirstname UserLastname)) {
+        $Change->{ 'ChangeBuilder' . $Postfix } = $ChangeBuilderUser{$Postfix};
+    }
+
+    # get work order agent user
+    my %WorkOrderAgentUser = $Self->{UserObject}->GetUserData(
+        UserID => $WorkOrder->{WorkOrderAgentID},
+        Cached => 1,
+    );
+    for my $Postfix (qw(UserLogin UserFirstname UserLastname)) {
+        $Change->{ 'WorkOrderAgentBuilder' . $Postfix } = $WorkOrderAgentUser{$Postfix};
+    }
+
+    # get work order state list
+    my $WorkOrderStateList = $Self->{GeneralCatalogObject}->ItemList(
+        Class => 'ITSM::ChangeManagement::WorkOrder::State',
+    );
+
+    # temp color for changes
+    my %CurWorkOrderSignal = (
+        109 => 'greenled',
+    );
+
+    # output meta block
+    $Self->{LayoutObject}->Block(
+        Name => 'Meta',
+        Data => {
+            %{$WorkOrder},
+            CurWorkOrderSignal => $CurWorkOrderSignal{ $WorkOrder->{WorkOrderStateID} },
+            CurWorkOrderState  => $WorkOrderStateList->{ $WorkOrder->{WorkOrderStateID} },
+        },
+    );
 
     # start template output
     $Output .= $Self->{LayoutObject}->Output(
         TemplateFile => 'AgentITSMWorkOrderZoom',
         Data         => {
-
-            # ...
-            %Param,
-
-            # ...
+            ChangeNumber  => $Change->{ChangeNumber},
+            ChangeBuilder => $Change->{ChangeBuilder},
+            %{$WorkOrder},
         },
     );
 
