5385b6546a2e6157bdf64fea8c4aeabb10f08cf2
Markus Esche <markus.esche@otrs.com>
2009-10-19 16:19:28 +0000

    Extended meta data information.

--- Kernel/Modules/AgentITSMChangeZoom.pm
+++ Kernel/Modules/AgentITSMChangeZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMChangeZoom.pm - the OTRS::ITSM::ChangeManagement change zoom module
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMChangeZoom.pm,v 1.2 2009-10-19 15:13:10 mae Exp $
+# $Id: AgentITSMChangeZoom.pm,v 1.3 2009-10-19 16:19:28 mae Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -16,9 +16,10 @@ use warnings;
 
 use Kernel::System::ITSMChange;
 use Kernel::System::LinkObject;
+use Kernel::System::GeneralCatalog;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -33,8 +34,9 @@ sub new {
             $Self->{LayoutObject}->FatalError( Message => "Got no $Object!" );
         }
     }
-    $Self->{ChangeObject} = Kernel::System::ITSMChange->new(%Param);
-    $Self->{LinkObject}   = Kernel::System::LinkObject->new(%Param);
+    $Self->{ChangeObject}         = Kernel::System::ITSMChange->new(%Param);
+    $Self->{LinkObject}           = Kernel::System::LinkObject->new(%Param);
+    $Self->{GeneralCatalogObject} = Kernel::System::GeneralCatalog->new(%Param);
 
     # get config of frontend module
     $Self->{Config} = $Self->{ConfigObject}->Get("ITSMChangeManagement::Frontend::$Self->{Action}");
@@ -82,12 +84,32 @@ sub Run {
     );
     $Output .= $Self->{LayoutObject}->NavigationBar();
 
+    my @Postfixes = qw(UserLogin UserFirstname UserLastname);
+
+    # get change manager data
+    my %ChangeManagerUser = $Self->{UserObject}->GetUserData(
+        UserID => $Change->{ChangeManagerID},
+        Cached => 1,
+    );
+    for my $Postfix (@Postfixes) {
+        $Change->{ 'ChangeManager' . $Postfix } = $ChangeManagerUser{$Postfix};
+    }
+
+    # get change builder data
+    my %ChangeBuilderUser = $Self->{UserObject}->GetUserData(
+        UserID => $Change->{ChangeBuilderID},
+        Cached => 1,
+    );
+    for my $Postfix (@Postfixes) {
+        $Change->{ 'ChangeBuilder' . $Postfix } = $ChangeBuilderUser{$Postfix};
+    }
+
     # get create user data
     my %CreateUser = $Self->{UserObject}->GetUserData(
         UserID => $Change->{CreateBy},
         Cached => 1,
     );
-    for my $Postfix (qw(UserLogin UserFirstname UserLastname)) {
+    for my $Postfix (@Postfixes) {
         $Change->{ 'Create' . $Postfix } = $CreateUser{$Postfix};
     }
 
@@ -96,29 +118,42 @@ sub Run {
         UserID => $Change->{ChangeBy},
         Cached => 1,
     );
-    for my $Postfix (qw(UserLogin UserFirstname UserLastname)) {
+    for my $Postfix (@Postfixes) {
         $Change->{ 'Change' . $Postfix } = $ChangeUser{$Postfix};
     }
 
-    # temp state for changes
-    my %CurChangeState = (
-        109 => 'accepted',
-    );
-
+    # TODO: GeneralCatalog-Preferences definition of LED color
+    #  CurInciSignal => $InciSignals{ $LastVersion->{CurInciStateType} },
     # temp color for changes
     my %CurChangeSignal = (
-        109 => 'greenled',
+        requested          => 'yellowled',
+        accepted           => 'greenled',
+        'pending approval' => 'yellowled',
+        rejected           => 'grayled',
+        approved           => 'greenled',
+        'in progress'      => 'yellowled',
+        successfull        => 'greenled',
+        failed             => 'redled',
+        canceled           => 'grayled',
     );
 
-    # FIXME: GeneralCatalog-Preferences definition of LED color
-    #  CurInciSignal => $InciSignals{ $LastVersion->{CurInciStateType} },
     # output meta block
     $Self->{LayoutObject}->Block(
         Name => 'Meta',
         Data => {
             %{$Change},
-            CurChangeSignal => $CurChangeSignal{ $Change->{ChangeStateID} },
-            CurChangeState  => $CurChangeState{ $Change->{ChangeStateID} },
+            CurChangeSignal => $CurChangeSignal{
+                (
+                    $Self->{GeneralCatalogObject}->ItemGet(
+                        ItemID => $Change->{ChangeStateID},
+                        ) || {}
+                    )->{Name}
+                },
+            CurChangeState => (
+                $Self->{GeneralCatalogObject}->ItemGet(
+                    ItemID => $Change->{ChangeStateID},
+                    ) || {}
+                )->{Name},
         },
     );
 
