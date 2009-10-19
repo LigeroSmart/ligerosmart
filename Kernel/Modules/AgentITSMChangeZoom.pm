91c2996285352d876332923082e0e0c7a143a685
Markus Esche <markus.esche@otrs.com>
2009-10-19 15:13:10 +0000

    First draft of ChangeZoom.

--- Kernel/Modules/AgentITSMChangeZoom.pm
+++ Kernel/Modules/AgentITSMChangeZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMChangeZoom.pm - the OTRS::ITSM::ChangeManagement change zoom module
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMChangeZoom.pm,v 1.1 2009-10-11 23:15:11 ub Exp $
+# $Id: AgentITSMChangeZoom.pm,v 1.2 2009-10-19 15:13:10 mae Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,9 +15,10 @@ use strict;
 use warnings;
 
 use Kernel::System::ITSMChange;
+use Kernel::System::LinkObject;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -33,6 +34,7 @@ sub new {
         }
     }
     $Self->{ChangeObject} = Kernel::System::ITSMChange->new(%Param);
+    $Self->{LinkObject}   = Kernel::System::LinkObject->new(%Param);
 
     # get config of frontend module
     $Self->{Config} = $Self->{ConfigObject}->Get("ITSMChangeManagement::Frontend::$Self->{Action}");
@@ -43,21 +45,115 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
 
+    # get params
+    my $ChangeID = $Self->{ParamObject}->GetParam( Param => "ChangeID" );
+
+    # check needed stuff
+    if ( !$ChangeID ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "No ChangeID is given!",
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # get Change
+    my $Change = $Self->{ChangeObject}->ChangeGet(
+        ChangeID => $ChangeID,
+        UserID   => $Self->{UserID},
+    );
+
+    if ( !$Change ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Change $Change not found in database!",
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # strip header on max 80 chars
+    $Change->{Title} =~ s{ \A (.{80}) (.*) \z }{ $1 }xms;
+
+    # break words after 80 chars
+    $Change->{Description}   =~ s{ (\S{80}) }{ $1\n }xmsg;
+    $Change->{Justification} =~ s{ (\S{80}) }{ $1\n }xmsg;
+
     # output header
     my $Output = $Self->{LayoutObject}->Header(
-        Title =>    # ...,
+        Value => $Change->{Title},
     );
     $Output .= $Self->{LayoutObject}->NavigationBar();
+
+    # get create user data
+    my %CreateUser = $Self->{UserObject}->GetUserData(
+        UserID => $Change->{CreateBy},
+        Cached => 1,
+    );
+    for my $Postfix (qw(UserLogin UserFirstname UserLastname)) {
+        $Change->{ 'Create' . $Postfix } = $CreateUser{$Postfix};
+    }
+
+    # get change user data
+    my %ChangeUser = $Self->{UserObject}->GetUserData(
+        UserID => $Change->{ChangeBy},
+        Cached => 1,
+    );
+    for my $Postfix (qw(UserLogin UserFirstname UserLastname)) {
+        $Change->{ 'Change' . $Postfix } = $ChangeUser{$Postfix};
+    }
+
+    # temp state for changes
+    my %CurChangeState = (
+        109 => 'accepted',
+    );
+
+    # temp color for changes
+    my %CurChangeSignal = (
+        109 => 'greenled',
+    );
+
+    # FIXME: GeneralCatalog-Preferences definition of LED color
+    #  CurInciSignal => $InciSignals{ $LastVersion->{CurInciStateType} },
+    # output meta block
+    $Self->{LayoutObject}->Block(
+        Name => 'Meta',
+        Data => {
+            %{$Change},
+            CurChangeSignal => $CurChangeSignal{ $Change->{ChangeStateID} },
+            CurChangeState  => $CurChangeState{ $Change->{ChangeStateID} },
+        },
+    );
+
+    # get linked objects
+    my $LinkListWithData = $Self->{LinkObject}->LinkListWithData(
+        Object => 'ITSMChange',
+        Key    => $ChangeID,
+        State  => 'Valid',
+        UserID => $Self->{UserID},
+    );
+
+    # get link table view mode
+    my $LinkTableViewMode = $Self->{ConfigObject}->Get('LinkObject::ViewMode');
+
+    # create the link table
+    my $LinkTableStrg = $Self->{LayoutObject}->LinkObjectTableCreate(
+        LinkListWithData => $LinkListWithData,
+        ViewMode         => $LinkTableViewMode,
+    );
+
+    # output the link table
+    if ($LinkTableStrg) {
+        $Self->{LayoutObject}->Block(
+            Name => 'LinkTable' . $LinkTableViewMode,
+            Data => {
+                LinkTableStrg => $LinkTableStrg,
+            },
+        );
+    }
 
     # start template output
     $Output .= $Self->{LayoutObject}->Output(
         TemplateFile => 'AgentITSMChangeZoom',
         Data         => {
-
-            # ...
-            %Param,
-
-            # ...
+            %{$Change},
         },
     );
 
