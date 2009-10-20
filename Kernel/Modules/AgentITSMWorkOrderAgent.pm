148fa353c2e7f26e1cbdd6d5737d0dda53987539
Renée Bäcker <renee.baecker@otrs.com>
2009-10-20 14:16:36 +0000

    add the remaining config items. add handling of 'save'

--- Kernel/Modules/AgentITSMWorkOrderAgent.pm
+++ Kernel/Modules/AgentITSMWorkOrderAgent.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMWorkOrderAgent.pm - the OTRS::ITSM::ChangeManagement work order agent edit module
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMWorkOrderAgent.pm,v 1.2 2009-10-20 13:59:02 reb Exp $
+# $Id: AgentITSMWorkOrderAgent.pm,v 1.3 2009-10-20 14:16:36 reb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::ITSMChange;
 use Kernel::System::ITSMChange::WorkOrder;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -66,6 +66,31 @@ sub Run {
             Message => "WorkOrder $WorkOrder not found in database!",
             Comment => 'Please contact the admin.',
         );
+    }
+
+    # update workorder
+    if ( $Self->{Subaction} eq 'Save' ) {
+        my $Success = $Self->{WorkOrderObject}->WorkOrderUpdate(
+            WorkOrderID      => $WorkOrder->{WorkOrderID},
+            WorkOrderAgentID => $Self->{ParamObject}->GetParam( Param => 'SelectedUser' ),
+            UserID           => $Self->{UserID},
+        );
+
+        if ( !$Success ) {
+
+            # show error message
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "Was not able to update WorkOrder $WorkOrder!",
+                Comment => 'Please contact the admin.',
+            );
+        }
+        else {
+
+            # redirect to zoom mask
+            return $Self->{LayoutObject}->Redirect(
+                OP => "Action=AgentITSMWorkOrderZoom&WorkOrderID=$WorkOrder->{WorkOrderID}",
+            );
+        }
     }
 
     # get change that workorder belongs to
