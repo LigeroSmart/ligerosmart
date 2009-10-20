12550aca705504f884cf4d08610d26e45b8e7528
Renée Bäcker <renee.baecker@otrs.com>
2009-10-20 16:30:14 +0000

    first draft of AgentITSMWorkOrderEdit

--- Kernel/Modules/AgentITSMWorkOrderEdit.pm
+++ Kernel/Modules/AgentITSMWorkOrderEdit.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMWorkOrderEdit.pm - the OTRS::ITSM::ChangeManagement work order edit module
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMWorkOrderEdit.pm,v 1.1 2009-10-11 23:15:11 ub Exp $
+# $Id: AgentITSMWorkOrderEdit.pm,v 1.2 2009-10-20 16:30:14 reb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,9 +15,10 @@ use strict;
 use warnings;
 
 use Kernel::System::ITSMChange::WorkOrder;
+use Kernel::System::ITSMChange;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -32,7 +33,9 @@ sub new {
             $Self->{LayoutObject}->FatalError( Message => "Got no $Object!" );
         }
     }
+
     $Self->{WorkOrderObject} = Kernel::System::ITSMChange::WorkOrder->new(%Param);
+    $Self->{ChangeObject}    = Kernel::System::ITSMChange->new(%Param);
 
     # get config of frontend module
     $Self->{Config} = $Self->{ConfigObject}->Get("ITSMChangeManagement::Frontend::$Self->{Action}");
@@ -43,21 +46,85 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
 
+    my $WorkOrderID = $Self->{ParamObject}->GetParam( Param => 'WorkOrderID' );
+
+    # check needed stuff
+    if ( !$WorkOrderID ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => 'No WorkOrderID is given!',
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # get workorder data
+    my $WorkOrder = $Self->{WorkOrderObject}->WorkOrderGet(
+        WorkOrderID => $WorkOrderID,
+        UserID      => $Self->{UserID},
+    );
+
+    if ( !$WorkOrder ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "WorkOrder $WorkOrderID not found in database!",
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # update workorder
+    if ( $Self->{Subaction} eq 'Save' ) {
+        my $Success = $Self->{WorkOrderObject}->WorkOrderUpdate(
+            WorkOrderID => $WorkOrder->{WorkOrderID},
+            Instruction => $Self->{ParamObject}->GetParam( Param => 'Instruction' ),
+            Title       => $Self->{ParamObject}->GetParam( Param => 'Title' ),
+            UserID      => $Self->{UserID},
+        );
+
+        if ( !$Success ) {
+
+            # show error message
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "Was not able to update WorkOrder $WorkOrder->{WorkOrderID}!",
+                Comment => 'Please contact the admin.',
+            );
+        }
+        else {
+
+            # redirect to zoom mask
+            return $Self->{LayoutObject}->Redirect(
+                OP => "Action=AgentITSMWorkOrderZoom&WorkOrderID=$WorkOrder->{WorkOrderID}",
+            );
+        }
+    }
+
+    # get change that workorder belongs to
+    my $Change = $Self->{ChangeObject}->ChangeGet(
+        ChangeID => $WorkOrder->{ChangeID},
+        UserID   => $Self->{UserID},
+    );
+
+    if ( !$Change ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Could not find Change for WorkOrder $WorkOrder!",
+            Comment => 'Please contact the admin.',
+        );
+    }
+
     # output header
     my $Output = $Self->{LayoutObject}->Header(
-        Title =>    # ...,
+        Title => $WorkOrder->{Title},
     );
     $Output .= $Self->{LayoutObject}->NavigationBar();
+
+    $Self->{LayoutObject}->Block(
+        Name => 'RichText',
+    );
 
     # start template output
     $Output .= $Self->{LayoutObject}->Output(
         TemplateFile => 'AgentITSMWorkOrderEdit',
         Data         => {
-
-            # ...
             %Param,
-
-            # ...
+            %{$Change},
+            %{$WorkOrder},
         },
     );
 
