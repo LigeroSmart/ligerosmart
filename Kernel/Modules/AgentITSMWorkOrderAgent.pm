0f836e920a9d2b5ccaa5eb5f6eebd23b696394a4
Renée Bäcker <renee.baecker@otrs.com>
2009-10-20 13:59:28 +0000

    add ajax agent user search for WorkOrderAgent

--- Kernel/Modules/AgentITSMWorkOrderAgent.pm
+++ Kernel/Modules/AgentITSMWorkOrderAgent.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMWorkOrderAgent.pm - the OTRS::ITSM::ChangeManagement work order agent edit module
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMWorkOrderAgent.pm,v 1.1 2009-10-11 23:15:11 ub Exp $
+# $Id: AgentITSMWorkOrderAgent.pm,v 1.2 2009-10-20 13:59:02 reb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -14,10 +14,11 @@ package Kernel::Modules::AgentITSMWorkOrderAgent;
 use strict;
 use warnings;
 
+use Kernel::System::ITSMChange;
 use Kernel::System::ITSMChange::WorkOrder;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -33,6 +34,7 @@ sub new {
         }
     }
     $Self->{WorkOrderObject} = Kernel::System::ITSMChange::WorkOrder->new(%Param);
+    $Self->{ChangeObject}    = Kernel::System::ITSMChange->new(%Param);
 
     # get config of frontend module
     $Self->{Config} = $Self->{ConfigObject}->Get("ITSMChangeManagement::Frontend::$Self->{Action}");
@@ -43,9 +45,71 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
 
+    my $WorkOrderID = $Self->{ParamObject}->GetParam( Param => 'WorkOrderID' );
+
+    # check needed stuff
+    if ( !$WorkOrderID ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => 'No WorkOrderID is given!',
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # get workorder data
+    my $WorkOrder = $Self->{WorkOrderObject}->WorkOrderGet(
+        WorkOrderID => $WorkOrderID,
+        UserID      => $Self->{UserID},
+    );
+
+    if ( !$WorkOrder ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "WorkOrder $WorkOrder not found in database!",
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # get change that workorder belongs to
+    my $Change = $Self->{ChangeObject}->ChangeGet(
+        ChangeID => $WorkOrder->{ChangeID},
+        UserID   => $Self->{UserID},
+    );
+
+    if ( !$Change ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Could not find Change for WorkOrder $WorkOrder!",
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # build customer search autocomplete field
+    my $AutoCompleteConfig
+        = $Self->{ConfigObject}->Get('ITSMChange::Frontend::UserSearchAutoComplete');
+    if ( $AutoCompleteConfig->{Active} ) {
+        $Self->{LayoutObject}->Block(
+            Name => 'UserSearchAutoComplete',
+            Data => {
+                minQueryLength      => $AutoCompleteConfig->{MinQueryLength}      || 2,
+                queryDelay          => $AutoCompleteConfig->{QueryDelay}          || 0.1,
+                typeAhead           => $AutoCompleteConfig->{TypeAhead}           || 'false',
+                maxResultsDisplayed => $AutoCompleteConfig->{MaxResultsDisplayed} || 20,
+            },
+        );
+        $Self->{LayoutObject}->Block(
+            Name => 'UserSearchAutoCompleteDivStart',
+        );
+        $Self->{LayoutObject}->Block(
+            Name => 'UserSearchAutoCompleteDivEnd',
+        );
+    }
+    else {
+        $Self->{LayoutObject}->Block(
+            Name => 'SearchUserButton',
+        );
+    }
+
     # output header
     my $Output = $Self->{LayoutObject}->Header(
-        Title =>    # ...,
+        Title => $WorkOrder->{Title},
     );
     $Output .= $Self->{LayoutObject}->NavigationBar();
 
@@ -53,11 +117,9 @@ sub Run {
     $Output .= $Self->{LayoutObject}->Output(
         TemplateFile => 'AgentITSMWorkOrderAgent',
         Data         => {
-
-            # ...
             %Param,
-
-            # ...
+            %{$Change},
+            %{$WorkOrder},
         },
     );
 
