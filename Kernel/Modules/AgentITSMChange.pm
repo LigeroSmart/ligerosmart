b8f9225d4c31aad33354510bf7da4cbb775e4eb6
Markus Esche <markus.esche@otrs.com>
2009-10-21 08:00:48 +0000

    Added first draft of ChangeOverview.

--- Kernel/Modules/AgentITSMChange.pm
+++ Kernel/Modules/AgentITSMChange.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMChange.pm - the OTRS::ITSM::ChangeManagement change overview module
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMChange.pm,v 1.1 2009-10-11 23:22:20 ub Exp $
+# $Id: AgentITSMChange.pm,v 1.2 2009-10-21 07:59:31 mae Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -14,10 +14,11 @@ package Kernel::Modules::AgentITSMChange;
 use strict;
 use warnings;
 
+use Kernel::System::GeneralCatalog;
 use Kernel::System::ITSMChange;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -27,12 +28,14 @@ sub new {
     bless( $Self, $Type );
 
     # check needed objects
-    for my $Object (qw(ParamObject DBObject LayoutObject LogObject ConfigObject)) {
+    for my $Object (qw(ParamObject DBObject LayoutObject LogObject ConfigObject UserObject)) {
         if ( !$Self->{$Object} ) {
             $Self->{LayoutObject}->FatalError( Message => "Got no $Object!" );
         }
     }
-    $Self->{ChangeObject} = Kernel::System::ITSMChange->new(%Param);
+
+    $Self->{GeneralCatalogObject} = Kernel::System::GeneralCatalog->new(%Param);
+    $Self->{ChangeObject}         = Kernel::System::ITSMChange->new(%Param);
 
     # get config of frontend module
     $Self->{Config} = $Self->{ConfigObject}->Get("ITSMChangeManagement::Frontend::$Self->{Action}");
@@ -42,6 +45,109 @@ sub new {
 
 sub Run {
     my ( $Self, %Param ) = @_;
+
+    # get page
+    my $Page = $Self->{ParamObject}->GetParam( Param => 'Page' ) || 1;
+
+    my %SearchResult = (
+        Result       => 0,
+        ChangesAvail => 0,
+    );
+
+    # get change object
+    my $Changes = $Self->{ChangeObject}->ChangeSearch(
+        UserID => $Self->{UserID},
+    );
+
+    $Changes ||= [];
+
+    $SearchResult{ChangesAvail} = scalar @{$Changes} || 0;
+
+    if ($Changes) {
+        $Self->{LayoutObject}->Block(
+            Name => 'Change',
+            Data => {
+                %Param,
+                %SearchResult,
+            },
+        );
+    }
+
+    # TODO: GeneralCatalog-Preferences definition of LED color
+    #  CurInciSignal => $InciSignals{ $LastVersion->{CurInciStateType} },
+    # temp color for changes
+    my %CurChangeSignal = (
+        requested          => 'yellowled',
+        accepted           => 'greenled',
+        'pending approval' => 'yellowled',
+        rejected           => 'grayled',
+        approved           => 'greenled',
+        'in progress'      => 'yellowled',
+        successful         => 'greenled',
+        failed             => 'redled',
+        canceled           => 'grayled',
+    );
+
+    my $CssClass = '';
+    for my $ChangeID ( @{$Changes} ) {
+
+        # get current change
+        my $Change = $Self->{ChangeObject}->ChangeGet(
+            UserID   => $Self->{UserID},
+            ChangeID => $ChangeID,
+        ) || {};
+
+        # get change state from general catalog
+        my $CurChangeState = $Self->{GeneralCatalogObject}->ItemGet(
+            ItemID => $Change->{ChangeStateID},
+        ) || {};
+
+        # set output object
+        $CssClass = $CssClass eq 'searchpassive' ? 'searchactive' : 'searchpassive';
+
+        # set work order count
+        $Change->{WorkOrderIDs} ||= [];
+        $Change->{WorkOrderCount} = scalar @{ $Change->{WorkOrderIDs} } || 0;
+
+        # set change builder
+        # user Postfixes
+        my @Postfixes = qw(UserLogin UserFirstname UserLastname);
+
+        if ( $Change->{ChangeBuilderID} ) {
+
+            # get change manager data
+            my %ChangeBuilderUser = $Self->{UserObject}->GetUserData(
+                UserID => $Change->{ChangeBuilderID},
+                Cached => 1,
+            );
+            for my $Postfix (@Postfixes) {
+                if ( $Postfix eq 'UserFirstname' ) {
+                    $Change->{ 'ChangeBuilder' . $Postfix } = '(' . $ChangeBuilderUser{$Postfix};
+                }
+                elsif ( $Postfix eq 'UserLastname' ) {
+                    $Change->{ 'ChangeBuilder' . $Postfix } = $ChangeBuilderUser{$Postfix} . ')';
+                }
+                else {
+                    $Change->{ 'ChangeBuilder' . $Postfix } = $ChangeBuilderUser{$Postfix};
+                }
+            }
+        }
+        else {
+            $Change->{ChangeBuilderUserLogin} = '-';
+        }
+
+        $Self->{LayoutObject}->Block(
+            Name => 'ChangeRow',
+            Data => {
+                %Param,
+                %SearchResult,
+                %{$Change},
+                CssClass        => $CssClass,
+                CurChangeState  => $CurChangeState->{Name},
+                CurChangeSignal => $CurChangeSignal{ $CurChangeState->{Name} },
+            },
+        );
+    }
 
     # investigate refresh
     my $Refresh = $Self->{UserRefreshTime} ? 60 * $Self->{UserRefreshTime} : undef;
@@ -57,11 +163,8 @@ sub Run {
     $Output .= $Self->{LayoutObject}->Output(
         TemplateFile => 'AgentITSMChange',
         Data         => {
-
-            # ...
             %Param,
-
-            # ...
+            %SearchResult,
         },
     );
 
