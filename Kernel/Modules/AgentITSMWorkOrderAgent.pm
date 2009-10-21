778e131cece4bf086bdb879ca6677a65d8965cf5
Renée Bäcker <renee.baecker@otrs.com>
2009-10-21 09:58:16 +0000

    fix typo

--- Kernel/Modules/AgentITSMWorkOrderAgent.pm
+++ Kernel/Modules/AgentITSMWorkOrderAgent.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMWorkOrderAgent.pm - the OTRS::ITSM::ChangeManagement work order agent edit module
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMWorkOrderAgent.pm,v 1.9 2009-10-21 08:55:19 reb Exp $
+# $Id: AgentITSMWorkOrderAgent.pm,v 1.10 2009-10-21 09:58:16 reb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::ITSMChange::WorkOrder;
 use Kernel::System::User;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.9 $) [1];
+$VERSION = qw($Revision: 1.10 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -217,7 +217,7 @@ sub Run {
         );
     }
 
-    # build customer search autocomplete field
+    # build user search autocomplete field
     my $AutoCompleteConfig
         = $Self->{ConfigObject}->Get('ITSMChange::Frontend::UserSearchAutoComplete');
     if ( $AutoCompleteConfig->{Active} ) {
