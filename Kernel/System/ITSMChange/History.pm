87dfa3268930d7fe546c2fa9e97b74c625f6b7de
Udo Bretz <udo.bretz@otrs.com>
2009-10-22 16:14:50 +0000

    Added constructor.

--- Kernel/System/ITSMChange/History.pm
+++ Kernel/System/ITSMChange/History.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMChange/History.pm - all change and workorder history functions
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: History.pm,v 1.1 2009-10-11 23:23:00 ub Exp $
+# $Id: History.pm,v 1.2 2009-10-22 16:14:50 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 =head1 NAME
 
@@ -34,6 +34,27 @@ All history functions.
 # NOTE: Look at ITSMConfigurationManagement, but use new EventHandling Module
 # (currently in ITSMCore, will be in OTRS 2.5 framework later)
 
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (qw(DBObject ConfigObject EncodeObject LogObject MainObject TimeObject)) {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    # set default debug flag
+    $Self->{Debug} ||= 0;
+
+    # create additional objects
+    # ...
+
+    return $Self;
+}
+
 1;
 
 =back
@@ -50,6 +71,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.1 $ $Date: 2009-10-11 23:23:00 $
+$Revision: 1.2 $ $Date: 2009-10-22 16:14:50 $
 
 =cut
