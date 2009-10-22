f6b32bf21508b605df3828361268c17592cfad6f
Bernhard Schmalhofer <bernhard.schmalhofer@otrs.com>
2009-10-22 16:29:43 +0000

    Added planned start time and planned end time.

--- Kernel/Modules/AgentITSMWorkOrderAdd.pm
+++ Kernel/Modules/AgentITSMWorkOrderAdd.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMWorkOrderAdd.pm - the OTRS::ITSM::ChangeManagement work order add module
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMWorkOrderAdd.pm,v 1.3 2009-10-21 08:52:53 reb Exp $
+# $Id: AgentITSMWorkOrderAdd.pm,v 1.4 2009-10-22 16:29:43 bes Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::ITSMChange::WorkOrder;
 use Kernel::System::ITSMChange;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -69,46 +69,90 @@ sub Run {
         );
     }
 
-    # save all GET parameters in %GetParam
+    # store all needed parameters in %GetParam to make it reloadable
     my %GetParam;
     for my $ParamName (qw(WorkOrderTitle Instruction)) {
         $GetParam{$ParamName} = $Self->{ParamObject}->GetParam( Param => $ParamName );
     }
-
-    # update workorder
-    if ( $Self->{Subaction} eq 'Save' && $GetParam{WorkOrderTitle} ) {
-        my $WorkOrderID = $Self->{WorkOrderObject}->WorkOrderAdd(
-            ChangeID       => $ChangeID,
-            Instruction    => $GetParam{Instruction},
-            WorkOrderTitle => $GetParam{WorkOrderTitle},
-            UserID         => $Self->{UserID},
-        );
-
-        if ( !$WorkOrderID ) {
-
-            # show error message
-            return $Self->{LayoutObject}->ErrorScreen(
-                Message => "Was not able to add WorkOrder!",
-                Comment => 'Please contact the admin.',
-            );
-        }
-        else {
-
-            # redirect to zoom mask
-            return $Self->{LayoutObject}->Redirect(
-                OP => "Action=AgentITSMWorkOrderZoom&WorkOrderID=$WorkOrderID",
-            );
+    for my $TimeType (qw(PlannedStartTime PlannedEndTime)) {
+        for my $TimePart (qw(Year Month Day Hour Minute)) {
+            my $ParamName = $TimeType . $TimePart;
+            $GetParam{$ParamName} = $Self->{ParamObject}->GetParam( Param => $ParamName );
         }
     }
-    elsif ( $Self->{Subaction} eq 'Save' && !$GetParam{WorkOrderTitle} ) {
 
-        # show invalid message
-        $Self->{LayoutObject}->Block(
-            Name => 'InvalidTitle',
-        );
+    # add workorder
+    my %Invalid;
+    if ( $Self->{Subaction} eq 'Save' ) {
 
-        # don't show title
-        $Param{CurrentTitle} = '';
+        # add only if WorkOrderTitle is given
+        if ( !$GetParam{WorkOrderTitle} ) {
+            $Invalid{Title} = 'missing';
+        }
+
+        # check whether complete times are passed and build the time stamps
+        my %SystemTime;
+        TIMETYPE:
+        for my $TimeType (qw(PlannedStartTime PlannedEndTime)) {
+            for my $TimePart (qw(Year Month Day Hour Minute)) {
+                my $ParamName = $TimeType . $TimePart;
+                if ( !defined $GetParam{$ParamName} ) {
+                    $Self->{LogObject}
+                        ->Log( Priority => 'error', Message => "Need $ParamName!" );
+                    next TIMETYPE;
+                }
+            }
+
+            # format as timestamp
+            $GetParam{$TimeType} = sprintf '%04d-%02d-%02d %02d:%02d:00',
+                $GetParam{ $TimeType . 'Year' },
+                $GetParam{ $TimeType . 'Month' },
+                $GetParam{ $TimeType . 'Day' },
+                $GetParam{ $TimeType . 'Hour' },
+                $GetParam{ $TimeType . 'Minute' };
+
+            # sanity check the assembled timestamp
+            $SystemTime{$TimeType} = $Self->{TimeObject}->TimeStamp2SystemTime(
+                String => $GetParam{$TimeType},
+            );
+            if ( !$SystemTime{$TimeType} ) {
+                $Invalid{$TimeType} = 'invalid format';
+            }
+        }
+
+        # check the ordering of the times
+        if (
+            $SystemTime{PlannedStartTime}
+            && $SystemTime{PlannedEndTime}
+            && $SystemTime{PlannedStartTime} >= $SystemTime{PlannedEndTime}
+            )
+        {
+            $Invalid{PlannedEndTime} = 'Start time is equal or greater that the end time.';
+        }
+
+        if ( !%Invalid ) {
+            my $WorkOrderID = $Self->{WorkOrderObject}->WorkOrderAdd(
+                %GetParam,
+                ChangeID => $ChangeID,
+                UserID   => $Self->{UserID},
+            );
+
+            if ( !$WorkOrderID ) {
+
+                # show error message
+                return $Self->{LayoutObject}->ErrorScreen(
+                    Message => "Was not able to add WorkOrder!",
+                    Comment => 'Please contact the admin.',
+                );
+            }
+            else {
+
+                # redirect to zoom mask
+                return $Self->{LayoutObject}->Redirect(
+                    OP => "Action=AgentITSMWorkOrderZoom&WorkOrderID=$WorkOrderID",
+                );
+            }
+        }
     }
 
     # output header
@@ -120,6 +164,40 @@ sub Run {
     $Self->{LayoutObject}->Block(
         Name => 'RichText',
     );
+
+    # set the time selection
+    for my $TimeType (qw(PlannedStartTime PlannedEndTime)) {
+
+        # set default value for $DiffTime
+        # When no time is given yet, then use the current time plus the difftime
+        # When an explicit time was retrieved, $DiffTime is not used
+        my $DiffTime = $TimeType eq 'PlannedStartTime' ? 0 : 60 * 60;
+
+        # add selection for the time
+        my $TimeSelectionString = $Self->{LayoutObject}->BuildDateSelection(
+            %GetParam,
+            Format   => 'DateInputFormatLong',
+            Prefix   => $TimeType,
+            DiffTime => $DiffTime,
+        );
+
+        $Self->{LayoutObject}->Block(
+            Name => $TimeType,
+            Data => {
+                $TimeType . 'String' => $TimeSelectionString,
+                }
+        );
+    }
+
+    # The blocks InvalidPlannedStartTime and InvalidPlannedEndTime need to be filled after
+    # the blocks PlannedStartTime and PlannedEndTime
+    for my $Param ( keys %Invalid ) {
+
+        # show invalid message
+        $Self->{LayoutObject}->Block(
+            Name => 'Invalid' . $Param,
+        );
+    }
 
     # start template output
     $Output .= $Self->{LayoutObject}->Output(
