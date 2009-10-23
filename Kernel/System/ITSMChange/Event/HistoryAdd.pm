8c20657f5642964e0817f077723bb8a86e589dac
Udo Bretz <udo.bretz@otrs.com>
2009-10-23 09:51:49 +0000

    Added more detailed test logging in HistoryAdd event handling modules.

--- Kernel/System/ITSMChange/Event/HistoryAdd.pm
+++ Kernel/System/ITSMChange/Event/HistoryAdd.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMChange/Event/HistoryAdd.pm - HistoryAdd event module for ITSMChange
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: HistoryAdd.pm,v 1.2 2009-10-22 21:06:28 ub Exp $
+# $Id: HistoryAdd.pm,v 1.3 2009-10-23 09:51:49 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::ITSMChange::History;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 =head1 NAME
 
@@ -65,7 +65,7 @@ sub Run {
     # do history stuff
 
     # ... do history update for change add, change update, change...
-    #$Self->{LogObject}->Dum_per( '', "ChangeEvent: $Param{Event} " );
+    #$Self->{LogObject}->Dum_per( '', 'ChangeEvent: ' . $Param{Event} . ', %Param: ', \%Param );
 
     return 1;
 }
@@ -86,6 +86,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.2 $ $Date: 2009-10-22 21:06:28 $
+$Revision: 1.3 $ $Date: 2009-10-23 09:51:49 $
 
 =cut
