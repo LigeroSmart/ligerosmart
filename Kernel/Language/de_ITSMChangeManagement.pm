5a16d729c17cd73f42023c57631237a0cec6891e
Bernhard Schmalhofer <bernhard.schmalhofer@otrs.com>
2009-10-29 13:16:30 +0000

    Added an onclick handler which checks the inputs.

--- Kernel/Language/de_ITSMChangeManagement.pm
+++ Kernel/Language/de_ITSMChangeManagement.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de_ITSMChangeManagement.pm - the german translation of ITSMChangeManagement
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: de_ITSMChangeManagement.pm,v 1.2 2009-10-29 10:57:35 bes Exp $
+# $Id: de_ITSMChangeManagement.pm,v 1.3 2009-10-29 13:16:30 bes Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -24,7 +24,12 @@ sub Data {
 
     return if ref $Lang ne 'HASH';
 
-    $Lang->{'A change must have a title!'} = 'Änderungen brauchen einen Titel!';
+    $Lang->{'A change must have a title!'}        = 'Änderungen brauchen einen Titel!';
+    $Lang->{'A workorder must have a title!'}     = 'Arbeitsaufträge brauchen einen Titel!';
+    $Lang->{'The planned start time is invalid!'} = 'Der geplante Startzeitpunk ist ungültig!';
+    $Lang->{'The planned end time is invalid!'}   = 'Der geplante Endzeitpunkt ist ungültig!';
+    $Lang->{'The planned start time must be before the planned end time!'}
+        = 'Der geplante Start muss vor dem geplanten Ende liegen!';
 
     return 1;
 }
