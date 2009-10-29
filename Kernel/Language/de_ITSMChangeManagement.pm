23cab674bca91eb1599250bd1773b7e66bab9a6d
Bernhard Schmalhofer <bernhard.schmalhofer@otrs.com>
2009-10-29 10:57:35 +0000

    Added an onclick-handler for param validation.

--- Kernel/Language/de_ITSMChangeManagement.pm
+++ Kernel/Language/de_ITSMChangeManagement.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de_ITSMChangeManagement.pm - the german translation of ITSMChangeManagement
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: de_ITSMChangeManagement.pm,v 1.1 2009-10-11 23:21:41 ub Exp $
+# $Id: de_ITSMChangeManagement.pm,v 1.2 2009-10-29 10:57:35 bes Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -24,7 +24,7 @@ sub Data {
 
     return if ref $Lang ne 'HASH';
 
-    $Lang->{''} = '';
+    $Lang->{'A change must have a title!'} = 'Änderungen brauchen einen Titel!';
 
     return 1;
 }
