218bd01176d89c14330cea740f4ad4414a25315d
Renée Bäcker <renee.baecker@otrs.com>
2009-10-29 13:01:54 +0000

    first draft for workorder history

---
+++ Kernel/Modules/AgentITSMWorkOrderHistory.pm
@@ -0,0 +1,161 @@
+# --
+# Kernel/Modules/AgentITSMWorkOrderHistory.pm - the OTRS::ITSM::ChangeManagement workorder history module
+# Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
+# --
+# $Id: AgentITSMWorkOrderHistory.pm,v 1.1 2009-10-29 13:01:54 reb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AgentITSMWorkOrderHistory;
+
+use strict;
+use warnings;
+
+use Kernel::System::ITSMChange;
+use Kernel::System::ITSMChange::ITSMWorkOrder;
+use Kernel::System::ITSMChange::History;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (qw(ParamObject DBObject LayoutObject LogObject ConfigObject)) {
+        if ( !$Self->{$Object} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $Object!" );
+        }
+    }
+
+    # create needed objects
+    $Self->{ChangeObject}    = Kernel::System::ITSMChange->new(%Param);
+    $Self->{WorkOrderObject} = Kernel::System::ITSMChange::ITSMWorkOrder->new(%Param);
+    $Self->{HistoryObject}   = Kernel::System::ITSMChange::History->new(%Param);
+
+    # get config of frontend module
+    $Self->{Config} = $Self->{ConfigObject}->Get("ITSMChangeManagement::Frontend::$Self->{Action}");
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # get needed change id
+    my $WorkOrderID = $Self->{ParamObject}->GetParam( Param => 'WorkOrderID' );
+
+    # check needed stuff
+    if ( !$WorkOrderID ) {
+
+        # error page
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => 'Can\'t show history, no WorkOrderID is given!',
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # get change information
+    my $WorkOrder = $Self->{WorkOrderObject}->WorkOrderGet(
+        WorkOrderID => $WorkOrderID,
+        UserID      => $Self->{UserID},
+    );
+
+    # check error
+    if ( !$WorkOrder ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "WorkOrder $WorkOrderID not found in database!",
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # get change information
+    my $Change = $Self->{ChangeObject}->ChangeGet(
+        ChangeID => $WorkOrder->{ChangeID},
+        UserID   => $Self->{UserID},
+    );
+
+    # check error
+    if ( !$Change ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => 'Change ' . $WorkOrder->{ChangeID} . ' not found in database!',
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # get history entries
+    my $HistoryEntriesRef = $Self->{HistoryObject}->WorkOrderHistoryGet(
+        WorkOrderID => $WorkOrderID,
+        UserID      => $Self->{UserID},
+    );
+
+    # get order direction
+    my @HistoryLines = @{$HistoryEntriesRef};
+    if ( $Self->{ConfigObject}->Get('ITSMChange::Frontend::HistoryOrder') eq 'reverse' ) {
+        @HistoryLines = reverse @{$HistoryEntriesRef};
+    }
+
+    # create table
+    my $Counter = 1;
+    for my $HistoryEntry (@HistoryLines) {
+        $Counter++;
+
+        # data for a single row
+        my %Data = (
+            %{$HistoryEntry},
+            Content => $HistoryEntry->{ContentNew},
+        );
+
+        # show 'nice' output
+        $Data{Content} = $Self->{LayoutObject}->{LanguageObject}->Get(
+            'ChangeHistory::' . $Data{HistoryType} . '", ' . $Data{Content}
+        );
+
+        # seperate each searchresult line by using several css
+        if ( $Counter % 2 ) {
+            $Data{css} = 'searchpassive';
+        }
+        else {
+            $Data{css} = 'searchactive';
+        }
+        $Self->{LayoutObject}->Block(
+            Name => 'Row',
+            Data => {%Data},
+        );
+
+        $Self->{LayoutObject}->Block(
+            Name => 'ChangeZoom',
+            Data => {%Data},
+        );
+    }
+
+    # output header
+    my $Output = $Self->{LayoutObject}->Header(
+        Title => 'WorkOrderHistory',
+    );
+    $Output .= $Self->{LayoutObject}->NavigationBar();
+
+    # start template output
+    $Output .= $Self->{LayoutObject}->Output(
+        TemplateFile => 'AgentITSMWorkOrderHistory',
+        Data         => {
+            %Param,
+            %{$Change},
+            %{$WorkOrder},
+        },
+    );
+
+    # add footer
+    $Output .= $Self->{LayoutObject}->Footer();
+
+    return $Output;
+}
+
+1;
