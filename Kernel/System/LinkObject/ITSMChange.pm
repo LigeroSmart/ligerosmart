9cee45d7ab06ed18aa6d923e82429fb7480f79ac
Udo Bretz <udo.bretz@otrs.com>
2009-11-06 10:11:37 +0000

    Removed test.

--- Kernel/System/LinkObject/ITSMChange.pm
+++ Kernel/System/LinkObject/ITSMChange.pm
@@ -2,7 +2,7 @@
 # Kernel/System/LinkObject/ITSMChange.pm - to link change objects
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: ITSMChange.pm,v 1.3 2009-11-06 09:47:49 ub Exp $
+# $Id: ITSMChange.pm,v 1.4 2009-11-06 10:11:37 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::ITSMChange;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -349,8 +349,6 @@ sub LinkAddPost {
 
     return 1;
 }
-
-# test
 
 =item LinkDeletePre()
 
