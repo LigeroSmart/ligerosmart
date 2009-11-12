543e0997079faa5f391187e3362464a538f36574
Bernhard Schmalhofer <bernhard.schmalhofer@otrs.com>
2009-11-12 11:14:33 +0000

    Added the missing ChangeObject.

--- Kernel/System/ITSMChange/ITSMWorkOrder/Permission/CABCheck.pm
+++ Kernel/System/ITSMChange/ITSMWorkOrder/Permission/CABCheck.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMChange/ITSMWorkOrder/Permission/CABCheck.pm - CAB based permission check
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: CABCheck.pm,v 1.3 2009-11-10 13:17:41 bes Exp $
+# $Id: CABCheck.pm,v 1.4 2009-11-12 11:14:33 bes Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 =head1 NAME
 
@@ -117,6 +117,9 @@ sub new {
         $Self->{$_} = $Param{$_} || die "Got no $_!";
     }
 
+    # create additional objects
+    $Self->{ChangeObject} = Kernel::System::ITSMChange->new( %{$Self} );
+
     return $Self;
 }
 
@@ -188,7 +191,7 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Id: CABCheck.pm,v 1.3 2009-11-10 13:17:41 bes Exp $
+$Id: CABCheck.pm,v 1.4 2009-11-12 11:14:33 bes Exp $
 
 =cut
 
