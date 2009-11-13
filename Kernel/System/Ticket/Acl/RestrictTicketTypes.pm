7425d4646cfc6e39e63c631252e7df3577f872e6
Udo Bretz <udo.bretz@otrs.com>
2009-11-13 17:02:43 +0000

    Renamed RestrictRFCTicketType.pm to RestrictTicketTypes.pm.

--- Kernel/System/Ticket/Acl/RestrictTicketTypes.pm
+++ Kernel/System/Ticket/Acl/RestrictTicketTypes.pm
@@ -1,22 +1,23 @@
 # --
-# Kernel/System/Ticket/Acl/RestrictRFCTicketType.pm - acl module
-# - restrict the usage of the ticket type 'RfC' to certain groups -
+# Kernel/System/Ticket/Acl/RestrictTicketTypes.pm - acl module
+# - restrict the usage of the ticket types as defined in
+# - sysconfig option 'ITSMChange::AddChangeLinkTicketTypes' to certain groups -
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: RestrictRFCTicketType.pm,v 1.3 2009-11-13 16:56:43 ub Exp $
+# $Id: RestrictTicketTypes.pm,v 1.1 2009-11-13 17:01:56 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::System::Ticket::Acl::RestrictRFCTicketType;
+package Kernel::System::Ticket::Acl::RestrictTicketTypes;
 
 use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.1 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
