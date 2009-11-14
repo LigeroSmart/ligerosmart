70366638f583b75ebbf841e1d8721c930b375a8a
Udo Bretz <udo.bretz@otrs.com>
2009-11-14 13:58:26 +0000

    Switched to the english words for changes and workorders in german translation, as this is consistent to other ITSM modules.

--- Kernel/Language/de_ITSMChangeManagement.pm
+++ Kernel/Language/de_ITSMChangeManagement.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de_ITSMChangeManagement.pm - the german translation of ITSMChangeManagement
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: de_ITSMChangeManagement.pm,v 1.4 2009-11-12 09:00:56 bes Exp $
+# $Id: de_ITSMChangeManagement.pm,v 1.5 2009-11-14 13:57:32 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.4 $) [1];
+$VERSION = qw($Revision: 1.5 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -24,9 +24,9 @@ sub Data {
 
     return if ref $Lang ne 'HASH';
 
-    $Lang->{'A change must have a title!'}        = 'Änderungen brauchen einen Titel!';
-    $Lang->{'A workorder must have a title!'}     = 'Arbeitsaufträge brauchen einen Titel!';
-    $Lang->{'Add Change'}                         = 'Neue Änderung';
+    $Lang->{'A change must have a title!'}        = 'Ein Change benötigt einen Titel!';
+    $Lang->{'A workorder must have a title!'}     = 'Eine Workorder benötigt einen Titel!';
+    $Lang->{'Add Change'}                         = 'Change hinzufügen';
     $Lang->{'The planned start time is invalid!'} = 'Der geplante Startzeitpunk ist ungültig!';
     $Lang->{'The planned end time is invalid!'}   = 'Der geplante Endzeitpunkt ist ungültig!';
     $Lang->{'The planned start time must be before the planned end time!'}
