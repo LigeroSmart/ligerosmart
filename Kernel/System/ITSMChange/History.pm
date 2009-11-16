d4f3ed6b582b068a35682ee3f645d305120daeea
Renée Bäcker <renee.baecker@otrs.com>
2009-11-16 07:48:20 +0000

    fix order of history entries

--- Kernel/System/ITSMChange/History.pm
+++ Kernel/System/ITSMChange/History.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMChange/History.pm - all change and workorder history functions
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: History.pm,v 1.16 2009-11-13 18:38:47 reb Exp $
+# $Id: History.pm,v 1.17 2009-11-16 07:48:20 reb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::User;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.16 $) [1];
+$VERSION = qw($Revision: 1.17 $) [1];
 
 =head1 NAME
 
@@ -232,7 +232,7 @@ sub WorkOrderHistoryGet {
             . 'ch.create_by, ch.create_time, type_id, cht.name, fieldname '
             . 'FROM change_history ch, change_history_type cht '
             . 'WHERE ch.type_id = cht.id '
-            . 'AND workorder_id = ? ORDER BY change_time',
+            . 'AND workorder_id = ? ORDER BY ch.create_time, ch.id',
         Bind => [ \$Param{WorkOrderID} ],
     );
 
@@ -323,7 +323,7 @@ sub ChangeHistoryGet {
             . 'ch.create_by, ch.create_time, type_id, cht.name, fieldname '
             . 'FROM change_history ch, change_history_type cht '
             . 'WHERE ch.type_id = cht.id '
-            . 'AND change_id = ? ORDER BY change_time',
+            . 'AND change_id = ? ORDER BY ch.create_time, ch.id',
         Bind => [ \$Param{ChangeID} ],
     );
 
@@ -612,6 +612,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.16 $ $Date: 2009-11-13 18:38:47 $
+$Revision: 1.17 $ $Date: 2009-11-16 07:48:20 $
 
 =cut
