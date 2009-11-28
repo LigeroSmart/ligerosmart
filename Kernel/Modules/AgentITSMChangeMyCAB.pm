ba0d92b9592f17069dd7632fa8af538665ef6a42
Udo Bretz <udo.bretz@otrs.com>
2009-11-28 16:31:37 +0000

    Added new overview frontend modules.

---
+++ Kernel/Modules/AgentITSMChangeMyCAB.pm
@@ -0,0 +1,233 @@
+# --
+# Kernel/Modules/AgentITSMChangeMyCAB.pm - the OTRS::ITSM::ChangeManagement MyCAB overview module
+# Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
+# --
+# $Id: AgentITSMChangeMyCAB.pm,v 1.1 2009-11-28 16:30:30 ub Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AgentITSMChangeMyCAB;
+
+use strict;
+use warnings;
+
+use Kernel::System::ITSMChange;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (qw(ParamObject DBObject LayoutObject LogObject ConfigObject UserObject)) {
+        if ( !$Self->{$Object} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $Object!" );
+        }
+    }
+
+    # create needed objects
+    $Self->{ChangeObject} = Kernel::System::ITSMChange->new(%Param);
+
+    # get config of frontend module
+    $Self->{Config} = $Self->{ConfigObject}->Get("ITSMChange::Frontend::$Self->{Action}");
+
+    # get filter and view params
+    $Self->{Filter} = $Self->{ParamObject}->GetParam( Param => 'Filter' ) || 'All';
+    $Self->{View}   = $Self->{ParamObject}->GetParam( Param => 'View' )   || '';
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # check permissions
+    my $Access = $Self->{ChangeObject}->Permission(
+        Type   => $Self->{Config}->{Permission},
+        UserID => $Self->{UserID},
+    );
+
+    # error screen
+    if ( !$Access ) {
+        return $Self->{LayoutObject}->NoPermission(
+            Message    => "You need $Self->{Config}->{Permission} permissions!",
+            WithHeader => 'yes',
+        );
+    }
+
+    # store last screen, used for backlinks
+    $Self->{SessionObject}->UpdateSessionID(
+        SessionID => $Self->{SessionID},
+        Key       => 'LastScreenChanges',
+        Value     => $Self->{RequestedURL},
+    );
+
+    # get sorting parameters
+    my $SortBy = $Self->{ParamObject}->GetParam( Param => 'SortBy' )
+        || $Self->{Config}->{'SortBy::Default'}
+        || 'ChangeNumber';
+
+    # get ordering parameters
+    my $OrderBy = $Self->{ParamObject}->GetParam( Param => 'OrderBy' )
+        || $Self->{Config}->{'Order::Default'}
+        || 'Up';
+
+    my @SortByArray  = ($SortBy);
+    my @OrderByArray = ($OrderBy);
+
+    # investigate refresh
+    my $Refresh = $Self->{UserRefreshTime} ? 60 * $Self->{UserRefreshTime} : undef;
+
+    # starting with page ...
+    my $Output = $Self->{LayoutObject}->Header( Refresh => $Refresh );
+    $Output .= $Self->{LayoutObject}->NavigationBar();
+    $Self->{LayoutObject}->Print( Output => \$Output );
+    $Output = '';
+
+    # find out which columns should be shown
+    my @ShowColumns;
+    if ( $Self->{Config}->{ShowColumns} ) {
+
+        # get all possible columns from config
+        my %PossibleColumn = %{ $Self->{Config}->{ShowColumns} };
+
+        # get the column names that should be shown
+        COLUMNNAME:
+        for my $Name ( keys %PossibleColumn ) {
+            next COLUMNNAME if !$PossibleColumn{$Name};
+            push @ShowColumns, $Name;
+        }
+    }
+
+    # set default search filter
+    my %Filters = (
+        All => {
+            Name   => 'All',
+            Prio   => 1000,
+            Search => {
+                CABAgents        => [ $Self->{UserID} ],
+                OrderBy          => \@SortByArray,
+                OrderByDirection => \@OrderByArray,
+                Limit            => 1000,
+                UserID           => $Self->{UserID},
+            },
+        },
+    );
+
+    # set other filters based on change state
+    if ( $Self->{Config}->{'Filter::ChangeStates'} ) {
+
+        # define position of the filter in the frontend
+        my $PrioCounter = 1000;
+
+        # get all change states that should be used as filters
+        CHANGESTATE:
+        for my $ChangeState ( @{ $Self->{Config}->{'Filter::ChangeStates'} } ) {
+
+            # do not use empty change states
+            next CHANGESTATE if !$ChangeState;
+
+            # check if state is valid by looking up the state id
+            my $ChangeStateID = $Self->{ChangeObject}->ChangeStateLookup(
+                ChangeState => $ChangeState,
+            );
+
+            # do not use invalid change states
+            next CHANGESTATE if !$ChangeStateID;
+
+            # increase the PrioCounter
+            $PrioCounter++;
+
+            # add filter for the current change state
+            $Filters{$ChangeState} = {
+                Name   => $ChangeState,
+                Prio   => $PrioCounter,
+                Search => {
+                    CABAgents        => [ $Self->{UserID} ],
+                    ChangeStates     => [$ChangeState],
+                    OrderBy          => \@SortByArray,
+                    OrderByDirection => \@OrderByArray,
+                    Limit            => 1000,
+                    UserID           => $Self->{UserID},
+                },
+            };
+        }
+    }
+
+    # check if filter is valid
+    if ( !$Filters{ $Self->{Filter} } ) {
+        $Self->{LayoutObject}->FatalError( Message => "Invalid Filter: $Self->{Filter}!" );
+    }
+
+    # search changes which match the selected filter
+    my $ChangeIDsRef = $Self->{ChangeObject}->ChangeSearch(
+        %{ $Filters{ $Self->{Filter} }->{Search} },
+    );
+
+    # display all navbar filters
+    my %NavBarFilter;
+    for my $Filter ( keys %Filters ) {
+
+        # count the number of changes for each filter
+        my $Count = $Self->{ChangeObject}->ChangeSearch(
+            %{ $Filters{$Filter}->{Search} },
+            Result => 'COUNT',
+        );
+
+        # display the navbar filter
+        $NavBarFilter{ $Filters{$Filter}->{Prio} } = {
+            Count  => $Count,
+            Filter => $Filter,
+            %{ $Filters{$Filter} },
+        };
+    }
+
+    # show changes
+    my $LinkPage = 'Filter='
+        . $Self->{LayoutObject}->Ascii2Html( Text => $Self->{Filter} )
+        . '&View=' . $Self->{LayoutObject}->Ascii2Html( Text => $Self->{View} )
+        . '&SortBy=' . $Self->{LayoutObject}->Ascii2Html( Text => $SortBy )
+        . '&OrderBy=' . $Self->{LayoutObject}->Ascii2Html( Text => $OrderBy )
+        . '&';
+    my $LinkSort = 'Filter='
+        . $Self->{LayoutObject}->Ascii2Html( Text => $Self->{Filter} )
+        . '&View=' . $Self->{LayoutObject}->Ascii2Html( Text => $Self->{View} )
+        . '&';
+    my $LinkFilter = 'SortBy=' . $Self->{LayoutObject}->Ascii2Html( Text => $SortBy )
+        . '&OrderBy=' . $Self->{LayoutObject}->Ascii2Html( Text => $OrderBy )
+        . '&View=' . $Self->{LayoutObject}->Ascii2Html( Text => $Self->{View} )
+        . '&';
+    $Output .= $Self->{LayoutObject}->ITSMChangeListShow(
+
+        ChangeIDs => $ChangeIDsRef,
+        Total     => scalar @{$ChangeIDsRef},
+
+        View => $Self->{View},
+
+        Filter     => $Self->{Filter},
+        Filters    => \%NavBarFilter,
+        FilterLink => $LinkFilter,
+
+        TitleName  => 'Overview: My CAB',
+        TitleValue => $Self->{Filter},
+
+        Env      => $Self,
+        LinkPage => $LinkPage,
+        LinkSort => $LinkSort,
+
+        ShowColumns => \@ShowColumns,
+    );
+
+    $Output .= $Self->{LayoutObject}->Footer();
+    return $Output;
+}
+
+1;
