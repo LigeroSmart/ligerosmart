df5326738e4f7bb2175533542b6b9d0fbefe21ae
Renée Bäcker <renee.baecker@otrs.com>
2009-12-11 13:40:24 +0000

    add translation for requested time

--- Kernel/Language/de_ITSMChangeManagement.pm
+++ Kernel/Language/de_ITSMChangeManagement.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de_ITSMChangeManagement.pm - the german translation of ITSMChangeManagement
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: de_ITSMChangeManagement.pm,v 1.12 2009-11-25 18:13:04 ub Exp $
+# $Id: de_ITSMChangeManagement.pm,v 1.13 2009-12-11 13:40:24 reb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.12 $) [1];
+$VERSION = qw($Revision: 1.13 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -31,6 +31,7 @@ sub Data {
     $Lang->{'The planned end time is invalid!'}   = 'Der geplante Endzeitpunkt ist ungültig!';
     $Lang->{'The planned start time must be before the planned end time!'}
         = 'Der geplante Start muss vor dem geplanten Ende liegen!';
+    $Lang->{'Requested (by customer) Date'}       = 'Wunschtermin (des Kunden)';
 
     # Change menu
     $Lang->{'ITSM Change'} = 'Change';
