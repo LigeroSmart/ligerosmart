42be31518592086864c02c69f401968efa22d6ce
Udo Bretz <udo.bretz@otrs.com>
2009-12-14 19:12:49 +0000

    Improved code style and some refactoring.

--- Kernel/Output/HTML/LayoutITSMChange.pm
+++ Kernel/Output/HTML/LayoutITSMChange.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/LayoutITSMChange.pm - provides generic HTML output for ITSMChange
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: LayoutITSMChange.pm,v 1.26 2009-12-14 19:12:00 ub Exp $
+# $Id: LayoutITSMChange.pm,v 1.27 2009-12-14 19:12:49 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use POSIX qw(ceil);
 use Kernel::Output::HTML::Layout;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.26 $) [1];
+$VERSION = qw($Revision: 1.27 $) [1];
 
 =over 4
 
@@ -803,8 +803,7 @@ sub _ITSMChangeGetWorkOrderGraph {
         $TickValue{"${TimeType}Padding"}
             = ( $TickValue{"${TimeType}Ticks"} == 1 && $TickValue{"${TimeType}Padding"} == 100 )
             ? 99
-            : $TickValue{"${TimeType}Padding"}
-            ;
+            : $TickValue{"${TimeType}Padding"};
 
         # get trailing space
         $TickValue{"${TimeType}Trailing"}
