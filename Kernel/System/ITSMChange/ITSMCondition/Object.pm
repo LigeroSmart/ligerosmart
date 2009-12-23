81dcd11e8a82c45cd3b9b897622caed2b2894eb0
Markus Esche <markus.esche@otrs.com>
2009-12-23 13:19:18 +0000

    Implemented ObjectAdd and ObjectLookup.

--- Kernel/System/ITSMChange/ITSMCondition/Object.pm
+++ Kernel/System/ITSMChange/ITSMCondition/Object.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMChange/ITSMCondition/Object.pm - all condition object functions
 # Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
 # --
-# $Id: Object.pm,v 1.1 2009-12-23 10:15:31 mae Exp $
+# $Id: Object.pm,v 1.2 2009-12-23 13:19:18 mae Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 =head1 NAME
 
@@ -34,7 +34,8 @@ All functions for condition objects in ITSMChangeManagement.
 Add a new object.
 
     my $ConditionID = $ConditionObject->ObjectAdd(
-        UserID   => 1,
+        UserID => 1,
+        Name   => 'ConditionObject',
     );
 
 =cut
@@ -43,7 +44,7 @@ sub ObjectAdd {
     my ( $Self, %Param ) = @_;
 
     # check needed stuff
-    for my $Argument (qw(UserID)) {
+    for my $Argument (qw(UserID Name)) {
         if ( !$Param{$Argument} ) {
             $Self->{LogObject}->Log(
                 Priority => 'error',
@@ -53,7 +54,44 @@ sub ObjectAdd {
         }
     }
 
-    my $ObjectID;
+    # make lookup with given name for checks
+    my $CheckObjectID = $Self->ObjectLookup(
+        UserID => $Param{UserID},
+        Name   => $Param{Name},
+    );
+
+    # check if object name is already given
+    if ($CheckObjectID) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => "Condition object ($Param{Name}) already exists!",
+        );
+        return;
+    }
+
+    # add new object name to database
+    return if !$Self->{DBObject}->Do(
+        SQL => 'INSERT INTO condition_object '
+            . '(name) '
+            . 'VALUES (?)',
+        Bind => [ \$Param{Name} ],
+    );
+
+    # get id of created object
+    my $ObjectID = $Self->ObjectLookup(
+        UserID => $Param{UserID},
+        Name   => $Param{Name},
+    );
+
+    # check if object could be added
+    if ( !$ObjectID ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => "ObjectAdd() failed!",
+        );
+        return;
+    }
+
     return $ObjectID;
 }
 
@@ -102,6 +140,20 @@ sub ObjectGet {
 
 =item ObjectLookup()
 
+This method does a lookup for a condition object. If a object
+id is given, it returns the name of the object. If the objects
+name is given, the appropriate id is returned.
+
+    my $ObjectName = $ConditionObject->ObjectLookup(
+        UserID   => 1234,
+        ObjectID => 4321
+    );
+
+    my $ObjectID = $ConditionObject->ObjectLookup(
+        UserID     => 1234,
+        Name => 'ConditionObject',
+    );
+
 =cut
 
 sub ObjectLookup {
@@ -118,7 +170,47 @@ sub ObjectLookup {
         }
     }
 
-    return;
+    # check if both parameters are given
+    if ( $Param{ObjectID} && $Param{Name} ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => 'Need ObjectID or Name - not both!',
+        );
+        return;
+    }
+
+    # check if both parameters are not given
+    if ( !$Param{ObjectID} && !$Param{Name} ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => 'Need ObjectID or Name - none is given!',
+        );
+        return;
+    }
+
+    # prepare SQL statements
+    if ( $Param{ObjectID} ) {
+        return if !$Self->{DBObject}->Prepare(
+            SQL   => 'SELECT name FROM condition_object WHERE id = ?',
+            Bind  => [ \$Param{ObjectID} ],
+            Limit => 1,
+        );
+    }
+    elsif ( $Param{Name} ) {
+        return if !$Self->{DBObject}->Prepare(
+            SQL   => 'SELECT id FROM condition_object WHERE name = ?',
+            Bind  => [ \$Param{Name} ],
+            Limit => 1,
+        );
+    }
+
+    # fetch the result
+    my $ObjectLookup;
+    while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
+        $ObjectLookup = $Row[0];
+    }
+
+    return $ObjectLookup;
 }
 
 =item ObjectList()
@@ -192,6 +284,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.1 $ $Date: 2009-12-23 10:15:31 $
+$Revision: 1.2 $ $Date: 2009-12-23 13:19:18 $
 
 =cut
