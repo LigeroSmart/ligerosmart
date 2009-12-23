9be7493308054dad2a3afe00135f31d31b399045
Markus Esche <markus.esche@otrs.com>
2009-12-23 08:15:01 +0000

    Added condition backend to repo.

---
+++ Kernel/System/ITSMChange/ITSMCondition.pm
@@ -0,0 +1,260 @@
+# --
+# Kernel/System/ITSMChange/ITSMCondition.pm - all condition functions
+# Copyright (C) 2003-2009 OTRS AG, http://otrs.com/
+# --
+# $Id: ITSMCondition.pm,v 1.1 2009-12-23 08:15:01 mae Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::ITSMChange::ITSMCondition;
+
+use strict;
+use warnings;
+
+# TODO: may we need an event handler in the condtion framework
+# I thin so ;)
+#use base qw(Kernel::System::EventHandler);
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=head1 NAME
+
+Kernel::System::ITSMChange::ITSMCondition - condition lib
+
+=head1 SYNOPSIS
+
+All functions for conditions in ITSMChangeManagement.
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+    use Kernel::Config;
+    use Kernel::System::Encode;
+    use Kernel::System::Log;
+    use Kernel::System::DB;
+    use Kernel::System::Main;
+    use Kernel::System::Time;
+    use Kernel::System::ITSMChange::ITSMCondition;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $EncodeObject = Kernel::System::Encode->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $LogObject = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+    );
+    my $MainObject = Kernel::System::Main->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+    );
+    my $TimeObject = Kernel::System::Time->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+    );
+    my $DBObject = Kernel::System::DB->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+    );
+    my $ConditionObject = Kernel::System::ITSMChange::ITSMCondition->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        DBObject     => $DBObject,
+        TimeObject   => $TimeObject,
+        MainObject   => $MainObject,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (qw(DBObject ConfigObject EncodeObject LogObject MainObject TimeObject)) {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    # set the debug flag
+    $Self->{Debug} = $Param{Debug} || 0;
+
+    # create additional objects
+
+    # init of event handler
+    #$Self->EventHandlerInit(
+    #    Config     => 'ITSMCondition::EventModule',
+    #    BaseObject => 'ConditionObject',
+    #    Objects    => {
+    #        %{$Self},
+    #    },
+    #);
+
+    return $Self;
+}
+
+=item ConditionAdd()
+
+Add a new condition.
+
+    my $ConditionID = $ConditionObject->ConditionAdd(
+        ChangeID => 123,
+        UserID   => 1,
+    );
+
+=cut
+
+sub ConditionAdd {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(ChangeID UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    my $ConditionID;
+    return $ConditionID;
+}
+
+=item ConditionUpdate()
+
+=cut
+
+sub ConditionUpdate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(ConditionID UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    return 1;
+}
+
+=item ConditionGet()
+
+=cut
+
+sub ConditionGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Attribute (qw(ConditionID UserID)) {
+        if ( !$Param{$Attribute} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Attribute!",
+            );
+            return;
+        }
+    }
+
+    my %ConditionData;
+    return \%ConditionData;
+}
+
+=item ConditionList()
+
+return a list of all conditions ids of a given change id as array reference
+
+    my $ConditionIDsRef = $ConditionObject->ConditionList(
+        ChangeID => 5,
+        UserID   => 1,
+    );
+
+=cut
+
+sub ConditionList {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Attribute (qw(ChangeID UserID)) {
+        if ( !$Param{$Attribute} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Attribute!",
+            );
+            return;
+        }
+    }
+
+    my @ConditionIDs;
+    return \@ConditionIDs;
+}
+
+=item ConditionDelete()
+
+Delete a condition.
+
+    my $Success = $ConditionObject->ConditionDelete(
+        ConditionID => 123,
+        UserID      => 1,
+    );
+
+=cut
+
+sub ConditionDelete {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Attribute (qw(ConditionID UserID)) {
+        if ( !$Param{$Attribute} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Attribute!",
+            );
+            return;
+        }
+    }
+
+    return 1;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (http://otrs.org/).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2009-12-23 08:15:01 $
+
+=cut
