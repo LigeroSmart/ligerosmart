720c6eb7214fc293e656eec566a962e7aac6c4e6
Bernhard Schmalhofer <bernhard.schmalhofer@otrs.com>
2010-01-08 13:11:32 +0000

    Changed the notification event rule for ChangeAdd from 'notify when ChangeState matches "requested"' to a catch all rule.

--- var/packagesetup/ITSMChangeManagement.pm
+++ var/packagesetup/ITSMChangeManagement.pm
@@ -2,7 +2,7 @@
 # ITSMChangeManagement.pm - code to excecute during package installation
 # Copyright (C) 2003-2010 OTRS AG, http://otrs.com/
 # --
-# $Id: ITSMChangeManagement.pm,v 1.19 2010-01-07 14:13:12 reb Exp $
+# $Id: ITSMChangeManagement.pm,v 1.20 2010-01-08 13:11:32 bes Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -33,7 +33,7 @@ use Kernel::System::User;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.19 $) [1];
+$VERSION = qw($Revision: 1.20 $) [1];
 
 =head1 NAME
 
@@ -719,11 +719,11 @@ sub _AddNotifications {
     my @Notifications = (
         {
             Name       => 'requested changes',
-            Attribute  => 'ChangeState',
+            Attribute  => '',
             Event      => 'ChangeAdd',
             ValidID    => 1,
             Comment    => 'inform recipients that a change was requested',
-            Rule       => 'requested',
+            Rule       => '',
             Recipients => [ 'ChangeInitiators', 'ChangeManager', 'ChangeBuilder' ],
         },
         {
@@ -1158,6 +1158,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.19 $ $Date: 2010-01-07 14:13:12 $
+$Revision: 1.20 $ $Date: 2010-01-08 13:11:32 $
 
 =cut
