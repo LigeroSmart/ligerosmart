63743ba1b06cd83907d03064bbf2248043c2b2e6
Udo Bretz <udo.bretz@otrs.com>
2010-01-11 11:14:00 +0000

    Small POD update, made parameter AttributesChanged optional.

--- Kernel/System/ITSMChange/ITSMCondition/Expression.pm
+++ Kernel/System/ITSMChange/ITSMCondition/Expression.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMChange/ITSMCondition/Expression.pm - all condition expression functions
 # Copyright (C) 2003-2010 OTRS AG, http://otrs.com/
 # --
-# $Id: Expression.pm,v 1.8 2010-01-08 16:07:53 mae Exp $
+# $Id: Expression.pm,v 1.9 2010-01-11 11:14:00 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.8 $) [1];
+$VERSION = qw($Revision: 1.9 $) [1];
 
 =head1 NAME
 
@@ -342,7 +342,7 @@ Returns the boolean value of an expression.
 
     my $Match = $ConditionObject->ExpressionMatch(
         ExpressionID      => 123,
-        AttributesChanged => [ 'ChangeTitle', 'ChangeStateID', ],
+        AttributesChanged => [ 'ChangeTitle', 'ChangeStateID' ],  # (optional)
         UserID            => 1,
     );
 
@@ -478,6 +478,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.8 $ $Date: 2010-01-08 16:07:53 $
+$Revision: 1.9 $ $Date: 2010-01-11 11:14:00 $
 
 =cut
