e6ef0cd59401d299807ff1a97ce20294fdff0759
Renée Bäcker <renee.baecker@otrs.com>
2010-01-12 11:31:19 +0000

    Add Stats module for history searches.

---
+++ Kernel/System/Stats/Dynamic/ITSMChangeManagementHistory.pm
@@ -0,0 +1,187 @@
+# --
+# Kernel/System/Stats/Dynamic/ITSMChangeManagementHistory.pm - all advice functions
+# Copyright (C) 2003-2010 OTRS AG, http://otrs.com/
+# --
+# $Id: ITSMChangeManagementHistory.pm,v 1.1 2010-01-12 11:31:19 reb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::Stats::Dynamic::ITSMChangeManagementHistory;
+
+use strict;
+use warnings;
+
+use Kernel::System::ITSMChange;
+use Kernel::System::ITSMChange::History;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (
+        qw(DBObject ConfigObject LogObject UserObject TimeObject MainObject EncodeObject)
+        )
+    {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    # create needed objects
+    $Self->{ChangeObject}  = Kernel::System::ITSMChange->new( %{$Self} );
+    $Self->{HistoryObject} = Kernel::System::ITSMChange::History->new( %{$Self} );
+
+    return $Self;
+}
+
+sub GetObjectName {
+    my ( $Self, %Param ) = @_;
+
+    return 'ITSMChangeManagementHistory';
+}
+
+sub GetObjectAttributes {
+    my ( $Self, %Param ) = @_;
+
+    # get change state list
+    my $ChangeStates = $Self->{ChangeObject}->ChangePossibleStatesGet(
+        UserID => 1,
+    );
+    my %ChangeStateList = map { $_->{Key} => $_->{Value} } @{$ChangeStates};
+
+    my @ObjectAttributes = (
+        {
+            Name             => 'Change State',
+            UseAsXvalue      => 1,
+            UseAsValueSeries => 1,
+            UseAsRestriction => 1,
+            Element          => 'NewValues',
+            Block            => 'MultiSelectField',
+            Values           => \%ChangeStateList,
+        },
+        {
+            Name             => 'Timeperiod',
+            UseAsXvalue      => 1,
+            UseAsValueSeries => 1,
+            UseAsRestriction => 1,
+            Element          => 'TimePeriod',
+            TimePeriodFormat => 'DateInputFormat',    # 'DateInputFormatLong',
+            Block            => 'Time',
+            Values           => {
+                TimeStart => 'ChangeTimeNewerDate',
+                TimeStop  => 'ChangeTimeOlderDate',
+            },
+        },
+    );
+
+    return @ObjectAttributes;
+}
+
+sub GetStatElement {
+    my ( $Self, %Param ) = @_;
+
+    # search tickets
+    my $IDs = $Self->{HistoryObject}->HistorySearch(
+        UserID    => 1,
+        Type      => 'Change',
+        Attribute => 'ChangeStateID',
+
+        #Result     => 'COUNT',
+        Limit => 100_000_000,
+        %Param,
+    );
+
+    my @ChangeNumbers;
+    if ( $IDs && ref $IDs eq 'ARRAY' ) {
+
+        ID:
+        for my $ID ( @{$IDs} ) {
+            my $Change = $Self->{ChangeObject}->ChangeGet(
+                ChangeID => $ID,
+                UserID   => 1,
+            );
+
+            next ID if !$Change;
+
+            push @ChangeNumbers, $Change->{ChangeNumber};
+        }
+    }
+
+    return join "\n", @ChangeNumbers;
+}
+
+sub ExportWrapper {
+    my ( $Self, %Param ) = @_;
+
+    # wrap ids to used spelling
+    for my $Use (qw(UseAsValueSeries UseAsRestriction UseAsXvalue)) {
+        ELEMENT:
+        for my $Element ( @{ $Param{$Use} } ) {
+            next ELEMENT if !$Element || !$Element->{SelectedValues};
+            my $ElementName = $Element->{Element};
+            my $Values      = $Element->{SelectedValues};
+
+            if ( $ElementName eq 'StateIDs' ) {
+                my $StateList = $Self->{ChangeObject}->ChangePossibleStatesGet( UserID => 1 );
+                ID:
+                for my $ID ( @{$Values} ) {
+                    next ID if !$ID;
+
+                    STATE:
+                    for my $State ( @{$StateList} ) {
+                        next STATE if $ID->{Content} ne $State->{Key};
+                        $ID->{Content} = $State->{Value};
+                    }
+                }
+            }
+        }
+    }
+    return \%Param;
+}
+
+sub ImportWrapper {
+    my ( $Self, %Param ) = @_;
+
+    # wrap used spelling to ids
+    for my $Use (qw(UseAsValueSeries UseAsRestriction UseAsXvalue)) {
+        ELEMENT:
+        for my $Element ( @{ $Param{$Use} } ) {
+            next ELEMENT if !$Element || !$Element->{SelectedValues};
+            my $ElementName = $Element->{Element};
+            my $Values      = $Element->{SelectedValues};
+
+            if ( $ElementName eq 'StateIDs' ) {
+                ID:
+                for my $ID ( @{$Values} ) {
+                    next ID if !$ID;
+
+                    my $StateID = $Self->{ChangeObject}->ChangeStateLookup(
+                        ChangeState => $ID->{Content},
+                        Cache       => 1,
+                    );
+                    if ($StateID) {
+                        $ID->{Content} = $StateID;
+                    }
+                    else {
+                        $Self->{LogObject}->Log(
+                            Priority => 'error',
+                            Message  => "Import: Can' find state $ID->{Content}!"
+                        );
+                        $ID = undef;
+                    }
+                }
+            }
+        }
+    }
+    return \%Param;
+}
+
+1;
