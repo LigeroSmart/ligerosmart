1777e0a1b75a11e88bb4d113aff64815df074645
Bernhard Schmalhofer <bernhard.schmalhofer@otrs.com>
2010-01-21 11:57:28 +0000

    Show valid and invalid templates in the overview.

--- Kernel/Modules/AgentITSMTemplateOverview.pm
+++ Kernel/Modules/AgentITSMTemplateOverview.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMTemplateOverview.pm - the template overview module
 # Copyright (C) 2003-2010 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMTemplateOverview.pm,v 1.4 2010-01-21 11:34:14 bes Exp $
+# $Id: AgentITSMTemplateOverview.pm,v 1.5 2010-01-21 11:57:28 bes Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::ITSMChange;
 use Kernel::System::ITSMChange::Template;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.4 $) [1];
+$VERSION = qw($Revision: 1.5 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -119,12 +119,14 @@ sub Run {
             # increase the PrioCounter
             $PrioCounter++;
 
-            # add filter for the current template type
+            # add filter for the current template type,
+            # valid and invalid templates are shown
             $Filters{$TemplateType} = {
                 Name   => "TemplateType::$TemplateType",
                 Prio   => $PrioCounter,
                 Search => {
                     TemplateType => $TemplateType,
+                    Valid        => 0,
                     UserID       => $Self->{UserID},
                 },
             };
@@ -143,10 +145,12 @@ sub Run {
     else {
 
         # add default filter
+        # all valid and invalid templates are shown
         $Filters{All} = {
             Name   => 'All',
             Prio   => 1000,
             Search => {
+                Valid  => 0,
                 UserID => $Self->{UserID},
             },
         };
