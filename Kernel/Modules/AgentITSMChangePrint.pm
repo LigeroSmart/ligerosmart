366888178a49c1fe8a16486844ddf4419fd96067
Bernhard Schmalhofer <bernhard.schmalhofer@otrs.com>
2010-01-22 14:43:33 +0000

    Added dummy PDF-generation to the Print-frontends.

--- Kernel/Modules/AgentITSMChangePrint.pm
+++ Kernel/Modules/AgentITSMChangePrint.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMChangePrint.pm - the OTRS::ITSM::ChangeManagement change print module
 # Copyright (C) 2003-2010 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMChangePrint.pm,v 1.3 2010-01-22 14:22:24 bes Exp $
+# $Id: AgentITSMChangePrint.pm,v 1.4 2010-01-22 14:43:33 bes Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,9 +15,10 @@ use strict;
 use warnings;
 
 use Kernel::System::ITSMChange;
+use Kernel::System::PDF;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -35,6 +36,7 @@ sub new {
 
     # create additional objects
     $Self->{ChangeObject} = Kernel::System::ITSMChange->new(%Param);
+    $Self->{PDFObject}    = Kernel::System::PDF->new(%Param);
 
     # get config of frontend module
     $Self->{Config} = $Self->{ConfigObject}->Get("ITSMChange::Frontend::$Self->{Action}");
@@ -87,25 +89,102 @@ sub Run {
         );
     }
 
-    # output header
-    my $Output = $Self->{LayoutObject}->Header(
-        Title => 'ChangePrint',
-    );
-    $Output .= $Self->{LayoutObject}->NavigationBar();
+    # generate pdf output
+    if ( $Self->{PDFObject} ) {
+        my $PrintedBy = $Self->{LayoutObject}->{LanguageObject}->Get('printed by');
+        my $Time      = $Self->{LayoutObject}->Output( Template => '$Env{"Time"}' );
+        my $Url       = ' ';
+        if ( $ENV{REQUEST_URI} ) {
+            $Url = $Self->{ConfigObject}->Get('HttpType') . '://'
+                . $Self->{ConfigObject}->Get('FQDN')
+                . $ENV{REQUEST_URI};
+        }
+        my %Page;
 
-    # start template output
-    $Output .= $Self->{LayoutObject}->Output(
-        TemplateFile => 'AgentITSMChangePrint',
-        Data         => {
-            %Param,
-            %{$Change},
-        },
-    );
+        # get maximum number of pages
+        $Page{MaxPages} = $Self->{ConfigObject}->Get('PDF::MaxPages');
+        if ( !$Page{MaxPages} || $Page{MaxPages} < 1 || $Page{MaxPages} > 1000 ) {
+            $Page{MaxPages} = 100;
+        }
+        my $HeaderRight  = 'TODO';
+        my $HeadlineLeft = $HeaderRight;
+        my $Title        = $HeaderRight;
+        if ( $Change->{ChangeTitle} ) {
+            $HeadlineLeft = $Change->{ChangeTitle};
+            $Title .= ' / ' . $Change->{ChangeTitle};
+        }
 
-    # add footer
-    $Output .= $Self->{LayoutObject}->Footer();
+        $Page{MarginTop}    = 30;
+        $Page{MarginRight}  = 40;
+        $Page{MarginBottom} = 40;
+        $Page{MarginLeft}   = 40;
+        $Page{HeaderRight}  = $HeaderRight;
+        $Page{HeadlineLeft} = $HeadlineLeft;
+        $Page{HeadlineRight}
+            = $PrintedBy . ' '
+            . $Self->{UserFirstname} . ' '
+            . $Self->{UserLastname} . ' ('
+            . $Self->{UserEmail} . ') '
+            . $Time;
+        $Page{FooterLeft} = $Url;
+        $Page{PageText}   = $Self->{LayoutObject}->{LanguageObject}->Get('Page');
+        $Page{PageCount}  = 1;
 
-    return $Output;
+        # create new pdf document
+        $Self->{PDFObject}->DocumentNew(
+            Title  => $Self->{ConfigObject}->Get('Product') . ': ' . $Title,
+            Encode => $Self->{LayoutObject}->{UserCharset},
+        );
+
+        # create first pdf page
+        $Self->{PDFObject}->PageNew(
+            %Page, FooterRight => $Page{PageText} . ' ' . $Page{PageCount},
+        );
+        $Page{PageCount}++;
+
+        # return the pdf document
+        my $Filename = 'change_' . $Change->{ChangeNumber};
+        my ( $s, $m, $h, $D, $M, $Y ) = $Self->{TimeObject}->SystemTime2Date(
+            SystemTime => $Self->{TimeObject}->SystemTime(),
+        );
+        $M = sprintf( '%02d', $M );
+        $D = sprintf( '%02d', $D );
+        $h = sprintf( '%02d', $h );
+        $m = sprintf( '%02d', $m );
+        my $PDFString = $Self->{PDFObject}->DocumentOutput();
+        return $Self->{LayoutObject}->Attachment(
+            Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
+            ContentType => "application/pdf",
+            Content     => $PDFString,
+            Type        => 'attachment',
+        );
+    }
+
+    # generate html output
+    else {
+
+        # output header
+        my $Output = $Self->{LayoutObject}->Header(
+            Title => 'ChangePrint',
+        );
+        $Output .= $Self->{LayoutObject}->NavigationBar();
+
+        # start template output
+        $Output .= $Self->{LayoutObject}->Output(
+            TemplateFile => 'AgentITSMChangePrint',
+            Data         => {
+                %Param,
+                %{$Change},
+            },
+        );
+
+        # add footer
+        $Output .= $Self->{LayoutObject}->Footer();
+
+        # return output
+        return $Output;
+    }
+
 }
 
 1;
--- Kernel/Modules/AgentITSMWorkOrderPrint.pm
+++ Kernel/Modules/AgentITSMWorkOrderPrint.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMWorkOrderPrint.pm - the OTRS::ITSM::ChangeManagement workorder print module
 # Copyright (C) 2003-2010 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMWorkOrderPrint.pm,v 1.5 2010-01-22 14:22:24 bes Exp $
+# $Id: AgentITSMWorkOrderPrint.pm,v 1.6 2010-01-22 14:43:33 bes Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,9 +15,10 @@ use strict;
 use warnings;
 
 use Kernel::System::ITSMChange::ITSMWorkOrder;
+use Kernel::System::PDF;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.5 $) [1];
+$VERSION = qw($Revision: 1.6 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -35,6 +36,7 @@ sub new {
 
     # create additional objects
     $Self->{WorkOrderObject} = Kernel::System::ITSMChange::ITSMWorkOrder->new(%Param);
+    $Self->{PDFObject}       = Kernel::System::PDF->new(%Param);
 
     # get config of frontend module
     $Self->{Config} = $Self->{ConfigObject}->Get("ITSMWorkOrder::Frontend::$Self->{Action}");
@@ -87,6 +89,79 @@ sub Run {
         );
     }
 
+    # generate pdf output
+    if ( $Self->{PDFObject} ) {
+        my $PrintedBy = $Self->{LayoutObject}->{LanguageObject}->Get('printed by');
+        my $Time      = $Self->{LayoutObject}->Output( Template => '$Env{"Time"}' );
+        my $Url       = ' ';
+        if ( $ENV{REQUEST_URI} ) {
+            $Url = $Self->{ConfigObject}->Get('HttpType') . '://'
+                . $Self->{ConfigObject}->Get('FQDN')
+                . $ENV{REQUEST_URI};
+        }
+        my %Page;
+
+        # get maximum number of pages
+        $Page{MaxPages} = $Self->{ConfigObject}->Get('PDF::MaxPages');
+        if ( !$Page{MaxPages} || $Page{MaxPages} < 1 || $Page{MaxPages} > 1000 ) {
+            $Page{MaxPages} = 100;
+        }
+        my $HeaderRight  = 'TODO';
+        my $HeadlineLeft = $HeaderRight;
+        my $Title        = $HeaderRight;
+        if ( $WorkOrder->{WorkOrderTitle} ) {
+            $HeadlineLeft = $WorkOrder->{WorkOrderTitle};
+            $Title .= ' / ' . $WorkOrder->{WorkOrderTitle};
+        }
+
+        $Page{MarginTop}    = 30;
+        $Page{MarginRight}  = 40;
+        $Page{MarginBottom} = 40;
+        $Page{MarginLeft}   = 40;
+        $Page{HeaderRight}  = $HeaderRight;
+        $Page{HeadlineLeft} = $HeadlineLeft;
+        $Page{HeadlineRight}
+            = $PrintedBy . ' '
+            . $Self->{UserFirstname} . ' '
+            . $Self->{UserLastname} . ' ('
+            . $Self->{UserEmail} . ') '
+            . $Time;
+        $Page{FooterLeft} = $Url;
+        $Page{PageText}   = $Self->{LayoutObject}->{LanguageObject}->Get('Page');
+        $Page{PageCount}  = 1;
+
+        # create new pdf document
+        $Self->{PDFObject}->DocumentNew(
+            Title  => $Self->{ConfigObject}->Get('Product') . ': ' . $Title,
+            Encode => $Self->{LayoutObject}->{UserCharset},
+        );
+
+        # create first pdf page
+        $Self->{PDFObject}->PageNew(
+            %Page, FooterRight => $Page{PageText} . ' ' . $Page{PageCount},
+        );
+        $Page{PageCount}++;
+
+        # return the pdf document
+        my $Filename = 'workorder_' . $WorkOrder->{WorkOrderNumber};
+        my ( $s, $m, $h, $D, $M, $Y ) = $Self->{TimeObject}->SystemTime2Date(
+            SystemTime => $Self->{TimeObject}->SystemTime(),
+        );
+        $M = sprintf( '%02d', $M );
+        $D = sprintf( '%02d', $D );
+        $h = sprintf( '%02d', $h );
+        $m = sprintf( '%02d', $m );
+        my $PDFString = $Self->{PDFObject}->DocumentOutput();
+        return $Self->{LayoutObject}->Attachment(
+            Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
+            ContentType => "application/pdf",
+            Content     => $PDFString,
+            Type        => 'attachment',
+        );
+    }
+
+    # alternatively generate html output
+
     # output header
     my $Output = $Self->{LayoutObject}->Header(
         Title => 'WorkOrderPrint',
@@ -105,6 +180,7 @@ sub Run {
     # add footer
     $Output .= $Self->{LayoutObject}->Footer();
 
+    # return output
     return $Output;
 }
 
