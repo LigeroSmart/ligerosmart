dfd8d48d9cbbdf1208464ee580a3c4ec593543fd
Udo Bretz <udo.bretz@otrs.com>
2010-01-24 14:40:21 +0000

    Fixed bug in getting the allowed services for a customer.

--- Kernel/Output/HTML/ITSMChangeOverviewSmall.pm
+++ Kernel/Output/HTML/ITSMChangeOverviewSmall.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/ITSMChangeOverviewSmall.pm.pm
 # Copyright (C) 2003-2010 OTRS AG, http://otrs.com/
 # --
-# $Id: ITSMChangeOverviewSmall.pm,v 1.9 2010-01-23 19:50:55 ub Exp $
+# $Id: ITSMChangeOverviewSmall.pm,v 1.10 2010-01-24 14:40:21 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::LinkObject;
 use Kernel::System::Service;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.9 $) [1];
+$VERSION = qw($Revision: 1.10 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -91,7 +91,7 @@ sub Run {
 
         # get all services the customer user is allowed to use
         %CustomerUserServices = $Self->{ServiceObject}->CustomerUserServiceMemberList(
-            CustomerUserLogin => 'Test',
+            CustomerUserLogin => $Self->{UserID},
             Result            => 'HASH',
             DefaultServices   => 1,
         );
