cfcf944b92775ac29cd49beab293743806b868e8
Udo Bretz <udo.bretz@otrs.com>
2010-01-27 23:13:37 +0000

    Added functionality to edit and add expressions. Some code improvements.

--- Kernel/Modules/AgentITSMChangeConditionEdit.pm
+++ Kernel/Modules/AgentITSMChangeConditionEdit.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMChangeConditionEdit.pm - the OTRS::ITSM::ChangeManagement condition edit module
 # Copyright (C) 2003-2010 OTRS AG, http://otrs.com/
 # --
-# $Id: AgentITSMChangeConditionEdit.pm,v 1.9 2010-01-27 23:12:11 ub Exp $
+# $Id: AgentITSMChangeConditionEdit.pm,v 1.10 2010-01-27 23:13:37 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::ITSMChange::ITSMCondition;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.9 $) [1];
+$VERSION = qw($Revision: 1.10 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
