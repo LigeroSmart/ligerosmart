5a9fdbc2a0d964efd93f625ceeb46e31651a55c0
Udo Bretz <udo.bretz@otrs.com>
2010-01-27 22:39:01 +0000

    Small POD fix.

--- Kernel/System/ITSMChange/ITSMCondition/Expression.pm
+++ Kernel/System/ITSMChange/ITSMCondition/Expression.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMChange/ITSMCondition/Expression.pm - all condition expression functions
 # Copyright (C) 2003-2010 OTRS AG, http://otrs.com/
 # --
-# $Id: Expression.pm,v 1.23 2010-01-27 22:33:59 ub Exp $
+# $Id: Expression.pm,v 1.24 2010-01-27 22:39:01 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.23 $) [1];
+$VERSION = qw($Revision: 1.24 $) [1];
 
 =head1 NAME
 
@@ -123,7 +123,7 @@ Update a condition expression.
         ObjectID     => 234,        # (optional)
         AttributeID  => 345,        # (optional)
         OperatorID   => 456,        # (optional)
-        Selector     => 1234,      # (optional)
+        Selector     => 1234,       # (optional)
         CompareValue => 'rejected', # (optional)
         UserID       => 1,
     );
@@ -602,6 +602,6 @@ did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 
 =head1 VERSION
 
-$Revision: 1.23 $ $Date: 2010-01-27 22:33:59 $
+$Revision: 1.24 $ $Date: 2010-01-27 22:39:01 $
 
 =cut
