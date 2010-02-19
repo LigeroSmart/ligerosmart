7fed01ed3226324e1b014500225a807a8a4206b2
Udo Bretz <udo.bretz@otrs.com>
2010-02-19 21:59:50 +0000

    Improved code layout.

--- Kernel/System/PostMaster/Filter/SystemMonitoring.pm
+++ Kernel/System/PostMaster/Filter/SystemMonitoring.pm
@@ -2,7 +2,7 @@
 # Kernel/System/PostMaster/Filter/SystemMonitoring.pm - Basic System Monitoring Interface
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: SystemMonitoring.pm,v 1.8 2010-02-15 19:43:54 ub Exp $
+# $Id: SystemMonitoring.pm,v 1.9 2010-02-19 21:59:50 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.8 $) [1];
+$VERSION = qw($Revision: 1.9 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -68,152 +68,153 @@ sub Run {
     }
 
     # check if sender is of interest
-    if (
-        $Param{GetParam}->{From}
-        && $Param{GetParam}->{From} =~ /$Self->{Config}->{FromAddressRegExp}/i
-        )
-    {
+    return 1 if !$Param{GetParam}->{From};
+    return 1 if $Param{GetParam}->{From} !~ /$Self->{Config}->{FromAddressRegExp}/i;
 
-        # Try to get State, Host and Service from email subject
-        my @SubjectLines = split /\n/, $Param{GetParam}->{Subject};
-        for my $Line (@SubjectLines) {
-            for (qw(State Host Service)) {
-                if ( $Line =~ /$Self->{Config}->{ $_ . 'RegExp' }/ ) {
-                    $Self->{$_} = $1;
-                }
+    # Try to get State, Host and Service from email subject
+    my @SubjectLines = split /\n/, $Param{GetParam}->{Subject};
+    for my $Line (@SubjectLines) {
+        for (qw(State Host Service)) {
+            if ( $Line =~ /$Self->{Config}->{ $_ . 'RegExp' }/ ) {
+                $Self->{$_} = $1;
             }
-        }
-
-        # Try to get State, Host and Service from email body
-        my @BodyLines = split /\n/, $Param{GetParam}->{Body};
-        for my $Line (@BodyLines) {
-            for (qw(State Host Service)) {
-                if ( $Line =~ /$Self->{Config}->{ $_ . 'RegExp' }/ ) {
-                    $Self->{$_} = $1;
-                }
-            }
-        }
-
-        # we need State and Host to proceed
-        if ( $Self->{State} && $Self->{Host} ) {
-
-            # Check for Service
-            $Self->{Service} ||= $Self->{Config}->{DefaultService};
-
-            $LogMessage = " - "
-                . "Host: $Self->{Host}, "
-                . "State: $Self->{State}, "
-                . "Service: $Self->{Service}";
-
-            # Is there a ticket for this Host/Service pair?
-            my %Query = (
-                Result    => 'ARRAY',
-                Limit     => 1,
-                UserID    => 1,
-                StateType => 'Open',
-            );
-            for my $Type (qw(Host Service)) {
-                $Query{ 'TicketFreeKey' . $Self->{Config}->{ 'FreeText' . $Type } } = $Type;
-                $Query{ 'TicketFreeText' . $Self->{Config}->{ 'FreeText' . $Type } }
-                    = $Self->{$Type};
-            }
-
-            # search tickets
-            my @TicketIDs = $Self->{TicketObject}->TicketSearch(%Query);
-
-            # get the first and only ticket id
-            my $TicketID = shift @TicketIDs;
-
-            # OK, found ticket to deal with
-            if ($TicketID) {
-
-                # get ticket number
-                my $TicketNumber = $Self->{TicketObject}->TicketNumberLookup(
-                    TicketID => $TicketID,
-                    UserID   => 1,
-                );
-
-                # build subject
-                $Param{GetParam}->{Subject} = $Self->{TicketObject}->TicketSubjectBuild(
-                    TicketNumber => $TicketNumber,
-                    Subject      => $Param{GetParam}->{Subject},
-                );
-
-                # set sender type and article type
-                $Param{GetParam}->{'X-OTRS-FollowUp-SenderType'}  = $Self->{Config}->{SenderType};
-                $Param{GetParam}->{'X-OTRS-FollowUp-ArticleType'} = $Self->{Config}->{ArticleType};
-
-                # Set Article Free Field for State
-                my $ArticleFreeTextNumber = $Self->{Config}->{'FreeTextState'};
-                $Param{GetParam}->{ 'X-OTRS-FollowUp-ArticleKey' . $ArticleFreeTextNumber }
-                    = 'State';
-                $Param{GetParam}->{ 'X-OTRS-FollowUp-ArticleValue' . $ArticleFreeTextNumber }
-                    = $Self->{State};
-
-                if ( $Self->{State} =~ /$Self->{Config}->{CloseTicketRegExp}/ ) {
-
-                    # Close Ticket Condition -> Take Close Action
-                    if ( $Self->{Config}->{CloseActionState} ne 'OLD' ) {
-                        $Param{GetParam}->{'X-OTRS-FollowUp-State'}
-                            = $Self->{Config}->{CloseActionState};
-
-                        my $TimeStamp = $Self->{TimeObject}->SystemTime2TimeStamp(
-                            SystemTime => $Self->{TimeObject}->SystemTime()
-                                + $Self->{Config}->{ClosePendingTime},
-                        );
-                        $Param{GetParam}->{'X-OTRS-State-PendingTime'} = $TimeStamp;
-                    }
-                    $LogMessage = 'Recovered' . $LogMessage;
-                }
-                else {
-
-                    # Attach note to existing ticket
-                    $LogMessage = 'New Notice' . $LogMessage;
-                }
-            }
-            elsif ( $Self->{State} =~ /$Self->{Config}->{NewTicketRegExp}/ ) {
-
-                # Create Ticket Condition -> Create new Ticket and record Host and Service
-                for (qw(Host Service)) {
-
-                    my $TicketFreeTextNumber = $Self->{Config}->{ 'FreeText' . $_ };
-
-                    $Param{GetParam}->{ 'X-OTRS-TicketKey' . $TicketFreeTextNumber } = $_;
-                    $Param{GetParam}->{ 'X-OTRS-TicketValue' . $TicketFreeTextNumber }
-                        = $Self->{$_};
-                }
-
-                # Set Article Free Field for State
-                my $ArticleFreeTextNumber = $Self->{Config}->{'FreeTextState'};
-                $Param{GetParam}->{ 'X-OTRS-ArticleKey' . $ArticleFreeTextNumber } = 'State';
-                $Param{GetParam}->{ 'X-OTRS-ArticleValue' . $ArticleFreeTextNumber }
-                    = $Self->{State};
-
-                # set sender type and article type
-                $Param{GetParam}->{'X-OTRS-SenderType'}  = $Self->{Config}->{SenderType};
-                $Param{GetParam}->{'X-OTRS-ArticleType'} = $Self->{Config}->{ArticleType};
-
-                $LogMessage = 'New Ticket' . $LogMessage;
-            }
-            else {
-
-                # No existing ticket and no open condition -> drop silently
-                $Param{GetParam}->{'X-OTRS-Ignore'} = 'yes';
-                $LogMessage = 'Mail Dropped, no matching ticket found, no open on this state '
-                    . $LogMessage;
-            }
-        }
-        else {
-            $LogMessage
-                = 'SystemMonitoring: Could not find host address and/or state in mail => Ignoring';
-        }
-        if ($LogMessage) {
-            $Self->{LogObject}->Log(
-                Priority => 'notice',
-                Message  => 'SystemMonitoring Mail: ' . $LogMessage,
-            );
         }
     }
+
+    # Try to get State, Host and Service from email body
+    my @BodyLines = split /\n/, $Param{GetParam}->{Body};
+    for my $Line (@BodyLines) {
+        for (qw(State Host Service)) {
+            if ( $Line =~ /$Self->{Config}->{ $_ . 'RegExp' }/ ) {
+                $Self->{$_} = $1;
+            }
+        }
+    }
+
+    # we need State and Host to proceed
+    if ( !$Self->{State} || !$Self->{Host} ) {
+
+        $Self->{LogObject}->Log(
+            Priority => 'notice',
+            Message  => 'SystemMonitoring Mail: '
+                . 'SystemMonitoring: Could not find host address '
+                . 'and/or state in mail => Ignoring',
+        );
+        return 1;
+    }
+
+    # Check for Service
+    $Self->{Service} ||= $Self->{Config}->{DefaultService};
+
+    # define log message
+    $LogMessage = " - "
+        . "Host: $Self->{Host}, "
+        . "State: $Self->{State}, "
+        . "Service: $Self->{Service}";
+
+    # Is there a ticket for this Host/Service pair?
+    my %Query = (
+        Result    => 'ARRAY',
+        Limit     => 1,
+        UserID    => 1,
+        StateType => 'Open',
+    );
+    for my $Type (qw(Host Service)) {
+        $Query{ 'TicketFreeKey' . $Self->{Config}->{ 'FreeText' . $Type } } = $Type;
+        $Query{ 'TicketFreeText' . $Self->{Config}->{ 'FreeText' . $Type } }
+            = $Self->{$Type};
+    }
+
+    # search tickets
+    my @TicketIDs = $Self->{TicketObject}->TicketSearch(%Query);
+
+    # get the first and only ticket id
+    my $TicketID = shift @TicketIDs;
+
+    # OK, found ticket to deal with
+    if ($TicketID) {
+
+        # get ticket number
+        my $TicketNumber = $Self->{TicketObject}->TicketNumberLookup(
+            TicketID => $TicketID,
+            UserID   => 1,
+        );
+
+        # build subject
+        $Param{GetParam}->{Subject} = $Self->{TicketObject}->TicketSubjectBuild(
+            TicketNumber => $TicketNumber,
+            Subject      => $Param{GetParam}->{Subject},
+        );
+
+        # set sender type and article type
+        $Param{GetParam}->{'X-OTRS-FollowUp-SenderType'}  = $Self->{Config}->{SenderType};
+        $Param{GetParam}->{'X-OTRS-FollowUp-ArticleType'} = $Self->{Config}->{ArticleType};
+
+        # Set Article Free Field for State
+        my $ArticleFreeTextNumber = $Self->{Config}->{'FreeTextState'};
+        $Param{GetParam}->{ 'X-OTRS-FollowUp-ArticleKey' . $ArticleFreeTextNumber }
+            = 'State';
+        $Param{GetParam}->{ 'X-OTRS-FollowUp-ArticleValue' . $ArticleFreeTextNumber }
+            = $Self->{State};
+
+        if ( $Self->{State} =~ /$Self->{Config}->{CloseTicketRegExp}/ ) {
+
+            # Close Ticket Condition -> Take Close Action
+            if ( $Self->{Config}->{CloseActionState} ne 'OLD' ) {
+                $Param{GetParam}->{'X-OTRS-FollowUp-State'} = $Self->{Config}->{CloseActionState};
+
+                my $TimeStamp = $Self->{TimeObject}->SystemTime2TimeStamp(
+                    SystemTime => $Self->{TimeObject}->SystemTime()
+                        + $Self->{Config}->{ClosePendingTime},
+                );
+                $Param{GetParam}->{'X-OTRS-State-PendingTime'} = $TimeStamp;
+            }
+            $LogMessage = 'Recovered' . $LogMessage;
+        }
+        else {
+
+            # Attach note to existing ticket
+            $LogMessage = 'New Notice' . $LogMessage;
+        }
+    }
+    elsif ( $Self->{State} =~ /$Self->{Config}->{NewTicketRegExp}/ ) {
+
+        # Create Ticket Condition -> Create new Ticket and record Host and Service
+        for (qw(Host Service)) {
+
+            my $TicketFreeTextNumber = $Self->{Config}->{ 'FreeText' . $_ };
+
+            $Param{GetParam}->{ 'X-OTRS-TicketKey' . $TicketFreeTextNumber }   = $_;
+            $Param{GetParam}->{ 'X-OTRS-TicketValue' . $TicketFreeTextNumber } = $Self->{$_};
+        }
+
+        # Set Article Free Field for State
+        my $ArticleFreeTextNumber = $Self->{Config}->{'FreeTextState'};
+        $Param{GetParam}->{ 'X-OTRS-ArticleKey' . $ArticleFreeTextNumber }   = 'State';
+        $Param{GetParam}->{ 'X-OTRS-ArticleValue' . $ArticleFreeTextNumber } = $Self->{State};
+
+        # set sender type and article type
+        $Param{GetParam}->{'X-OTRS-SenderType'}  = $Self->{Config}->{SenderType};
+        $Param{GetParam}->{'X-OTRS-ArticleType'} = $Self->{Config}->{ArticleType};
+
+        $LogMessage = 'New Ticket' . $LogMessage;
+    }
+    else {
+
+        # No existing ticket and no open condition -> drop silently
+        $Param{GetParam}->{'X-OTRS-Ignore'} = 'yes';
+        $LogMessage = 'Mail Dropped, no matching ticket found,'
+            . ' no open on this state ' . $LogMessage;
+    }
+
+    # logging
+    if ($LogMessage) {
+        $Self->{LogObject}->Log(
+            Priority => 'notice',
+            Message  => 'SystemMonitoring Mail: ' . $LogMessage,
+        );
+    }
+
     return 1;
 }
 
