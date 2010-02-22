f321902448684ad64426ba10299d8ac9a04ef24b
Michiel Beijen <michiel.beijen@otrs.com>
2010-02-22 12:21:15 +0000

    Updated Czech translation, by O2BS.com, s r.o. Jakub Hanus.

--- Kernel/Language/cz_ITSMTicket.pm
+++ Kernel/Language/cz_ITSMTicket.pm
@@ -1,9 +1,10 @@
 # --
 # Kernel/Language/cz_ITSMTicket.pm - the czech translation of ITSMTicket
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # Copyright (C) 2007-2008 Milen Koutev
+# Copyright (C) 2010 O2BS.com, s r.o. Jakub Hanus
 # --
-# $Id: cz_ITSMTicket.pm,v 1.6 2009-07-20 12:27:08 ub Exp $
+# $Id: cz_ITSMTicket.pm,v 1.7 2010-02-22 12:21:15 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -16,7 +17,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.6 $) [1];
+$VERSION = qw($Revision: 1.7 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -25,20 +26,20 @@ sub Data {
 
     return if ref $Lang ne 'HASH';
 
-    $Lang->{'Due Date'}                     = 'Koneèní datum';
+    $Lang->{'Due Date'}                     = 'Nejzaz¹í Termín';
     $Lang->{'Decision'}                     = 'Øe¹ení';
-    $Lang->{'Reason'}                       = 'Dùvod';
-    $Lang->{'Decision Date'}                = 'Datum rozhodnutí';
-    $Lang->{'Add decision to ticket'}       = 'Dodat øe¹ení k tiketu';
-    $Lang->{'Decision Result'}              = 'Výsledek rozhodnutí';
-    $Lang->{'Review Required'}              = 'Vy¾aduje pøehled';
-    $Lang->{'closed with workaround'}       = 'Uzavøen s obchodním rozhodnutím';
-    $Lang->{'Additional ITSM Fields'}       = 'Dodateèné ITSM pole';
-    $Lang->{'Change ITSM fields of ticket'} = 'Zmìnit ITSM polí tiketu';
+    $Lang->{'Reason'}                       = 'Pøíèina';
+    $Lang->{'Decision Date'}                = 'Datum Øe¹ení';
+    $Lang->{'Add decision to ticket'}       = 'Pøidat øe¹ení k tiketu';
+    $Lang->{'Decision Result'}              = 'Výsledek Øe¹ení';
+    $Lang->{'Review Required'}              = 'Vy¾aduje Pøehled';
+    $Lang->{'closed with workaround'}       = 'uzavøeno doèasným øe¹ením';
+    $Lang->{'Additional ITSM Fields'}       = 'Doplòková ITSM pole';
+    $Lang->{'Change ITSM fields of ticket'} = 'Zmìna ITSM polí v tiketu';
     $Lang->{'Repair Start Time'}            = 'Èas zahájení opravy';
     $Lang->{'Recovery Start Time'}          = 'Èas zahájení obnovení';
-    $Lang->{'Change the ITSM fields!'}      = '';
-    $Lang->{'Add a decision!'}              = '';
+    $Lang->{'Change the ITSM fields!'}      = 'Zmìòte ITMS pole!';
+    $Lang->{'Add a decision!'}              = 'Doplòte øe¹ení!';
 
     return 1;
 }
