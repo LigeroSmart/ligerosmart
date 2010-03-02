b7b9f7af066a01a3b3a2df7d2780fc067f273294
Michiel Beijen <michiel.beijen@otrs.com>
2010-03-02 07:51:46 +0000

    Fixed charset issues

--- Kernel/Language/nl_AgentSurvey.pm
+++ Kernel/Language/nl_AgentSurvey.pm
@@ -1,8 +1,8 @@
 ï»¿# --
 # Kernel/Language/nl_AgentSurvey.pm - the nl language for AgentSurvey
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: nl_AgentSurvey.pm,v 1.2 2009-10-30 08:28:52 mb Exp $
+# $Id: nl_AgentSurvey.pm,v 1.3 2010-03-02 07:51:46 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,23 +15,23 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 sub Data {
     my $Self = shift;
 
-    $Self->{Translation}->{'Survey'} = 'EnquÃªte';
+    $Self->{Translation}->{'Survey'} = 'Enquête';
     $Self->{Translation}->{'Can\'t set new status! No questions defined.'}
-        = 'Kan status niet aanpassen! Definieer eerst vragen voor deze enquÃªte.';
+        = 'Kan status niet aanpassen! Definieer eerst vragen voor deze enquête.';
     $Self->{Translation}->{'Can\'t set new status! Questions incomplete.'}
-        = 'Kan status niet aanpassen! Er zijn incomplete vragen in deze enquÃªte.';
+        = 'Kan status niet aanpassen! Er zijn incomplete vragen in deze enquête.';
     $Self->{Translation}->{'Status changed.'} = 'Status aangepast';
     $Self->{Translation}->{'Change Status'}     = 'Status aanpassen';
-    $Self->{Translation}->{'Sent requests'}   = 'Verzonden enquÃªtes';
+    $Self->{Translation}->{'Sent requests'}   = 'Verzonden enquêtes';
     $Self->{Translation}->{'Received surveys'}    = 'Ontvangen stemmen';
     $Self->{Translation}->{'answered'}          = 'beantwoord';
     $Self->{Translation}->{'not answered'}      = 'niet beantwoord';
-    $Self->{Translation}->{'Surveys'}           = 'EnquÃªtes';
+    $Self->{Translation}->{'Surveys'}           = 'Enquêtes';
     $Self->{Translation}->{'Invalid'}           = 'Ongeldig';
     $Self->{Translation}->{'Introduction'}      = 'Introductie';
     $Self->{Translation}->{'Internal'}          = 'Interne';
@@ -41,13 +41,13 @@ sub Data {
     $Self->{Translation}->{'YesNo'}             = 'ja/nee';
     $Self->{Translation}->{'List'}              = 'Overzicht';
     $Self->{Translation}->{'Textarea'}          = 'Vrije tekst';
-    $Self->{Translation}->{'A Survey Module'}   = 'Een enquÃªte-module';
+    $Self->{Translation}->{'A Survey Module'}   = 'Een enquête-module';
     $Self->{Translation}->{'Survey Title is required!'}
         = 'Geef een titel op voor deze enquÃªte.';
     $Self->{Translation}->{'Survey Introduction is required!'}
-        = 'Maak een introductietekst voor deze enquÃªte.';
+        = 'Maak een introductietekst voor deze enquête.';
     $Self->{Translation}->{'Survey Description is required!'}
-        = 'Geef een omschrijving op voor deze enquÃªte!';
+        = 'Geef een omschrijving op voor deze enquête!';
     $Self->{Translation}->{'Survey NotificationSender is required!'}
         = 'Vul een adres in voor de afzender';
     $Self->{Translation}->{'Survey NotificationSubject is required!'}
