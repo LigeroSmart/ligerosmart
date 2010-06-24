7fa24f5fe51d4cd07cf2ab223fe6d308bbdf8bb4
Udo Bretz <udo.bretz@otrs.com>
2010-06-24 16:41:18 +0000

    Small code cleanup. No need to test again if change data exists here.

--- Kernel/System/ITSMChange.pm
+++ Kernel/System/ITSMChange.pm
@@ -2,7 +2,7 @@
 # Kernel/System/ITSMChange.pm - all change functions
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: ITSMChange.pm,v 1.247 2010-06-22 00:18:42 ub Exp $
+# $Id: ITSMChange.pm,v 1.248 2010-06-24 16:41:18 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -30,7 +30,7 @@ use Kernel::System::Cache;
 use base qw(Kernel::System::EventHandler);
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.247 $) [1];
+$VERSION = qw($Revision: 1.248 $) [1];
 
 =head1 NAME
 
@@ -678,17 +678,13 @@ sub ChangeGet {
         # add result to change data
         %ChangeData = ( %ChangeData, %{$ChangeFreeText} );
 
-        # set cache only if change data exists
-        if (%ChangeData) {
-
-            # set cache
-            $Self->{CacheObject}->Set(
-                Type  => 'ITSMChangeManagement',
-                Key   => $CacheKey,
-                Value => \%ChangeData,
-                TTL   => $Self->{CacheTTL},
-            );
-        }
+        # set cache (change data exists at this point, it was checked before)
+        $Self->{CacheObject}->Set(
+            Type  => 'ITSMChangeManagement',
+            Key   => $CacheKey,
+            Value => \%ChangeData,
+            TTL   => $Self->{CacheTTL},
+        );
     }
 
     # set name of change state
@@ -3670,6 +3666,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.247 $ $Date: 2010-06-22 00:18:42 $
+$Revision: 1.248 $ $Date: 2010-06-24 16:41:18 $
 
 =cut
