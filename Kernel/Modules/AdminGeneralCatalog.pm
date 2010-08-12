60b20480b60045658e13795ee1814d231a7f5b33
Carlos Garcia <carlos.garcia@otrs.com>
2010-08-12 22:58:36 +0000

    Modified server side validation.

--- Kernel/Modules/AdminGeneralCatalog.pm
+++ Kernel/Modules/AdminGeneralCatalog.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AdminGeneralCatalog.pm - admin frontend of general catalog management
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminGeneralCatalog.pm,v 1.24 2010-08-12 18:59:16 cg Exp $
+# $Id: AdminGeneralCatalog.pm,v 1.25 2010-08-12 22:58:36 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::GeneralCatalog;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.24 $) [1];
+$VERSION = qw($Revision: 1.25 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -334,7 +334,7 @@ sub Run {
 
         # check class
         return $Self->{LayoutObject}->Redirect( OP => "Action=$Self->{Action}" )
-            if !$ItemData{Class} || $ItemData{Class} eq 'NEW';
+            if $ItemData{Class} eq 'NEW';
 
         # save to database
         my $Success;
