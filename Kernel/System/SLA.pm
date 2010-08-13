4b1191a77f0887a40b84002f009fa802085eaff0
Daniel Zamorano <daniel.zamorano@otrs.com>
2010-08-13 16:04:11 +0000

    Ported to otrs3

--- Kernel/System/SLA.pm
+++ Kernel/System/SLA.pm
@@ -1,9 +1,9 @@
 # --
 # Kernel/System/SLA.pm - all sla function
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: SLA.pm,v 1.5 2009-07-21 00:12:08 ub Exp $
-# $OldId: SLA.pm,v 1.36 2009/07/21 00:09:04 ub Exp $
+# $Id: SLA.pm,v 1.6 2010-08-13 16:04:11 dz Exp $
+# $OldId: SLA.pm,v 1.37 2010/06/17 21:39:40 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,9 +22,8 @@ use Kernel::System::Valid;
 # ---
 use Kernel::System::GeneralCatalog;
 # ---
-
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.5 $) [1];
+$VERSION = qw($Revision: 1.6 $) [1];
 
 =head1 NAME
 
@@ -93,6 +92,7 @@ sub new {
     }
     $Self->{CheckItemObject} = Kernel::System::CheckItem->new( %{$Self} );
     $Self->{ValidObject}     = Kernel::System::Valid->new( %{$Self} );
+
 # ---
 # ITSM
 # ---
@@ -279,6 +279,7 @@ sub SLAGet {
         );
         return;
     }
+
 # ---
 # ITSM
 # ---
@@ -470,7 +471,6 @@ sub SLAAdd {
 # ---
     $Param{MinTimeBetweenIncidents} ||= 0;
 # ---
-
     # cleanup given params
     for my $Argument (qw(Name Comment)) {
         $Self->{CheckItemObject}->StringClean(
@@ -645,7 +645,6 @@ sub SLAUpdate {
 # ---
     $Param{MinTimeBetweenIncidents} ||= 0;
 # ---
-
     # cleanup given params
     for my $Argument (qw(Name Comment)) {
         $Self->{CheckItemObject}->StringClean(
@@ -776,16 +775,16 @@ sub SLAPreferencesGet {
 
 =head1 TERMS AND CONDITIONS
 
-This software is part of the OTRS project (http://otrs.org/).
+This software is part of the OTRS project (L<http://otrs.org/>).
 
 This software comes with ABSOLUTELY NO WARRANTY. For details, see
 the enclosed file COPYING for license information (AGPL). If you
-did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =cut
 
 =head1 VERSION
 
-$Revision: 1.5 $ $Date: 2009-07-21 00:12:08 $
+$Revision: 1.6 $ $Date: 2010-08-13 16:04:11 $
 
 =cut
--- scripts/test/SLA.t
+++ scripts/test/SLA.t
@@ -1,9 +1,9 @@
 # --
 # SLA.t - SLA tests
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: SLA.t,v 1.3 2009-06-30 14:55:23 ub Exp $
-# $OldId: SLA.t,v 1.10 2009/02/16 12:41:12 tr Exp $
+# $Id: SLA.t,v 1.4 2010-08-13 16:04:11 dz Exp $
+# $OldId: SLA.t,v 1.11 2010/06/22 22:00:52 dz Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -12,6 +12,7 @@
 
 use strict;
 use warnings;
+use vars (qw($Self));
 use utf8;
 
 use vars qw($Self);
