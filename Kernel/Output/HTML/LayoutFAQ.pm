8c0143ef8954fcd8ce17bf3b147d8ad1879fe0de
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-11-02 13:09:57 +0000

    Added FAQListShow()

--- Kernel/Output/HTML/LayoutFAQ.pm
+++ Kernel/Output/HTML/LayoutFAQ.pm
@@ -2,7 +2,7 @@
 # Kernel/Output/HTML/LayoutFAQ.pm - provides generic agent HTML output
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: LayoutFAQ.pm,v 1.9 2010-10-28 20:57:15 cr Exp $
+# $Id: LayoutFAQ.pm,v 1.10 2010-11-02 13:09:57 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.9 $) [1];
+$VERSION = qw($Revision: 1.10 $) [1];
 
 sub AgentFAQCategoryListOption {
 
@@ -158,6 +158,305 @@ sub GetFAQItemVotingRateColor {
         }
     }
     return $CssTmp;
+}
+
+=item FAQListShow()
+
+Returns a list of FAQ items as sortable list with pagination.
+
+This function is similar to L<Kernel::Output::HTML::LayoutTicket::TicketListShow()>
+in F<Kernel/Output/HTML/LayoutTicket.pm>.
+
+    my $Output = $LayoutObject->ITSMChangeListShow(
+        FAQIDs  => $FAQIDsRef,                            # total list of FAQ ids, that can be listed
+        Total      => scalar @{ $ChangeIDsRef },          # total number of list items, in this case
+        View       => $Self->{View},                      # optional, the default value is 'Small'
+        Filter     => 'All',
+        Filters    => \%NavBarFilter,
+        FilterLink => $LinkFilter,
+        TitleName  => 'Overview: FAQ',
+        TitleValue => $Self->{Filter},
+        Env        => $Self,
+        LinkPage   => $LinkPage,
+        LinkSort   => $LinkSort,
+        Frontend   => 'Agent',                           # optional (Agent|Customer|Public), default: Agent, indicates from which frontend this function was called
+    );
+
+=cut
+
+sub FAQListShow {
+    my ( $Self, %Param ) = @_;
+
+    # take object ref to local, remove it from %Param (prevent memory leak)
+    my $Env = delete $Param{Env};
+
+    # lookup latest used view mode
+    if ( !$Param{View} && $Self->{ 'UserFAQOverview' . $Env->{Action} } ) {
+        $Param{View} = $Self->{ 'UserFAQOverview' . $Env->{Action} };
+    }
+
+    # set frontend
+    my $Frontend = $Param{Frontend} || 'Agent';
+
+    # set defaut view mode to 'small'
+    my $View = $Param{View} || 'Small';
+
+    # store latest view mode
+    $Self->{SessionObject}->UpdateSessionID(
+        SessionID => $Self->{SessionID},
+        Key       => 'UserFAQOverview' . $Env->{Action},
+        Value     => $View,
+    );
+
+    # get backend from config
+    my $Backends = $Self->{ConfigObject}->Get('FAQ::Frontend::Overview');
+    if ( !$Backends ) {
+        return $Env->{LayoutObject}->FatalError(
+            Message => 'Need config option FAQ::Frontend::Overview',
+        );
+    }
+
+    # check for hash-ref
+    if ( ref $Backends ne 'HASH' ) {
+        return $Env->{LayoutObject}->FatalError(
+            Message => 'Config option FAQ::Frontend::Overview needs to be a HASH ref!',
+        );
+    }
+
+    # check for config key
+    if ( !$Backends->{$View} ) {
+        return $Env->{LayoutObject}->FatalError(
+            Message => "No config option found for the view '$View'!",
+        );
+    }
+
+    # nav bar
+    my $StartHit = $Self->{ParamObject}->GetParam(
+        Param => 'StartHit',
+    ) || 1;
+
+    # get personal page shown count
+    my $PageShownPreferencesKey = 'UserFAQOverview' . $View . 'PageShown';
+    my $PageShown               = $Self->{$PageShownPreferencesKey} || 10;
+    my $Group                   = 'FAQOverview' . $View . 'PageShown';
+
+    # check start option, if higher then elements available, set
+    # it to the last overview page (Thanks to Stefan Schmidt!)
+    if ( $StartHit > $Param{Total} ) {
+        my $Pages = int( ( $Param{Total} / $PageShown ) + 0.99999 );
+        $StartHit = ( ( $Pages - 1 ) * $PageShown ) + 1;
+    }
+
+    # get data selection
+    my %Data;
+    my $Config = $Self->{ConfigObject}->Get('PreferencesGroups');
+    if ( $Config && $Config->{$Group} && $Config->{$Group}->{Data} ) {
+        %Data = %{ $Config->{$Group}->{Data} };
+    }
+
+    # set page limit and build page nav
+    my $Limit = $Param{Limit} || 20_000;
+    my %PageNav = $Env->{LayoutObject}->PageNavBar(
+        Limit     => $Limit,
+        StartHit  => $StartHit,
+        PageShown => $PageShown,
+        AllHits   => $Param{Total} || 0,
+        Action    => 'Action=' . $Env->{LayoutObject}->{Action},
+        Link      => $Param{LinkPage},
+    );
+
+    # build shown ticket a page
+    $Param{RequestedURL}    = "Action=$Self->{Action}";
+    $Param{Group}           = $Group;
+    $Param{PreferencesKey}  = $PageShownPreferencesKey;
+    $Param{PageShownString} = $Self->BuildSelection(
+        Name       => $PageShownPreferencesKey,
+        SelectedID => $PageShown,
+        Data       => \%Data,
+    );
+
+    # build navbar content
+    $Env->{LayoutObject}->Block(
+        Name => 'OverviewNavBar',
+        Data => \%Param,
+    );
+
+    # back link
+    if ( $Param{LinkBack} ) {
+        $Env->{LayoutObject}->Block(
+            Name => 'OverviewNavBarPageBack',
+            Data => \%Param,
+        );
+    }
+
+    # get filters
+    if ( $Param{Filters} ) {
+
+        # get given filters
+        my @NavBarFilters;
+        for my $Prio ( sort keys %{ $Param{Filters} } ) {
+            push @NavBarFilters, $Param{Filters}->{$Prio};
+        }
+
+        # build filter content
+        $Env->{LayoutObject}->Block(
+            Name => 'OverviewNavBarFilter',
+            Data => {
+                %Param,
+            },
+        );
+
+        # loop over filters
+        my $Count = 0;
+        for my $Filter (@NavBarFilters) {
+
+            # increment filter count and build filter item
+            $Count++;
+            $Env->{LayoutObject}->Block(
+                Name => 'OverviewNavBarFilterItem',
+                Data => {
+                    %Param,
+                    %{$Filter},
+                },
+            );
+
+            # filter is selected
+            if ( $Filter->{Filter} eq $Param{Filter} ) {
+                $Env->{LayoutObject}->Block(
+                    Name => 'OverviewNavBarFilterItemSelected',
+                    Data => {
+                        %Param,
+                        %{$Filter},
+                    },
+                );
+
+            }
+            else {
+                $Env->{LayoutObject}->Block(
+                    Name => 'OverviewNavBarFilterItemSelectedNot',
+                    Data => {
+                        %Param,
+                        %{$Filter},
+                    },
+                );
+
+            }
+        }
+    }
+
+    # loop over configured backends
+    for my $Backend ( keys %{$Backends} ) {
+
+        # build navbar view mode
+        $Env->{LayoutObject}->Block(
+            Name => 'OverviewNavBarViewMode',
+            Data => {
+                %Param,
+                %{ $Backends->{$Backend} },
+                Filter => $Param{Filter},
+                View   => $Backend,
+            },
+        );
+
+        # current view is configured in backend
+        if ( $View eq $Backend ) {
+            $Env->{LayoutObject}->Block(
+                Name => 'OverviewNavBarViewModeSelected',
+                Data => {
+                    %Param,
+                    %{ $Backends->{$Backend} },
+                    Filter => $Param{Filter},
+                    View   => $Backend,
+                },
+            );
+        }
+        else {
+            $Env->{LayoutObject}->Block(
+                Name => 'OverviewNavBarViewModeNotSelected',
+                Data => {
+                    %Param,
+                    %{ $Backends->{$Backend} },
+                    Filter => $Param{Filter},
+                    View   => $Backend,
+                },
+            );
+        }
+    }
+
+    # check if page nav is available
+    if (%PageNav) {
+        $Env->{LayoutObject}->Block(
+            Name => 'OverviewNavBarPageNavBar',
+            Data => \%PageNav,
+        );
+
+        # don't show context settings in AJAX case (e. g. in customer ticket history),
+        #   because the submit with page reload will not work there
+        if ( !$Param{AJAX} ) {
+            $Env->{LayoutObject}->Block(
+                Name => 'ContextSettings',
+                Data => {
+                    %PageNav,
+                    %Param,
+                },
+            );
+        }
+    }
+
+    # build html content
+    my $OutputNavBar = $Env->{LayoutObject}->Output(
+        TemplateFile => 'AgentFAQOverviewNavBar',
+        Data         => {%Param},
+    );
+
+    # create output
+    my $OutputRaw = '';
+    if ( !$Param{Output} ) {
+        $Env->{LayoutObject}->Print(
+            Output => \$OutputNavBar,
+        );
+    }
+    else {
+        $OutputRaw .= $OutputNavBar;
+    }
+
+    # load module
+    if ( !$Self->{MainObject}->Require( $Backends->{$View}->{Module} ) ) {
+        return $Env->{LayoutObject}->FatalError();
+    }
+
+    # check for backend object
+    my $Object = $Backends->{$View}->{Module}->new( %{$Env} );
+    return if !$Object;
+
+    # run module
+    my $Output = $Object->Run(
+        %Param,
+        Limit     => $Limit,
+        StartHit  => $StartHit,
+        PageShown => $PageShown,
+        AllHits   => $Param{Total} || 0,
+        Frontend  => $Frontend,
+    );
+
+    # create output
+    if ( !$Param{Output} ) {
+        $Env->{LayoutObject}->Print(
+            Output => \$Output,
+        );
+    }
+    else {
+        $OutputRaw .= $Output;
+    }
+
+    # create overview nav bar
+    $Env->{LayoutObject}->Block(
+        Name => 'OverviewNavBar',
+        Data => {%Param},
+    );
+
+    # return content if available
+    return $OutputRaw;
 }
 
 1;
