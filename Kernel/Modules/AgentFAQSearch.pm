811ffe76e77caef9eeff03b33665f52df252edd7
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-11-03 20:23:37 +0000

    Fixed names in Browser search engines (MetaSearch)

--- Kernel/Modules/AgentFAQSearch.pm
+++ Kernel/Modules/AgentFAQSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQSearch.pm - module for FAQ search
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQSearch.pm,v 1.8 2010-11-03 19:31:03 ub Exp $
+# $Id: AgentFAQSearch.pm,v 1.9 2010-11-03 20:23:37 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::SearchProfile;
 use Kernel::System::CSV;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.8 $) [1];
+$VERSION = qw($Revision: 1.9 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -82,11 +82,11 @@ sub Run {
     # check request
     if ( $Self->{Subaction} eq 'OpenSearchDescriptionFAQNumber' ) {
         my $Output = $Self->{LayoutObject}->Output(
-            TemplateFile => 'AgentFAQSearchOpenSearchDescription',
+            TemplateFile => 'AgentFAQSearchOpenSearchDescriptionFAQNumber',
             Data         => \%Param,
         );
         return $Self->{LayoutObject}->Attachment(
-            Filename    => 'FAQSearchDescription.xml',
+            Filename    => 'OpenSearchDescriptionFAQNumber.xml',
             ContentType => 'text/xml',
             Content     => $Output,
             Type        => 'inline',
@@ -220,7 +220,7 @@ sub Run {
             ContentSearchPrefix => '*',
             ContentSearchSuffix => '*',
             %GetParam,
-            UserID => $Self->{UserID},
+            UserID              => $Self->{UserID},
         );
 
         # CSV output
