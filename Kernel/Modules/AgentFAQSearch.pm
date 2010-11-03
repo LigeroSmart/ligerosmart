b4af072f30953c750ee6a7a2be006494b4808444
Udo Bretz <udo.bretz@otrs.com>
2010-11-03 18:42:53 +0000

    Small whitespace change.

--- Kernel/Modules/AgentFAQSearch.pm
+++ Kernel/Modules/AgentFAQSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQSearch.pm - module for FAQ search
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQSearch.pm,v 1.6 2010-11-03 18:23:00 ub Exp $
+# $Id: AgentFAQSearch.pm,v 1.7 2010-11-03 18:42:53 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::SearchProfile;
 use Kernel::System::CSV;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.6 $) [1];
+$VERSION = qw($Revision: 1.7 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -211,8 +211,8 @@ sub Run {
 
         # perform FAQ search
         my @ViewableFAQIDs = $Self->{FAQObject}->FAQSearch(
-            OrderBy          => [ $Self->{SortBy} ],
-            OrderByDirection => [ $Self->{OrderBy} ],
+            OrderBy             => [ $Self->{SortBy} ],
+            OrderByDirection    => [ $Self->{OrderBy} ],
             Limit               => $Self->{SearchLimit},
             UserID              => $Self->{UserID},
             States              => $Self->{InterfaceStates  },
