3311d24cccf80ab09d64fddb901ba2299f267a0c
Udo Bretz <udo.bretz@otrs.com>
2010-11-08 18:54:57 +0000

    No need to include encode object here.

--- Kernel/System/FAQ.pm
+++ Kernel/System/FAQ.pm
@@ -2,7 +2,7 @@
 # Kernel/System/FAQ.pm - all faq funktions
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: FAQ.pm,v 1.109 2010-11-08 18:52:17 ub Exp $
+# $Id: FAQ.pm,v 1.110 2010-11-08 18:54:57 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,6 @@ use strict;
 use warnings;
 
 use MIME::Base64;
-use Kernel::System::Encode;
 use Kernel::System::Cache;
 use Kernel::System::User;
 use Kernel::System::Group;
@@ -25,7 +24,7 @@ use Kernel::System::Ticket;
 use Kernel::System::Web::UploadCache;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.109 $) [1];
+$VERSION = qw($Revision: 1.110 $) [1];
 
 =head1 NAME
 
@@ -3891,6 +3890,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.109 $ $Date: 2010-11-08 18:52:17 $
+$Revision: 1.110 $ $Date: 2010-11-08 18:54:57 $
 
 =cut
