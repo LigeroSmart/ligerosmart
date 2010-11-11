7c53233a6aede4d617a6b4f7d12de7d9484a27bd
Udo Bretz <udo.bretz@otrs.com>
2010-11-11 18:21:18 +0000

    Small whitespace change.

--- Kernel/Modules/AgentFAQAdd.pm
+++ Kernel/Modules/AgentFAQAdd.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQAdd.pm - agent frontend to add faq articles
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQAdd.pm,v 1.2 2010-11-11 15:29:12 ub Exp $
+# $Id: AgentFAQAdd.pm,v 1.3 2010-11-11 18:21:18 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::Web::UploadCache;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
