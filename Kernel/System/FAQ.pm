df08c538efcf20ecd4d477baf3a86eb456394bf1
Udo Bretz <udo.bretz@otrs.com>
2010-11-12 18:49:52 +0000

    Improved code style.

--- Kernel/System/FAQ.pm
+++ Kernel/System/FAQ.pm
@@ -2,7 +2,7 @@
 # Kernel/System/FAQ.pm - all faq funktions
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: FAQ.pm,v 1.116 2010-11-12 18:39:24 ub Exp $
+# $Id: FAQ.pm,v 1.117 2010-11-12 18:49:52 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -24,7 +24,7 @@ use Kernel::System::Ticket;
 use Kernel::System::Web::UploadCache;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.116 $) [1];
+$VERSION = qw($Revision: 1.117 $) [1];
 
 =head1 NAME
 
@@ -581,8 +581,16 @@ sub AttachmentAdd {
     }
 
     # try to modify the the file name by adding a number if it exists already
-    for ( my $Count = 1; $Count <= 50; $Count++ ) {
+    my $Count;
+    while ( $Count < 50 ) {
+
+        # increase counter
+        $Count++;
+
+        # if the file name exists
         if ( exists $UsedFile{$NewFileName} ) {
+
+            # filename has a file name extension (e.g. test.jpg)
             if ( $Param{Filename} =~ m{ \A (.*) \. (.+?) \z }xms ) {
                 $NewFileName = "$1-$Count.$2";
             }
@@ -3980,6 +3988,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.116 $ $Date: 2010-11-12 18:39:24 $
+$Revision: 1.117 $ $Date: 2010-11-12 18:49:52 $
 
 =cut
