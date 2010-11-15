dcecbdde03dab6324952323c81564c477fef9a58
Udo Bretz <udo.bretz@otrs.com>
2010-11-15 22:30:50 +0000

    Added comma.

--- Kernel/Modules/AgentFAQSearch.pm
+++ Kernel/Modules/AgentFAQSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQSearch.pm - module for FAQ search
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQSearch.pm,v 1.14 2010-11-12 22:40:43 cr Exp $
+# $Id: AgentFAQSearch.pm,v 1.15 2010-11-15 22:30:50 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::SearchProfile;
 use Kernel::System::CSV;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.14 $) [1];
+$VERSION = qw($Revision: 1.15 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -573,7 +573,7 @@ sub Run {
             NoCache     => 1,
             ContentType => 'text/html',
             Content     => $Output,
-            Type        => 'inline'
+            Type        => 'inline',
         );
     }
 
@@ -589,7 +589,7 @@ sub Run {
             ContentType => 'text/html',
             Charset     => $Self->{LayoutObject}->{UserCharset},
             Content     => $Output,
-            Type        => 'inline'
+            Type        => 'inline',
         );
     }
 
