598f0a345fe7b51b5d8a304cd09b37b0656381bb
Udo Bretz <udo.bretz@otrs.com>
2010-11-16 00:07:34 +0000

    Improved no category permission error message.

--- Kernel/Modules/AgentFAQZoom.pm
+++ Kernel/Modules/AgentFAQZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQZoom.pm - to get a closer view
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQZoom.pm,v 1.11 2010-11-15 23:33:36 ub Exp $
+# $Id: AgentFAQZoom.pm,v 1.12 2010-11-16 00:07:34 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::FAQ;
 use Kernel::System::User;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.11 $) [1];
+$VERSION = qw($Revision: 1.12 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -99,7 +99,10 @@ sub Run {
     );
 
     if ( !$Permission ) {
-        $Self->{LayoutObject}->FatalError( Message => "Permission denied!" );
+        return $Self->{LayoutObject}->NoPermission(
+            Message    => 'You have no permission for this category!',
+            WithHeader => 'yes',
+        );
     }
 
     # ---------------------------------------------------------- #
