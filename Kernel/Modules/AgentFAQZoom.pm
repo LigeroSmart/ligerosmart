e195cc34141f1fb73b9194f02041e01e428a8e6f
Udo Bretz <udo.bretz@otrs.com>
2010-11-16 15:13:01 +0000

    Replaced FAQID with ItemID.

--- Kernel/Modules/AgentFAQZoom.pm
+++ Kernel/Modules/AgentFAQZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQZoom.pm - to get a closer view
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQZoom.pm,v 1.12 2010-11-16 00:07:34 ub Exp $
+# $Id: AgentFAQZoom.pm,v 1.13 2010-11-16 15:13:01 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::FAQ;
 use Kernel::System::User;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.12 $) [1];
+$VERSION = qw($Revision: 1.13 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -286,7 +286,7 @@ sub Run {
             if ( $Self->{MainObject}->Require( $Menus{$Menu}->{Module} ) ) {
                 my $Object = $Menus{$Menu}->{Module}->new(
                     %{$Self},
-                    FAQID => $FAQData{ItemID},
+                    ItemID => $FAQData{ItemID},
                 );
 
                 # set classes
