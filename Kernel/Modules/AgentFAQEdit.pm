75ac89cc2c0a3d3b896e4c096e97ac58298b5a48
Udo Bretz <udo.bretz@otrs.com>
2010-11-17 16:01:11 +0000

    Removed a space.

--- Kernel/Modules/AgentFAQEdit.pm
+++ Kernel/Modules/AgentFAQEdit.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQEdit.pm - agent frontend to edit faq articles
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQEdit.pm,v 1.3 2010-11-17 15:57:20 ub Exp $
+# $Id: AgentFAQEdit.pm,v 1.4 2010-11-17 16:01:11 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::Web::UploadCache;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -309,7 +309,7 @@ sub Run {
                 $Self->{LogObject}->Log(
                     Priority => 'error',
                     Message  => "Could not update the inline image URLs "
-                        . "for FAQ Item# ' $GetParam{ItemID}'!",
+                        . "for FAQ Item# '$GetParam{ItemID}'!",
                 );
             }
         }
