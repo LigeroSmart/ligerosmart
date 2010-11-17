9f6c1b256f6f8752d4d5ab8eb13851a38213fb92
Udo Bretz <udo.bretz@otrs.com>
2010-11-17 14:40:39 +0000

    Removed not needed block calls.

--- Kernel/Modules/AgentFAQEdit.pm
+++ Kernel/Modules/AgentFAQEdit.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQEdit.pm - agent frontend to edit faq articles
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQEdit.pm,v 1.1 2010-11-17 14:38:46 ub Exp $
+# $Id: AgentFAQEdit.pm,v 1.2 2010-11-17 14:40:39 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::Web::UploadCache;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -316,10 +316,6 @@ sub Run {
 
 sub _MaskNew {
     my ( $Self, %Param ) = @_;
-
-    # show action list
-    $Self->{LayoutObject}->Block( Name => 'ActionList' );
-    $Self->{LayoutObject}->Block( Name => 'ActionOverview' );
 
     # get valid list
     my %ValidList        = $Self->{ValidObject}->ValidList();
