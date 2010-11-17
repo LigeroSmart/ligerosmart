c9e7cbc45c18d239738b1b2bb83082398d956116
Udo Bretz <udo.bretz@otrs.com>
2010-11-17 14:38:46 +0000

    Added first version of AgentFAQEdit. Attachment handling still incomplete.

---
+++ Kernel/Modules/AgentFAQEdit.pm
@@ -0,0 +1,523 @@
+# --
+# Kernel/Modules/AgentFAQEdit.pm - agent frontend to edit faq articles
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# --
+# $Id: AgentFAQEdit.pm,v 1.1 2010-11-17 14:38:46 ub Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AgentFAQEdit;
+
+use strict;
+use warnings;
+
+use Kernel::System::FAQ;
+use Kernel::System::Web::UploadCache;
+use Kernel::System::Valid;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check all needed objects
+    for my $Object (qw(ParamObject DBObject LayoutObject ConfigObject LogObject)) {
+        if ( !$Self->{$Object} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $Object!" );
+        }
+    }
+
+    # create needed objects
+    $Self->{FAQObject}         = Kernel::System::FAQ->new(%Param);
+    $Self->{UploadCacheObject} = Kernel::System::Web::UploadCache->new(%Param);
+    $Self->{ValidObject}       = Kernel::System::Valid->new(%Param);
+
+    # get config of frontend module
+    $Self->{Config} = $Self->{ConfigObject}->Get("FAQ::Frontend::$Self->{Action}") || '';
+
+    # get form id
+    $Self->{FormID} = $Self->{ParamObject}->GetParam( Param => 'FormID' );
+
+    # create form id
+    if ( !$Self->{FormID} ) {
+        $Self->{FormID} = $Self->{UploadCacheObject}->FormIDCreate();
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # permission check
+    if ( !$Self->{AccessRw} ) {
+        return $Self->{LayoutObject}->NoPermission(
+            Message    => 'You need rw permission!',
+            WithHeader => 'yes',
+        );
+    }
+
+    # get parameters
+    my %GetParam;
+    for my $ParamName (
+        qw(ItemID Title CategoryID StateID LanguageID ValidID Keywords Approved Field1 Field2 Field3 Field4 Field5 Field6 )
+        )
+    {
+        $GetParam{$ParamName} = $Self->{ParamObject}->GetParam( Param => $ParamName );
+    }
+
+    # check needed stuff
+    if ( !$GetParam{ItemID} ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => 'No ItemID is given!',
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # get FAQ item data
+    my %FAQData = $Self->{FAQObject}->FAQGet(
+        ItemID => $GetParam{ItemID},
+        UserID => $Self->{UserID},
+    );
+    if ( !%FAQData ) {
+        return $Self->{LayoutObject}->ErrorScreen();
+    }
+
+    # check user permission
+    my $Permission = $Self->{FAQObject}->CheckCategoryUserPermission(
+        UserID     => $Self->{UserID},
+        CategoryID => $FAQData{CategoryID},
+    );
+
+    # show error message
+    if ( !$Permission ) {
+        return $Self->{LayoutObject}->NoPermission(
+            Message    => 'You have no permission for this category!',
+            WithHeader => 'yes',
+        );
+    }
+
+    # ------------------------------------------------------------ #
+    # show the faq edit screen
+    # ------------------------------------------------------------ #
+    if ( !$Self->{Subaction} ) {
+
+        # header
+        my $Output = $Self->{LayoutObject}->Header( Type => 'Small' );
+
+        # html output
+        $Output .= $Self->_MaskNew(
+            %FAQData,
+            FormID => $Self->{FormID},
+        );
+
+        # footer
+        $Output .= $Self->{LayoutObject}->Footer();
+
+        return $Output;
+    }
+
+    # ------------------------------------------------------------ #
+    # save the faq
+    # ------------------------------------------------------------ #
+    elsif ( $Self->{Subaction} eq 'Save' ) {
+
+        # challenge token check for write action
+        $Self->{LayoutObject}->ChallengeTokenCheck();
+
+        # header
+        my $Output = $Self->{LayoutObject}->Header( Type => 'Small' );
+
+        # check required parameters
+        my %Error;
+        for my $ParamName (qw(Title CategoryID)) {
+
+            # if required field is not given, add server error class
+            if ( !$GetParam{$ParamName} ) {
+                $Error{ $ParamName . 'ServerError' } = 'ServerError';
+            }
+        }
+
+        # check if an attachment must be deleted
+        ATTACHMENT:
+        for my $Number ( 1 .. 32 ) {
+
+            # check if the delete button was pressed for this attachment
+            my $Delete = $Self->{ParamObject}->GetParam( Param => "AttachmentDelete$Number" );
+
+            # check next attachment if it was not pressed
+            next ATTACHMENT if !$Delete;
+
+            # remember that we need to show the page again
+            $Error{Attachment} = 1;
+
+            # remove the attachment from the upload cache
+            $Self->{UploadCacheObject}->FormIDRemoveFile(
+                FormID => $Self->{FormID},
+                FileID => $Number,
+            );
+        }
+
+        # check if there was an attachment upload
+        if ( $Self->{ParamObject}->GetParam( Param => 'AttachmentUpload' ) ) {
+
+            # remember that we need to show the page again
+            $Error{Attachment} = 1;
+
+            # get the uploaded attachment
+            my %UploadStuff = $Self->{ParamObject}->GetUploadAll(
+                Param  => 'FileUpload',
+                Source => 'string',
+            );
+
+            # add attachment to the upload cache
+            $Self->{UploadCacheObject}->FormIDAddFile(
+                FormID => $Self->{FormID},
+                %UploadStuff,
+            );
+        }
+
+        # send server error if any required parameter is missing
+        # or an attachment was deleted or uploaded
+        if (%Error) {
+
+            # if there was an attachment delete or upload
+            # we do not want to show validation errors for other fields
+            if ( $Error{Attachment} ) {
+                %Error = ();
+            }
+
+            # get all attachments meta data
+            my @Attachments = $Self->{UploadCacheObject}->FormIDGetAllFilesMeta(
+                FormID => $Self->{FormID},
+            );
+
+            # html output
+            $Output .= $Self->_MaskNew(
+                Attachments => \@Attachments,
+                %GetParam,
+                %Error,
+                FormID => $Self->{FormID},
+            );
+
+            # footer
+            $Output .= $Self->{LayoutObject}->Footer();
+
+            return $Output;
+        }
+
+        # update the new faq article
+        my $Success = $Self->{FAQObject}->FAQUpdate(
+            %GetParam,
+            UserID => $Self->{UserID},
+        );
+
+        # show error if faq could not be updated
+        if ( !$Success ) {
+            return $Self->{LayoutObject}->ErrorScreen();
+        }
+
+        # get all attachments from upload cache
+        my @Attachments = $Self->{UploadCacheObject}->FormIDGetAllFilesData(
+            FormID => $Self->{FormID},
+        );
+
+        # write attachments
+        ATTACHMENT:
+        for my $Attachment (@Attachments) {
+
+            # check if attachment is an inline attachment
+            my $Inline = 0;
+            if ( $Attachment->{ContentID} ) {
+
+                # remember that it is inline
+                $Inline = 1;
+
+                # rember if this inline attachment is used in any faq article
+                my $ContentIDFound;
+
+                # check all fields for content id
+                FIELD:
+                for my $Number ( 1 .. 6 ) {
+
+                    # get faq field
+                    my $Field = $GetParam{ 'Field' . $Number };
+
+                    # skip empty fields
+                    next FIELD if !$Field;
+
+                    # skip fields that do not contain the content id
+                    next FIELD if $Field !~ m{ $Attachment->{ContentID} }xms;
+
+                    # found the content id
+                    $ContentIDFound = 1;
+
+                    # we do not need to search further
+                    last FIELD;
+                }
+
+                # we do not want to keep this attachment,
+                # because it was deleted in the richt text editor
+                next ATTACHMENT if !$ContentIDFound;
+            }
+
+            # add attachment
+            my $FileID = $Self->{FAQObject}->AttachmentAdd(
+                %{$Attachment},
+                ItemID => $GetParam{ItemID},
+                Inline => $Inline,
+                UserID => $Self->{UserID},
+            );
+
+            # check error
+            if ( !$FileID ) {
+                return $Self->{LayoutObject}->FatalError();
+            }
+
+            next ATTACHMENT if !$Inline;
+            next ATTACHMENT if !$Self->{LayoutObject}->{BrowserRichText};
+
+            # rewrite the URLs of the inline images for the uploaded pictures
+            my $Ok = $Self->{FAQObject}->FAQInlineAttachmentURLUpdate(
+                Attachment => $Attachment,
+                FormID     => $Self->{FormID},
+                ItemID     => $GetParam{ItemID},
+                FileID     => $FileID,
+                UserID     => $Self->{UserID},
+            );
+
+            # check error
+            if ( !$Ok ) {
+                $Self->{LogObject}->Log(
+                    Priority => 'error',
+                    Message  => "Could not update the inline image URLs "
+                        . "for FAQ Item# '$Param{ItemID}'!",
+                );
+            }
+        }
+
+        # delete the upload cache
+        $Self->{UploadCacheObject}->FormIDRemove( FormID => $Self->{FormID} );
+
+        # reload the parent window and close popup
+        return $Self->{LayoutObject}->PopupClose(
+            Reload => 1,
+        );
+    }
+}
+
+sub _MaskNew {
+    my ( $Self, %Param ) = @_;
+
+    # show action list
+    $Self->{LayoutObject}->Block( Name => 'ActionList' );
+    $Self->{LayoutObject}->Block( Name => 'ActionOverview' );
+
+    # get valid list
+    my %ValidList        = $Self->{ValidObject}->ValidList();
+    my %ValidListReverse = reverse %ValidList;
+
+    my %Data;
+
+    # build valid selection
+    $Data{ValidOption} = $Self->{LayoutObject}->BuildSelection(
+        Data       => \%ValidList,
+        Name       => 'ValidID',
+        SelectedID => $Param{ValidID} || $ValidListReverse{valid},
+    );
+
+    # get categories (with category long names) where user has rights
+    my $UserCategoriesLongNames = $Self->{FAQObject}->GetUserCategoriesLongNames(
+        Type   => 'rw',
+        UserID => $Self->{UserID},
+    );
+
+    # set no server error class as default
+    $Param{CategoryIDServerError} ||= '';
+
+    # build category selection
+    $Data{CategoryOption} = $Self->{LayoutObject}->BuildSelection(
+        Data         => $UserCategoriesLongNames,
+        Name         => 'CategoryID',
+        SelectedID   => $Param{CategoryID},
+        PossibleNone => 1,
+        Class        => 'Validate_RequiredDropdown ' . $Param{CategoryIDServerError},
+        Translation  => 0,
+    );
+
+    # get the language list
+    my %Languages = $Self->{FAQObject}->LanguageList(
+        UserID => $Self->{UserID},
+    );
+
+    # get the selected language
+    my $SelectedLanguage;
+    if ( $Param{LanguageID} && $Languages{ $Param{LanguageID} } ) {
+
+        # get language from given language id
+        $SelectedLanguage = $Languages{ $Param{LanguageID} };
+    }
+    else {
+
+        # use the user language, or if not found 'en'
+        $SelectedLanguage = $Self->{LayoutObject}->{UserLanguage} || 'en';
+    }
+
+    # build the language selection
+    $Data{LanguageOption} = $Self->{LayoutObject}->BuildSelection(
+        Data          => \%Languages,
+        Name          => 'LanguageID',
+        SelectedValue => $SelectedLanguage,
+        Translation   => 0,
+    );
+
+    # get the states list
+    my %States = $Self->{FAQObject}->StateList(
+        UserID => $Self->{UserID},
+    );
+
+    # get the selected state
+    my $SelectedState;
+    if ( $Param{StateID} && $States{ $Param{StateID} } ) {
+
+        # get state from given state id
+        $SelectedState = $States{ $Param{StateID} };
+    }
+    else {
+
+        # get default state
+        $SelectedState = $Self->{ConfigObject}->Get('FAQ::Default::State') || 'internal (agent)';
+    }
+
+    # build the state selection
+    $Data{StateOption} = $Self->{LayoutObject}->BuildSelection(
+        Data          => \%States,
+        Name          => 'StateID',
+        SelectedValue => $SelectedState,
+        Translation   => 1,
+    );
+
+    # show faq edit screen
+    $Self->{LayoutObject}->Block(
+        Name => 'FAQEdit',
+        Data => {
+            %Param,
+            %Data,
+        },
+    );
+
+    # show approval field
+    if ( $Self->{ConfigObject}->Get('FAQ::ApprovalRequired') ) {
+
+        # check permission
+        my %Groups = reverse $Self->{GroupObject}->GroupMemberList(
+            UserID => $Self->{UserID},
+            Type   => 'ro',
+            Result => 'HASH',
+        );
+
+        # get the faq approval group from config
+        my $ApprovalGroup = $Self->{ConfigObject}->Get('FAQ::ApprovalGroup') || '';
+
+        # build the approval selection if there is an approval group
+        if ( exists $Groups{$ApprovalGroup} ) {
+
+            $Data{ApprovalOption} = $Self->{LayoutObject}->BuildSelection(
+                Name => 'Approved',
+                Data => {
+                    0 => 'No',
+                    1 => 'Yes',
+                },
+                SelectedID => $Param{Approved} || 0,
+            );
+            $Self->{LayoutObject}->Block(
+                Name => 'Approval',
+                Data => {%Data},
+            );
+        }
+    }
+
+    # show the attachment upload button
+    $Self->{LayoutObject}->Block(
+        Name => 'AttachmentUpload',
+        Data => {%Param},
+    );
+
+    # show attachments
+    ATTACHMENT:
+    for my $Attachment ( @{ $Param{Attachments} } ) {
+
+        # do not show inline images as attachments
+        # (they have a content id)
+        if ( $Attachment->{ContentID} && $Self->{LayoutObject}->{BrowserRichText} ) {
+            next ATTACHMENT;
+        }
+
+        $Self->{LayoutObject}->Block(
+            Name => 'Attachment',
+            Data => $Attachment,
+        );
+    }
+
+    # add rich text editor javascript
+    # only if activated and the browser can handle it
+    # otherwise just a textarea is shown
+    if ( $Self->{LayoutObject}->{BrowserRichText} ) {
+        $Self->{LayoutObject}->Block(
+            Name => 'RichText',
+            Data => {%Param},
+        );
+    }
+
+    # get the config of FAQ fields that should be shown
+    my %Fields;
+    FIELD:
+    for my $Number ( 1 .. 6 ) {
+
+        # get config of FAQ field
+        my $Config = $Self->{ConfigObject}->Get( 'FAQ::Item::Field' . $Number );
+
+        # skip over not shown fields
+        next FIELD if !$Config->{Show};
+
+        # store only the config of fields that should be shown
+        $Fields{ "Field" . $Number } = $Config;
+    }
+
+    # sort shown fields by priority
+    for my $Field ( sort { $Fields{$a}->{Prio} <=> $Fields{$b}->{Prio} } keys %Fields ) {
+
+        # get the state type data of this field
+        my $StateTypeData = $Self->{FAQObject}->StateTypeGet(
+            Name   => $Fields{$Field}->{Show},
+            UserID => $Self->{UserID},
+        );
+
+        # show the field
+        $Self->{LayoutObject}->Block(
+            Name => 'FAQItemField',
+            Data => {
+                Field     => $Field,
+                Caption   => $Fields{$Field}->{'Caption'},
+                StateName => $StateTypeData->{Name},
+                Content   => $Param{$Field} || '',
+            },
+        );
+    }
+
+    # generate output
+    return $Self->{LayoutObject}->Output(
+        TemplateFile => 'AgentFAQEdit',
+        Data         => \%Param,
+    );
+}
+
+1;
