d3fb9685c5fee56d778dc1706186f0991cdbf0dc
Udo Bretz <udo.bretz@otrs.com>
2010-11-22 14:55:32 +0000

    Small comment fix.

--- Kernel/Modules/AgentFAQZoom.pm
+++ Kernel/Modules/AgentFAQZoom.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQZoom.pm - to get a closer view
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQZoom.pm,v 1.18 2010-11-20 10:58:58 ub Exp $
+# $Id: AgentFAQZoom.pm,v 1.19 2010-11-22 14:55:32 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::FAQ;
 use Kernel::System::User;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.18 $) [1];
+$VERSION = qw($Revision: 1.19 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -316,7 +316,7 @@ sub Run {
         }
     }
 
-    #output Flaq title block
+    # output flag title block
     $Self->{ShowFlag} = 0;
     if ( $Self->{ShowFlag} ) {
         $Self->{LayoutObject}->Block(
