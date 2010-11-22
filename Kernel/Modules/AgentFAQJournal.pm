7670bb4154ca2aca69ca24e13969b86a093921bb
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-11-22 22:58:56 +0000

    Initial Version

---
+++ Kernel/Modules/AgentFAQJournal.pm
@@ -0,0 +1,295 @@
+# --
+# Kernel/Modules/AgentFAQJournal.pm - module for FAQ search
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# --
+# $Id: AgentFAQJournal.pm,v 1.1 2010-11-22 22:58:56 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AgentFAQJournal;
+
+use strict;
+use warnings;
+
+use Kernel::System::FAQ;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (
+        qw(ParamObject DBObject LayoutObject LogObject ConfigObject MainObject EncodeObject)
+        )
+    {
+        if ( !$Self->{$Object} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $Object!" );
+        }
+    }
+
+    # create additional objects
+    $Self->{FAQObject} = Kernel::System::FAQ->new(%Param);
+
+    # get config for frontend
+    $Self->{Config} = $Self->{ConfigObject}->Get("FAQ::Frontend::$Self->{Action}");
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # get config data
+    $Self->{StartHit} = int( $Self->{ParamObject}->GetParam( Param => 'StartHit' ) || 1 );
+    $Self->{JournalLimit} = $Self->{Config}->{JournalLimit} || 500;
+
+    # permission check
+    if ( !$Self->{AccessRo} ) {
+        return $Self->{LayoutObject}->NoPermission(
+            Message    => 'You need ro permission!',
+            WithHeader => 'yes',
+        );
+    }
+
+    # get Journal entries
+    my $Journal = $Self->{FAQObject}->HistoryGet( UserID => $Self->{UserID} );
+
+    # find out which columns should be shown
+    my @ShowColumns;
+    if ( $Self->{Config}->{ShowColumns} ) {
+
+        # get all possible columns from config
+        my %PossibleColumn = %{ $Self->{Config}->{ShowColumns} };
+
+        # get the column names that should be shown
+        COLUMNNAME:
+        for my $Name ( keys %PossibleColumn ) {
+            next COLUMNNAME if !$PossibleColumn{$Name};
+            push @ShowColumns, $Name;
+        }
+    }
+
+    # output header
+    my $Output = $Self->{LayoutObject}->Header(
+        Value => 'JAQ Journal',
+    );
+    $Output .= $Self->{LayoutObject}->NavigationBar();
+
+    # outout Journal
+    $Output .= $Self->_FAQJournalShow(
+        Journal     => $Journal,
+        Total       => scalar @{$Journal},
+        TitleName   => 'FAQ Journal',
+        Limit       => $Self->{JournalLimit},
+        ShowColumns => \@ShowColumns,
+    );
+
+    # build footer
+    $Output .= $Self->{LayoutObject}->Footer();
+
+    return $Output;
+}
+
+sub _FAQJournalShow {
+    my ( $Self, %Param ) = @_;
+
+    # lookup latest used view mode
+    if ( !$Param{View} && $Self->{ 'UserFAQJournalOverview' . $Self->{Action} } ) {
+        $Param{View} = $Self->{ 'UserFAQJournalOverview' . $Self->{Action} };
+    }
+
+    # set frontend
+    my $Frontend = 'Agent';
+
+    # set defaut view mode to 'small'
+    my $View = $Param{View} || 'Small';
+
+    # store latest view mode
+    $Self->{SessionObject}->UpdateSessionID(
+        SessionID => $Self->{SessionID},
+        Key       => 'UserFAQJournalOverview' . $Self->{Action},
+        Value     => $View,
+    );
+
+    # get backend from config
+    my $Backends = $Self->{ConfigObject}->Get('FAQ::Frontend::JournalOverview');
+    if ( !$Backends ) {
+        return $Self->{LayoutObject}->FatalError(
+            Message => 'Need config option FAQ::Frontend::Overview',
+        );
+    }
+
+    # check for hash-ref
+    if ( ref $Backends ne 'HASH' ) {
+        return $Self->{LayoutObject}->FatalError(
+            Message => 'Config option FAQ::Frontend::Overview needs to be a HASH ref!',
+        );
+    }
+
+    # check for config key
+    if ( !$Backends->{$View} ) {
+        return $Self->{LayoutObject}->FatalError(
+            Message => "No config option found for the view '$View'!",
+        );
+    }
+
+    # nav bar
+    my $StartHit = $Self->{ParamObject}->GetParam(
+        Param => 'StartHit',
+    ) || 1;
+
+    # get personal page shown count
+    my $PageShownPreferencesKey = 'UserFAQJournalOverview' . $View . 'PageShown';
+    my $PageShown               = $Self->{$PageShownPreferencesKey} || 10;
+    my $Group                   = 'FAQJournalOverview' . $View . 'PageShown';
+
+    # check start option, if higher then elements available, set
+    # it to the last overview page (Thanks to Stefan Schmidt!)
+    if ( $StartHit > $Param{Total} ) {
+        my $Pages = int( ( $Param{Total} / $PageShown ) + 0.99999 );
+        $StartHit = ( ( $Pages - 1 ) * $PageShown ) + 1;
+    }
+
+    # get data selection
+    my %Data;
+    my $Config = $Self->{ConfigObject}->Get('PreferencesGroups');
+    if ( $Config && $Config->{$Group} && $Config->{$Group}->{Data} ) {
+        %Data = %{ $Config->{$Group}->{Data} };
+    }
+
+    # set page limit and build page nav
+    my $Limit = $Param{Limit} || 20_000;
+    my %PageNav = $Self->{LayoutObject}->PageNavBar(
+        Limit     => $Limit,
+        StartHit  => $StartHit,
+        PageShown => $PageShown,
+        AllHits   => $Param{Total} || 0,
+        Action    => 'Action=' . $Self->{LayoutObject}->{Action},
+        Link      => $Param{LinkPage},
+    );
+
+    # build shown faq articles on a page
+    $Param{RequestedURL}    = "Action=$Self->{Action}";
+    $Param{Group}           = $Group;
+    $Param{PreferencesKey}  = $PageShownPreferencesKey;
+    $Param{PageShownString} = $Self->{LayoutObject}->BuildSelection(
+        Name       => $PageShownPreferencesKey,
+        SelectedID => $PageShown,
+        Data       => \%Data,
+    );
+
+    # build navbar content
+    $Self->{LayoutObject}->Block(
+        Name => 'OverviewNavBar',
+        Data => \%Param,
+    );
+
+    # loop over configured backends
+    for my $Backend ( keys %{$Backends} ) {
+
+        # build navbar view mode
+        $Self->{LayoutObject}->Block(
+            Name => 'OverviewNavBarViewMode',
+            Data => {
+                %Param,
+                %{ $Backends->{$Backend} },
+                Filter => $Param{Filter},
+                View   => $Backend,
+            },
+        );
+
+        # current view is configured in backend
+        if ( $View eq $Backend ) {
+            $Self->{LayoutObject}->Block(
+                Name => 'OverviewNavBarViewModeSelected',
+                Data => {
+                    %Param,
+                    %{ $Backends->{$Backend} },
+                    Filter => $Param{Filter},
+                    View   => $Backend,
+                },
+            );
+        }
+        else {
+            $Self->{LayoutObject}->Block(
+                Name => 'OverviewNavBarViewModeNotSelected',
+                Data => {
+                    %Param,
+                    %{ $Backends->{$Backend} },
+                    Filter => $Param{Filter},
+                    View   => $Backend,
+                },
+            );
+        }
+    }
+
+    # check if page nav is available
+    if (%PageNav) {
+        $Self->{LayoutObject}->Block(
+            Name => 'OverviewNavBarPageNavBar',
+            Data => \%PageNav,
+        );
+
+        # don't show context settings in AJAX case (e. g. in customer ticket history),
+        # because the submit with page reload will not work there
+        if ( !$Param{AJAX} ) {
+            $Self->{LayoutObject}->Block(
+                Name => 'ContextSettings',
+                Data => {
+                    %PageNav,
+                    %Param,
+                },
+            );
+        }
+    }
+
+    # build html content
+    my $OutputNavBar = $Self->{LayoutObject}->Output(
+        TemplateFile => 'AgentFAQOverviewNavBar',
+        Data         => {%Param},
+    );
+
+    # create output
+    my $OutputRaw .= $OutputNavBar;
+
+    # load module
+    if ( !$Self->{MainObject}->Require( $Backends->{$View}->{Module} ) ) {
+        return $Self->{LayoutObject}->FatalError();
+    }
+
+    # check for backend object
+    my $Object = $Backends->{$View}->{Module}->new( %{$Self} );
+    return if !$Object;
+
+    # run module
+    my $Output = $Object->Run(
+        %Param,
+        Limit     => $Limit,
+        StartHit  => $StartHit,
+        PageShown => $PageShown,
+        AllHits   => $Param{Total} || 0,
+        Frontend  => $Frontend,
+    );
+
+    $OutputRaw .= $Output;
+
+    # create overview nav bar
+    $Self->{LayoutObject}->Block(
+        Name => 'OverviewNavBar',
+        Data => {%Param},
+    );
+
+    # return content if available
+    return $OutputRaw;
+}
+
+1;
---
+++ Kernel/Output/HTML/FAQJournalOverviewSmall.pm
@@ -0,0 +1,176 @@
+# --
+# Kernel/Output/HTML/FAQJournalOverviewSmall.pm.pm
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# --
+# $Id: FAQJournalOverviewSmall.pm,v 1.1 2010-11-22 22:58:56 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::FAQJournalOverviewSmall;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for my $Object (
+        qw(ConfigObject LogObject DBObject LayoutObject UserID UserObject MainObject)
+        )
+    {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $Output;
+    my @ShowColumns;
+
+    # check needed stuff
+    for my $Needed (qw(PageShown StartHit)) {
+        if ( !$Param{$Needed} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!",
+            );
+            return;
+        }
+    }
+
+    # need Journal
+    if ( !$Param{Journal} ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => 'Need the Journal!',
+        );
+        return;
+    }
+
+    # store the journal locally
+    my @JournalEntries;
+    if ( $Param{Journal} && ref $Param{Journal} eq 'ARRAY' ) {
+        @JournalEntries = @{ $Param{Journal} };
+    }
+
+    # show Joural Entries as rows
+    if (@JournalEntries) {
+
+        # check ShowColumns parameter
+        if ( $Param{ShowColumns} && ref $Param{ShowColumns} eq 'ARRAY' ) {
+            @ShowColumns = @{ $Param{ShowColumns} };
+        }
+
+        # build column header blocks
+        if (@ShowColumns) {
+
+            # call main block
+            $Self->{LayoutObject}->Block( Name => 'RecordForm' );
+
+            for my $Column (@ShowColumns) {
+
+                # call header specific block
+                $Self->{LayoutObject}->Block(
+                    Name => 'Record' . $Column . 'Header',
+                    Data => {
+                        %Param,
+                    },
+                );
+            }
+        }
+
+        my $Counter = 0;
+
+        JournalEntry:
+        for my $JournalEntry (@JournalEntries) {
+            $Counter++;
+            if (
+                $Counter >= $Param{StartHit}
+                && $Counter < ( $Param{PageShown} + $Param{StartHit} )
+                )
+            {
+
+                # get FAQ data for corruption check
+                my %FAQ = $Self->{FAQObject}->FAQGet(
+                    FAQID  => $JournalEntry->{ItemID},
+                    UserID => $Self->{UserID},
+                );
+
+                next ID if !%FAQ;
+
+                # build record block
+                $Self->{LayoutObject}->Block(
+                    Name => 'Record',
+                    Data => {
+                        %Param,
+                        %{$JournalEntry},
+                        Counter => $Counter,
+                    },
+                );
+
+                # build column record blocks
+                if (@ShowColumns) {
+                    COLUMN:
+                    for my $Column (@ShowColumns) {
+                        $Self->{LayoutObject}->Block(
+                            Name => 'Record' . $Column,
+                            Data => {
+                                %Param,
+                                %{$JournalEntry},
+                            },
+                        );
+
+                        # show links if available
+                        $Self->{LayoutObject}->Block(
+                            Name => 'Record' . $Column . 'LinkStart',
+                            Data => {
+                                %Param,
+                                %{$JournalEntry},
+                            },
+                        );
+                        $Self->{LayoutObject}->Block(
+                            Name => 'Record' . $Column . 'LinkEnd',
+                            Data => {
+                                %Param,
+                                %{$JournalEntry},
+                            },
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    # otherwise set an No FAQ Jounal message
+    else {
+        $Self->{LayoutObject}->Block( Name => 'NoFAQFound' );
+    }
+
+    # use template
+    $Output .= $Self->{LayoutObject}->Output(
+        TemplateFile => 'AgentFAQJournalOverviewSmall',
+        Data         => {
+            %Param,
+            Type        => $Self->{ViewType},
+            ColumnCount => scalar @ShowColumns,
+        },
+    );
+
+    return $Output;
+}
+
+1;
