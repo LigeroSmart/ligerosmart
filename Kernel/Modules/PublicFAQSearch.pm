f4af6a9e44e91d708880fadf80431cd6d161c506
Udo Bretz <udo.bretz@otrs.com>
2010-11-23 11:10:23 +0000

    Moved comment.

--- Kernel/Modules/PublicFAQSearch.pm
+++ Kernel/Modules/PublicFAQSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/PublicFAQSearch.pm - Utilities for tickets
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: PublicFAQSearch.pm,v 1.2 2010-11-23 11:03:24 ub Exp $
+# $Id: PublicFAQSearch.pm,v 1.3 2010-11-23 11:10:23 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::SearchProfile;
 use Kernel::System::CSV;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -78,9 +78,9 @@ sub Run {
     # remember exclude attributes
     my @Excludes = $Self->{ParamObject}->GetArray( Param => 'Exclude' );
 
-    # get single params
     my %GetParam;
 
+    # get single params
     for my $ParamName (qw(Number Title Keyword Fulltext ResultForm)) {
 
         # get search string params (get submitted params)
