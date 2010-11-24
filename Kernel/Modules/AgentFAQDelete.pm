e880ad04ed6c9f928c432ba9c49d4dec9a847827
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-11-24 22:00:58 +0000

    Initial Version

---
+++ Kernel/Modules/AgentFAQDelete.pm
@@ -0,0 +1,149 @@
+# --
+# Kernel/Modules/AgentFAQDelete.pm - the  delete module
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# --
+# $Id: AgentFAQDelete.pm,v 1.1 2010-11-24 22:00:58 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AgentFAQDelete;
+
+use strict;
+use warnings;
+
+use Kernel::System::FAQ;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (
+        qw(ParamObject DBObject LayoutObject LogObject ConfigObject UserObject GroupObject)
+        )
+    {
+        if ( !$Self->{$Object} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $Object!" );
+        }
+    }
+
+    # create additional objects
+    $Self->{FAQObject} = Kernel::System::FAQ->new(%Param);
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # permission check
+    if ( !$Self->{AccessRo} ) {
+        return $Self->{LayoutObject}->NoPermission(
+            Message    => 'You need ro permission!',
+            WithHeader => 'yes',
+        );
+    }
+
+    # get params
+    my %GetParam;
+
+    # get needed Item id
+    $GetParam{ItemID} = $Self->{ParamObject}->GetParam( Param => 'ItemID' );
+
+    # check needed stuff
+    if ( !$GetParam{ItemID} ) {
+
+        # error page
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "No ItemID is given!",
+            Comment => 'Please contact the administrator.',
+        );
+    }
+
+    # get FAQ item data
+    my %FAQData = $Self->{FAQObject}->FAQGet(
+        ItemID => $GetParam{ItemID},
+        UserID => $Self->{UserID},
+    );
+    if ( !%FAQData ) {
+        return $Self->{LayoutObject}->ErrorScreen();
+    }
+
+    # check user permission
+    my $Permission = $Self->{FAQObject}->CheckCategoryUserPermission(
+        UserID     => $Self->{UserID},
+        CategoryID => $FAQData{CategoryID},
+    );
+
+    # show error message
+    if ( !$Permission ) {
+        return $Self->{LayoutObject}->NoPermission(
+            Message    => 'You have no permission for this category!',
+            WithHeader => 'yes',
+        );
+    }
+
+    if ( $Self->{Subaction} eq 'Delete' ) {
+
+        # delete the FAQ article
+        my $CouldDeleteItem = $Self->{FAQObject}->FAQDelete(
+            ItemID => $FAQData{ItemID},
+            UserID => $Self->{UserID},
+        );
+
+        if ($CouldDeleteItem) {
+
+            # redirect to explorer, when the deletion was successful
+            return $Self->{LayoutObject}->Redirect(
+                OP => "Action=AgentFAQExplorer;CategoryID=$FAQData{CategoryID}",
+            );
+        }
+        else {
+
+            # show error message, when delete failed
+            return $Self->{LayoutObject}->ErrorScreen(
+                Message => "Was not able to delete the FAQ article $FAQData{ItemID}!",
+                Comment => 'Please contact the administrator.',
+            );
+        }
+    }
+
+    # set the dialog type. As default, the dialog will have 2 buttons: Yes and No
+    my $DialogType = 'Confirmation';
+
+    # output content
+    my $Output .= $Self->{LayoutObject}->Output(
+        TemplateFile => 'AgentFAQDelete',
+        Data         => {
+            %Param,
+            %FAQData,
+        },
+    );
+
+    # build the returned data structure
+    my %Data = (
+        HTML       => $Output,
+        DialogType => $DialogType,
+    );
+
+    # return JSON-String because of AJAX-Mode
+    my $OutputJSON = $Self->{LayoutObject}->JSONEncode( Data => \%Data );
+
+    return $Self->{LayoutObject}->Attachment(
+        ContentType => 'application/json; charset=' . $Self->{LayoutObject}->{Charset},
+        Content     => $OutputJSON,
+        Type        => 'inline',
+        NoCache     => 1,
+    );
+}
+
+1;
---
+++ var/httpd/htdocs/js/FAQ.Agent.ConfirmationDialog.js
@@ -0,0 +1,118 @@
+// --
+// FAQ.Agent.ConfirmationDialog.js - provides the special module functions for the
+// confirmation dialogs
+// Copyright (C) 2001-2010 OTRS AG, http://otrs.org/\n";
+// --
+// $Id: FAQ.Agent.ConfirmationDialog.js,v 1.1 2010-11-24 22:00:58 cr Exp $
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var FAQ = FAQ || {};
+FAQ.Agent = FAQ.Agent || {};
+
+/**
+ * @namespace
+ * @exports TargetNS as FAQ.Agent.ConfirmationDialog
+ * @description
+ *      This namespace contains the special module functions for ConfirmationDialog.
+ */
+FAQ.Agent.ConfirmationDialog = (function (TargetNS) {
+
+    /**
+     * @variable
+     * @private
+     *     This variable stores the parameters that are passed from the DTL and contain all the data that the dialog needs.
+     */
+    var DialogData = [];
+
+    /**
+     * @function
+     * @private
+     * @return nothing
+     * @description Shows waiting dialog until search screen is ready.
+     */
+    function ShowWaitingDialog(PositionTop){
+        Core.UI.Dialog.ShowContentDialog('<div class="Spacing Center"><span class="AJAXLoader" title="' + Core.Config.Get('LoadingMsg') + '"></span></div>', '', PositionTop, 'Center', true);
+    }
+
+    /**
+     * @function
+     * @param {EventObject} event object of the clicked element.
+     * @return nothing
+     *      This function shows a confirmation dialog with 2 buttons: Yes and No
+     */
+    TargetNS.ShowConfirmationDialog = function (Event) {
+
+        // get global saved DialogData for this function
+        var LocalDialogData = DialogData[$(Event.target).attr('id')];
+
+        // define the position of the dialog
+        var PositionTop = $(window).scrollTop() + ($(window).height() * 0.3);
+
+        // show waiting dialog
+        ShowWaitingDialog(PositionTop);
+
+        // ajax call to the module that deletes the template
+        var Data = LocalDialogData.DialogContentQueryString;
+        Core.AJAX.FunctionCall(Core.Config.Get('Baselink'), Data, function (Response) {
+
+            // 'Confirmation' opens a dialog with 2 buttons: Yes and No
+            if (Response.DialogType == 'Confirmation') {
+
+                // define yes and no buttons
+                var Buttons = [{
+                    Label: LocalDialogData.TranslatedText.Yes,
+                    Class: "Primary",
+
+                    // define the function that is called when the 'Yes' button is pressed
+                    Function: function(){
+
+                        // disable Yes and No buttons to prevent multiple submits
+                        $('div.Dialog:visible div.ContentFooter button').attr('disabled', 'disabled');
+
+                        // redirect to the module that does the confirmed action after pressing the Yes button
+                        location.href = Core.Config.Get('Baselink') + LocalDialogData.ConfirmedActionQueryString;
+                    }
+                }, {
+                    Label: LocalDialogData.TranslatedText.No,
+                    Type: "Close"
+                }];
+            }
+
+            // 'Message' opens a dialog with 1 button: Ok
+            else if (Response.DialogType == 'Message') {
+
+                // define Ok button
+                var Buttons = [{
+                    Label: LocalDialogData.TranslatedText.Ok,
+                    Class: "Primary",
+                    Type: "Close"
+                }];
+            }
+
+            // show the confirmation dialog to confirm the action
+            Core.UI.Dialog.ShowContentDialog(Response.HTML, LocalDialogData.DialogTitle, PositionTop, "Center", true, Buttons);
+        }, 'json');
+        return false;
+    };
+
+    /**
+     * @function
+     * @param {EventObject} event object of the clicked element.
+     * @return nothing
+     *      This function shows a confirmation dialog with 2 buttons: Yes and No
+     */
+    TargetNS.BindConfirmationDialog = function (Data) {
+        DialogData[Data.ElementID] = Data;
+
+        // binding a click event to the defined element
+        $(DialogData[Data.ElementID].ElementSelector).bind('click', FAQ.Agent.ConfirmationDialog.ShowConfirmationDialog);
+    };
+
+    return TargetNS;
+}(FAQ.Agent.ConfirmationDialog || {}));
