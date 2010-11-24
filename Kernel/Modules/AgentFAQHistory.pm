19d450e96ad14aec8ec886285128bec596b340a3
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-11-24 16:37:05 +0000

    Initial Version

---
+++ Kernel/Modules/AgentFAQHistory.pm
@@ -0,0 +1,142 @@
+# --
+# Kernel/Modules/AgentFAQHistory.pm - the FAQ history module
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# --
+# $Id: AgentFAQHistory.pm,v 1.1 2010-11-24 16:37:05 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AgentFAQHistory;
+
+use strict;
+use warnings;
+
+use Kernel::System::FAQ;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (
+        qw(ParamObject DBObject LayoutObject LogObject UserObject GroupObject ConfigObject)
+        )
+    {
+        if ( !$Self->{$Object} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $Object!" );
+        }
+    }
+
+    # create additional objects
+    $Self->{FAQObject} = Kernel::System::FAQ->new(%Param);
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # permission check
+    if ( !$Self->{AccessRo} ) {
+        return $Self->{LayoutObject}->NoPermission(
+            Message    => 'You need ro permission!',
+            WithHeader => 'yes',
+        );
+    }
+
+    # get params
+    my %GetParam;
+
+    # get needed Item id
+    $GetParam{ItemID} = $Self->{ParamObject}->GetParam( Param => 'ItemID' );
+
+    # check needed stuff
+    if ( !$GetParam{ItemID} ) {
+
+        # error page
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => "Can't show history, as no ItemID is given!",
+            Comment => 'Please contact the administrator.',
+        );
+    }
+
+    # get FAQ item data
+    my %FAQData = $Self->{FAQObject}->FAQGet(
+        ItemID => $GetParam{ItemID},
+        UserID => $Self->{UserID},
+    );
+    if ( !%FAQData ) {
+        return $Self->{LayoutObject}->ErrorScreen();
+    }
+
+    # check user permission
+    my $Permission = $Self->{FAQObject}->CheckCategoryUserPermission(
+        UserID     => $Self->{UserID},
+        CategoryID => $FAQData{CategoryID},
+    );
+
+    # show error message
+    if ( !$Permission ) {
+        return $Self->{LayoutObject}->NoPermission(
+            Message    => 'You have no permission for this category!',
+            WithHeader => 'yes',
+        );
+    }
+
+    # get FAQ article history
+    my $History = $Self->{FAQObject}->FAQHistoryGet(
+        ItemID => $FAQData{ItemID},
+        UserID => $Self->{UserID},
+    );
+
+    for my $HistoryEntry ( @{$History} ) {
+
+        # replace ID to full user name on CreatedBy key
+        my %User = $Self->{UserObject}->GetUserData(
+            UserID => $HistoryEntry->{CreatedBy},
+            Cached => 1,
+        );
+        $HistoryEntry->{CreatedBy} = "$User{UserLogin} ($User{UserFirstname} $User{UserLastname})";
+
+        # call Row block
+        $Self->{LayoutObject}->Block(
+            Name => 'Row',
+            Data => {
+                %{$HistoryEntry},
+                }
+        );
+    }
+
+    # output header
+    my $Output = $Self->{LayoutObject}->Header(
+        Type  => 'Small',
+        Title => 'FAQHistory',
+    );
+
+    # start template output
+    $Output .= $Self->{LayoutObject}->Output(
+        TemplateFile => 'AgentFAQHistory',
+        Data         => {
+            %GetParam,
+            %FAQData,
+        },
+    );
+
+    # add footer
+    $Output .= $Self->{LayoutObject}->Footer(
+        Type => 'Small',
+    );
+
+    return $Output;
+}
+
+1;
