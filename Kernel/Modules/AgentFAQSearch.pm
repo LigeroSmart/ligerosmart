552e55ccfb22afe0a217b38d71b300f675225add
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-11-29 12:53:29 +0000

    Code Cleanup: Remove "Bulk" reference

--- Kernel/Modules/AgentFAQSearch.pm
+++ Kernel/Modules/AgentFAQSearch.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQSearch.pm - module for FAQ search
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQSearch.pm,v 1.22 2010-11-24 17:16:33 ub Exp $
+# $Id: AgentFAQSearch.pm,v 1.23 2010-11-29 12:53:29 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::SearchProfile;
 use Kernel::System::CSV;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.22 $) [1];
+$VERSION = qw($Revision: 1.23 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -541,7 +541,6 @@ sub Run {
                 Profile    => $Self->{Profile},
 
                 TitleName => 'Search Result',
-                Bulk      => 1,
                 Limit     => $Self->{SearchLimit},
 
                 Filter     => $Self->{Filter},
