55e185dc6b2be1d315274610404e8fe02ec0f291
Udo Bretz <udo.bretz@otrs.com>
2010-11-29 15:33:46 +0000

    Fixed small typo.

--- Kernel/Modules/AgentFAQJournal.pm
+++ Kernel/Modules/AgentFAQJournal.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQJournal.pm - module for FAQ journal
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQJournal.pm,v 1.3 2010-11-25 23:17:07 cr Exp $
+# $Id: AgentFAQJournal.pm,v 1.4 2010-11-29 15:33:46 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::FAQ;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.3 $) [1];
+$VERSION = qw($Revision: 1.4 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -80,7 +80,7 @@ sub Run {
 
     # output header
     my $Output = $Self->{LayoutObject}->Header(
-        Value => 'JAQ Journal',
+        Value => 'FAQ Journal',
     );
     $Output .= $Self->{LayoutObject}->NavigationBar();
 
