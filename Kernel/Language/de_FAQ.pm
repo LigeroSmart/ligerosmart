7cabca0c1b9218c2afb32e3862e206fc1cfbfe79
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-11-30 17:15:09 +0000

    Added translations from settings

--- Kernel/Language/de_FAQ.pm
+++ Kernel/Language/de_FAQ.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/de_FAQ.pm - the german translation of FAQ
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: de_FAQ.pm,v 1.20 2010-11-29 21:38:44 ub Exp $
+# $Id: de_FAQ.pm,v 1.21 2010-11-30 17:15:09 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.20 $) [1];
+$VERSION = qw($Revision: 1.21 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -64,6 +64,115 @@ sub Data {
     $Lang->{'A category needs at least one permission group!'}
         = 'Eine Kategorie muss mindestens eine Berechtigungs-Gruppe haben.';
     $Lang->{'Will be shown as comment in Explorer.'} = 'Wird im Explorer als Kommentar angezeigt.';
+
+    $Lang->{'Default category name.'}                            = 'Root Kategorie Name.';
+    $Lang->{'Rates for voting. Key must be in percent.'}         = 'Gewichtung für die Bewertung. Der Key muss in Prozent angegeben werden.';
+    $Lang->{'Show voting in defined interfaces.'}                = 'Interfaces in denen das Voting Feature angezeigt werden soll.';
+    $Lang->{'Languagekey which is defined in the language file *_FAQ.pm.'}
+        = 'LanguageKey für die Frage bei der Artikelbewertung. Wird in den Sprachfiles definiert.';
+    $Lang->{'Show FAQ path yes/no.'}                             = 'FAQ Pfad anzeigen ja/nein.';
+    $Lang->{'Decimal places of the voting result.'}              = 'Dezimalstellen des Ergebnisses bei der Artikelbewertung.';
+    $Lang->{'CSS color for the voting flag.'}                    = '';
+    $Lang->{'FAQ path separator.'}                               = 'Trennzeichen im FAQ Pfad.';
+    $Lang->{'Interfaces where the quicksearch should be shown.'} = 'Interfaces in denen das QuickSearch Feature angezeigt wird.';
+    $Lang->{'Show items of subcategories.'}                      = 'Artikel aus Subkategorien anzeigen ja/nein.';
+    $Lang->{'Show last change items in defined interfaces.'}     = 'Interfaces in denen das LastChange Feature angezeigt werden soll.';
+    $Lang->{'Number of shown items in last changes.'}            = 'Anzahl der zu anzeigenden Artikel in letzten Änderungen.';
+    $Lang->{'Show last created items in defined interfaces.'}    = 'Interfaces in denen das LastCreate Feature angezeigt werden soll.';
+    $Lang->{'Number of shown items in last created.'}            = 'Anzahl der anzuzeigenden Artikel in zuletzt erstellte Artikel.';
+    $Lang->{'Show top 10 items in defined interfaces.'}          = 'Interfaces in denen das Top 10 Feature angezeigt werden soll.';
+    $Lang->{'Number of shown items in the top 10 feature.'}      = 'Anzahl der anzuzeigenden Artikel im Top 10 Feature.';
+    $Lang->{'The identifier for a FAQ, e.g. FAQ#, KB#, MyFAQ#. The default is FAQ#.'}
+        = '';
+    $Lang->{'Default state for FAQ entry.'}                      = 'Standard Status eines FAQ-Eintrags.';
+    $Lang->{'Show WYSIWYG editor in agent interface.'}           = 'Anzeige eines WYSIWYG Editors im Agenten-Interface.';
+    $Lang->{'New FAQ articles need approval before they get published.'}
+        = 'Neue FAQ Artikel benötigen eine Freigabe vor der Veröffentlichung.';
+    $Lang->{'Group for the approval of FAQ articles.'}           = 'Gruppe für die Freigabe von FAQ Artikeln.';
+    $Lang->{'Queue for the approval of FAQ articles.'}           = 'Queue für die Freigabe von FAQ Artikeln.';
+    $Lang->{'Ticket subject for approval of FAQ article.'}       = 'Betreff des Tickets zur Freigabe eines FAQ Artikels.';
+    $Lang->{'Ticket body for approval of FAQ article.'}          = 'Body des Tickets zur Freigabe eines FAQ Artikels.';
+    $Lang->{'Default priority of tickets for the approval of FAQ articles.'}
+        = 'Standard Priorität von Tickets für die Freigabe von FAQ Artikeln.';
+    $Lang->{'Default state of tickets for the approval of FAQ articles.'}
+        = 'Standard Status von Tickets für die Freigabe von FAQ Artikeln.';
+    $Lang->{'Definition of FAQ item free text field.'}           = 'Definition der freien Textfelder.';
+    $Lang->{'This setting defines that a \'FAQ\' object can be linked with other \'FAQ\' objects using the \'Normal\' link type.'}
+        = 'Definiert, dass ein \'FAQ\'-Objekte mit dem Linktyp \'Normal\' mit anderen \'FAQ\'-Objekten verlinkt werden kann.';
+    $Lang->{'This setting defines that a \'FAQ\' object can be linked with other \'FAQ\' objects using the \'ParentChild\' link type.'}
+        = 'Definiert, dass ein \'FAQ\'-Objekte mit dem Linktyp \'ParentChild\' mit anderen \'FAQ\'-Objekten verlinkt werden kann.';
+    $Lang->{'This setting defines that a \'FAQ\' object can be linked with other \'Ticket\' objects using the \'Normal\' link type.'}
+        = 'Definiert, dass ein \'FAQ\'-Objekte mit dem Linktyp \'Normal\' mit anderen \'Ticket\'-Objekten verlinkt werden kann.';
+    $Lang->{'This setting defines that a \'FAQ\' object can be linked with other \'Ticket\' objects using the \'ParentChild\' link type.'}
+        = 'Definiert, dass ein \'FAQ\'-Objekte mit dem Linktyp \'ParentChild\' mit anderen \'Ticket\'-Objekten verlinkt werden kann.';
+    $Lang->{'Frontend module registration for the agent interface.'}    = '';
+    $Lang->{'Frontend module registration for the customer interface.'} = '';
+    $Lang->{'Frontend module registration for the public interface.'}   = '';
+    $Lang->{'Default value for the Action parameter for the public frontend. The Action parameter is used in the scripts of the system.'}
+        = 'Standardwert des Action-Parameters für den öffentlichen FAQ-Bereich. Der Action-Parameter wird von den Skripten des Systems benutzt.';
+    $Lang->{'Show FAQ Article with HTML.'}                              = 'HTML Darstellung der FAQ Artikel einschalten.';
+    $Lang->{'Module to generate html OpenSearch profile for short faq search.'}
+        = 'Modul zum Generieren des HTML "OpenSearch" Profils zur FAQ-Suche über das Browser-Suchfeld.';
+    $Lang->{'Defines where the \'Insert FAQ\' link will be displayed.'} = 'Definiert wo der Link \'FAQ einfügen\' angezeigt werden soll.';
+    $Lang->{'A filter for HTML output to add links behind a defined string. The element Image allows two input kinds. First the name of an image (e.g. faq.png). In this case the OTRS image path will be used. The second possibility is to insert the link to the image.'}
+        = 'Ein Filter zur automatischen Generierung von FAQ-Links, wenn ein Hinweis auf einen FAQ-Artikel identifiziert wird. Das Element Image erlaubt zwei Eingabeformen: Erstens der Name eines Icons (z. B. faq.png). In diesem Fall wird auf das Graphikverzeichnis des OTRS zugegriffen. Als zweite Möglichkeit kann man aber auch den direkten Link zur Grafik angeben (z. B. http://otrs.org/faq.png).';
+    $Lang->{'FAQ search backend router of the agent interface.'} = '';
+    $Lang->{'Defines an overview module to show the small view of a FAQ list.'}
+        = '';
+    $Lang->{'Defines the shown columns in the FAQ search. This option has no effect on the position of the column.'}
+        = '';
+    $Lang->{'Defines the default FAQ attribute for FAQ sorting in a FAQ search of the agent interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ order of a search result in the agent interface. Up: oldest on top. Down: latest on top.'}
+        = '';
+    $Lang->{'Maximum number of FAQ articles to be displayed in the result of a search in the agent interface.'}
+        = '';
+    $Lang->{'Defines the shown columns in the FAQ Explorer. This option has no effect on the position of the column.'}
+        = '';
+    $Lang->{'Defines the default FAQ attribute for FAQ sorting in a FAQ Explorer of the agent interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ order of a Explorer result in the agent interface. Up: oldest on top. Down: latest on top.'}
+        = '';
+    $Lang->{'Maximum number of FAQ articles to be displayed in the result of a Explorer in the agent interface.'}
+        = '';
+    $Lang->{'Parameters for the pages (in which the FAQ items are shown) of the small FAQ overview.'}
+        = '';
+    $Lang->{'Shows a link in the menu to go back in the FAQ zoom view of the agent interface.'}
+        = '';
+    $Lang->{'Shows a link in the menu to edit a FAQ in the its zoom view of the agent interface.'}
+        = '';
+    $Lang->{'Shows a link in the menu to access the history of a FAQ in its zoom view of the agent interface'}
+        = '';
+    $Lang->{'Shows a link in the menu to print a FAQ in the its zoom view of the agent interface.'}
+        = '';
+    $Lang->{'Shows a link in the menu that allows linking a FAQ with another object in the zoom view of such FAQ of the agent interface.'}
+        = '';
+    $Lang->{'Shows a link in the menu that allows to delete a FAQ in its zoom view in the agent interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ attribute for FAQ sorting in a FAQ search of the customer interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ order of a search result in the customer interface. Up: oldest on top. Down: latest on top.'}
+        = '';
+    $Lang->{'Maximum number of FAQ articles to be displayed in the result of a search in the customer interface.'}
+        = '';
+    $Lang->{'Number of FAQ articles to be displayed in each page of a search result in the customer interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ attribute for FAQ sorting in a FAQ search of the public interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ order of a search result in the public interface. Up: oldest on top. Down: latest on top.'}
+        = '';
+    $Lang->{'Maximum number of FAQ articles to be displayed in the result of a search in the public interface.'}
+        = '';
+    $Lang->{'Number of FAQ articles to be displayed in each page of a search result in the public interface.'}
+        = '';
+    $Lang->{'Defines the shown columns in the FAQ journal. This option has no effect on the position of the column.'}
+        = '';
+    $Lang->{'Maximum number of FAQ articles to be displayed in the FAQ journal in the agent interface.'}
+        = '';
+    $Lang->{'Parameters for the pages (in which the FAQ items are shown) of the small FAQ journal overview.'}
+        = '';
+    $Lang->{'Defines an overview module to show the small view of a FAQ journal.'}
+        = '';
 
     return 1;
 }
--- Kernel/Language/pl_FAQ.pm
+++ Kernel/Language/pl_FAQ.pm
@@ -4,7 +4,7 @@
 # Copyright (C) 2008 Patryk ¦ciborek <patryk@sciborek.com>
 # Copyright (C) 2009 Artur Skalski <skal.ar at wp.pl>
 # --
-# $Id: pl_FAQ.pm,v 1.7 2010-11-19 10:34:46 ub Exp $
+# $Id: pl_FAQ.pm,v 1.8 2010-11-30 17:15:09 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.7 $) [1];
+$VERSION = qw($Revision: 1.8 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -65,6 +65,112 @@ sub Data {
     $Lang->{'Agent groups which can access this category.'}  = 'Grupy agentów, które maj± dostêp do tej kategorii';
     $Lang->{'A category needs at least one permission group!'}   = 'Kategoria wymaga co najmniej jednej grupy uprawnieñ!';
     $Lang->{'Will be shown as comment in Explorer.'}         = 'Zostanie pokazany jako komentarz w eksplorerze.';
+
+    $Lang->{'Default category name.'}                                      = '';
+    $Lang->{'Rates for voting. Key must be in percent.'}                   = '';
+    $Lang->{'Show voting in defined interfaces.'}                          = '';
+    $Lang->{'Languagekey which is defined in the language file *_FAQ.pm.'} = '';
+    $Lang->{'Show FAQ path yes/no.'}                                       = '';
+    $Lang->{'Decimal places of the voting result.'}                        = '';
+    $Lang->{'CSS color for the voting flag.'}                              = '';
+    $Lang->{'FAQ path separator.'}                                         = '';
+    $Lang->{'Interfaces where the quicksearch should be shown.'}           = '';
+    $Lang->{'Show items of subcategories.'}                                = '';
+    $Lang->{'Show last change items in defined interfaces.'}               = '';
+    $Lang->{'Number of shown items in last changes.'}                      = '';
+    $Lang->{'Show last created items in defined interfaces.'}              = '';
+    $Lang->{'Number of shown items in last created.'}                      = '';
+    $Lang->{'Show top 10 items in defined interfaces.'}                    = '';
+    $Lang->{'Number of shown items in the top 10 feature.'}                = '';
+    $Lang->{'The identifier for a FAQ, e.g. FAQ#, KB#, MyFAQ#. The default is FAQ#.'}
+        = '';
+    $Lang->{'Default state for FAQ entry.'}                                = '';
+    $Lang->{'Show WYSIWYG editor in agent interface.'}                     = '';
+    $Lang->{'New FAQ articles need approval before they get published.'}   = '';
+    $Lang->{'Group for the approval of FAQ articles.'}                     = '';
+    $Lang->{'Queue for the approval of FAQ articles.'}                     = '';
+    $Lang->{'Ticket subject for approval of FAQ article.'}                 = '';
+    $Lang->{'Ticket body for approval of FAQ article.'}                    = '';
+    $Lang->{'Default priority of tickets for the approval of FAQ articles.'}
+        = '';
+    $Lang->{'Default state of tickets for the approval of FAQ articles.'}  = '';
+    $Lang->{'Definition of FAQ item free text field.'}                     = '';
+    $Lang->{'This setting defines that a \'FAQ\' object can be linked with other \'FAQ\' objects using the \'Normal\' link type.'}
+        = '';
+    $Lang->{'This setting defines that a \'FAQ\' object can be linked with other \'FAQ\' objects using the \'ParentChild\' link type.'}
+        = '';
+    $Lang->{'This setting defines that a \'FAQ\' object can be linked with other \'Ticket\' objects using the \'Normal\' link type.'}
+        = '';
+    $Lang->{'This setting defines that a \'FAQ\' object can be linked with other \'Ticket\' objects using the \'ParentChild\' link type.'}
+        = '';
+    $Lang->{'Frontend module registration for the agent interface.'}    = '';
+    $Lang->{'Frontend module registration for the customer interface.'} = '';
+    $Lang->{'Frontend module registration for the public interface.'}   = '';
+    $Lang->{'Default value for the Action parameter for the public frontend. The Action parameter is used in the scripts of the system.'}
+        = '';
+    $Lang->{'Show FAQ Article with HTML.'}                              = '';
+    $Lang->{'Module to generate html OpenSearch profile for short faq search.'}
+        = '';
+    $Lang->{'Defines where the \'Insert FAQ\' link will be displayed.'} = '';
+    $Lang->{'A filter for HTML output to add links behind a defined string. The element Image allows two input kinds. First the name of an image (e.g. faq.png). In this case the OTRS image path will be used. The second possibility is to insert the link to the image.'}
+        = '';
+    $Lang->{'FAQ search backend router of the agent interface.'} = '';
+    $Lang->{'Defines an overview module to show the small view of a FAQ list.'}
+        = '';
+    $Lang->{'Defines the shown columns in the FAQ search. This option has no effect on the position of the column.'}
+        = '';
+    $Lang->{'Defines the default FAQ attribute for FAQ sorting in a FAQ search of the agent interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ order of a search result in the agent interface. Up: oldest on top. Down: latest on top.'}
+        = '';
+    $Lang->{'Maximum number of FAQ articles to be displayed in the result of a search in the agent interface.'}
+        = '';
+    $Lang->{'Defines the shown columns in the FAQ Explorer. This option has no effect on the position of the column.'}
+        = '';
+    $Lang->{'Defines the default FAQ attribute for FAQ sorting in a FAQ Explorer of the agent interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ order of a Explorer result in the agent interface. Up: oldest on top. Down: latest on top.'}
+        = '';
+    $Lang->{'Maximum number of FAQ articles to be displayed in the result of a Explorer in the agent interface.'}
+        = '';
+    $Lang->{'Parameters for the pages (in which the FAQ items are shown) of the small FAQ overview.'}
+        = '';
+    $Lang->{'Shows a link in the menu to go back in the FAQ zoom view of the agent interface.'}
+        = '';
+    $Lang->{'Shows a link in the menu to edit a FAQ in the its zoom view of the agent interface.'}
+        = '';
+    $Lang->{'Shows a link in the menu to access the history of a FAQ in its zoom view of the agent interface'}
+        = '';
+    $Lang->{'Shows a link in the menu to print a FAQ in the its zoom view of the agent interface.'}
+        = '';
+    $Lang->{'Shows a link in the menu that allows linking a FAQ with another object in the zoom view of such FAQ of the agent interface.'}
+        = '';
+    $Lang->{'Shows a link in the menu that allows to delete a FAQ in its zoom view in the agent interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ attribute for FAQ sorting in a FAQ search of the customer interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ order of a search result in the customer interface. Up: oldest on top. Down: latest on top.'}
+        = '';
+    $Lang->{'Maximum number of FAQ articles to be displayed in the result of a search in the customer interface.'}
+        = '';
+    $Lang->{'Number of FAQ articles to be displayed in each page of a search result in the customer interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ attribute for FAQ sorting in a FAQ search of the public interface.'}
+        = '';
+    $Lang->{'Defines the default FAQ order of a search result in the public interface. Up: oldest on top. Down: latest on top.'}
+        = '';
+    $Lang->{'Maximum number of FAQ articles to be displayed in the result of a search in the public interface.'}
+        = '';
+    $Lang->{'Number of FAQ articles to be displayed in each page of a search result in the public interface.'}
+        = '';
+    $Lang->{'Defines the shown columns in the FAQ journal. This option has no effect on the position of the column.'}
+        = '';
+    $Lang->{'Maximum number of FAQ articles to be displayed in the FAQ journal in the agent interface.'}
+        = '';
+    $Lang->{'Parameters for the pages (in which the FAQ items are shown) of the small FAQ journal overview.'}
+        = '';
+    $Lang->{'Defines an overview module to show the small view of a FAQ journal.'}
+        = '';
 
     return 1;
 }
