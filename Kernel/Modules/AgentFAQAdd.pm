377c8f138bbd7f3288664827604ae39104178c02
Udo Bretz <udo.bretz@otrs.com>
2010-12-01 10:04:29 +0000

    Changed FAQ Add screen to look more like AgentTicketPhone.

--- Kernel/Modules/AgentFAQAdd.pm
+++ Kernel/Modules/AgentFAQAdd.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQAdd.pm - agent frontend to add faq articles
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQAdd.pm,v 1.13 2010-11-26 11:39:54 ub Exp $
+# $Id: AgentFAQAdd.pm,v 1.14 2010-12-01 10:04:07 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::Web::UploadCache;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.13 $) [1];
+$VERSION = qw($Revision: 1.14 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -297,10 +297,6 @@ sub Run {
 
 sub _MaskNew {
     my ( $Self, %Param ) = @_;
-
-    # show action list
-    $Self->{LayoutObject}->Block( Name => 'ActionList' );
-    $Self->{LayoutObject}->Block( Name => 'ActionOverview' );
 
     # get valid list
     my %ValidList        = $Self->{ValidObject}->ValidList();
