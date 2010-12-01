d88a1d257b3a4d20974f0499ac3ad04d6d198373
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2010-12-01 20:25:54 +0000

    Change PrintTag to made the correct translation

--- Kernel/Modules/AgentFAQPrint.pm
+++ Kernel/Modules/AgentFAQPrint.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentFAQPrint.pm - print layout for agent interface
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQPrint.pm,v 1.8 2010-12-01 03:17:09 cr Exp $
+# $Id: AgentFAQPrint.pm,v 1.9 2010-12-01 20:25:54 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::PDF;
 use Kernel::System::FAQ;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.8 $) [1];
+$VERSION = qw($Revision: 1.9 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -212,11 +212,7 @@ sub Run {
         $Page{PageCount}++;
 
         # type of print tag
-        my $PrintTag = '';
-
-        $PrintTag = 'FAQ Article';
-        $PrintTag = ( $Self->{LayoutObject}->{LanguageObject}->Get($PrintTag) ) . ' ' .
-            ( $Self->{LayoutObject}->{LanguageObject}->Get('Print') );
+        my $PrintTag = $Self->{LayoutObject}->{LanguageObject}->Get('FAQ Article Print');
 
         # output headline
         $Self->{PDFObject}->Text(
