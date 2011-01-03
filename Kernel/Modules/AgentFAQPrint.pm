0e1eac5f87f307b190fc859339dad271182ae688
Udo Bretz <udo.bretz@otrs.com>
2011-01-03 10:37:30 +0000

    Removed not needed parentheses after built-in functions like push.

--- Kernel/Modules/AgentFAQPrint.pm
+++ Kernel/Modules/AgentFAQPrint.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/AgentFAQPrint.pm - print layout for agent interface
-# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentFAQPrint.pm,v 1.11 2010-12-27 16:31:53 cr Exp $
+# $Id: AgentFAQPrint.pm,v 1.12 2011-01-03 10:37:30 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -20,7 +20,7 @@ use Kernel::System::PDF;
 use Kernel::System::FAQ;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.11 $) [1];
+$VERSION = qw($Revision: 1.12 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -377,7 +377,7 @@ sub _PDFOutputFAQHeaderInfo {
             Key   => $Self->{LayoutObject}->{LanguageObject}->Get('Language') . ':',
             Value => $Self->{LayoutObject}->{LanguageObject}->Get( $FAQData{Language} ),
         };
-        push( @{$TableLeft}, $Row );
+        push @{$TableLeft}, $Row;
     }
 
     # approval state row, feature is enabled
@@ -387,7 +387,7 @@ sub _PDFOutputFAQHeaderInfo {
             Key   => $Self->{LayoutObject}->{LanguageObject}->Get('Approval') . ':',
             Value => $Self->{LayoutObject}->{LanguageObject}->Get( $FAQData{Approval} ),
         };
-        push( @{$TableLeft}, $Row );
+        push @{$TableLeft}, $Row;
     }
 
     # create right table
