83d87fa048a678e6998a824113fb52f3118771d6
Udo Bretz <udo.bretz@otrs.com>
2011-01-03 10:48:03 +0000

    Removed not needed parentheses after built-in functions like push.

--- Kernel/Modules/CustomerFAQPrint.pm
+++ Kernel/Modules/CustomerFAQPrint.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/CustomerFAQPrint.pm - print layout for agent interface
-# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: CustomerFAQPrint.pm,v 1.12 2010-12-27 16:42:10 cr Exp $
+# $Id: CustomerFAQPrint.pm,v 1.13 2011-01-03 10:44:26 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::PDF;
 use Kernel::System::FAQ;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.12 $) [1];
+$VERSION = qw($Revision: 1.13 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -302,7 +302,7 @@ sub _PDFOutputFAQHeaderInfo {
             Key   => $Self->{LayoutObject}->{LanguageObject}->Get('Language') . ':',
             Value => $Self->{LayoutObject}->{LanguageObject}->Get( $FAQData{Language} ),
         };
-        push( @{$TableLeft}, $Row );
+        push @{$TableLeft}, $Row;
     }
 
     # create right table
--- Kernel/Modules/PublicFAQPrint.pm
+++ Kernel/Modules/PublicFAQPrint.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/PublicFAQPrint.pm - print layout for agent interface
-# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: PublicFAQPrint.pm,v 1.7 2010-12-27 16:47:47 cr Exp $
+# $Id: PublicFAQPrint.pm,v 1.8 2011-01-03 10:48:03 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::PDF;
 use Kernel::System::FAQ;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.7 $) [1];
+$VERSION = qw($Revision: 1.8 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -283,7 +283,7 @@ sub _PDFOutputFAQHeaderInfo {
             Key   => $Self->{LayoutObject}->{LanguageObject}->Get('Language') . ':',
             Value => $Self->{LayoutObject}->{LanguageObject}->Get( $FAQData{Language} ),
         };
-        push( @{$TableLeft}, $Row );
+        push @{$TableLeft}, $Row;
     }
 
     # create right table
