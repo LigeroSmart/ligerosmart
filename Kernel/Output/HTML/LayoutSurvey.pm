ce11b2778c982dca480983cb0d87f1a70e510464
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2011-01-07 13:17:00 +0000

    Initial Version

---
+++ Kernel/Output/HTML/LayoutSurvey.pm
@@ -0,0 +1,337 @@
+# --
+# Kernel/Output/HTML/LayoutSurvey.pm - provides generic HTML output for Survey
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: LayoutSurvey.pm,v 1.1 2011-01-07 13:17:00 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::LayoutSurvey;
+
+use strict;
+use warnings;
+
+use Kernel::Output::HTML::Layout;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+=over 4
+
+=item SurveyListShow()
+
+Returns a list of surveys list with pagination.
+
+This function is similar to L<Kernel::Output::HTML::LayoutTicket::TicketListShow()>
+in F<Kernel/Output/HTML/LayoutTicket.pm>.
+
+    my $Output = $LayoutObject->SurveyListShow(
+        SurveyIDs  => $SurveyIDsRef,                      # total list of surveys ids, that can be listed
+        Total      => scalar @{ $SurveyIDsRef },          # total number of list items, changes in this case
+        View       => $Self->{View},                      # optional, the default value is 'Small'
+        Filter     => 'All',
+        Filters    => \%NavBarFilter,
+        FilterLink => $LinkFilter,
+        TitleName  => 'Overview: Survey',
+        TitleValue => $Self->{Filter},
+        Env        => $Self,
+        LinkPage   => $LinkPage,
+    );
+
+=cut
+
+sub SurveyListShow {
+    my ( $Self, %Param ) = @_;
+
+    # take object ref to local, remove it from %Param (prevent memory leak)
+    my $Env = delete $Param{Env};
+
+    # lookup latest used view mode
+    if ( !$Param{View} && $Self->{ 'UserSurveyOverview' . $Env->{Action} } ) {
+        $Param{View} = $Self->{ 'UserSurveyOverview' . $Env->{Action} };
+    }
+
+    # set frontend
+    my $Frontend = $Param{Frontend} || 'Agent';
+
+    # set defaut view mode to 'small'
+    my $View = $Param{View} || 'Small';
+
+    # store latest view mode
+    $Self->{SessionObject}->UpdateSessionID(
+        SessionID => $Self->{SessionID},
+        Key       => 'UserSurveyOverview' . $Env->{Action},
+        Value     => $View,
+    );
+
+    # get backend from config
+    my $Backends = $Self->{ConfigObject}->Get('Survey::Frontend::Overview');
+    if ( !$Backends ) {
+        return $Env->{LayoutObject}->FatalError(
+            Message => 'Need config option Survey::Frontend::Overview',
+        );
+    }
+
+    # check for hash-ref
+    if ( ref $Backends ne 'HASH' ) {
+        return $Env->{LayoutObject}->FatalError(
+            Message => 'Config option Survey::Frontend::Overview needs to be a HASH ref!',
+        );
+    }
+
+    # check for config key
+    if ( !$Backends->{$View} ) {
+        return $Env->{LayoutObject}->FatalError(
+            Message => "No config option found for the view '$View'!",
+        );
+    }
+
+    # nav bar
+    my $StartHit = $Self->{ParamObject}->GetParam(
+        Param => 'StartHit',
+    ) || 1;
+
+    # get personal page shown count
+    my $PageShownPreferencesKey = 'UserSurveyOverview' . $View . 'PageShown';
+    my $PageShown               = $Self->{$PageShownPreferencesKey} || 10;
+    my $Group                   = 'SurveyOverview' . $View . 'PageShown';
+
+    # check start option, if higher then elements available, set
+    # it to the last overview page (Thanks to Stefan Schmidt!)
+    if ( $StartHit > $Param{Total} ) {
+        my $Pages = int( ( $Param{Total} / $PageShown ) + 0.99999 );
+        $StartHit = ( ( $Pages - 1 ) * $PageShown ) + 1;
+    }
+
+    # get data selection
+    my %Data;
+    my $Config = $Self->{ConfigObject}->Get('PreferencesGroups');
+    if ( $Config && $Config->{$Group} && $Config->{$Group}->{Data} ) {
+        %Data = %{ $Config->{$Group}->{Data} };
+    }
+
+    # set page limit and build page nav
+    my $Limit = $Param{Limit} || 20_000;
+    my %PageNav = $Env->{LayoutObject}->PageNavBar(
+        Limit     => $Limit,
+        StartHit  => $StartHit,
+        PageShown => $PageShown,
+        AllHits   => $Param{Total} || 0,
+        Action    => 'Action=' . $Env->{LayoutObject}->{Action},
+        Link      => $Param{LinkPage},
+    );
+
+    # build shown surveys per page
+    $Param{RequestedURL}    = "Action=$Self->{Action}";
+    $Param{Group}           = $Group;
+    $Param{PreferencesKey}  = $PageShownPreferencesKey;
+    $Param{PageShownString} = $Self->BuildSelection(
+        Name       => $PageShownPreferencesKey,
+        SelectedID => $PageShown,
+        Data       => \%Data,
+    );
+
+    # build navbar content
+    $Env->{LayoutObject}->Block(
+        Name => 'OverviewNavBar',
+        Data => \%Param,
+    );
+
+    # back link
+    if ( $Param{LinkBack} ) {
+        $Env->{LayoutObject}->Block(
+            Name => 'OverviewNavBarPageBack',
+            Data => \%Param,
+        );
+    }
+
+    # get filters
+    if ( $Param{Filters} ) {
+
+        # get given filters
+        my @NavBarFilters;
+        for my $Prio ( sort keys %{ $Param{Filters} } ) {
+            push @NavBarFilters, $Param{Filters}->{$Prio};
+        }
+
+        # build filter content
+        $Env->{LayoutObject}->Block(
+            Name => 'OverviewNavBarFilter',
+            Data => {
+                %Param,
+            },
+        );
+
+        # loop over filters
+        my $Count = 0;
+        for my $Filter (@NavBarFilters) {
+
+            # increment filter count and build filter item
+            $Count++;
+            $Env->{LayoutObject}->Block(
+                Name => 'OverviewNavBarFilterItem',
+                Data => {
+                    %Param,
+                    %{$Filter},
+                },
+            );
+
+            # filter is selected
+            if ( $Filter->{Filter} eq $Param{Filter} ) {
+                $Env->{LayoutObject}->Block(
+                    Name => 'OverviewNavBarFilterItemSelected',
+                    Data => {
+                        %Param,
+                        %{$Filter},
+                    },
+                );
+
+            }
+            else {
+                $Env->{LayoutObject}->Block(
+                    Name => 'OverviewNavBarFilterItemSelectedNot',
+                    Data => {
+                        %Param,
+                        %{$Filter},
+                    },
+                );
+
+            }
+        }
+    }
+
+    # loop over configured backends
+    for my $Backend ( keys %{$Backends} ) {
+
+        # build navbar view mode
+        $Env->{LayoutObject}->Block(
+            Name => 'OverviewNavBarViewMode',
+            Data => {
+                %Param,
+                %{ $Backends->{$Backend} },
+                Filter => $Param{Filter},
+                View   => $Backend,
+            },
+        );
+
+        # current view is configured in backend
+        if ( $View eq $Backend ) {
+            $Env->{LayoutObject}->Block(
+                Name => 'OverviewNavBarViewModeSelected',
+                Data => {
+                    %Param,
+                    %{ $Backends->{$Backend} },
+                    Filter => $Param{Filter},
+                    View   => $Backend,
+                },
+            );
+        }
+        else {
+            $Env->{LayoutObject}->Block(
+                Name => 'OverviewNavBarViewModeNotSelected',
+                Data => {
+                    %Param,
+                    %{ $Backends->{$Backend} },
+                    Filter => $Param{Filter},
+                    View   => $Backend,
+                },
+            );
+        }
+    }
+
+    # check if page nav is available
+    if (%PageNav) {
+        $Env->{LayoutObject}->Block(
+            Name => 'OverviewNavBarPageNavBar',
+            Data => \%PageNav,
+        );
+
+        # don't show context settings in AJAX case (e. g. in customer ticket history),
+        #   because the submit with page reload will not work there
+        if ( !$Param{AJAX} ) {
+            $Env->{LayoutObject}->Block(
+                Name => 'ContextSettings',
+                Data => {
+                    %PageNav,
+                    %Param,
+                },
+            );
+        }
+    }
+
+    # build html content
+    my $OutputNavBar = $Env->{LayoutObject}->Output(
+        TemplateFile => 'AgentSurveyOverviewNavBar',
+        Data         => {%Param},
+    );
+
+    # create output
+    my $OutputRaw = '';
+    if ( !$Param{Output} ) {
+        $Env->{LayoutObject}->Print(
+            Output => \$OutputNavBar,
+        );
+    }
+    else {
+        $OutputRaw .= $OutputNavBar;
+    }
+
+    # load module
+    if ( !$Self->{MainObject}->Require( $Backends->{$View}->{Module} ) ) {
+        return $Env->{LayoutObject}->FatalError();
+    }
+
+    # check for backend object
+    my $Object = $Backends->{$View}->{Module}->new( %{$Env} );
+    return if !$Object;
+
+    # run module
+    my $Output = $Object->Run(
+        %Param,
+        Limit     => $Limit,
+        StartHit  => $StartHit,
+        PageShown => $PageShown,
+        AllHits   => $Param{Total} || 0,
+        Frontend  => $Frontend,
+    );
+
+    # create output
+    if ( !$Param{Output} ) {
+        $Env->{LayoutObject}->Print(
+            Output => \$Output,
+        );
+    }
+    else {
+        $OutputRaw .= $Output;
+    }
+
+    # create overview nav bar
+    $Env->{LayoutObject}->Block(
+        Name => 'OverviewNavBar',
+        Data => {%Param},
+    );
+
+    # return content if available
+    return $OutputRaw;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2011-01-07 13:17:00 $
+
+=cut
---
+++ Kernel/Output/HTML/SurveyOverviewSmall.pm
@@ -0,0 +1,202 @@
+# --
+# Kernel/Output/HTML/SurveyOverviewSmall.pm.pm
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: SurveyOverviewSmall.pm,v 1.1 2011-01-07 13:17:00 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::SurveyOverviewSmall;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for my $Object (
+        qw(ConfigObject LogObject DBObject LayoutObject UserID UserObject MainObject)
+        )
+    {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Needed (qw(PageShown StartHit)) {
+        if ( !$Param{$Needed} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!",
+            );
+            return;
+        }
+    }
+
+    # need SurveyIDs
+    if ( !$Param{SurveyIDs} ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => 'Need the SurveyIDs!',
+        );
+        return;
+    }
+
+    # store the SurveyIDs locally
+    my @IDs = @{ $Param{SurveyIDs} };
+
+    # check ShowColumns parameter
+    my @ShowColumns;
+    if ( $Param{ShowColumns} && ref $Param{ShowColumns} eq 'ARRAY' ) {
+        @ShowColumns = @{ $Param{ShowColumns} };
+    }
+
+    my $Output = '';
+
+    # show surveys if there are some
+    if (@IDs) {
+
+        # build column header blocks
+        # currently sorting is not implemented on survey, leave the code for future implementation
+        if (@ShowColumns) {
+            for my $Column (@ShowColumns) {
+
+                # create needed variables
+                my $CSS = '';
+                my $OrderBy;
+
+                # set the correct Set CSS class and order by link
+                if ( $Param{SortBy} && ( $Param{SortBy} eq $Column ) ) {
+                    if ( $Param{OrderBy} && ( $Param{OrderBy} eq 'Up' ) ) {
+                        $OrderBy = 'Down';
+                        $CSS .= ' SortDescending';
+                    }
+                    else {
+                        $OrderBy = 'Up';
+                        $CSS .= ' SortAscending';
+                    }
+                }
+                else {
+                    $OrderBy = 'Up';
+                }
+
+                # output each header
+                $Self->{LayoutObject}->Block(
+                    Name => 'Record' . $Column . 'Header',
+                    Data => {
+                        %Param,
+                        CSS     => $CSS,
+                        OrderBy => $OrderBy,
+                    },
+                );
+            }
+        }
+
+        my $Counter = 0;
+
+        ID:
+        for my $ID (@IDs) {
+            $Counter++;
+            if (
+                $Counter >= $Param{StartHit}
+                && $Counter < ( $Param{PageShown} + $Param{StartHit} )
+                )
+            {
+
+                # get survey data
+                my %Data = $Self->{SurveyObject}->SurveyGet(
+                    UserID   => $Self->{UserID},
+                    SurveyID => $ID,
+                );
+
+                next ID if !%Data;
+
+                # get user data for needed user types
+                USERTYPE:
+                for my $UserType (qw(Create Change)) {
+                    $Data{ $UserType . 'LeftParenthesis' }  = '(';
+                    $Data{ $UserType . 'RightParenthesis' } = ')';
+                }
+
+                # build record block
+                $Self->{LayoutObject}->Block(
+                    Name => 'Record',
+                    Data => {
+                        %Param,
+                        %Data,
+                    },
+                );
+
+                # build column record blocks
+                if (@ShowColumns) {
+                    COLUMN:
+                    for my $Column (@ShowColumns) {
+                        $Self->{LayoutObject}->Block(
+                            Name => 'Record' . $Column,
+                            Data => {
+                                %Param,
+                                %Data,
+                            },
+                        );
+
+                        # show links if available
+                        $Self->{LayoutObject}->Block(
+                            Name => 'Record' . $Column . 'LinkStart',
+                            Data => {
+                                %Param,
+                                %Data,
+                            },
+                        );
+                        $Self->{LayoutObject}->Block(
+                            Name => 'Record' . $Column . 'LinkEnd',
+                            Data => {
+                                %Param,
+                                %Data,
+                            },
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    # if there are no surveys to show, a no data found message is displayed in the table
+    else {
+        $Self->{LayoutObject}->Block(
+            Name => 'NoDataFoundMsg',
+            Data => {
+                TotalColumns => scalar @ShowColumns,
+            },
+        );
+    }
+
+    # use template
+    $Output .= $Self->{LayoutObject}->Output(
+        TemplateFile => 'AgentSurveyOverviewSmall',
+        Data         => {
+            %Param,
+            Type        => $Self->{ViewType},
+            ColumnCount => scalar @ShowColumns,
+        },
+    );
+
+    return $Output;
+}
+
+1;
