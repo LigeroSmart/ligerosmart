a46c025332972f41ed59642e481985c5c2849fa0
Daniel Zamorano <daniel.zamorano@otrs.com>
2011-01-11 20:01:09 +0000

    Porting Survey Public Interface to OTRS 3.0.

--- Kernel/Modules/PublicSurvey.pm
+++ Kernel/Modules/PublicSurvey.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/PublicSurvey.pm - a survey module
-# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: PublicSurvey.pm,v 1.20 2010-12-29 17:11:59 dz Exp $
+# $Id: PublicSurvey.pm,v 1.21 2011-01-11 20:01:09 dz Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::Survey;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.20 $) [1];
+$VERSION = qw($Revision: 1.21 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -107,7 +107,15 @@ sub Run {
         $Output = $Self->{LayoutObject}->CustomerHeader( Title => 'Survey' );
 
         # print the main table.
-        $Self->{LayoutObject}->Block( Name => 'PublicSurveyComplete' );
+        $Self->{LayoutObject}->Block(
+            Name => 'PublicSurveyMessage',
+            Data => {
+                MessageType   => 'Information',
+                MessageHeader => 'Thank you for your feedback.',
+                Message       => 'The survey is finished.',
+            },
+        );
+
         $Output .= $Self->{LayoutObject}->Output(
             TemplateFile => 'PublicSurvey',
             Data         => {%Param},
@@ -138,9 +146,11 @@ sub Run {
 
     if ($UsedSurveyKey) {
         $Self->{LayoutObject}->Block(
-            Name => 'PublicSurveyError',
+            Name => 'PublicSurveyMessage',
             Data => {
-                Message => 'You have already answered the survey, Thank you for your feedbak!.',
+                MessageType   => 'Information',
+                MessageHeader => 'Thank you for your feedback.',
+                Message       => 'You have already answered the survey.',
             },
         );
     }
@@ -198,9 +208,12 @@ sub Run {
     }
     else {
         $Self->{LayoutObject}->Block(
-            Name => 'PublicSurveyError',
+            Name => 'PublicSurveyMessage',
             Data => {
-                Message => 'Invalid survey key.',
+                MessageType   => 'Error!',
+                MessageHeader => 'Invalid survey key.',
+                Message =>
+                    'The inserted survey key is invalid, if you followed a link maybe this is obsolete or broken.',
             },
         );
     }
@@ -209,6 +222,116 @@ sub Run {
         Data         => {%Param},
     );
     $Output .= $Self->{LayoutObject}->CustomerFooter();
+    return $Output;
+}
+
+sub _MaskShowSurvey {
+    my ( $Self, %Param ) = @_;
+
+    my %ServerError;
+    if ( $Param{ServerError} ) {
+        %ServerError = %{ $Param{ServerError} };
+    }
+
+    my %FormElements;
+    if ( $Param{FormElements} ) {
+        %FormElements = %{ $Param{FormElements} };
+    }
+
+    my $Output;
+
+    # if SurveyID the SurveyEdit should be loaded (popup)
+    my $Title;
+    my $Type;
+    if ( $Param{SurveyID} ) {
+        $Title = 'Survey Edit';
+
+        # for header and footer
+        $Type = 'Small';
+    }
+    else {
+        $Title = 'Add New Survey';
+    }
+
+    $Output .= $Self->{LayoutObject}->Header(
+        Title => $Title,
+        Type  => $Type,
+    );
+
+    my $SelectedQueues;
+    if ( !$Param{SurveyID} ) {
+        $Output .= $Self->{LayoutObject}->NavigationBar();
+
+    }
+    else {
+        my %Survey = $Self->{SurveyObject}->SurveyGet( SurveyID => $Param{SurveyID} );
+
+        # get selected queues
+        $SelectedQueues = $Survey{Queues};
+    }
+
+    my %Queues      = $Self->{QueueObject}->GetAllQueues();
+    my $QueueString = $Self->{LayoutObject}->BuildSelection(
+        Data         => \%Queues,
+        Name         => 'Queues',
+        Size         => 6,
+        Multiple     => 1,
+        PossibleNone => 0,
+        Sort         => 'AlphanumericValue',
+        Translation  => 0,
+        SelectedID   => $FormElements{Queues} || $SelectedQueues,
+    );
+
+    my $Block;
+    if ( !$Param{SurveyID} ) {
+        $Block = 'SurveyAdd';
+    }
+    else {
+        $Block = 'SurveyEdit';
+    }
+
+    # print the form
+    $Self->{LayoutObject}->Block(
+        Name => $Block,
+        Data => {
+            %Param,
+            QueueString        => $QueueString,
+            NotificationSender => $FormElements{NotificationSender}
+                || $Self->{ConfigObject}->Get('Survey::NotificationSender'),
+            NotificationSubject => $FormElements{NotificationSubject}
+                || $Self->{ConfigObject}->Get('Survey::NotificationSubject'),
+            NotificationBody => $FormElements{NotificationBody}
+                || $Self->{ConfigObject}->Get('Survey::NotificationBody'),
+            %ServerError,
+            %FormElements,
+        },
+    );
+
+    # generates generic errors for javascript
+    for my $NeededItem (
+        qw( Title Introduction Description NotificationSender NotificationSubject NotificationBody )
+        )
+    {
+        $Self->{LayoutObject}->Block(
+            Name => $Block . 'GenericError',
+            Data => {
+                ItemName => $NeededItem . 'Error',
+            },
+        );
+    }
+
+    for my $Item ( keys %ServerError ) {
+        $Self->{LayoutObject}->Block(
+            Name => $Block . 'GenericServerError',
+            Data => {
+                ItemName => $Item,
+            },
+        );
+    }
+
+    $Output .= $Self->{LayoutObject}->Output( TemplateFile => 'AgentSurvey' );
+    $Output .= $Self->{LayoutObject}->Footer( Type => $Type );
+
     return $Output;
 }
 
