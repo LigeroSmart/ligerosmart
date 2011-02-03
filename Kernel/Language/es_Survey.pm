7f8d3eb35940feb120b2794f3e1a9e4ae6f65241
Daniel Zamorano <daniel.zamorano@otrs.com>
2011-02-03 23:19:38 +0000

    Improved Spanish translation.

--- Kernel/Language/es_Survey.pm
+++ Kernel/Language/es_Survey.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/es_Survey.pm - translation file
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: es_Survey.pm,v 1.1 2011-01-20 17:20:21 dz Exp $
+# $Id: es_Survey.pm,v 1.2 2011-02-03 23:19:38 dz Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -42,7 +42,7 @@ sub Data {
     $Self->{Translation}->{'go back to questions'} = 'regresar a preguntas';
     $Self->{Translation}->{'Possible Answers For'} = 'Posibles respuestas para';
     $Self->{Translation}->{'Add Answer'} = 'Agregar Respuesta';
-    $Self->{Translation}->{'This doesn\'t have several answers, a textarea will be displayed.'} = 
+    $Self->{Translation}->{'This doesn\'t have several answers, a textarea will be displayed.'} =
         'Esta pregunta no tiene varias respuestas, un area de texto será mostrada';
     $Self->{Translation}->{'Edit Answer'} = 'Editar Respuesta';
     $Self->{Translation}->{'go back to edit question'} = 'volver a editar pregunta';
@@ -78,34 +78,36 @@ sub Data {
     # SysConfig
     $Self->{Translation}->{'A Survey Module.'} = 'Un Módulo de Encuestas';
     $Self->{Translation}->{'A module to edit survey questions.'} = 'Un módulo para editar las preguntas de una encuesta';
-    $Self->{Translation}->{'Days starting from the latest customer survey email between no customer survey email is sent, ( 0 means Always send it ) .'} = 
+    $Self->{Translation}->{'Days starting from the latest customer survey email between no customer survey email is sent, ( 0 means Always send it ) .'} =
         'Dias comenzando desde la última en cuesta enviada al cliente ( 0 significa enviala siempre ) .';
-    $Self->{Translation}->{'Default body for the notification email to customers about new survey.'} = 
+    $Self->{Translation}->{'Default body for the notification email to customers about new survey.'} =
         'Cuerpo default para el email de notificación a los clientes sobre la encuesta.';
-    $Self->{Translation}->{'Default sender for the notification email to customers about new survey.'} = 
+    $Self->{Translation}->{'Default sender for the notification email to customers about new survey.'} =
         'Remitente default para el email de notificación a los clientes sobre la encuesta. ';
-    $Self->{Translation}->{'Default subject for the notification email to customers about new survey.'} = 
+    $Self->{Translation}->{'Default subject for the notification email to customers about new survey.'} =
         'Asunto default para el email de notificación a los clientes sobre la encuesta.';
-    $Self->{Translation}->{'Defines an overview module to show the small view of a survey list.'} = 
+    $Self->{Translation}->{'Defines an overview module to show the small view of a survey list.'} =
         'Define una módulo resumen para mostrar la vista pequeña de la lista de encuestas.';
-    $Self->{Translation}->{'Defines the shown columns in the survey overview. This option has no effect on the position of the column.'} = 
+    $Self->{Translation}->{'Defines the shown columns in the survey overview. This option has no effect on the position of the column.'} =
         'Define las columnas mostradas en la vista \'Resumen de Estadisticas\'';
-    $Self->{Translation}->{'Frontend module registration for survey zoom in the agent interface.'} = 
+    $Self->{Translation}->{'Frontend module registration for survey zoom in the agent interface.'} =
         'Registro de módulo frontend SurveyZoom en la interface del agente.';
-    $Self->{Translation}->{'Frontend module registration for the PublicSurvey object in the public Survey area.'} = 
+    $Self->{Translation}->{'Frontend module registration for the PublicSurvey object in the public Survey area.'} =
         'Registro de módulo frontend PublicSurvey en la interface pública.';
-    $Self->{Translation}->{'If this regex matches, no customer survey will be sent.'} = '';
-    $Self->{Translation}->{'Parameters for the pages (in which the surveys are shown) of the small survey overview.'} = '';
-    $Self->{Translation}->{'Public Survey.'} = '';
-    $Self->{Translation}->{'Survey Overview "Small" Limit'} = '';
-    $Self->{Translation}->{'Survey Zoom Module.'} = '';
-    $Self->{Translation}->{'Survey limit per page for Survey Overview "Small"'} = '';
-    $Self->{Translation}->{'The identifier for a survey, e.g. Survey#, MySurvey#. The default is Survey#.'} = '';
-    $Self->{Translation}->{'Ticket event module to send automatically survey email requests to customers if a ticket gets closed.'} = '';
-
-    #
-    # OBSOLETE ENTRIES FOR REFERENCE, DO NOT TRANSLATE!
-    #
+    $Self->{Translation}->{'If this regex matches, no customer survey will be sent.'} =
+        'Si esta expresión regular concuerda, la encuesta no será enviada';
+    $Self->{Translation}->{'Parameters for the pages (in which the surveys are shown) of the small survey overview.'} =
+        'Parametros para las páginas (en que las encuestas son mostradas) de la vista de resumen pequeña.';
+    $Self->{Translation}->{'Public Survey.'} =
+        'Encuesta Pública';
+    $Self->{Translation}->{'Survey Overview "Small" Limit'} = 'Limite de la vista de resumen pequeña';
+    $Self->{Translation}->{'Survey Zoom Module.'} = 'Modulo de Detalle de Encuesta';
+    $Self->{Translation}->{'Survey limit per page for Survey Overview "Small"'} =
+        'Limite de encuestas por página para la vista de resumen pequeña';
+    $Self->{Translation}->{'The identifier for a survey, e.g. Survey#, MySurvey#. The default is Survey#.'} =
+        'El identificador para una encuesta, ejemplo Survey#, MySurvey#. Por defecto es Survey#.';
+    $Self->{Translation}->{'Ticket event module to send automatically survey email requests to customers if a ticket gets closed.'} =
+        'Modulo de evento de Ticket para enviar automaticamente correos de solicitudes de encuesta a clientes si el ticket se ha cerrado';
 
 }
 
