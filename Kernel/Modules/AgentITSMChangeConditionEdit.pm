ab47df5dcd32d551b6c4c3f273aa6ba1921d42bc
Udo Bretz <udo.bretz@otrs.com>
2011-03-17 17:45:10 +0000

    Fixed bug# 7067 - Condition Edit screen does not store the number zero 0 for value fields.

--- Kernel/Modules/AgentITSMChangeConditionEdit.pm
+++ Kernel/Modules/AgentITSMChangeConditionEdit.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentITSMChangeConditionEdit.pm - the OTRS::ITSM::ChangeManagement condition edit module
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentITSMChangeConditionEdit.pm,v 1.43 2011-03-15 14:11:33 ub Exp $
+# $Id: AgentITSMChangeConditionEdit.pm,v 1.44 2011-03-17 17:45:10 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::ITSMChange::ITSMCondition;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.43 $) [1];
+$VERSION = qw($Revision: 1.44 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -223,8 +223,10 @@ sub Run {
                         AttributeID  => $ExpressionData{AttributeID},
                         OperatorID   => $ExpressionData{OperatorID},
                         Selector     => $ExpressionData{Selector},
-                        CompareValue => $ExpressionData{CompareValue} || '',
-                        UserID       => $Self->{UserID},
+                        CompareValue => defined $ExpressionData{CompareValue}
+                        ? $ExpressionData{CompareValue}
+                        : '',
+                        UserID => $Self->{UserID},
                     );
 
                     # check error
@@ -268,8 +270,10 @@ sub Run {
                     AttributeID  => $ExpressionData{AttributeID},
                     OperatorID   => $ExpressionData{OperatorID},
                     Selector     => $ExpressionData{Selector},
-                    CompareValue => $ExpressionData{CompareValue} || '',
-                    UserID       => $Self->{UserID},
+                    CompareValue => defined $ExpressionData{CompareValue}
+                    ? $ExpressionData{CompareValue}
+                    : '',
+                    UserID => $Self->{UserID},
                 );
 
                 # check error
