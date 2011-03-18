1b737b58110fd76aae7742d4b52a30bc8a87528e
Michiel Beijen <michiel.beijen@otrs.com>
2011-03-18 12:46:26 +0000

    Fixed bug#7076 - en_GB and en_CA translations in FAQ are missing

---
+++ Kernel/Language/en_CA_FAQ.pm
@@ -0,0 +1,34 @@
+# --
+# Kernel/Language/en_CA_FAQ.pm - translation file for Canadian English
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: en_CA_FAQ.pm,v 1.1 2011-03-18 12:44:39 mb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::en_CA_FAQ;
+
+use strict;
+
+sub Data {
+    my $Self = shift;
+
+    # Tags that are substituted:
+    $Self->{Translation}->{'LatestChangedItems'} = 'Latest updated FAQ articles';
+    $Self->{Translation}->{'LatestCreatedItems'} = 'Latest created FAQ articles';
+    $Self->{Translation}->{'Top10Items'} = 'Top 10 FAQ articles';
+    $Self->{Translation}->{'SubCategoryOf'} = 'Subcategory of';
+    $Self->{Translation}->{'StartDay'} = 'Start day';
+    $Self->{Translation}->{'StartMonth'} = 'Start month';
+    $Self->{Translation}->{'StartYear'} = 'Start year';
+    $Self->{Translation}->{'EndDay'} = 'End day';
+    $Self->{Translation}->{'EndMonth'} = 'End month';
+    $Self->{Translation}->{'EndYear'} = 'End year';
+    $Self->{Translation}->{'ArticleVotingQuestion'} = 'How helpful was this article? Please give us your rating and help to improve the FAQ Database. Thank You!';
+
+}
+
+1;
