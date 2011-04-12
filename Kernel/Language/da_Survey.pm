4a8590a1ff966f3aa36c010dad982b38789d7cbc
Michiel Beijen <michiel.beijen@otrs.com>
2011-04-12 05:39:27 +0000

    Restored translation file contents.

--- Kernel/Language/da_Survey.pm
+++ Kernel/Language/da_Survey.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/da_Survey.pm - translation file
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: da_Survey.pm,v 1.2 2011-02-16 22:15:11 dz Exp $
+# $Id: da_Survey.pm,v 1.3 2011-04-12 05:39:27 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -16,28 +16,56 @@ use strict;
 sub Data {
     my $Self = shift;
 
+    # Template: AAASurvey
+    $Self->{Translation}->{'- Change Status -'} = '';
+    $Self->{Translation}->{'Add New Survey'} = '';
+    $Self->{Translation}->{'Survey Edit'} = '';
+    $Self->{Translation}->{'Survey Edit Questions'} = '';
+    $Self->{Translation}->{'Question Edit'} = '';
+    $Self->{Translation}->{'Answer Edit'} = '';
+    $Self->{Translation}->{'Can\'t set new status! No questions defined.'} = 'Kan ikke sætte ny status! Der er ikke defineret nogen spørgsmål.';
+    $Self->{Translation}->{'Status changed.'} = 'Status ændret!';
+    $Self->{Translation}->{'Thank you for your feedback.'} = 'Tak for din besvarelse.';
+    $Self->{Translation}->{'The survey is finished.'} = '';
+    $Self->{Translation}->{'Complete'} = '';
+    $Self->{Translation}->{'Incomplete'} = '';
+    $Self->{Translation}->{'Checkbox'} = '';
+    $Self->{Translation}->{'Checkbox (List)'} = '';
+    $Self->{Translation}->{'Radio'} = '';
+    $Self->{Translation}->{'Radio (List)'} = '';
+    $Self->{Translation}->{'Stats Overview'} = '';
+    $Self->{Translation}->{'Survey Description'} = '';
+    $Self->{Translation}->{'Survey Introduction'} = '';
+    $Self->{Translation}->{'Textarea'} = 'Tekstområde';
+    $Self->{Translation}->{'Yes/No'} = '';
+    $Self->{Translation}->{'YesNo'} = 'JaNej';
+    $Self->{Translation}->{'answered'} = 'besvaret';
+    $Self->{Translation}->{'not answered'} = 'ikke besvaret';
+    $Self->{Translation}->{'Stats Detail'} = '';
+
     # Template: AgentSurvey
     $Self->{Translation}->{'Create New Survey'} = '';
     $Self->{Translation}->{'Introduction'} = '';
     $Self->{Translation}->{'Internal Description'} = '';
-    $Self->{Translation}->{'Survey Edit'} = '';
+    $Self->{Translation}->{'Edit General Info'} = '';
+    $Self->{Translation}->{'Survey#'} = 'Undersøgelse#';
     $Self->{Translation}->{'General Info'} = '';
-    $Self->{Translation}->{'Stats Overview'} = '';
+    $Self->{Translation}->{'Stats Overview of'} = '';
     $Self->{Translation}->{'Requests Table'} = '';
     $Self->{Translation}->{'Send Time'} = '';
     $Self->{Translation}->{'Vote Time'} = '';
     $Self->{Translation}->{'Details'} = '';
-    $Self->{Translation}->{'No questions saved for this survey.'} = '';
     $Self->{Translation}->{'Survey Stat Details'} = '';
     $Self->{Translation}->{'go back to stats overview'} = '';
     $Self->{Translation}->{'Go Back'} = '';
 
     # Template: AgentSurveyEditQuestions
-    $Self->{Translation}->{'Survey Edit Questions'} = '';
+    $Self->{Translation}->{'Edit Questions'} = '';
     $Self->{Translation}->{'Add Question'} = '';
     $Self->{Translation}->{'Type the question'} = '';
     $Self->{Translation}->{'Survey Questions'} = '';
     $Self->{Translation}->{'Question'} = '';
+    $Self->{Translation}->{'No questions saved for this survey.'} = '';
     $Self->{Translation}->{'Edit Question'} = '';
     $Self->{Translation}->{'go back to questions'} = '';
     $Self->{Translation}->{'Possible Answers For'} = '';
@@ -55,88 +83,45 @@ sub Data {
     $Self->{Translation}->{'Notification Sender'} = '';
     $Self->{Translation}->{'Notification Subject'} = '';
     $Self->{Translation}->{'Notification Body'} = '';
-    $Self->{Translation}->{'Created Time'} = '';
-    $Self->{Translation}->{'Created By'} = '';
-    $Self->{Translation}->{'Changed Time'} = '';
     $Self->{Translation}->{'Changed By'} = '';
 
     # Template: AgentSurveyZoom
     $Self->{Translation}->{'Survey Information'} = '';
     $Self->{Translation}->{'Sent requests'} = '';
     $Self->{Translation}->{'Received surveys'} = '';
-    $Self->{Translation}->{'Edit General Info'} = '';
-    $Self->{Translation}->{'Edit Questions'} = '';
     $Self->{Translation}->{'Stats Details'} = '';
     $Self->{Translation}->{'Survey Details'} = '';
     $Self->{Translation}->{'Survey Results Graph'} = '';
     $Self->{Translation}->{'No stat results.'} = '';
 
     # Template: PublicSurvey
-    $Self->{Translation}->{'Survey'} = '';
+    $Self->{Translation}->{'Survey'} = 'Undersøgelse';
     $Self->{Translation}->{'Please answer the next questions'} = '';
+    $Self->{Translation}->{'Show my answers'} = '';
+    $Self->{Translation}->{'Survey Title'} = '';
+    $Self->{Translation}->{'These are your answers.'} = '';
 
     # SysConfig
-    $Self->{Translation}->{'A Survey Module.'} = '';
+    $Self->{Translation}->{'A Survey Module.'} = 'Et undersøgelsesmodul.';
     $Self->{Translation}->{'A module to edit survey questions.'} = '';
-    $Self->{Translation}->{'Configure your own log text for PGP.'} = '';
-    $Self->{Translation}->{'Custom text for the page shown to customers that have no tickets yet.'} = '';
     $Self->{Translation}->{'Days starting from the latest customer survey email between no customer survey email is sent, ( 0 means Always send it ) .'} = '';
     $Self->{Translation}->{'Default body for the notification email to customers about new survey.'} = '';
     $Self->{Translation}->{'Default sender for the notification email to customers about new survey.'} = '';
     $Self->{Translation}->{'Default subject for the notification email to customers about new survey.'} = '';
     $Self->{Translation}->{'Defines an overview module to show the small view of a survey list.'} = '';
-    $Self->{Translation}->{'Defines the default front-end (HTML) theme to be used by the agents and customers. The default themes are Standard and Lite. If you like, you can add your own theme. Please refer the administrator manual located at http://doc.otrs.org/.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 1 for articles to add a new article attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 10 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 11 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 12 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 13 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 14 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 15 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 16 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 2 for articles to add a new article attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 2 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 3 for articles to add a new article attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 3 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 4 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 5 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 6 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 7 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 8 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the free key field number 9 for tickets to add a new ticket attribute.'} = '';
-    $Self->{Translation}->{'Defines the module used to store the session data. With "DB" the frontend server can be splitted from the db server. "FS" is faster.'} = '';
+    $Self->{Translation}->{'Defines the default height for Richtext views for SurveyZoom elements.'} = '';
     $Self->{Translation}->{'Defines the shown columns in the survey overview. This option has no effect on the position of the column.'} = '';
+    $Self->{Translation}->{'Enable or disable the ShowVoteData screen on public interface to show data of an specific votation when customer tries to answer a survey by second time.'} = '';
     $Self->{Translation}->{'Frontend module registration for survey zoom in the agent interface.'} = '';
     $Self->{Translation}->{'Frontend module registration for the PublicSurvey object in the public Survey area.'} = '';
     $Self->{Translation}->{'If this regex matches, no customer survey will be sent.'} = '';
     $Self->{Translation}->{'Parameters for the pages (in which the surveys are shown) of the small survey overview.'} = '';
     $Self->{Translation}->{'Public Survey.'} = '';
-    $Self->{Translation}->{'Set this to yes if you trust in all your public and private pgp keys, even if they are not certified with a trusted signature.'} = '';
-    $Self->{Translation}->{'Sets the configuration level of the administrator. Depending on the config level, some sysconfig options will be not shown. The config levels are in in ascending order: Expert, Advanced, Beginner. The higher the config level is (e.g. Beginner is the highest), the less likely is it that the user can accidentally configure the system in a way that it is not usable any more.'} = '';
-    $Self->{Translation}->{'Shows time use complete description (days, hours, minutes), if set to "Yes"; or just first letter (d, h, m), if set to "No".'} = '';
     $Self->{Translation}->{'Survey Overview "Small" Limit'} = '';
     $Self->{Translation}->{'Survey Zoom Module.'} = '';
     $Self->{Translation}->{'Survey limit per page for Survey Overview "Small"'} = '';
     $Self->{Translation}->{'The identifier for a survey, e.g. Survey#, MySurvey#. The default is Survey#.'} = '';
     $Self->{Translation}->{'Ticket event module to send automatically survey email requests to customers if a ticket gets closed.'} = '';
-
-    $Self->{Translation}->{'Survey Introduction'} = '';
-    $Self->{Translation}->{'Survey Description'} = '';
-    $Self->{Translation}->{'This field is required'} = '';
-    $Self->{Translation}->{'Survey Introduction'} = '';
-    $Self->{Translation}->{'Survey Description'} = '';
-    $Self->{Translation}->{'Complete'} = '';
-    $Self->{Translation}->{'Incomplete'} = '';
-    $Self->{Translation}->{'Survey#'} = '';
-    $Self->{Translation}->{'Default value'} = '';
-
-    $Self->{Translation}->{'Enable or disable the ShowVoteData screen on public interface to show data of an specific votation when customer tries to answer a survey by second time.'} = '';
-    $Self->{Translation}->{'All parameters for the Survey object in the agent interface.'} = '';
-    $Self->{Translation}->{'Defines the default height for Richtext views for SurveyZoom elements.'} = '';
-
-    #
-    # OBSOLETE ENTRIES FOR REFERENCE, DO NOT TRANSLATE!
-    #
 
 }
 
