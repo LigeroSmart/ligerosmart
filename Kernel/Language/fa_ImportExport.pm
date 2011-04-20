b28f38314f1acd162079e247b2842260969580c5
Udo Bretz <udo.bretz@otrs.com>
2011-04-20 10:30:23 +0000

    Upated persian translation, thanks to Masoud Ramezani!

--- Kernel/Language/fa_ImportExport.pm
+++ Kernel/Language/fa_ImportExport.pm
@@ -2,7 +2,7 @@
 # Kernel/Language/fa_ImportExport.pm - translation file
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: fa_ImportExport.pm,v 1.6 2011-01-24 20:49:14 ub Exp $
+# $Id: fa_ImportExport.pm,v 1.7 2011-04-20 10:30:23 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -28,29 +28,30 @@ sub Data {
 
     # Template: AdminImportExport
     $Self->{Translation}->{'Import/Export Management'} = 'مدیریت ورود/صدور';
-    $Self->{Translation}->{'Add template'} = '';
-    $Self->{Translation}->{'Create a template to import and export object information.'} = '';
+    $Self->{Translation}->{'Add template'} = 'افزودن قالب';
+    $Self->{Translation}->{'Create a template to import and export object information.'} = 'ساخت قالبی برای ورود و صدور اطلاعات آبجکت';
     $Self->{Translation}->{'Start Import'} = 'شروع عملیات ورود';
     $Self->{Translation}->{'Start Export'} = 'شروع عملیات صدور';
-    $Self->{Translation}->{'Delete Template'} = '';
+    $Self->{Translation}->{'Delete Template'} = 'حذف قالب';
     $Self->{Translation}->{'Step'} = 'گام';
     $Self->{Translation}->{'Edit common information'} = 'ویرایش اطلاعات عمومی';
-    $Self->{Translation}->{'Object is required!'} = '';
-    $Self->{Translation}->{'Format is required!'} = '';
+    $Self->{Translation}->{'Object is required!'} = 'آبجکت مورد نیاز است!';
+    $Self->{Translation}->{'Format is required!'} = 'قالب‌بندی مورد نیاز است!';
     $Self->{Translation}->{'Edit object information'} = 'ویرایش اطلاعات آبجکتی';
     $Self->{Translation}->{'Edit format information'} = 'ویرایش اطلاعات قالب‌بندی';
-    $Self->{Translation}->{' is required!'} = '';
+    $Self->{Translation}->{' is required!'} = ' مورد نیاز است!';
     $Self->{Translation}->{'Edit mapping information'} = 'ویرایش اطلاعات نگاشت';
-    $Self->{Translation}->{'No map elements found.'} = '';
-    $Self->{Translation}->{'Add Mapping Element'} = '';
+    $Self->{Translation}->{'No map elements found.'} = 'هیچ عنصر نگاشتی یافت نشد.';
+    $Self->{Translation}->{'Add Mapping Element'} = 'افزودن عنصر نگاشت';
     $Self->{Translation}->{'Edit search information'} = 'ویرایش اطلاعات جستجو';
     $Self->{Translation}->{'Restrict export per search'} = 'محدودسازی عملیات صدور به ازای جستجو';
     $Self->{Translation}->{'Import information'} = 'ورود اطلاعات';
     $Self->{Translation}->{'Source File'} = 'فایل منبع';
+    $Self->{Translation}->{'Template List'} = 'فهرست قالب‌ها';
 
     # SysConfig
-    $Self->{Translation}->{'Format backend module registration for the import/export module.'} = '';
-    $Self->{Translation}->{'Import and export object information.'} = '';
+    $Self->{Translation}->{'Format backend module registration for the import/export module.'} = 'قالب‌بندی ثبت ماژول برای ماژول ورود/صدور';
+    $Self->{Translation}->{'Import and export object information.'} = 'ورود و صدور اطلاعات آبجکت';
     $Self->{Translation}->{'Import/Export'} = 'ورود/صدور';
 
     #
