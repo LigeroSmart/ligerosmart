39ea22d279897349cfa2ca871dedc3e2310cf6d0
Michiel Beijen <michiel.beijen@otrs.com>
2011-08-23 12:41:29 +0000

    Fixed bug#7651 - Public and Customer frontends do not expose OpenSearch description. http://bugs.otrs.org/show_bug.cgi?id=7651

---
+++ Kernel/Output/HTML/CustomerHeaderMetaFAQSearch.pm
@@ -0,0 +1,75 @@
+# --
+# Kernel/Output/HTML/CustomerHeaderMetaFAQSearch.pm
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+# $Id: CustomerHeaderMetaFAQSearch.pm,v 1.1 2011-08-23 12:40:44 mb Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::CustomerHeaderMetaFAQSearch;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (qw(ConfigObject LogObject LayoutObject TimeObject)) {
+        $Self->{$_} = $Param{$_} || die "Got no $_!";
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $Session = '';
+    if ( !$Self->{LayoutObject}->{SessionIDCookie} ) {
+        $Session = ';' . $Self->{LayoutObject}->{SessionName} . '='
+            . $Self->{LayoutObject}->{SessionID};
+    }
+
+    # build open search description for FAQ number
+    my $Title = $Self->{ConfigObject}->Get('ProductName');
+
+    $Title .= '(' . $Self->{ConfigObject}->Get('FAQ::FAQHook') . ')';
+    $Self->{LayoutObject}->Block(
+        Name => 'MetaLink',
+        Data => {
+            Rel   => 'search',
+            Title => $Title,
+            Href  => '$Env{"Baselink"}Action=' . $Param{Config}->{Action}
+                . ';Subaction=OpenSearchDescriptionFAQNumber' . $Session,
+        },
+    );
+
+    # build open search description for FAQ fulltext
+    my $Fulltext = $Self->{LayoutObject}->{LanguageObject}->Get('FAQFulltext');
+    $Title = $Self->{ConfigObject}->Get('ProductName');
+    $Title .= '(' . $Fulltext . ')';
+    $Self->{LayoutObject}->Block(
+        Name => 'MetaLink',
+        Data => {
+            Rel   => 'search',
+            Title => $Title,
+            Href  => '$Env{"Baselink"}Action=' . $Param{Config}->{Action}
+                . ';Subaction=OpenSearchDescriptionFulltext' . $Session,
+        },
+    );
+
+    return 1;
+}
+
+1;
