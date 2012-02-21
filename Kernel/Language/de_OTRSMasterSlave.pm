3b8f1bc056eaf35f704ea311485072acc53a58b5
Carlos Garcia <carlos.garcia@otrs.com>
2012-02-21 01:03:45 +0000

    Modified from iso-8859-1 to utf-8.

--- Kernel/Language/de_OTRSMasterSlave.pm
+++ Kernel/Language/de_OTRSMasterSlave.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Language/de_OTRSMasterSlave.pm - provides de language translation
-# Copyright (C) 2003-2011 OTRS AG, http://otrs.com/
+# Copyright (C) 2003-2012 OTRS AG, http://otrs.com/
 # --
-# $Id: de_OTRSMasterSlave.pm,v 1.1 2011-10-10 09:30:05 te Exp $
+# $Id: de_OTRSMasterSlave.pm,v 1.2 2012-02-21 01:03:45 cg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -14,7 +14,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 sub Data {
     my $Self = shift;
@@ -25,15 +25,15 @@ sub Data {
     $Self->{Translation}->{'Slave of Ticket#'}      = 'Slave von Ticket#';
     $Self->{Translation}->{'MasterSlave'}    = 'MasterSlave';
     $Self->{Translation}->{'Manage Master/Slave'} = 'Master/Slave verwalten';
-    $Self->{Translation}->{'Change the MasterSlave state of the ticket.'}           = 'Den MasterSlave Status des Tickets ‰ndern.';
-    $Self->{Translation}->{'Define free text field for master ticket feature.'}                   = 'Das Freitextfeld f¸r die MasterSlave Erweiterung festlegen.';
+    $Self->{Translation}->{'Change the MasterSlave state of the ticket.'}           = 'Den MasterSlave Status des Tickets √§ndern.';
+    $Self->{Translation}->{'Define free text field for master ticket feature.'}                   = 'Das Freitextfeld f√ºr die MasterSlave Erweiterung festlegen.';
     $Self->{Translation}->{'Enable the advanced MasterSlave part of the feature.'}             = 'Das erweiterte Verhalten der MasterSlave Erweiterung aktivieren.';
     $Self->{Translation}->{'Enable the feature to unset the MasterSlave state of a ticket in the advanced MasterSlave mode.'}               = 'Aktiviere den Modus um den MasterSlave Status eines Tickets im erweiterten MasterSlave Verhalten aufzuheben.';
-    $Self->{Translation}->{'Enable the feature to update the MasterSlave state of a ticket in the advanced MasterSlave mode.'}                   = 'Aktiviere den Modus um den MasterSlave Status eines Tickets im erweiterten MasterSlave Verhalten zu ‰ndern.';
+    $Self->{Translation}->{'Enable the feature to update the MasterSlave state of a ticket in the advanced MasterSlave mode.'}                   = 'Aktiviere den Modus um den MasterSlave Status eines Tickets im erweiterten MasterSlave Verhalten zu √§ndern.';
     $Self->{Translation}->{'Enable the feature that slave tickets follow the master ticket to a new master in the advanced MasterSlave mode.'}                = 'Aktiviere den Modus das Slave Tickets dem Master Ticket im erweiterten MasterSlave Verhalten zum neuen Master folgen.';
     $Self->{Translation}->{'This module is preparing master/slave pulldown in email and phone ticket.'}             = 'Legt das Modul fest das das DropDown Feld im Email- und Telefonticket Dialog bereitstellt.';
-    $Self->{Translation}->{'Shows a link in the menu to change the MasterSlave status of a ticket in the ticket zoom view of the agent interface.'}       = 'Zeigt einen Link im Men¸ um den MasterSlave Status eines Tickets im Ticket Zoom Interface des Agenten an.';
-    $Self->{Translation}->{'Required permissions to use the ticket MasterSlave screen of a zoomed ticket in the agent interface.'}              = 'Benˆtigte Berechtigungen um den MasterSlave Dialog im Zoom Ticket Dialog des Agenteninterface anzeigen zu lassen.';
+    $Self->{Translation}->{'Shows a link in the menu to change the MasterSlave status of a ticket in the ticket zoom view of the agent interface.'}       = 'Zeigt einen Link im Men√º um den MasterSlave Status eines Tickets im Ticket Zoom Interface des Agenten an.';
+    $Self->{Translation}->{'Required permissions to use the ticket MasterSlave screen of a zoomed ticket in the agent interface.'}              = 'Ben√∂tigte Berechtigungen um den MasterSlave Dialog im Zoom Ticket Dialog des Agenteninterface anzeigen zu lassen.';
 
     return 1;
 }
