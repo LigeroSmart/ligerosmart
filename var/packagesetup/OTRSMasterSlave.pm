67f4cf880e8af97d7bb0c199cff5f732caa5974c
Thorsten Eckel <thorsten.eckel@otrs.com>
2012-05-10 10:44:34 +0000

    MasterSlave have to be unistalled before migration.

--- var/packagesetup/OTRSMasterSlave.pm
+++ var/packagesetup/OTRSMasterSlave.pm
@@ -2,7 +2,7 @@
 # OTRSMasterSlave.pm - code to excecute during package installation
 # Copyright (C) 2003-2012 OTRS AG, http://otrs.com/
 # --
-# $Id: OTRSMasterSlave.pm,v 1.10 2012-05-10 10:39:48 te Exp $
+# $Id: OTRSMasterSlave.pm,v 1.11 2012-05-10 10:44:34 te Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -25,7 +25,7 @@ use Kernel::System::SysConfig;
 use Kernel::System::LinkObject;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.10 $) [1];
+$VERSION = qw($Revision: 1.11 $) [1];
 
 =head1 NAME
 
@@ -172,14 +172,15 @@ sub CodeInstall {
     # otherwise set the dynamic fields
     my $MasterSlaveDynamicFieldID = $Self->_CheckMasterSlaveData();
     if ($MasterSlaveDynamicFieldID) {
+
+        if ( $Self->{PackageObject}->PackageIsInstalled( Name => 'MasterSlave' ) ) {
+            $Self->{PackageObject}->RepositoryRemove( Name => 'MasterSlave' );
+        }
+
         $Self->_MigrateOTRSMasterSlave(
             DynamicFieldID => $MasterSlaveDynamicFieldID,
             MasterSlave    => 1,
         );
-        return 1;
-        if ( $Self->{PackageObject}->PackageIsInstalled( Name => 'MasterSlave' ) ) {
-            $Self->{PackageObject}->RepositoryRemove( Name => 'MasterSlave' );
-        }
     }
     else {
         $Self->_SetDynamicFields();
@@ -506,6 +507,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.10 $ $Date: 2012-05-10 10:39:48 $
+$Revision: 1.11 $ $Date: 2012-05-10 10:44:34 $
 
 =cut
