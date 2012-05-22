b13f5f75301c14b01bf0969826c3c710725a5be7
Michiel Beijen <michiel.beijen@otrs.com>
2012-05-22 08:30:40 +0000

    Fixed bug#8501 - SystemMonitoring doesn't send an ACK to Nagios/Icinga. Thanks to Bjoern Petersen for the fix! http://bugs.otrs.org/show_bug.cgi?id=8501

--- Kernel/System/Ticket/Event/NagiosAcknowledge.pm
+++ Kernel/System/Ticket/Event/NagiosAcknowledge.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/Ticket/Event/NagiosAcknowledge.pm - acknowlege nagios tickets
-# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: NagiosAcknowledge.pm,v 1.11 2011-07-01 10:05:55 mb Exp $
+# $Id: NagiosAcknowledge.pm,v 1.12 2012-05-22 08:30:40 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use LWP::UserAgent;
 use URI::Escape qw();
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.11 $) [1];
+$VERSION = qw($Revision: 1.12 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -57,7 +57,10 @@ sub Run {
     return 1 if !$Type;
 
     # check if it's a Nagios related ticket
-    my %Ticket = $Self->{TicketObject}->TicketGet( TicketID => $Param{TicketID} );
+    my %Ticket = $Self->{TicketObject}->TicketGet(
+        TicketID      => $Param{TicketID},
+        DynamicFields => 1,
+    );
     if ( !$Ticket{ $Self->{Fhost} } ) {
         $Self->{LogObject}->Log( Priority => 'debug', Message => "No Nagios Ticket!" );
         return 1;
