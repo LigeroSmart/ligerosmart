03d30818d6f8d61da27e1790c351c104f197e814
Udo Bretz <udo.bretz@otrs.com>
2012-10-18 17:12:25 +0000

    Updated Polish translation, thanks to Pawel!

--- Kernel/Language/pl_ImportExport.pm
+++ Kernel/Language/pl_ImportExport.pm
@@ -1,8 +1,9 @@
 # --
 # Kernel/Language/pl_ImportExport.pm - translation file
-# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# Copyright (C) 2011-2012 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
-# $Id: pl_ImportExport.pm,v 1.11 2011-11-24 15:42:26 ub Exp $
+# $Id: pl_ImportExport.pm,v 1.12 2012-10-18 17:12:25 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,42 +18,53 @@ sub Data {
     my $Self = shift;
 
     # Template: AAAImportExport
-    $Self->{Translation}->{'Add mapping template'} = '';
+    $Self->{Translation}->{'Add mapping template'} = 'Dodaj szablon mapowania';
     $Self->{Translation}->{'Charset'} = 'Kodowanie';
     $Self->{Translation}->{'Colon (:)'} = 'Dwukropek (:)';
     $Self->{Translation}->{'Column'} = 'Kolumna';
     $Self->{Translation}->{'Column Separator'} = 'Separator kolumny';
     $Self->{Translation}->{'Dot (.)'} = 'Kropka (.)';
-    $Self->{Translation}->{'Semicolon (;)'} = 'Orednik (;)';
+    $Self->{Translation}->{'Semicolon (;)'} = 'Średnik (;)';
     $Self->{Translation}->{'Tabulator (TAB)'} = 'Tabulator (TAB)';
-    $Self->{Translation}->{'Include Column Headers'} = '';
+    $Self->{Translation}->{'Include Column Headers'} = 'Umieść nagłówki kolumn';
 
     # Template: AdminImportExport
-    $Self->{Translation}->{'Import/Export Management'} = 'Zarz1dzanie Importem/Exportem';
-    $Self->{Translation}->{'Add template'} = '';
-    $Self->{Translation}->{'Create a template to import and export object information.'} = '';
-    $Self->{Translation}->{'Start Import'} = 'Rozpocznij Import';
-    $Self->{Translation}->{'Start Export'} = 'Rozpocznij Export';
-    $Self->{Translation}->{'Delete Template'} = '';
-    $Self->{Translation}->{'Step'} = '';
-    $Self->{Translation}->{'Edit common information'} = '';
-    $Self->{Translation}->{'Object is required!'} = '';
-    $Self->{Translation}->{'Format is required!'} = '';
-    $Self->{Translation}->{'Edit object information'} = '';
-    $Self->{Translation}->{'Edit format information'} = '';
-    $Self->{Translation}->{' is required!'} = '';
-    $Self->{Translation}->{'Edit mapping information'} = '';
-    $Self->{Translation}->{'No map elements found.'} = '';
-    $Self->{Translation}->{'Add Mapping Element'} = '';
-    $Self->{Translation}->{'Edit search information'} = '';
-    $Self->{Translation}->{'Restrict export per search'} = '';
-    $Self->{Translation}->{'Import information'} = '';
-    $Self->{Translation}->{'Source File'} = 'Plik Yród3owy';
+    $Self->{Translation}->{'Import/Export Management'} = 'Zarządzanie Importem/Exportem';
+    $Self->{Translation}->{'Add template'} = 'Dodaj szablon';
+    $Self->{Translation}->{'Create a template to import and export object information.'} = 'Utwórz szablon do importu i eksportu danych obiektów.';
+    $Self->{Translation}->{'Start Import'} = 'Rozpocznij import';
+    $Self->{Translation}->{'Start Export'} = 'Rozpocznij eksport';
+    $Self->{Translation}->{'Delete Template'} = 'Usuń szablon';
+    $Self->{Translation}->{'Step'} = 'Krok';
+    $Self->{Translation}->{'Edit common information'} = 'Edytuj informacje wspólne';
+    $Self->{Translation}->{'Object is required!'} = 'Obiekt jest wymagany!';
+    $Self->{Translation}->{'Format is required!'} = 'Format jest wymagany!';
+    $Self->{Translation}->{'Edit object information'} = 'Edytuj informacje obiektu';
+    $Self->{Translation}->{'Edit format information'} = 'Edytuj format obiektu';
+    $Self->{Translation}->{' is required!'} = ' jest wymagane!';
+    $Self->{Translation}->{'Edit mapping information'} = 'Edytuj informacje mapowania';
+    $Self->{Translation}->{'No map elements found.'} = 'Nie znaleziono elementów mapy.';
+    $Self->{Translation}->{'Add Mapping Element'} = 'Dodaj element mapowania';
+    $Self->{Translation}->{'Edit search information'} = 'Edytuj informacje wyszukiwania';
+    $Self->{Translation}->{'Restrict export per search'} = 'Ogranicz eksport przez wyszukanie';
+    $Self->{Translation}->{'Import information'} = 'Importuj informacje';
+    $Self->{Translation}->{'Source File'} = 'Plik źródłowy';
 
     # SysConfig
-    $Self->{Translation}->{'Format backend module registration for the import/export module.'} = '';
-    $Self->{Translation}->{'Import and export object information.'} = '';
-    $Self->{Translation}->{'Import/Export'} = 'Import/Export';
+    $Self->{Translation}->{'Format backend module registration for the import/export module.'} = 'Moduł formatowania backend dla modułu import/eksport.';
+    $Self->{Translation}->{'Import and export object information.'} = 'Importuj i eksportuj informacje obiektów.';
+    $Self->{Translation}->{'Import/Export'} = 'Import/eksport';
+
+    #
+    # IB mod
+    #
+    $Self->{Translation}->{'Template List'} = 'Lista szablonów';
+    $Self->{Translation}->{'Class'} = 'Klasa';
+    $Self->{Translation}->{'Maximum number of one element'} = 'Maksymalna liczba wystąpień pojedynczego elementu';
+    $Self->{Translation}->{'Empty fields indicate that the current values are kept'} = 'Puste pola oznaczają zachowanie bieżącej wartości';
+    $Self->{Translation}->{'Element required, please insert data'} = 'Element wymagany, podaj wartość';
+    $Self->{Translation}->{'Config Item'} = 'CI';
+    $Self->{Translation}->{'Delete Template'} = 'Usuń szablon';
 
     #
     # OBSOLETE ENTRIES FOR REFERENCE, DO NOT TRANSLATE!
