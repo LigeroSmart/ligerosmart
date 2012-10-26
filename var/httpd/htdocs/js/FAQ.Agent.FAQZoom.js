7b071bdcb5663b04870879c2c2003e0d3f5dd8e4
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2012-10-26 20:03:55 +0000

    Initial version, to support iframes.

---
+++ var/httpd/htdocs/js/FAQ.Agent.FAQZoom.js
@@ -0,0 +1,49 @@
+// --
+// FAQ.Agent.FAQZoom.js - provides the special module functions for FAQZoom
+// Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+// --
+// $Id: FAQ.Agent.FAQZoom.js,v 1.1 2012-10-26 20:03:55 cr Exp $
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var FAQ = FAQ || {};
+FAQ.Agent = FAQ.Agent || {};
+
+/**
+ * @namespace
+ * @exports TargetNS as FAQ.Agent.TicketZoom
+ * @description
+ *      This namespace contains the special module functions for TicketZoom.
+ */
+FAQ.Agent.FAQZoom = (function (TargetNS) {
+
+    /**
+     * @function
+     * @param {jQueryObject} $Iframe The iframe which should be auto-heighted
+     * @return nothing
+     *      This function initializes the special module functions
+     */
+    TargetNS.IframeAutoHeight = function ($Iframe) {
+        if (isJQueryObject($Iframe)) {
+            var NewHeight = $Iframe.contents().height();
+            if (!NewHeight || isNaN(NewHeight)) {
+                NewHeight = Core.Config.Get('FAQ::Frontend::AgentHTMLFieldHeightDefault');
+            }
+            else {
+                if (NewHeight > Core.Config.Get('FAQ::Frontend::AgentHTMLFieldHeightMax')) {
+                    NewHeight = Core.Config.Get('FAQ::Frontend::AgentHTMLFieldHeightMax');
+                }
+            }
+            $Iframe.height(NewHeight + 'px');
+        }
+    };
+
+
+
+    return TargetNS;
+}(FAQ.Agent.FAQZoom || {}));
---
+++ var/httpd/htdocs/js/FAQ.Customer.FAQZoom.js
@@ -0,0 +1,49 @@
+// --
+// FAQ.Customer.FAQZoom.js - provides the special module functions for FAQZoom
+// Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+// --
+// $Id: FAQ.Customer.FAQZoom.js,v 1.1 2012-10-26 20:03:55 cr Exp $
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var FAQ = FAQ || {};
+FAQ.Customer = FAQ.Customer || {};
+
+/**
+ * @namespace
+ * @exports TargetNS as FAQ.Customer.TicketZoom
+ * @description
+ *      This namespace contains the special module functions for TicketZoom.
+ */
+FAQ.Customer.FAQZoom = (function (TargetNS) {
+
+    /**
+     * @function
+     * @param {jQueryObject} $Iframe The iframe which should be auto-heighted
+     * @return nothing
+     *      This function initializes the special module functions
+     */
+    TargetNS.IframeAutoHeight = function ($Iframe) {
+        if (isJQueryObject($Iframe)) {
+            var NewHeight = $Iframe.contents().height();
+            if (!NewHeight || isNaN(NewHeight)) {
+                NewHeight = Core.Config.Get('FAQ::Frontend::CustomerHTMLFieldHeightDefault');
+            }
+            else {
+                if (NewHeight > Core.Config.Get('FAQ::Frontend::CustomerHTMLFieldHeightMax')) {
+                    NewHeight = Core.Config.Get('FAQ::Frontend::CustomerHTMLFieldHeightMax');
+                }
+            }
+            $Iframe.height(NewHeight + 'px');
+        }
+    };
+
+
+
+    return TargetNS;
+}(FAQ.Customer.FAQZoom || {}));
