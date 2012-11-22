7bf824a57ab3676287d95e4c9f05661f62f301ee
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2012-11-22 03:52:01 +0000

    Updated translations.

--- Kernel/Language/de_OTRSMasterSlave.pm
+++ Kernel/Language/de_OTRSMasterSlave.pm
@@ -1,41 +1,96 @@
 # --
-# Kernel/Language/de_OTRSMasterSlave.pm - provides de language translation
+# Kernel/Language/de_OTRSMasterSlave.pm - translation file
 # Copyright (C) 2003-2012 OTRS AG, http://otrs.com/
 # --
-# $Id: de_OTRSMasterSlave.pm,v 1.2 2012-02-21 01:03:45 cg Exp $
+# $Id: de_OTRSMasterSlave.pm,v 1.3 2012-11-22 03:52:01 cr Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 # --
+
 package Kernel::Language::de_OTRSMasterSlave;
 
 use strict;
-use warnings;
-
-use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
 
 sub Data {
     my $Self = shift;
 
-    $Self->{Translation}->{'New Master Ticket'}      = 'Neues Master Ticket';
-    $Self->{Translation}->{'Unset Master Ticket'}      = 'Master Ticket aufheben';
-    $Self->{Translation}->{'Unset Slave Ticket'}      = 'Slave Ticket aufheben';
-    $Self->{Translation}->{'Slave of Ticket#'}      = 'Slave von Ticket#';
-    $Self->{Translation}->{'MasterSlave'}    = 'MasterSlave';
-    $Self->{Translation}->{'Manage Master/Slave'} = 'Master/Slave verwalten';
-    $Self->{Translation}->{'Change the MasterSlave state of the ticket.'}           = 'Den MasterSlave Status des Tickets ändern.';
-    $Self->{Translation}->{'Define free text field for master ticket feature.'}                   = 'Das Freitextfeld für die MasterSlave Erweiterung festlegen.';
-    $Self->{Translation}->{'Enable the advanced MasterSlave part of the feature.'}             = 'Das erweiterte Verhalten der MasterSlave Erweiterung aktivieren.';
-    $Self->{Translation}->{'Enable the feature to unset the MasterSlave state of a ticket in the advanced MasterSlave mode.'}               = 'Aktiviere den Modus um den MasterSlave Status eines Tickets im erweiterten MasterSlave Verhalten aufzuheben.';
-    $Self->{Translation}->{'Enable the feature to update the MasterSlave state of a ticket in the advanced MasterSlave mode.'}                   = 'Aktiviere den Modus um den MasterSlave Status eines Tickets im erweiterten MasterSlave Verhalten zu ändern.';
-    $Self->{Translation}->{'Enable the feature that slave tickets follow the master ticket to a new master in the advanced MasterSlave mode.'}                = 'Aktiviere den Modus das Slave Tickets dem Master Ticket im erweiterten MasterSlave Verhalten zum neuen Master folgen.';
-    $Self->{Translation}->{'This module is preparing master/slave pulldown in email and phone ticket.'}             = 'Legt das Modul fest das das DropDown Feld im Email- und Telefonticket Dialog bereitstellt.';
-    $Self->{Translation}->{'Shows a link in the menu to change the MasterSlave status of a ticket in the ticket zoom view of the agent interface.'}       = 'Zeigt einen Link im Menü um den MasterSlave Status eines Tickets im Ticket Zoom Interface des Agenten an.';
-    $Self->{Translation}->{'Required permissions to use the ticket MasterSlave screen of a zoomed ticket in the agent interface.'}              = 'Benötigte Berechtigungen um den MasterSlave Dialog im Zoom Ticket Dialog des Agenteninterface anzeigen zu lassen.';
+    # Template: AgentTicketBulk
+    $Self->{Translation}->{'(work units)'} = '';
+    $Self->{Translation}->{'MasterTicket'} = '';
 
-    return 1;
+    # Template: AgentTicketMasterSlave
+    $Self->{Translation}->{'Manage Master/Slave'} = 'Master/Slave verwalten';
+
+    # SysConfig
+    $Self->{Translation}->{'Allows adding notes in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Change the MasterSlave state of the ticket.'} = 'Den MasterSlave Status des Tickets ändern.';
+    $Self->{Translation}->{'Define dynamic field name for master ticket feature.'} = '';
+    $Self->{Translation}->{'Defines if a ticket lock is required in the ticket MasterSlave screen of a zoomed ticket in the agent interface (if the ticket isn\'t locked yet, the ticket gets locked and the current agent will be set automatically as its owner).'} =
+        '';
+    $Self->{Translation}->{'Defines the default next state of a ticket after adding a note, in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Defines the default ticket priority in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Defines the default type of the note in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Defines the history comment for the ticket MasterSlave screen action, which gets used for ticket history in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Defines the history type for the ticket MasterSlave screen action, which gets used for ticket history in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Defines the next state of a ticket after adding a note, in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Enable the advanced MasterSlave part of the feature.'} = 'Das erweiterte Verhalten der MasterSlave Erweiterung aktivieren.';
+    $Self->{Translation}->{'Enable the feature that slave tickets follow the master ticket to a new master in the advanced MasterSlave mode.'} =
+        'Aktiviere den Modus das Slave Tickets dem Master Ticket im erweiterten MasterSlave Verhalten zum neuen Master folgen.';
+    $Self->{Translation}->{'Enable the feature to change the MasterSlave state of a ticket in the advanced MasterSlave mode.'} =
+        '';
+    $Self->{Translation}->{'Enable the feature to unset the MasterSlave state of a ticket in the advanced MasterSlave mode.'} =
+        'Aktiviere den Modus um den MasterSlave Status eines Tickets im erweiterten MasterSlave Verhalten aufzuheben.';
+    $Self->{Translation}->{'If a note is added by an agent, sets the state of the ticket in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Registration of the ticket event module.'} = '';
+    $Self->{Translation}->{'Required permissions to use the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        'Benötigte Berechtigungen um den MasterSlave Dialog im Zoom Ticket Dialog des Agenteninterface anzeigen zu lassen.';
+    $Self->{Translation}->{'Sets the default body text for notes added in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Sets the default subject for notes added in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Sets the responsible agent of the ticket in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Sets the service in the ticket MasterSlave screen of a zoomed ticket in the agent interface (Ticket::Service needs to be activated).'} =
+        '';
+    $Self->{Translation}->{'Sets the ticket owner in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Sets the ticket type in the ticket MasterSlave screen of a zoomed ticket in the agent interface (Ticket::Type needs to be activated).'} =
+        '';
+    $Self->{Translation}->{'Shows a link in the menu to change the MasterSlave status of a ticket in the ticket zoom view of the agent interface.'} =
+        'Zeigt einen Link im Menü um den MasterSlave Status eines Tickets im Ticket Zoom Interface des Agenten an.';
+    $Self->{Translation}->{'Shows a list of all the involved agents on this ticket, in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Shows a list of all the possible agents (all agents with note permissions on the queue/ticket) to determine who should be informed about this note, in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Shows the ticket priority options in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'Shows the title fields in the ticket MasterSlave screen of a zoomed ticket in the agent interface.'} =
+        '';
+    $Self->{Translation}->{'This module is preparing master/slave pulldown in email and phone ticket.'} =
+        'Legt das Modul fest das das DropDown Feld im Email- und Telefonticket Dialog bereitstellt.';
+
+    #
+    # OBSOLETE ENTRIES FOR REFERENCE, DO NOT TRANSLATE!
+    #
+    $Self->{Translation}->{'Define free text field for master ticket feature.'} = 'Das Freitextfeld für die MasterSlave Erweiterung festlegen.';
+    $Self->{Translation}->{'Enable the feature to update the MasterSlave state of a ticket in the advanced MasterSlave mode.'} =
+        'Aktiviere den Modus um den MasterSlave Status eines Tickets im erweiterten MasterSlave Verhalten zu ändern.';
+    $Self->{Translation}->{'MasterSlave'} = 'MasterSlave';
+    $Self->{Translation}->{'New Master Ticket'} = 'Neues Master Ticket';
+    $Self->{Translation}->{'Slave of Ticket#'} = 'Slave von Ticket#';
+    $Self->{Translation}->{'Unset Master Ticket'} = 'Master Ticket aufheben';
+    $Self->{Translation}->{'Unset Slave Ticket'} = 'Slave Ticket aufheben';
+
 }
 
 1;
