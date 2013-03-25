a9476eb627fb5bd041019ba3680a37bcc7878ea7
Udo Bretz <udo.bretz@otrs.com>
2013-03-25 19:08:00 +0000

    Improved permission checks in LinkObject.

--- Kernel/System/LinkObject/Service.pm
+++ Kernel/System/LinkObject/Service.pm
@@ -2,7 +2,7 @@
 # Kernel/System/LinkObject/Service.pm - to link service objects
 # Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
 # --
-# $Id: Service.pm,v 1.11 2013-03-23 14:18:16 ub Exp $
+# $Id: Service.pm,v 1.12 2013-03-25 19:08:00 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -14,10 +14,11 @@ package Kernel::System::LinkObject::Service;
 use strict;
 use warnings;
 
+use Kernel::System::Group;
 use Kernel::System::Service;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.11 $) [1];
+$VERSION = qw($Revision: 1.12 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -32,6 +33,7 @@ sub new {
     }
 
     # create additional objects
+    $Self->{GroupObject}   = Kernel::System::Group->new( %{$Self} );
     $Self->{ServiceObject} = Kernel::System::Service->new( %{$Self} );
 
     return $Self;
@@ -98,6 +100,77 @@ sub LinkListWithData {
     }
 
     return 1;
+}
+
+=item ObjectPermission()
+
+checks read permission for a given object and UserID.
+
+    $Permission = $LinkObject->ObjectPermission(
+        Object  => 'Service',
+        Key     => 123,
+        UserID  => 1,
+    );
+
+=cut
+
+sub ObjectPermission {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Object Key UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    # check module registry of AgentITSMServiceZoom
+    my $ModuleReg = $Self->{ConfigObject}->Get('Frontend::Module')->{AgentITSMServiceZoom};
+
+    # do not grant access if frontend module is not registered
+    return if !$ModuleReg;
+
+    # grant access if module permisson has no Group or GroupRo defined
+    if ( !$ModuleReg->{GroupRo} && !$ModuleReg->{Group} ) {
+        return 1;
+    }
+
+    PERMISSION:
+    for my $Permission (qw(GroupRo Group)) {
+
+        next PERMISSION if !$ModuleReg->{$Permission};
+        next PERMISSION if ref $ModuleReg->{$Permission} ne 'ARRAY';
+
+        for my $Group ( @{ $ModuleReg->{$Permission} } ) {
+
+            # get the group id
+            my $GroupID = $Self->{GroupObject}->GroupLookup( Group => $Group );
+
+            my $Type;
+            if ( $Permission eq 'GroupRo' ) {
+                $Type = 'ro';
+            }
+            elsif ( $Permission eq 'Group' ) {
+                $Type = 'rw';
+            }
+
+            # get user groups, where the user has the appropriate privilege
+            my %Groups = $Self->{GroupObject}->GroupMemberList(
+                UserID => $Param{UserID},
+                Type   => $Type,
+                Result => 'HASH',
+            );
+
+            # grant access if agent is a member in the group
+            return 1 if $Groups{$GroupID};
+        }
+    }
+
+    return;
 }
 
 =item ObjectDescriptionGet()
