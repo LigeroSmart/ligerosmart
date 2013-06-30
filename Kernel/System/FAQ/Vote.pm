fd7e01ab0ba75c1adbbc4c1513fb681a01047b43
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2013-06-30 00:58:12 +0000

    Initial version.

---
+++ Kernel/System/FAQ/Vote.pm
@@ -0,0 +1,334 @@
+# --
+# Kernel/System/FAQ/Vote.pm - faq vote functions
+# Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
+# --
+# $Id: Vote.pm,v 1.1 2013-06-30 00:58:12 cr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::FAQ::Vote;
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+Kernel::System::FAQ::Vote - sub module of Kernel::System::FAQ
+
+=head1 SYNOPSIS
+
+All faq vote functions.
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item VoteAdd()
+
+add a vote
+
+    my $Success = $FAQObject->VoteAdd(
+        CreatedBy => 'Some Text',
+        ItemID    => '123456',
+        IP        => '54.43.30.1',
+        Interface => 'Some Text',
+        Rate      => 100,
+        UserID    => 1,
+    );
+
+Returns:
+
+    $Success = 1;              # or undef if vote could not be added
+
+=cut
+
+sub VoteAdd {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(CreatedBy ItemID IP Interface UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    return if !$Self->{DBObject}->Do(
+        SQL => 'INSERT INTO faq_voting (created_by, item_id, ip, interface, rate, created )'
+            . ' VALUES ( ?, ?, ?, ?, ?, current_timestamp )',
+        Bind => [
+            \$Param{CreatedBy}, \$Param{ItemID}, \$Param{IP}, \$Param{Interface},
+            \$Param{Rate},
+        ],
+    );
+
+    # delete cache
+    my $CacheKey = 'ItemVoteDataGet::' . $Param{ItemID};
+    $Self->{CacheObject}->Delete(
+        Type => 'FAQ',
+        Key  => $CacheKey,
+    );
+
+    return 1;
+}
+
+=item VoteDelete()
+
+delete a vote
+
+    my $DeleteSuccess = $FAQObject->VoteDelete(
+        VoteID => 1,
+        UserID => 1,
+    );
+
+Returns:
+
+    $DeleteSuccess = 1;              # or undef if vote could not be deleted
+
+=cut
+
+sub VoteDelete {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(VoteID UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    return if !$Self->{DBObject}->Do(
+        SQL  => 'DELETE FROM faq_voting WHERE id = ?',
+        Bind => [ \$Param{VoteID} ],
+    );
+
+    return 1;
+}
+
+=item VoteGet()
+
+get a vote information
+
+    my %VoteData = $FAQObject->VoteGet(
+        CreateBy  => 'Some Text',
+        ItemID    => '123456',
+        IP        => '127.0.0.1',
+        Interface => 'Some Text',
+        UserID    => 1,
+    );
+
+Returns:
+
+    %VoteData = (
+        ItemID    => 23,
+        Rate      => 50,                            # or 0 or 25 or 75 or 100
+        IP        => '192.168.0.1',
+        Interface => 1,                             # interface ID
+        CreatedBy => 1,
+        Created   => '2011-06-14 12:32:03',
+    );
+
+=cut
+
+sub VoteGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(CreateBy ItemID Interface IP UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    # db quote
+    for my $Argument (qw(CreatedBy Interface IP)) {
+        $Param{$Argument} = $Self->{DBObject}->Quote( $Param{$Argument} );
+    }
+    $Param{ItemID} = $Self->{DBObject}->Quote( $Param{ItemID}, 'Integer' );
+
+    my $Ext = "";
+    my $SQL = " SELECT created_by, item_id, interface, ip, created, rate FROM faq_voting WHERE";
+
+    # public
+    if ( $Param{Interface} eq '3' ) {
+        $Ext .= " ip = '$Param{IP}' AND item_id = $Param{ItemID}";
+    }
+
+    # customer
+    elsif ( $Param{Interface} eq '2' ) {
+        $Ext .= " created_by = '$Param{CreateBy}' AND item_id = $Param{ItemID}";
+    }
+
+    # internal
+    elsif ( $Param{Interface} eq '1' ) {
+        $Ext .= " created_by = '$Param{CreateBy}' AND item_id = $Param{ItemID}";
+    }
+    $SQL .= $Ext;
+
+    return if !$Self->{DBObject}->Prepare(
+        SQL   => $SQL,
+        Limit => 1,
+    );
+
+    my %Data;
+    while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
+        %Data = (
+            CreatedBy => $Row[0],
+            ItemID    => $Row[1],
+            Interface => $Row[2],
+            IP        => $Row[3],
+            Created   => $Row[4],
+            Rate      => $Row[5],
+        );
+    }
+
+    return if !%Data;
+    return \%Data;
+}
+
+=item VoteSearch()
+
+returns an array with VoteIDs
+
+    my $VoteIDArrayref = $FAQObject->VoteSearch(
+        ItemID => 1,
+        UserID => 1,
+    );
+
+Returns:
+
+    $VoteIDArrayref = [
+        23,
+        45,
+    ];
+
+=cut
+
+sub VoteSearch {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(ItemID UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    return if !$Self->{DBObject}->Prepare(
+        SQL   => 'SELECT id FROM faq_voting WHERE item_id = ?',
+        Bind  => [ \$Param{ItemID} ],
+        Limit => $Param{Limit} || 500,
+    );
+
+    my @VoteIDs;
+    while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
+        push @VoteIDs, $Row[0];
+    }
+
+    return \@VoteIDs;
+}
+
+=item ItemVoteDataGet()
+
+Returns a hash reference with the number of votes and the vote result.
+
+    my $VoteDataHashRef = $FAQObject->ItemVoteDataGet(
+        ItemID => 123,
+        UserID => 1,
+    );
+
+Returns:
+
+    $VoteDataHashRef = {
+        Result => 75.0000,
+        Votes  => 5
+    };
+
+=cut
+
+sub ItemVoteDataGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(ItemID UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    # check cache
+    my $CacheKey = 'ItemVoteDataGet::' . $Param{ItemID};
+    my $Cache    = $Self->{CacheObject}->Get(
+        Type => 'FAQ',
+        Key  => $CacheKey,
+    );
+    return $Cache if $Cache;
+
+    # get vote from db
+    return if !$Self->{DBObject}->Prepare(
+        SQL   => 'SELECT count(*), avg(rate) FROM faq_voting WHERE item_id = ?',
+        Bind  => [ \$Param{ItemID} ],
+        Limit => $Param{Limit} || 500,
+    );
+
+    # fetch the result
+    my %Data;
+    if ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
+        $Data{Votes}  = $Row[0];
+        $Data{Result} = $Row[1];
+    }
+
+    # cache result
+    $Self->{CacheObject}->Set(
+        Type  => 'FAQ',
+        Key   => $CacheKey,
+        Value => \%Data,
+        TTL   => 60 * 60 * 24 * 2,
+    );
+
+    return \%Data;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (http://otrs.org/).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2013-06-30 00:58:12 $
+
+=cut
