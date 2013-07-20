7f7988f44243e3e80930401154f2d61185192d81
Michiel Beijen <michiel.beijen@otrs.com>
2013-07-20 21:26:09 +0200

    Fixed copyright.

--- var/packagesetup/ITSMIncidentProblemManagement.pm
+++ var/packagesetup/ITSMIncidentProblemManagement.pm
@@ -742,12 +742,10 @@ sub _GetITSMDynamicFieldsDefinition {
 
 =head1 TERMS AND CONDITIONS
 
-This Software is part of the OTRS project (L<http://otrs.org/>).
+This software is part of the OTRS project (L<http://otrs.org/>).
 
 This software comes with ABSOLUTELY NO WARRANTY. For details, see
-the enclosed file COPYING for license information (GPL). If you
-did not receive this file, see L<http://www.gnu.org/licenses/gpl-2.0.txt>.
-
-=cut
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =cut
