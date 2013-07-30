65c10e28b5f9a77e9741433c0845868273f1c4f0
Udo Bretz <udo.bretz@otrs.com>
2013-07-30 16:35:56 +0200

    Changed customer search autocomplete implementation to work with new OTRS 3.3.x style.

--- var/httpd/htdocs/js/ITSM.Agent.CustomerSearch.js
+++ var/httpd/htdocs/js/ITSM.Agent.CustomerSearch.js
@@ -25,68 +25,50 @@ ITSM.Agent.CustomerSearch = (function (TargetNS) {
         CustomerKey: ''
     };
 
-
     /**
      * @function
      * @param {jQueryObject} $Element The jQuery object of the input field with autocomplete
-     * @param {Boolean} ActiveAutoComplete Set to false, if autocomplete should only be started by click on a button next to the input field
      * @return nothing
      *      This function initializes the special module functions
      */
-    TargetNS.Init = function ($Element, ActiveAutoComplete) {
-
-        if (typeof ActiveAutoComplete === 'undefined') {
-            ActiveAutoComplete = true;
-        }
-        else {
-            ActiveAutoComplete = !!ActiveAutoComplete;
-        }
+    TargetNS.Init = function ($Element) {
 
         if (isJQueryObject($Element)) {
-            $Element.autocomplete({
-                minLength: ActiveAutoComplete ? Core.Config.Get('CustomerAutocomplete.MinQueryLength') : 500,
-                delay: Core.Config.Get('CustomerAutocomplete.QueryDelay'),
-                source: function (Request, Response) {
-                    var URL = Core.Config.Get('Baselink'), Data = {
-                        Action: 'AgentCustomerSearch',
-                        Term: Request.term,
-                        MaxResults: Core.Config.Get('CustomerAutocomplete.MaxResultsDisplayed')
-                    };
-                    Core.AJAX.FunctionCall(URL, Data, function (Result) {
-                        var Data = [];
-                        $.each(Result, function () {
-                            Data.push({
-                                label: this.CustomerValue + " (" + this.CustomerKey + ")",
-                                value: this.CustomerValue
+
+            Core.UI.Autocomplete.Init(
+                    $Element,
+                    function (Request, Response) {
+                        var URL = Core.Config.Get('Baselink'), Data = {
+                            Action: 'AgentCustomerSearch',
+                            Term: Request.term,
+                            MaxResults: Core.UI.Autocomplete.GetConfig('MaxResultsDisplayed')
+                        };
+
+                        $Element.data('AutoCompleteXHR', Core.AJAX.FunctionCall(URL, Data, function (Result) {
+                            var Data = [];
+                            $.each(Result, function () {
+                                Data.push({
+                                    label: this.CustomerValue + " (" + this.CustomerKey + ")",
+                                    value: this.CustomerValue
+                                });
                             });
-                        });
-                        Response(Data);
-                    });
-                },
-                select: function (Event, UI) {
+                            Response(Data);
+                        }));
+                    },
+                    function (Event, UI) {
 
-                    var CustomerKey = UI.item.label.replace(/.*\((.*)\)$/, '$1');
+                        var CustomerKey = UI.item.label.replace(/.*\((.*)\)$/, '$1');
 
-                    $Element.val(UI.item.value);
+                        $Element.val(UI.item.value);
 
-                    // set hidden field SelectedCustomerUser
-                    // escape possible colons (:) in element id because jQuery can not handle it in id attribute selectors
-                    $('#' + Core.App.EscapeSelector($Element.attr('id')) + 'Selected').val(CustomerKey);
+                        // set hidden field SelectedCustomerUser
+                        // escape possible colons (:) in element id because jQuery can not handle it in id attribute selectors
+                        $('#' + Core.App.EscapeSelector($Element.attr('id')) + 'Selected').val(CustomerKey);
 
-                    return false;
-                }
-            });
-
-            if (!ActiveAutoComplete) {
-
-                $Element.after('<button id="' + $Element.attr('id') + 'Search" type="button">' + Core.Config.Get('CustomerAutocomplete.SearchButtonText') + '</button>');
-                // escape possible colons (:) in element id because jQuery can not handle it in id attribute selectors
-                $('#' + Core.App.EscapeSelector($Element.attr('id')) + 'Search').click(function () {
-                    $Element.autocomplete("option", "minLength", 0);
-                    $Element.autocomplete("search");
-                    $Element.autocomplete("option", "minLength", 500);
-                });
-            }
+                        return false;
+                    },
+                    'CustomerSearch'
+            );
         }
 
         // On unload remove old selected data. If the page is reloaded (with F5) this data stays in the field and invokes an ajax request otherwise
