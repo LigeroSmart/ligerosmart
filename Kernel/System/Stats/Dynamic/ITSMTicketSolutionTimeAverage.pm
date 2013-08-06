ac57846476cfe9bc202fb45f95b445fafc3d1c06
Udo Bretz <udo.bretz@otrs.com>
2013-08-06 17:38:49 +0200

    Replaced %Param with %{$Self} in constructor calls for dynamic fields.

--- Kernel/System/Stats/Dynamic/ITSMTicketSolutionTimeAverage.pm
+++ Kernel/System/Stats/Dynamic/ITSMTicketSolutionTimeAverage.pm
@@ -47,8 +47,8 @@ sub new {
     $Self->{SLAObject}            = Kernel::System::SLA->new( %{$Self} );
     $Self->{TypeObject}           = Kernel::System::Type->new( %{$Self} );
     $Self->{GeneralCatalogObject} = Kernel::System::GeneralCatalog->new( %{$Self} );
-    $Self->{DynamicFieldObject}   = Kernel::System::DynamicField->new(%Param);
-    $Self->{BackendObject}        = Kernel::System::DynamicField::Backend->new(%Param);
+    $Self->{DynamicFieldObject}   = Kernel::System::DynamicField->new( %{$Self} );
+    $Self->{BackendObject}        = Kernel::System::DynamicField::Backend->new( %{$Self} );
 
     # get the dynamic fields for ticket object
     $Self->{DynamicField} = $Self->{DynamicFieldObject}->DynamicFieldListGet(
