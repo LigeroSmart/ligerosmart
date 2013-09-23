5bba1e6ed01746b252020602b8626310c9d111c0
Marc Nilius <marc.nilius@otrs.com>
2013-09-23 13:10:19 +0200

    Tidied.

--- var/packagesetup/TimeAccounting.pm
+++ var/packagesetup/TimeAccounting.pm
@@ -1,10 +1,10 @@
 # --
 # TimeAccounting.pm - code to excecute during package installation
-# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2013 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
 package var::packagesetup::TimeAccounting;
@@ -16,8 +16,6 @@ use Kernel::System::SysConfig;
 use Kernel::System::CacheInternal;
 use Kernel::System::Group;
 use Kernel::System::Valid;
-
-use vars qw(@ISA);
 
 =head1 NAME
 
