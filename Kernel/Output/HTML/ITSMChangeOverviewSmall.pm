497573ba0c0faf327aeb5a8d6cfd63a4877ed7af
Udo Bretz <udo.bretz@otrs.com>
2013-10-07 19:40:32 +0200

    Fixed sorting icons for CI overviews. Thanks to Carlos Rodriguez!

--- Kernel/Output/HTML/ITSMChangeOverviewSmall.pm
+++ Kernel/Output/HTML/ITSMChangeOverviewSmall.pm
@@ -122,7 +122,7 @@ sub Run {
         for my $Column (@ShowColumns) {
 
             # create needed veriables
-            my $CSS = '';
+            my $CSS = 'OverviewHeader';
             my $OrderBy;
 
             # remove ID if necesary
@@ -144,11 +144,11 @@ sub Run {
             if ( $Param{SortBy} && ( $Param{SortBy} eq $Column ) ) {
                 if ( $Param{OrderBy} && ( $Param{OrderBy} eq 'Up' ) ) {
                     $OrderBy = 'Down';
-                    $CSS .= ' SortDescending';
+                    $CSS .= ' SortDescendingLarge';
                 }
                 else {
                     $OrderBy = 'Up';
-                    $CSS .= ' SortAscending';
+                    $CSS .= ' SortAscendingLarge';
                 }
             }
             else {
