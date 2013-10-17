bc6c835cc205bb77b2586c80d3ed7f8c2ca25bc5
Udo Bretz <udo.bretz@otrs.com>
2013-10-17 13:33:30 +0200

    Fixed bug 9830 - New user preference setting "Screen after creating a workorder" has no effect on AgentITSMWorkOrderAddFromTemplate.

--- Kernel/Modules/AgentITSMWorkOrderAddFromTemplate.pm
+++ Kernel/Modules/AgentITSMWorkOrderAddFromTemplate.pm
@@ -199,9 +199,20 @@ sub Run {
                 );
             }
 
+            # get redirect screen
+            my $NextScreen = $Self->{UserCreateWorkOrderNextMask} || 'AgentITSMWorkOrderZoom';
+
+            # add the correct id
+            if ( $NextScreen eq 'AgentITSMWorkOrderZoom' ) {
+                $NextScreen .= ";WorkOrderID=$WorkOrderID";
+            }
+            elsif ( $NextScreen eq 'AgentITSMChangeZoom' ) {
+                $NextScreen .= ";ChangeID=$ChangeID";
+            }
+
             # load new URL in parent window and close popup
             return $Self->{LayoutObject}->PopupClose(
-                URL => "Action=AgentITSMWorkOrderZoom;WorkOrderID=$WorkOrderID",
+                URL => "Action=$NextScreen",
             );
         }
     }
