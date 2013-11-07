2662de7bf724adcbfc0fc1e5cb25534f8aee5f2f
Udo Bretz <udo.bretz@otrs.com>
2013-11-07 15:14:47 +0100

    Updated simplified chinese translation. Thanks to Michael Shi!

--- Kernel/Language/zh_CN_ITSMTicket.pm
+++ Kernel/Language/zh_CN_ITSMTicket.pm
@@ -16,33 +16,33 @@ sub Data {
     my $Self = shift;
 
     # Template: AAAITSMIncidentProblemManagement
-    $Self->{Translation}->{'Add decision to ticket'} = '增加决定到 Ticket';
+    $Self->{Translation}->{'Add decision to ticket'} = '增加决定';
     $Self->{Translation}->{'Decision Date'} = '决定日期';
     $Self->{Translation}->{'Decision Result'} = '决定结果';
     $Self->{Translation}->{'Due Date'} = '截止日期';
     $Self->{Translation}->{'Reason'} = '理由';
-    $Self->{Translation}->{'Recovery Start Time'} = '恢复运作时间';
+    $Self->{Translation}->{'Recovery Start Time'} = '恢复开始时间';
     $Self->{Translation}->{'Repair Start Time'} = '修复开始时间';
-    $Self->{Translation}->{'Review Required'} = '审查申请';
-    $Self->{Translation}->{'closed with workaround'} = '替代方法而关闭';
+    $Self->{Translation}->{'Review Required'} = '需要复审';
+    $Self->{Translation}->{'closed with workaround'} = '关闭(变通)';
 
     # Template: AgentTicketActionCommon
-    $Self->{Translation}->{'Change Decision of Ticket'} = '';
-    $Self->{Translation}->{'Change ITSM fields of ticket'} = '为该 Ticket 更改 ITSM 域';
-    $Self->{Translation}->{'Service Incident State'} = '';
+    $Self->{Translation}->{'Change Decision of Ticket'} = '修改工单决定';
+    $Self->{Translation}->{'Change ITSM fields of ticket'} = '修改ITSM字段';
+    $Self->{Translation}->{'Service Incident State'} = '服务故障状态';
 
     # Template: AgentTicketAddtlITSMField
 
     # Template: AgentTicketDecision
 
     # Template: AgentTicketEmail
-    $Self->{Translation}->{'Link ticket'} = '';
+    $Self->{Translation}->{'Link ticket'} = '链接工单';
 
     # Template: AgentTicketOverviewMedium
 
     # Template: AgentTicketOverviewPreview
-    $Self->{Translation}->{'Criticality'} = '';
-    $Self->{Translation}->{'Impact'} = '';
+    $Self->{Translation}->{'Criticality'} = '重要';
+    $Self->{Translation}->{'Impact'} = '影响';
 
     # Template: AgentTicketPhone
 
@@ -55,13 +55,13 @@ sub Data {
     # Template: CustomerTicketZoom
 
     # SysConfig
-    $Self->{Translation}->{'Add a decision!'} = '增加一个决议!';
-    $Self->{Translation}->{'Additional ITSM Fields'} = '额外的 ITSM 域';
+    $Self->{Translation}->{'Add a decision!'} = '添加决定!';
+    $Self->{Translation}->{'Additional ITSM Fields'} = 'ITSM字段';
     $Self->{Translation}->{'Allows adding notes in the additional ITSM field screen of the agent interface.'} =
         '';
     $Self->{Translation}->{'Allows adding notes in the decision screen of the agent interface.'} =
         '';
-    $Self->{Translation}->{'Change the ITSM fields!'} = '更改 ITSM 域!';
+    $Self->{Translation}->{'Change the ITSM fields!'} = '修改ITSM字段!';
     $Self->{Translation}->{'Decision'} = '决定';
     $Self->{Translation}->{'Defines if a ticket lock is required in the additional ITSM field screen of the agent interface (if the ticket isn\'t locked yet, the ticket gets locked and the current agent will be set automatically as its owner).'} =
         '';
@@ -161,7 +161,7 @@ sub Data {
     #
     # OBSOLETE ENTRIES FOR REFERENCE, DO NOT TRANSLATE!
     #
-    $Self->{Translation}->{'Approved'} = '批准';
+    $Self->{Translation}->{'Approved'} = '通过审批';
     $Self->{Translation}->{'Postponed'} = '推迟';
     $Self->{Translation}->{'Pre-approved'} = '预先核准';
     $Self->{Translation}->{'Rejected'} = '拒绝';
