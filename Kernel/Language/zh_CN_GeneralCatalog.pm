cc0761bc6720a146290b92b81bc03f10af19c7f6
Udo Bretz <udo.bretz@otrs.com>
2013-11-07 15:09:46 +0100

    Updated simplified chinese translation. Thanks to Michael Shi!

--- Kernel/Language/zh_CN_GeneralCatalog.pm
+++ Kernel/Language/zh_CN_GeneralCatalog.pm
@@ -16,19 +16,19 @@ sub Data {
     my $Self = shift;
 
     # Template: AAAGeneralCatalog
-    $Self->{Translation}->{'Functionality'} = '功能用途';
+    $Self->{Translation}->{'Functionality'} = '功能';
 
     # Template: AdminGeneralCatalog
-    $Self->{Translation}->{'General Catalog Management'} = '总目录管理';
-    $Self->{Translation}->{'Add Catalog Item'} = '增加目录项目';
-    $Self->{Translation}->{'Add Catalog Class'} = '增加新目录分级';
-    $Self->{Translation}->{'Catalog Class'} = '目录分级';
+    $Self->{Translation}->{'General Catalog Management'} = '目录管理';
+    $Self->{Translation}->{'Add Catalog Item'} = '添加目录项目';
+    $Self->{Translation}->{'Add Catalog Class'} = '添加新目录类';
+    $Self->{Translation}->{'Catalog Class'} = '目录类';
 
     # SysConfig
-    $Self->{Translation}->{'Create and manage the General Catalog.'} = '';
+    $Self->{Translation}->{'Create and manage the General Catalog.'} = '创建和管理目录';
     $Self->{Translation}->{'Frontend module registration for the AdminGeneralCatalog configuration in the admin area.'} =
         '';
-    $Self->{Translation}->{'General Catalog'} = '';
+    $Self->{Translation}->{'General Catalog'} = '目录';
     $Self->{Translation}->{'Parameters for the example comment 2 of the general catalog attributes.'} =
         '';
     $Self->{Translation}->{'Parameters for the example permission groups of the general catalog attributes.'} =
