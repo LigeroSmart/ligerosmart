34640f5c6b55b1f349b0a6ebcd1ff05c7f4f42ba
Udo Bretz <udo.bretz@otrs.com>
2013-11-07 15:23:20 +0100

    Updated simplified chinese translation. Thanks to Michael Shi!

--- Kernel/Language/zh_CN_ImportExport.pm
+++ Kernel/Language/zh_CN_ImportExport.pm
@@ -20,49 +20,54 @@ sub Data {
     $Self->{Translation}->{'Charset'} = '字符集';
     $Self->{Translation}->{'Colon (:)'} = '冒号 (:)';
     $Self->{Translation}->{'Column'} = '列';
+    $Self->{Translation}->{'Comma (,)'} = '逗号(,)';
     $Self->{Translation}->{'Column Separator'} = '列分隔符';
     $Self->{Translation}->{'Dot (.)'} = '句号 (.)';
     $Self->{Translation}->{'Semicolon (;)'} = '分号 (;)';
     $Self->{Translation}->{'Tabulator (TAB)'} = '制表键 (TAB)';
-    $Self->{Translation}->{'Include Column Headers'} = '';
-    $Self->{Translation}->{'Import summary for'} = '';
-    $Self->{Translation}->{'Imported records'} = '';
-    $Self->{Translation}->{'Exported records'} = '';
-    $Self->{Translation}->{'Records'} = '';
-    $Self->{Translation}->{'Skipped'} = '';
+    $Self->{Translation}->{'Include Column Headers'} = '包括列标题';
+    $Self->{Translation}->{'Import summary for'} = '导入总结';
+    $Self->{Translation}->{'Imported records'} = '导入记录';
+    $Self->{Translation}->{'Exported records'} = '导出记录';
+    $Self->{Translation}->{'Records'} = '记录';
+    $Self->{Translation}->{'Skipped'} = '跳过的';
 
     # Template: AdminImportExport
     $Self->{Translation}->{'Import/Export Management'} = '导入/导出管理';
-    $Self->{Translation}->{'Create a template to import and export object information.'} = '';
+    $Self->{Translation}->{'Create a template to import and export object information.'} = '创建模板导入和导出对象信息。';
     $Self->{Translation}->{'Start Import'} = '开始导入';
     $Self->{Translation}->{'Start Export'} = '开始导出';
-    $Self->{Translation}->{'Delete Template'} = '';
+    $Self->{Translation}->{'Delete Template'} = '删除模板';
     $Self->{Translation}->{'Step'} = '步骤';
     $Self->{Translation}->{'Edit common information'} = '编辑共用信息';
-    $Self->{Translation}->{'Name is required!'} = '';
-    $Self->{Translation}->{'Object is required!'} = '';
-    $Self->{Translation}->{'Format is required!'} = '';
+    $Self->{Translation}->{'The name is required!'} = '名称是必需的！';
+    $Self->{Translation}->{'Object is required!'} = '对象是必需的！';
+    $Self->{Translation}->{'Format is required!'} = '格式是必需的';
     $Self->{Translation}->{'Edit object information'} = '编辑对像信息';
     $Self->{Translation}->{'Edit format information'} = '编辑格式信息';
-    $Self->{Translation}->{' is required!'} = '';
+    $Self->{Translation}->{' is required!'} = '是必需的!';
     $Self->{Translation}->{'Edit mapping information'} = '编辑映射信息';
-    $Self->{Translation}->{'No map elements found.'} = '';
-    $Self->{Translation}->{'Add Mapping Element'} = '';
+    $Self->{Translation}->{'No map elements found.'} = '没有找到映射的字段';
+    $Self->{Translation}->{'Add Mapping Element'} = '添加映射字段';
     $Self->{Translation}->{'Edit search information'} = '编辑搜索信息';
     $Self->{Translation}->{'Restrict export per search'} = '限制导出每个搜寻';
     $Self->{Translation}->{'Import information'} = '导入信息';
     $Self->{Translation}->{'Source File'} = '源文件';
-    $Self->{Translation}->{'Success'} = '';
-    $Self->{Translation}->{'Failed'} = '';
-    $Self->{Translation}->{'Duplicate names'} = '';
-    $Self->{Translation}->{'Last processed line number of import file'} = '';
+    $Self->{Translation}->{'Success'} = '成功';
+    $Self->{Translation}->{'Failed'} = '失败';
+    $Self->{Translation}->{'Duplicate names'} = '重复的名称';
+    $Self->{Translation}->{'Last processed line number of import file'} = '导入文件最后处理的行数';
     $Self->{Translation}->{'Ok'} = '';
+    $Self->{Translation}->{'Template List'} = '模板列表';
+    $Self->{Translation}->{'Empty fields indicate that the current values are kept'} = '空字段表示保持当前的值';
+    $Self->{Translation}->{'Column Separator is required!'} = '必须指定列分隔符!';
 
     # SysConfig
     $Self->{Translation}->{'Format backend module registration for the import/export module.'} =
         '';
-    $Self->{Translation}->{'Import and export object information.'} = '';
+    $Self->{Translation}->{'Import and export object information.'} = '导入和导出对象信息';
     $Self->{Translation}->{'Import/Export'} = '导入/导出';
+
 
     #
     # OBSOLETE ENTRIES FOR REFERENCE, DO NOT TRANSLATE!
