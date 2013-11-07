1a674c8280a6ebd6165f217b9cfb85c7f839ae12
Udo Bretz <udo.bretz@otrs.com>
2013-11-07 15:14:20 +0100

    Updated simplified chinese translation. Thanks to Michael Shi!

--- Kernel/Language/zh_CN_ITSMCore.pm
+++ Kernel/Language/zh_CN_ITSMCore.pm
@@ -16,49 +16,49 @@ sub Data {
     my $Self = shift;
 
     # Template: AAAITSMCore
-    $Self->{Translation}->{'Alternative to'} = '选择对象';
-    $Self->{Translation}->{'Availability'} = '供货情况';
+    $Self->{Translation}->{'Alternative to'} = '可供选择';
+    $Self->{Translation}->{'Availability'} = '可用性';
     $Self->{Translation}->{'Back End'} = '后端';
-    $Self->{Translation}->{'Connected to'} = '连接到';
-    $Self->{Translation}->{'Current State'} = '现状';
-    $Self->{Translation}->{'Demonstration'} = '示范';
-    $Self->{Translation}->{'Depends on'} = '取决于';
+    $Self->{Translation}->{'Connected to'} = '连接';
+    $Self->{Translation}->{'Current State'} = '当前状态';
+    $Self->{Translation}->{'Demonstration'} = '演示';
+    $Self->{Translation}->{'Depends on'} = '依赖';
     $Self->{Translation}->{'End User Service'} = '最终用户服务';
     $Self->{Translation}->{'Errors'} = '错误';
     $Self->{Translation}->{'Front End'} = '前端';
-    $Self->{Translation}->{'IT Management'} = 'IT 管理';
-    $Self->{Translation}->{'IT Operational'} = 'IT 运营';
-    $Self->{Translation}->{'Impact'} = '影响度';
-    $Self->{Translation}->{'Incident State'} = '事件状态';
+    $Self->{Translation}->{'IT Management'} = 'IT管理';
+    $Self->{Translation}->{'IT Operational'} = 'IT运营';
+    $Self->{Translation}->{'Impact'} = '影响';
+    $Self->{Translation}->{'Incident State'} = '故障状态';
     $Self->{Translation}->{'Includes'} = '包括';
     $Self->{Translation}->{'Other'} = '其它';
-    $Self->{Translation}->{'Part of'} = '部分于';
+    $Self->{Translation}->{'Part of'} = '属于';
     $Self->{Translation}->{'Project'} = '项目';
     $Self->{Translation}->{'Recovery Time'} = '恢复时间';
     $Self->{Translation}->{'Relevant to'} = '相关';
     $Self->{Translation}->{'Reporting'} = '报告';
-    $Self->{Translation}->{'Required for'} = '必需的';
-    $Self->{Translation}->{'Resolution Rate'} = '解决进度';
+    $Self->{Translation}->{'Required for'} = '需要';
+    $Self->{Translation}->{'Resolution Rate'} = '解决率';
     $Self->{Translation}->{'Response Time'} = '响应时间';
-    $Self->{Translation}->{'SLA Overview'} = 'SLA 概述';
+    $Self->{Translation}->{'SLA Overview'} = 'SLA概述';
     $Self->{Translation}->{'Service Overview'} = '服务概述';
     $Self->{Translation}->{'Service-Area'} = '服务区';
-    $Self->{Translation}->{'Training'} = '训练';
+    $Self->{Translation}->{'Training'} = '培训';
     $Self->{Translation}->{'Transactions'} = '交易';
     $Self->{Translation}->{'Underpinning Contract'} = '依据合同';
     $Self->{Translation}->{'allocation'} = '分配';
 
     # Template: AdminITSMCIPAllocate
-    $Self->{Translation}->{'Criticality <-> Impact <-> Priority'} = '危急程度 <-> 影响度 <-> 优先级别';
+    $Self->{Translation}->{'Criticality <-> Impact <-> Priority'} = '重要 <-> 影响 <-> 优先级';
     $Self->{Translation}->{'Manage the priority result of combinating Criticality <-> Impact.'} =
-        '';
-    $Self->{Translation}->{'Priority allocation'} = '';
+        '"重要 <-> 影响"之间的组合决定优先级';
+    $Self->{Translation}->{'Priority allocation'} = '优先级分配';
 
     # Template: AdminSLA
     $Self->{Translation}->{'Minimum Time Between Incidents'} = '最短的时间与事件';
 
     # Template: AdminService
-    $Self->{Translation}->{'Criticality'} = '危急程度';
+    $Self->{Translation}->{'Criticality'} = '重要';
 
     # Template: AgentITSMCustomerSearch
 
@@ -66,23 +66,23 @@ sub Data {
 
     # Template: AgentITSMSLAPrint
     $Self->{Translation}->{'SLA-Info'} = '';
-    $Self->{Translation}->{'Last changed'} = '';
-    $Self->{Translation}->{'Last changed by'} = '';
+    $Self->{Translation}->{'Last changed'} = '上次修改于';
+    $Self->{Translation}->{'Last changed by'} = '上次修改人';
 
     # Template: AgentITSMSLAZoom
-    $Self->{Translation}->{'SLA Information'} = '';
+    $Self->{Translation}->{'SLA Information'} = 'SLA信息';
     $Self->{Translation}->{'Associated Services'} = '关联的服务';
 
     # Template: AgentITSMService
 
     # Template: AgentITSMServicePrint
     $Self->{Translation}->{'Service-Info'} = '';
-    $Self->{Translation}->{'Current Incident State'} = '当前事件状态';
-    $Self->{Translation}->{'Associated SLAs'} = '关联的 SLAs';
+    $Self->{Translation}->{'Current Incident State'} = '当前故障状态';
+    $Self->{Translation}->{'Associated SLAs'} = '关联的SLAs';
 
     # Template: AgentITSMServiceZoom
-    $Self->{Translation}->{'Service Information'} = '';
-    $Self->{Translation}->{'Current incident state'} = '';
+    $Self->{Translation}->{'Service Information'} = '服务信息';
+    $Self->{Translation}->{'Current incident state'} = '当前故障状态';
 
     # SysConfig
     $Self->{Translation}->{'Frontend module registration for the AdminITSMCIPAllocate configuration in the admin area.'} =
@@ -99,7 +99,7 @@ sub Data {
         '';
     $Self->{Translation}->{'Frontend module registration for the AgentITSMServiceZoom object in the agent interface.'} =
         '';
-    $Self->{Translation}->{'Manage priority matrix.'} = '';
+    $Self->{Translation}->{'Manage priority matrix.'} = '管理优先级矩阵';
     $Self->{Translation}->{'Module to show back link in service menu.'} = '';
     $Self->{Translation}->{'Module to show back link in sla menu.'} = '';
     $Self->{Translation}->{'Module to show print link in service menu.'} = '';
@@ -165,6 +165,11 @@ sub Data {
     $Self->{Translation}->{'This setting defines the link type \'RelevantTo\'. If the source name and the target name contain the same value, the resulting link is a non-directional one. If the values are different, the resulting link is a directional link.'} =
         '';
     $Self->{Translation}->{'Width of ITSM textareas.'} = '';
+
+    # Missed Translation Items
+    $Self->{Translation}->{'Category <-> Impact <-> Priority'} = '类别 <-> 影响 <-> 优先级';
+    $Self->{Translation}->{'Manage the priority result of combinating Category <-> Impact.'} = '"类别 <-> 影响"之间的组合决定优先级';
+    $Self->{Translation}->{'Admin of the CIP matrix.'} = '管理CIP矩阵';
 
     #
     # OBSOLETE ENTRIES FOR REFERENCE, DO NOT TRANSLATE!
