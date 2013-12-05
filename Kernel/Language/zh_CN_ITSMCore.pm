48e65cbd4774c7ceac1a293afe67d13716fd4d7c
Udo Bretz <udo.bretz@otrs.com>
2013-12-05 12:26:28 +0100

    Updated simplified chinese translation. Thanks to Michael Shi!

--- Kernel/Language/zh_CN_ITSMCore.pm
+++ Kernel/Language/zh_CN_ITSMCore.pm
@@ -2,6 +2,8 @@
 # Kernel/Language/zh_CN_ITSMCore.pm - translation file
 # Copyright (C) 2001-2013 OTRS AG, http://otrs.com/
 # --
+# Copyright (C) 2013 Michael Shi <micshi at 163.com>
+# --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
@@ -55,7 +57,7 @@ sub Data {
     $Self->{Translation}->{'Priority allocation'} = '优先级分配';
 
     # Template: AdminSLA
-    $Self->{Translation}->{'Minimum Time Between Incidents'} = '最短的时间与事件';
+    $Self->{Translation}->{'Minimum Time Between Incidents'} = '故障间最短时间';
 
     # Template: AdminService
     $Self->{Translation}->{'Criticality'} = '重要';
