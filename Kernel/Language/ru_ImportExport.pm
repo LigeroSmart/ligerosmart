2980f27cd324926b4d56dc39c943a6d8cea712ab
Victor Merkushov <merkushov.victor@gmail.com>
2013-12-20 22:44:16 +0400

    Updated Russian translation

--- Kernel/Language/ru_ImportExport.pm
+++ Kernel/Language/ru_ImportExport.pm
@@ -1,6 +1,7 @@
 # --
 # Kernel/Language/ru_ImportExport.pm - translation file
 # Copyright (C) 2001-2013 OTRS AG, http://otrs.com/
+# Copyright (C) 2013 Yuriy Kolesnikov <ynkolesnikov at gmail.com>
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -24,43 +25,43 @@ sub Data {
     $Self->{Translation}->{'Dot (.)'} = 'Точка (.)';
     $Self->{Translation}->{'Semicolon (;)'} = 'Точка с запятой (;)';
     $Self->{Translation}->{'Tabulator (TAB)'} = 'Табуляция (TAB)';
-    $Self->{Translation}->{'Include Column Headers'} = '';
-    $Self->{Translation}->{'Import summary for'} = '';
-    $Self->{Translation}->{'Imported records'} = '';
-    $Self->{Translation}->{'Exported records'} = '';
-    $Self->{Translation}->{'Records'} = '';
-    $Self->{Translation}->{'Skipped'} = '';
+    $Self->{Translation}->{'Include Column Headers'} = 'Включить заголовки столбцов';
+    $Self->{Translation}->{'Import summary for'} = 'Отчет об импорте';
+    $Self->{Translation}->{'Imported records'} = 'Импортировано записей';
+    $Self->{Translation}->{'Exported records'} = 'Экспортировано записей';
+    $Self->{Translation}->{'Records'} = 'Записей';
+    $Self->{Translation}->{'Skipped'} = 'Пропущено';
 
     # Template: AdminImportExport
     $Self->{Translation}->{'Import/Export Management'} = 'Управление Импортом/Экспортом';
-    $Self->{Translation}->{'Create a template to import and export object information.'} = '';
+    $Self->{Translation}->{'Create a template to import and export object information.'} = 'Создайте шаблон для импорта и экспорта информации объектов';
     $Self->{Translation}->{'Start Import'} = 'Начать импорт';
     $Self->{Translation}->{'Start Export'} = 'Начать экспорт';
-    $Self->{Translation}->{'Delete Template'} = '';
+    $Self->{Translation}->{'Delete Template'} = 'Удалить шаблон';
     $Self->{Translation}->{'Step'} = 'Шаг';
     $Self->{Translation}->{'Edit common information'} = 'Редактировать общую информацию';
-    $Self->{Translation}->{'Object is required!'} = '';
-    $Self->{Translation}->{'Format is required!'} = '';
+    $Self->{Translation}->{'Object is required!'} = 'Объект обязателен!';
+    $Self->{Translation}->{'Format is required!'} = 'Формат обязателен!';
     $Self->{Translation}->{'Edit object information'} = 'Редактировать информацию об объекте';
     $Self->{Translation}->{'Edit format information'} = 'Редактировать формат данных';
-    $Self->{Translation}->{' is required!'} = '';
+    $Self->{Translation}->{' is required!'} = 'обязателен!';
     $Self->{Translation}->{'Edit mapping information'} = 'Редактировать информацию соответствия';
-    $Self->{Translation}->{'No map elements found.'} = '';
-    $Self->{Translation}->{'Add Mapping Element'} = '';
+    $Self->{Translation}->{'No map elements found.'} = 'Нет элементов сопоставления';
+    $Self->{Translation}->{'Add Mapping Element'} = 'Добавьте элемент сопоставления';
     $Self->{Translation}->{'Edit search information'} = 'Редактировать поисковую информацию';
     $Self->{Translation}->{'Restrict export per search'} = 'Ограничить экспорт поиском';
     $Self->{Translation}->{'Import information'} = 'Информация импорта';
     $Self->{Translation}->{'Source File'} = 'Исходный файл';
-    $Self->{Translation}->{'Success'} = '';
-    $Self->{Translation}->{'Failed'} = '';
-    $Self->{Translation}->{'Duplicate names'} = '';
-    $Self->{Translation}->{'Last processed line number of import file'} = '';
+    $Self->{Translation}->{'Success'} = 'Успешно';
+    $Self->{Translation}->{'Failed'} = 'Не удалось выполнить';
+    $Self->{Translation}->{'Duplicate names'} = 'Дублирующие имена';
+    $Self->{Translation}->{'Last processed line number of import file'} = 'Номер последней обработанной строки импортируемого файла';
     $Self->{Translation}->{'Ok'} = '';
 
     # SysConfig
     $Self->{Translation}->{'Format backend module registration for the import/export module.'} =
         '';
-    $Self->{Translation}->{'Import and export object information.'} = '';
+    $Self->{Translation}->{'Import and export object information.'} = 'Импорт и экспорт информации об объектах';
     $Self->{Translation}->{'Import/Export'} = 'Импорт/Экспорт';
 
     #
