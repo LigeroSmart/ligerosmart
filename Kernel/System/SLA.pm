b9782c468c398a1808f5345e9c17f5bf96a07462
Udo Bretz <udo.bretz@otrs.com>
2014-05-08 23:52:45 +0200

    Updated files to latest framework version.

--- Kernel/System/SLA.pm
+++ Kernel/System/SLA.pm
@@ -2,7 +2,7 @@
 # Kernel/System/SLA.pm - all sla functions
 # Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
 # --
-# $origin: https://github.com/OTRS/otrs/blob/430e91d1bf9a5396fad9df71641dd5d0f54592b0/Kernel/System/SLA.pm
+# $origin: https://github.com/OTRS/otrs/blob/0f77f00d00ab28ff64bf39a42d3dfe43e249d668/Kernel/System/SLA.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
