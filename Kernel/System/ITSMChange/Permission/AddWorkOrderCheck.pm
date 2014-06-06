d4b3ce402112dba29f9e648dcf70d1ab9de236f7
Udo Bretz <udo.bretz@otrs.com>
2014-06-06 17:49:14 +0200

    Improved the permission checks for AgentITSMWorkOrderAdd and AgentITSMWorkOrderAddFromTemplate.

---
+++ Kernel/System/ITSMChange/Permission/AddWorkOrderCheck.pm
@@ -0,0 +1,213 @@
+# --
+# Kernel/System/ITSMChange/Permission/AddWorkOrderCheck.pm - WorkOrderAdd and WorkOrderAddFromTemplate permission check
+# Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::ITSMChange::Permission::AddWorkOrderCheck;
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+Kernel::System::ITSMChange::Permission::AddWorkOrderCheck - WorkOrderAdd and WorkOrderAddFromTemplate permission check
+
+=head1 SYNOPSIS
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+    use Kernel::Config;
+    use Kernel::System::Encode;
+    use Kernel::System::Log;
+    use Kernel::System::Main;
+    use Kernel::System::Time;
+    use Kernel::System::DB;
+    use Kernel::System::ITSMChange;
+    use Kernel::System::User;
+    use Kernel::System::Group;
+    use Kernel::System::ITSMChange::Permission::AddWorkOrderCheck;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $EncodeObject = Kernel::System::Encode->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $LogObject = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+    );
+    my $MainObject = Kernel::System::Main->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+    );
+    my $TimeObject = Kernel::System::Time->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+    );
+    my $DBObject = Kernel::System::DB->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+    );
+    my $UserObject = Kernel::System::User->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+        TimeObject   => $TimeObject,
+        DBObject     => $DBObject,
+        EncodeObject => $EncodeObject,
+    );
+    my $GroupObject = Kernel::System::Group->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+        DBObject     => $DBObject,
+    );
+    my $ChangeObject = Kernel::System::ITSMChange->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        DBObject     => $DBObject,
+        TimeObject   => $TimeObject,
+        MainObject   => $MainObject,
+    );
+    my $CheckObject = Kernel::System::ITSMChange::Permission::AddWorkOrderCheck->new(
+        ConfigObject         => $ConfigObject,
+        EncodeObject         => $EncodeObject,
+        LogObject            => $LogObject,
+        MainObject           => $MainObject,
+        TimeObject           => $TimeObject,
+        DBObject             => $DBObject,
+        UserObject           => $UserObject,
+        GroupObject          => $GroupObject,
+        ChangeObject         => $ChangeObject,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for my $Object (
+        qw(ConfigObject EncodeObject LogObject MainObject TimeObject DBObject UserObject GroupObject ChangeObject)
+        )
+    {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    return $Self;
+}
+
+=item Run()
+
+This method does the check. 'ro' and 'rw' access is granted
+when the agent has the correct privileges in the group defined in the
+frontend module registration.
+
+    my $HasAccess = $CheckObject->Run(
+        UserID   => 123,
+        Type     => 'rw',     # 'ro' or 'rw'
+        ChangeID => 3333,     # optional for ChangeAdd
+    );
+
+=cut
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(UserID Type)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+
+            return;
+        }
+    }
+
+    # if no action is given, pass the checks to the following modules
+    return 1 if !$Param{Action};
+
+    # if no ChangeID is given, pass the checks to the following modules
+    return 1 if !$Param{ChangeID};
+
+    # access is passed to other permission modules if the action is none of the below
+    if ( $Param{Action} ne 'AgentITSMWorkOrderAdd' && $Param{Action} ne 'AgentITSMWorkOrderAddFromTemplate' ) {
+        return 1;
+    }
+
+    # get config for the relevant action
+    my $FrontendConfig
+        = $Self->{ConfigObject}->Get("ITSMChange::Frontend::$Param{Action}");
+
+    # get the required privilege, 'ro' or 'rw'
+    my $RequiredPriv;
+    if ( $FrontendConfig && $FrontendConfig->{Permission} ) {
+
+        # get the required priv from the frontend configuration
+        $RequiredPriv = $FrontendConfig->{Permission};
+    }
+
+    # access is passed to other permission modules if there is no required privilege
+    return 1 if !$RequiredPriv;
+
+    # get the required group for the frontend module
+    my $Group = $Self->{ConfigObject}->Get('Frontend::Module')->{ $Param{Action} }
+        ->{GroupRo}->[0];
+
+    # deny access, when the group is not found
+    return $Param{Counter} if !$Group;
+
+    # get the group id
+    my $GroupID = $Self->{GroupObject}->GroupLookup( Group => $Group );
+
+    # deny access, when the group is not found
+    return $Param{Counter} if !$GroupID;
+
+    # get user groups, where the user has the appropriate privilege
+    my %Groups = $Self->{GroupObject}->GroupMemberList(
+        UserID => $Param{UserID},
+        Type   => $RequiredPriv,
+        Result => 'HASH',
+    );
+
+    # access is passed to other permission modules if the agent has the appropriate type in the appropriate group
+    return 1 if $Groups{$GroupID};
+
+    # deny access otherwise
+    return;
+
+}
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (http://otrs.org/).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
+
+1;
