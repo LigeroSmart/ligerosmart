b57677f712621e3798801c9cc724bb2ef5e23473
Udo Bretz <udo.bretz@otrs.com>
2014-08-06 15:50:14 +0200

    Added event module to handle change condition updates if chnage or workorder dynamic fields are added, updated or deleted.

---
+++ Kernel/System/DynamicField/Event/UpdateITSMChangeConditions.pm
@@ -0,0 +1,50 @@
+# --
+# Kernel/System/DynamicField/Event/UpdateITSMChangeConditions.pm - event handler module for dynamic fields
+# Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::DynamicField::Event::UpdateITSMChangeConditions;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(:all);
+
+our @ObjectDependencies = (
+    'Kernel::System::Cache',
+    'Kernel::System::Log',
+);
+# our $ObjectManagerAware = 1;
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw(Data Event Config)) {
+        if ( !$Param{$_} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message => "Need $_!",
+            );
+            return;
+        }
+    }
+
+    return 1;
+}
+
+1;
