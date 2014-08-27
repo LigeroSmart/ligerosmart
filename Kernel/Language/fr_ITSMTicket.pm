96db50fc7043400b328e33aa87997d73d1e775a1
Udo Bretz <udo.bretz@otrs.com>
2014-08-27 20:52:52 +0200

    Added utf8 pragma.

--- Kernel/Language/fr_ITSMTicket.pm
+++ Kernel/Language/fr_ITSMTicket.pm
@@ -11,6 +11,7 @@ package Kernel::Language::fr_ITSMTicket;
 
 use strict;
 use warnings;
+use utf8;
 
 sub Data {
     my $Self = shift;
--- Kernel/Language/zh_CN_ITSMTicket.pm
+++ Kernel/Language/zh_CN_ITSMTicket.pm
@@ -13,6 +13,7 @@ package Kernel::Language::zh_CN_ITSMTicket;
 
 use strict;
 use warnings;
+use utf8;
 
 sub Data {
     my $Self = shift;
