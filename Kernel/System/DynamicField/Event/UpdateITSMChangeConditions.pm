9082706b4bc959661fdaf1f4a7d1eb53c5c5b9a5
Udo Bretz <udo.bretz@otrs.com>
2014-09-02 08:17:51 +0200

    Implemented evennt handler to for dynamic field events, so that conditions will be updated.

--- Kernel/System/DynamicField/Event/UpdateITSMChangeConditions.pm
+++ Kernel/System/DynamicField/Event/UpdateITSMChangeConditions.pm
@@ -15,6 +15,9 @@ use warnings;
 use Kernel::System::VariableCheck qw(:all);
 
 our @ObjectDependencies = (
+    'Kernel::System::Cache',
+    'Kernel::System::DB',
+    'Kernel::System::ITSMChange::ITSMCondition',
     'Kernel::System::Log',
 );
 
@@ -32,7 +35,7 @@ sub Run {
     my ( $Self, %Param ) = @_;
 
     # check needed stuff
-    for my $Argument (qw(Data Event Config)) {
+    for my $Argument (qw(Data Event Config UserID)) {
         if ( !$Param{$Argument} ) {
 
             $Kernel::OM->Get('Kernel::System::Log')->Log(
@@ -44,19 +47,113 @@ sub Run {
         }
     }
 
-    # TODO: Implement event handler (like described below)
+    # only for dynamic fields of the type ITSMChange or ITSMWorkOrder
+    return if !$Param{Data}->{NewData};
+    return if !$Param{Data}->{NewData}->{ObjectType};
+    if (
+        $Param{Data}->{NewData}->{ObjectType} ne 'ITSMChange'
+        && $Param{Data}->{NewData}->{ObjectType} ne 'ITSMWorkOrder'
+        )
+    {
+        return;
+    }
 
-    # return if DynamicField ObjectType is not ITSMChange or ITSMWorkOrder
+    # dynamic field has been added
+    if ( $Param{Event} eq 'DynamicFieldAdd' ) {
 
-    # handle DynamicFieldAdd:
-    #  - add new attribute to condition attribute table
+        # add new attribute to condition attribute table
+        my $Success = $Kernel::OM->Get('Kernel::System::ITSMChange::ITSMCondition')->AttributeAdd(
+            Name   => 'DynamicField_' . $Param{Data}->{NewData}->{Name},
+            UserID => $Param{UserID},
+        );
 
-    # handle DynamicFieldUpdate:
-    #  - search for old name in attribute table and update the name
+        # check error
+        if ( !$Success ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message =>
+                    "Could not add attribute '$Param{Data}->{NewData}->{Name}' to condition attribute table!",
+            );
+        }
+    }
 
-    # handle DynamicFieldDelete:
-    #  - first, delete all expressions and actions where this attribute is used
-    #  - second, delete this attribute from attribute table
+    # dynamic field has been updated
+    elsif ( $Param{Event} eq 'DynamicFieldUpdate' ) {
+
+        # lookup the ID of the old attribute
+        my $AttributeID
+            = $Kernel::OM->Get('Kernel::System::ITSMChange::ITSMCondition')->AttributeLookup(
+            Name => 'DynamicField_' . $Param{Data}->{OldData}->{Name},
+            );
+
+        # update the attribute name
+        my $Success
+            = $Kernel::OM->Get('Kernel::System::ITSMChange::ITSMCondition')->AttributeUpdate(
+            AttributeID => $AttributeID,
+            Name        => 'DynamicField_' . $Param{Data}->{NewData}->{Name},
+            UserID      => 1,
+            );
+
+        # check error
+        if ( !$Success ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message =>
+                    "Could not update attribute name from '$Param{Data}->{OldData}->{Name}' to '$Param{Data}->{NewData}->{Name}'!",
+            );
+        }
+    }
+
+    # dynamic field has been deleted
+    elsif ( $Param{Event} eq 'DynamicFieldDelete' ) {
+
+        # get all condition attributes
+        my $ConditionAttributes
+            = $Kernel::OM->Get('Kernel::System::ITSMChange::ITSMCondition')->AttributeList(
+            UserID => 1,
+            );
+
+        # reverse the list to lookup attribute names
+        my %Attribute2ID = reverse %{$ConditionAttributes};
+
+        # lookup attribute id
+        my $AttributeID = $Attribute2ID{ 'DynamicField_' . $Param{Data}->{NewData}->{Name} } || '';
+
+        # do nothing if no attribute with this name exsists
+        return if !$AttributeID;
+
+        # delete this attribute from expression table
+        $Kernel::OM->Get('Kernel::System::DB')->Do(
+            SQL => 'DELETE FROM condition_expression
+                    WHERE attribute_id = ?',
+            Bind => [
+                \$AttributeID,
+            ],
+        );
+
+        # delete this attribute from action table
+        $Kernel::OM->Get('Kernel::System::DB')->Do(
+            SQL => 'DELETE FROM condition_action
+                    WHERE attribute_id = ?',
+            Bind => [
+                \$AttributeID,
+            ],
+        );
+
+        # delete this attribute from attribute table
+        $Kernel::OM->Get('Kernel::System::DB')->Do(
+            SQL => 'DELETE FROM condition_attribute
+                    WHERE id = ?',
+            Bind => [
+                \$AttributeID,
+            ],
+        );
+
+        # delete cache
+        $Kernel::OM->Get('Kernel::System::Cache')->CleanUp(
+            Type => 'ITSMChangeManagement',
+        );
+    }
 
     return 1;
 }
