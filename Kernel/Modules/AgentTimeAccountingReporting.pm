dd2157f3b00469d10218d7b4bb8603bb022843df
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2014-09-22 12:02:29 -0500

    Set Reporting as default action of AgentTimeAccountingReporting.

--- Kernel/Modules/AgentTimeAccountingReporting.pm
+++ Kernel/Modules/AgentTimeAccountingReporting.pm
@@ -84,213 +84,9 @@ sub Run {
     my @WeekdayArray = ( 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', );
 
     # ---------------------------------------------------------- #
-    # time accounting reporting
-    # ---------------------------------------------------------- #
-    if ( $Self->{Subaction} eq 'Reporting' ) {
-        my %Frontend = ();
-        my %ShownUsers = $Self->{UserObject}->UserList( Type => 'Long', Valid => 0 );
-        my ( $Sec, $Min, $Hour, $CurrentDay, $Month, $Year )
-            = $Self->{TimeObject}->SystemTime2Date(
-            SystemTime => $Self->{TimeObject}->SystemTime(),
-            );
-
-        # permission check
-        return $Self->{LayoutObject}->NoPermission( WithHeader => 'yes' ) if !$Self->{AccessRw};
-
-        for my $Parameter (qw(Status Month Year ProjectStatusShow)) {
-            $Param{$Parameter} = $Self->{ParamObject}->GetParam( Param => $Parameter );
-        }
-
-        # Check Date
-        if ( !$Param{Year} || !$Param{Month} ) {
-            $Param{Year}  = $Year;
-            $Param{Month} = $Month;
-        }
-        else {
-            $Param{Month} = sprintf( "%02d", $Param{Month} );
-        }
-
-        # store last screen
-        $Self->{SessionObject}->UpdateSessionID(
-            SessionID => $Self->{SessionID},
-            Key       => 'LastScreen',
-            Value =>
-                "Action=$Self->{Action};Subaction=Reporting;Year=$Param{Year};Month=$Param{Month}",
-        );
-
-        $Param{Month_to_Text} = $MonthArray[ $Param{Month} ];
-
-        my %Month = ();
-        for my $ID ( 1 .. 12 ) {
-            $Month{ sprintf( "%02d", $ID ) } = $MonthArray[$ID];
-        }
-
-        $Frontend{MonthOption} = $Self->{LayoutObject}->BuildSelection(
-            Data        => \%Month,
-            SelectedID  => $Param{Month} || '',
-            Name        => 'Month',
-            Sort        => 'NumericKey',
-            Translation => 1,
-        );
-
-        my @Year = ( $Year - 4 .. $Year + 1 );
-
-        $Frontend{YearOption} = $Self->{LayoutObject}->BuildSelection(
-            Data        => \@Year,
-            SelectedID  => $Param{Year} || '',
-            Name        => 'Year',
-            Translation => 0,
-        );
-
-        ( $Param{YearBack}, $Param{MonthBack}, $Param{DayBack} )
-            = Add_Delta_YMD( $Param{Year}, $Param{Month}, 1, 0, -1, 0 );
-        ( $Param{YearNext}, $Param{MonthNext}, $Param{DayNext} )
-            = Add_Delta_YMD( $Param{Year}, $Param{Month}, 1, 0, 1, 0 );
-
-        my %UserReport = $Self->{TimeAccountingObject}->UserReporting(
-            Year   => $Param{Year},
-            Month  => $Param{Month},
-            UserID => $Param{UserID},
-        );
-
-        my %UserBasics = $Self->{TimeAccountingObject}->UserList();
-
-        USERID:
-        for my $UserID ( sort { $ShownUsers{$a} cmp $ShownUsers{$b} } keys %ShownUsers ) {
-            next USERID if !$UserReport{$UserID};
-
-            for my $Parameter (
-                qw(LeaveDay Overtime WorkingHours Sick LeaveDayRemaining OvertimeTotal)
-                )
-            {
-                $Param{$Parameter} = sprintf( "%.2f", ( $UserReport{$UserID}{$Parameter} || 0 ) );
-                $Param{ 'Total' . $Parameter } += $Param{$Parameter};
-            }
-
-            # Show Overtime if allowed
-            if ( !$UserBasics{$UserID}{ShowOvertime} ) {
-                $Param{Overtime}      = '';
-                $Param{OvertimeTotal} = '';
-            }
-
-            $Param{User}   = $ShownUsers{$UserID};
-            $Param{UserID} = $UserID;
-            $Self->{LayoutObject}->Block(
-                Name => 'User',
-                Data => { %Param, %Frontend },
-            );
-        }
-
-        for my $Parameter (
-            qw(TotalLeaveDay TotalOvertime TotalWorkingHours
-            TotalSick TotalLeaveDayRemaining TotalOvertimeTotal)
-            )
-        {
-            $Param{$Parameter} = sprintf( "%.2f", ( $Param{$Parameter} ) || 0 );
-        }
-
-        # show the report sort by projects
-        if ( !$Param{ProjectStatusShow} || $Param{ProjectStatusShow} eq 'valid' ) {
-            $Param{ProjectStatusShow} = 'all';
-        }
-        elsif ( $Param{ProjectStatusShow} eq 'all' ) {
-            $Param{ProjectStatusShow} = 'valid';
-        }
-
-        $Param{ShowProjects} = 'Show ' . $Param{ProjectStatusShow} . ' projects';
-
-        my %ProjectData = $Self->{TimeAccountingObject}->ProjectActionReporting(
-            Year  => $Param{Year},
-            Month => $Param{Month},
-        );
-
-        # REMARK: merge this project reporting list with the list in overview
-        PROJECTID:
-        for my $ProjectID (
-            sort { $ProjectData{$a}->{Name} cmp $ProjectData{$b}->{Name} }
-            keys %ProjectData
-            )
-        {
-            my $ProjectRef = $ProjectData{$ProjectID};
-            my $ActionsRef = $ProjectRef->{Actions};
-
-            $Param{Project} = '';
-            $Param{Status} = $ProjectRef->{Status} ? '' : 'passiv';
-
-            my $Total      = 0;
-            my $TotalTotal = 0;
-
-            next PROJECTID if $Param{ProjectStatusShow} eq 'all' && $Param{Status};
-
-            for my $ActionID (
-                sort { $ActionsRef->{$a}->{Name} cmp $ActionsRef->{$b}->{Name} }
-                keys %{$ActionsRef}
-                )
-            {
-                my $ActionRef = $ActionsRef->{$ActionID};
-
-                $Param{Action}     = $ActionRef->{Name};
-                $Param{Hours}      = sprintf( "%.2f", $ActionRef->{PerMonth} || 0 );
-                $Param{HoursTotal} = sprintf( "%.2f", $ActionRef->{Total} || 0 );
-                $Total      += $Param{Hours};
-                $TotalTotal += $Param{HoursTotal};
-                $Self->{LayoutObject}->Block(
-                    Name => 'Action',
-                    Data => {%Param},
-                );
-
-                if ( !$Param{Project} ) {
-                    $Param{Project} = $ProjectRef->{Name};
-                    my $ProjectDescription = $Self->{LayoutObject}->Ascii2Html(
-                        Text           => $ProjectRef->{Description},
-                        HTMLResultMode => 1,
-                        NewLine        => 50,
-                    );
-
-                    $Self->{LayoutObject}->Block(
-                        Name => 'Project',
-                        Data => {
-                            RowSpan            => ( 1 + scalar keys %{$ActionsRef} ),
-                            Status             => $Param{Status},
-                            ProjectDescription => $ProjectDescription,
-                            Project            => $ProjectRef->{Name},
-                            ProjectID          => $ProjectID,
-                        },
-                    );
-                }
-            }
-
-            $Param{Hours}      = sprintf( "%.2f", $Total );
-            $Param{HoursTotal} = sprintf( "%.2f", $TotalTotal );
-            $Param{TotalHours}      += $Total;
-            $Param{TotalHoursTotal} += $TotalTotal;
-            $Self->{LayoutObject}->Block(
-                Name => 'ActionTotal',
-                Data => { %Param, %Frontend },
-            );
-        }
-
-        $Param{TotalHours}      ||= 0;
-        $Param{TotalHoursTotal} ||= 0;
-
-        $Param{TotalHours}      = sprintf( "%.2f", $Param{TotalHours} );
-        $Param{TotalHoursTotal} = sprintf( "%.2f", $Param{TotalHoursTotal} );
-
-        # build output
-        my $Output = $Self->{LayoutObject}->Header( Title => 'Reporting' );
-        $Output .= $Self->{LayoutObject}->NavigationBar();
-        $Output .= $Self->{LayoutObject}->Output(
-            Data => { %Param, %Frontend },
-            TemplateFile => 'AgentTimeAccountingReporting'
-        );
-        $Output .= $Self->{LayoutObject}->Footer();
-        return $Output;
-    }
-
-    # ---------------------------------------------------------- #
     # time accounting project reporting
     # ---------------------------------------------------------- #
-    elsif ( $Self->{Subaction} eq 'ReportingProject' ) {
+    if ( $Self->{Subaction} eq 'ReportingProject' ) {
         my %Frontend = ();
 
         # permission check
@@ -436,9 +232,206 @@ sub Run {
     }
 
     # ---------------------------------------------------------- #
-    # show error screen
+    # time accounting reporting
     # ---------------------------------------------------------- #
-    return $Self->{LayoutObject}->ErrorScreen( Message => 'Invalid Subaction!' );
+    my %Frontend = ();
+    my %ShownUsers = $Self->{UserObject}->UserList( Type => 'Long', Valid => 0 );
+    my ( $Sec, $Min, $Hour, $CurrentDay, $Month, $Year )
+        = $Self->{TimeObject}->SystemTime2Date(
+        SystemTime => $Self->{TimeObject}->SystemTime(),
+        );
+
+    # permission check
+    return $Self->{LayoutObject}->NoPermission( WithHeader => 'yes' ) if !$Self->{AccessRw};
+
+    for my $Parameter (qw(Status Month Year ProjectStatusShow)) {
+        $Param{$Parameter} = $Self->{ParamObject}->GetParam( Param => $Parameter );
+    }
+
+    # Check Date
+    if ( !$Param{Year} || !$Param{Month} ) {
+        $Param{Year}  = $Year;
+        $Param{Month} = $Month;
+    }
+    else {
+        $Param{Month} = sprintf( "%02d", $Param{Month} );
+    }
+
+    # store last screen
+    $Self->{SessionObject}->UpdateSessionID(
+        SessionID => $Self->{SessionID},
+        Key       => 'LastScreen',
+        Value =>
+            "Action=$Self->{Action};Year=$Param{Year};Month=$Param{Month}",
+    );
+
+    $Param{Month_to_Text} = $MonthArray[ $Param{Month} ];
+
+    my %Month = ();
+    for my $ID ( 1 .. 12 ) {
+        $Month{ sprintf( "%02d", $ID ) } = $MonthArray[$ID];
+    }
+
+    $Frontend{MonthOption} = $Self->{LayoutObject}->BuildSelection(
+        Data        => \%Month,
+        SelectedID  => $Param{Month} || '',
+        Name        => 'Month',
+        Sort        => 'NumericKey',
+        Translation => 1,
+    );
+
+    my @Year = ( $Year - 4 .. $Year + 1 );
+
+    $Frontend{YearOption} = $Self->{LayoutObject}->BuildSelection(
+        Data        => \@Year,
+        SelectedID  => $Param{Year} || '',
+        Name        => 'Year',
+        Translation => 0,
+    );
+
+    ( $Param{YearBack}, $Param{MonthBack}, $Param{DayBack} )
+        = Add_Delta_YMD( $Param{Year}, $Param{Month}, 1, 0, -1, 0 );
+    ( $Param{YearNext}, $Param{MonthNext}, $Param{DayNext} )
+        = Add_Delta_YMD( $Param{Year}, $Param{Month}, 1, 0, 1, 0 );
+
+    my %UserReport = $Self->{TimeAccountingObject}->UserReporting(
+        Year   => $Param{Year},
+        Month  => $Param{Month},
+        UserID => $Param{UserID},
+    );
+
+    my %UserBasics = $Self->{TimeAccountingObject}->UserList();
+
+    USERID:
+    for my $UserID ( sort { $ShownUsers{$a} cmp $ShownUsers{$b} } keys %ShownUsers ) {
+        next USERID if !$UserReport{$UserID};
+
+        for my $Parameter (
+            qw(LeaveDay Overtime WorkingHours Sick LeaveDayRemaining OvertimeTotal)
+            )
+        {
+            $Param{$Parameter} = sprintf( "%.2f", ( $UserReport{$UserID}{$Parameter} || 0 ) );
+            $Param{ 'Total' . $Parameter } += $Param{$Parameter};
+        }
+
+        # Show Overtime if allowed
+        if ( !$UserBasics{$UserID}{ShowOvertime} ) {
+            $Param{Overtime}      = '';
+            $Param{OvertimeTotal} = '';
+        }
+
+        $Param{User}   = $ShownUsers{$UserID};
+        $Param{UserID} = $UserID;
+        $Self->{LayoutObject}->Block(
+            Name => 'User',
+            Data => { %Param, %Frontend },
+        );
+    }
+
+    for my $Parameter (
+        qw(TotalLeaveDay TotalOvertime TotalWorkingHours
+        TotalSick TotalLeaveDayRemaining TotalOvertimeTotal)
+        )
+    {
+        $Param{$Parameter} = sprintf( "%.2f", ( $Param{$Parameter} ) || 0 );
+    }
+
+    # show the report sort by projects
+    if ( !$Param{ProjectStatusShow} || $Param{ProjectStatusShow} eq 'valid' ) {
+        $Param{ProjectStatusShow} = 'all';
+    }
+    elsif ( $Param{ProjectStatusShow} eq 'all' ) {
+        $Param{ProjectStatusShow} = 'valid';
+    }
+
+    $Param{ShowProjects} = 'Show ' . $Param{ProjectStatusShow} . ' projects';
+
+    my %ProjectData = $Self->{TimeAccountingObject}->ProjectActionReporting(
+        Year  => $Param{Year},
+        Month => $Param{Month},
+    );
+
+    # REMARK: merge this project reporting list with the list in overview
+    PROJECTID:
+    for my $ProjectID (
+        sort { $ProjectData{$a}->{Name} cmp $ProjectData{$b}->{Name} }
+        keys %ProjectData
+        )
+    {
+        my $ProjectRef = $ProjectData{$ProjectID};
+        my $ActionsRef = $ProjectRef->{Actions};
+
+        $Param{Project} = '';
+        $Param{Status} = $ProjectRef->{Status} ? '' : 'passiv';
+
+        my $Total      = 0;
+        my $TotalTotal = 0;
+
+        next PROJECTID if $Param{ProjectStatusShow} eq 'all' && $Param{Status};
+
+        for my $ActionID (
+            sort { $ActionsRef->{$a}->{Name} cmp $ActionsRef->{$b}->{Name} }
+            keys %{$ActionsRef}
+            )
+        {
+            my $ActionRef = $ActionsRef->{$ActionID};
+
+            $Param{Action}     = $ActionRef->{Name};
+            $Param{Hours}      = sprintf( "%.2f", $ActionRef->{PerMonth} || 0 );
+            $Param{HoursTotal} = sprintf( "%.2f", $ActionRef->{Total} || 0 );
+            $Total      += $Param{Hours};
+            $TotalTotal += $Param{HoursTotal};
+            $Self->{LayoutObject}->Block(
+                Name => 'Action',
+                Data => {%Param},
+            );
+
+            if ( !$Param{Project} ) {
+                $Param{Project} = $ProjectRef->{Name};
+                my $ProjectDescription = $Self->{LayoutObject}->Ascii2Html(
+                    Text           => $ProjectRef->{Description},
+                    HTMLResultMode => 1,
+                    NewLine        => 50,
+                );
+
+                $Self->{LayoutObject}->Block(
+                    Name => 'Project',
+                    Data => {
+                        RowSpan            => ( 1 + scalar keys %{$ActionsRef} ),
+                        Status             => $Param{Status},
+                        ProjectDescription => $ProjectDescription,
+                        Project            => $ProjectRef->{Name},
+                        ProjectID          => $ProjectID,
+                    },
+                );
+            }
+        }
+
+        $Param{Hours}      = sprintf( "%.2f", $Total );
+        $Param{HoursTotal} = sprintf( "%.2f", $TotalTotal );
+        $Param{TotalHours}      += $Total;
+        $Param{TotalHoursTotal} += $TotalTotal;
+        $Self->{LayoutObject}->Block(
+            Name => 'ActionTotal',
+            Data => { %Param, %Frontend },
+        );
+    }
+
+    $Param{TotalHours}      ||= 0;
+    $Param{TotalHoursTotal} ||= 0;
+
+    $Param{TotalHours}      = sprintf( "%.2f", $Param{TotalHours} );
+    $Param{TotalHoursTotal} = sprintf( "%.2f", $Param{TotalHoursTotal} );
+
+    # build output
+    my $Output = $Self->{LayoutObject}->Header( Title => 'Reporting' );
+    $Output .= $Self->{LayoutObject}->NavigationBar();
+    $Output .= $Self->{LayoutObject}->Output(
+        Data => { %Param, %Frontend },
+        TemplateFile => 'AgentTimeAccountingReporting'
+    );
+    $Output .= $Self->{LayoutObject}->Footer();
+    return $Output;
 }
 
 1;
