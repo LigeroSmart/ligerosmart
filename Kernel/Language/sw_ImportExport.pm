a54c141a4bcf9cc9c01a4b24021a322e1a7e4465
Udo Bretz <udo.bretz@otrs.com>
2014-11-18 17:36:33 +0100

    Added new translation for Swahili.

---
+++ Kernel/Language/sw_ImportExport.pm
@@ -0,0 +1,69 @@
+# --
+# Kernel/Language/sw_ImportExport.pm - translation file
+# Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::sw_ImportExport;
+
+use strict;
+use warnings;
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    # Template: AAAImportExport
+    $Self->{Translation}->{'Add mapping template'} = 'Ongeza kielezo cha kuweka ramani';
+    $Self->{Translation}->{'Charset'} = 'Seti ya herufi';
+    $Self->{Translation}->{'Colon (:)'} = 'Colon (:)';
+    $Self->{Translation}->{'Column'} = 'Safuwima';
+    $Self->{Translation}->{'Column Separator'} = 'Kitenganishi safu wima';
+    $Self->{Translation}->{'Dot (.)'} = 'Nukta (.)';
+    $Self->{Translation}->{'Semicolon (;)'} = 'Alama ya nukta mkato (;)';
+    $Self->{Translation}->{'Tabulator (TAB)'} = 'Tabo (TAB)';
+    $Self->{Translation}->{'Include Column Headers'} = 'Ambatisha vichwa vya habari vya safuwima';
+    $Self->{Translation}->{'Import summary for'} = 'Leta muhtasari kwa ajili ya';
+    $Self->{Translation}->{'Imported records'} = 'Kumbukumbu zilizoletwa';
+    $Self->{Translation}->{'Exported records'} = 'Kumbukumbu zilizohamishwa';
+    $Self->{Translation}->{'Records'} = 'Kumbukumbu';
+    $Self->{Translation}->{'Skipped'} = 'Rukwa';
+
+    # Template: AdminImportExport
+    $Self->{Translation}->{'Import/Export Management'} = 'Usimamizi wa kuleta/Hamisha';
+    $Self->{Translation}->{'Create a template to import and export object information.'} = 'Tengeneza kiolezo kuleta na kuhamisha taarifa za kipengee';
+    $Self->{Translation}->{'Start Import'} = 'Anza kuleta';
+    $Self->{Translation}->{'Start Export'} = 'Anza kuhamisha';
+    $Self->{Translation}->{'Step'} = 'Hatua';
+    $Self->{Translation}->{'Edit common information'} = 'Hariri taarifa za kufana';
+    $Self->{Translation}->{'Name is required!'} = 'Jina linahitajika!';
+    $Self->{Translation}->{'Object is required!'} = 'Kipengee kinahitajika!';
+    $Self->{Translation}->{'Format is required!'} = 'Umbizo unahitajika';
+    $Self->{Translation}->{'Edit object information'} = 'Hariri taarifa za kipengee';
+    $Self->{Translation}->{'Edit format information'} = 'Hariri taarifa za umbizo';
+    $Self->{Translation}->{'is required!'} = 'Inahitajika';
+    $Self->{Translation}->{'Edit mapping information'} = 'Hariri tarifa za kuweka ramani';
+    $Self->{Translation}->{'No map elements found.'} = 'Hakuna elementi zilizopatikana';
+    $Self->{Translation}->{'Add Mapping Element'} = 'Ongeza elementi za kuweka ramani';
+    $Self->{Translation}->{'Edit search information'} = 'Hariri taarifa za kutafuta';
+    $Self->{Translation}->{'Restrict export per search'} = 'Zuia uhamishaji kwa kila utafutaji';
+    $Self->{Translation}->{'Import information'} = 'Leta taarifa';
+    $Self->{Translation}->{'Source File'} = 'Faili la chanzo';
+    $Self->{Translation}->{'Success'} = 'Mafanikio';
+    $Self->{Translation}->{'Failed'} = 'Shindwa';
+    $Self->{Translation}->{'Duplicate names'} = 'Jina limejirudia:';
+    $Self->{Translation}->{'Last processed line number of import file'} = 'Namba ya mstari unaokatika mchakato wa mwisho wa faili lililoletwa';
+    $Self->{Translation}->{'Ok'} = 'Sawa';
+
+    # SysConfig
+    $Self->{Translation}->{'Format backend module registration for the import/export module.'} =
+        'Umbiza usajili wa moduli wa mazingira ya nyuma kwa ajili ya moduli ya kuleta/kuhamisha';
+    $Self->{Translation}->{'Import and export object information.'} = 'Leta na hamisha taarifa za kipengee';
+    $Self->{Translation}->{'Import/Export'} = 'Leta/Hamisha';
+
+}
+
+1;
