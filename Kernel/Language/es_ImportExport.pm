85cd020fdaaed01a317e81c0f39cd5177fb84c8d
Udo Bretz <udo.bretz@otrs.com>
2015-03-29 14:28:34 +0200

    Updated translations from transifex.

--- Kernel/Language/es_ImportExport.pm
+++ Kernel/Language/es_ImportExport.pm
@@ -25,38 +25,38 @@ sub Data {
     $Self->{Translation}->{'Dot (.)'} = 'Punto (.)';
     $Self->{Translation}->{'Semicolon (;)'} = 'Punto y Coma (;)';
     $Self->{Translation}->{'Tabulator (TAB)'} = 'Tabulador (TAB)';
-    $Self->{Translation}->{'Include Column Headers'} = '';
-    $Self->{Translation}->{'Import summary for'} = '';
-    $Self->{Translation}->{'Imported records'} = '';
-    $Self->{Translation}->{'Exported records'} = '';
-    $Self->{Translation}->{'Records'} = '';
-    $Self->{Translation}->{'Skipped'} = '';
+    $Self->{Translation}->{'Include Column Headers'} = 'Incluir Cabecera de la Columna';
+    $Self->{Translation}->{'Import summary for'} = 'Importar resumen para';
+    $Self->{Translation}->{'Imported records'} = 'Registros importados';
+    $Self->{Translation}->{'Exported records'} = 'Registros exportados';
+    $Self->{Translation}->{'Records'} = 'Registros';
+    $Self->{Translation}->{'Skipped'} = 'Saltado';
 
     # Template: AdminImportExport
     $Self->{Translation}->{'Import/Export Management'} = 'Gestión de Importación/Exportación';
-    $Self->{Translation}->{'Create a template to import and export object information.'} = '';
+    $Self->{Translation}->{'Create a template to import and export object information.'} = 'Crear una plantilla para importar y exportar informacion del objeto.';
     $Self->{Translation}->{'Start Import'} = 'Iniciar Importación';
     $Self->{Translation}->{'Start Export'} = 'Iniciar Exportación';
     $Self->{Translation}->{'Step'} = 'Paso';
     $Self->{Translation}->{'Edit common information'} = 'Editar información común';
-    $Self->{Translation}->{'Name is required!'} = '';
+    $Self->{Translation}->{'Name is required!'} = 'El nombre es requerido';
     $Self->{Translation}->{'Object is required!'} = '¡Debe especificar Objeto!';
     $Self->{Translation}->{'Format is required!'} = '¡Debe especificar Formato!';
     $Self->{Translation}->{'Edit object information'} = 'Editar información de objeto';
     $Self->{Translation}->{'Edit format information'} = 'Editar información del formato';
-    $Self->{Translation}->{'is required!'} = '';
+    $Self->{Translation}->{'is required!'} = '¡es requerido!';
     $Self->{Translation}->{'Edit mapping information'} = 'Editar información de mapeo';
     $Self->{Translation}->{'No map elements found.'} = 'No se encontraron elementos de mapeo.';
-    $Self->{Translation}->{'Add Mapping Element'} = '';
+    $Self->{Translation}->{'Add Mapping Element'} = 'Añadir Mapeo de Elementos';
     $Self->{Translation}->{'Edit search information'} = 'Editar información de búsqueda';
     $Self->{Translation}->{'Restrict export per search'} = 'Restringir exportación por búsqueda';
     $Self->{Translation}->{'Import information'} = 'Importar información';
     $Self->{Translation}->{'Source File'} = 'Archivo origen';
-    $Self->{Translation}->{'Success'} = '';
-    $Self->{Translation}->{'Failed'} = '';
-    $Self->{Translation}->{'Duplicate names'} = '';
-    $Self->{Translation}->{'Last processed line number of import file'} = '';
-    $Self->{Translation}->{'Ok'} = '';
+    $Self->{Translation}->{'Success'} = 'Éxito';
+    $Self->{Translation}->{'Failed'} = 'Fracasado';
+    $Self->{Translation}->{'Duplicate names'} = 'Nombres duplicados';
+    $Self->{Translation}->{'Last processed line number of import file'} = 'Última número de línea procesada del archivo importar';
+    $Self->{Translation}->{'Ok'} = 'Ok';
 
     # SysConfig
     $Self->{Translation}->{'Format backend module registration for the import/export module.'} =
--- Kernel/Language/fr_ImportExport.pm
+++ Kernel/Language/fr_ImportExport.pm
@@ -25,16 +25,16 @@ sub Data {
     $Self->{Translation}->{'Dot (.)'} = 'Point (.)';
     $Self->{Translation}->{'Semicolon (;)'} = 'Point virgule (;)';
     $Self->{Translation}->{'Tabulator (TAB)'} = 'Tabulation (TAB)';
-    $Self->{Translation}->{'Include Column Headers'} = '';
-    $Self->{Translation}->{'Import summary for'} = '';
-    $Self->{Translation}->{'Imported records'} = '';
-    $Self->{Translation}->{'Exported records'} = '';
-    $Self->{Translation}->{'Records'} = '';
-    $Self->{Translation}->{'Skipped'} = '';
+    $Self->{Translation}->{'Include Column Headers'} = 'Inclure les en-têtes de colonnes';
+    $Self->{Translation}->{'Import summary for'} = 'Résumé de l\'import pour';
+    $Self->{Translation}->{'Imported records'} = 'Enregistrements importés';
+    $Self->{Translation}->{'Exported records'} = 'Enregistrements exportés';
+    $Self->{Translation}->{'Records'} = 'Enregistrements';
+    $Self->{Translation}->{'Skipped'} = 'Passé(s)';
 
     # Template: AdminImportExport
     $Self->{Translation}->{'Import/Export Management'} = 'Gestion de l\'Import/Export';
-    $Self->{Translation}->{'Create a template to import and export object information.'} = '';
+    $Self->{Translation}->{'Create a template to import and export object information.'} = 'Créer un modèle pour importer et exporter les informations d\'objet';
     $Self->{Translation}->{'Start Import'} = 'Démarrer Import';
     $Self->{Translation}->{'Start Export'} = 'Démarrer Export';
     $Self->{Translation}->{'Step'} = 'Etape';
@@ -44,7 +44,7 @@ sub Data {
     $Self->{Translation}->{'Format is required!'} = '';
     $Self->{Translation}->{'Edit object information'} = 'Editer les informations de l\'objet';
     $Self->{Translation}->{'Edit format information'} = 'Editer les informations de format';
-    $Self->{Translation}->{'is required!'} = '';
+    $Self->{Translation}->{'is required!'} = 'est requis !';
     $Self->{Translation}->{'Edit mapping information'} = 'Editer les informations de mappage';
     $Self->{Translation}->{'No map elements found.'} = '';
     $Self->{Translation}->{'Add Mapping Element'} = '';
@@ -52,16 +52,16 @@ sub Data {
     $Self->{Translation}->{'Restrict export per search'} = 'Restreindre l\'export par recherche';
     $Self->{Translation}->{'Import information'} = 'Informations d\'import';
     $Self->{Translation}->{'Source File'} = 'Fichier Source';
-    $Self->{Translation}->{'Success'} = '';
-    $Self->{Translation}->{'Failed'} = '';
-    $Self->{Translation}->{'Duplicate names'} = '';
-    $Self->{Translation}->{'Last processed line number of import file'} = '';
-    $Self->{Translation}->{'Ok'} = '';
+    $Self->{Translation}->{'Success'} = 'Réussi';
+    $Self->{Translation}->{'Failed'} = 'Echoué';
+    $Self->{Translation}->{'Duplicate names'} = 'Noms en double';
+    $Self->{Translation}->{'Last processed line number of import file'} = 'Numéro de la dernière ligne traitée dans le fichier d\'import';
+    $Self->{Translation}->{'Ok'} = 'Ok';
 
     # SysConfig
     $Self->{Translation}->{'Format backend module registration for the import/export module.'} =
         '';
-    $Self->{Translation}->{'Import and export object information.'} = '';
+    $Self->{Translation}->{'Import and export object information.'} = 'Importer et exporter des informations d\'objet';
     $Self->{Translation}->{'Import/Export'} = 'Importer/Exporter';
 
 }
