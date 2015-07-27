b312533cfcd57eaae27faf9b089170c269d4fc20
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2015-07-27 17:16:23 -0500

    Cleanup.

--- Kernel/Modules/AgentSurveyEdit.pm
+++ Kernel/Modules/AgentSurveyEdit.pm
@@ -1,5 +1,4 @@
 # --
-# Kernel/Modules/AgentSurveyEdit.pm - survey edit module
 # Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
--- Kernel/System/Survey/Answer.pm
+++ Kernel/System/Survey/Answer.pm
@@ -1,5 +1,4 @@
 # --
-# Kernel/System/Survey/Answer.pm - survey answer functions
 # Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
