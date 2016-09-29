52e3a74f3dbc14f7e8c5cc335c00054347c630fd
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2016-09-29 11:23:42 -0500

    Added support to choose and sort FAQ attribute columns for linked FAQ items (complex table mode).

--- CHANGES-FAQ.md
+++ CHANGES-FAQ.md
@@ -1,4 +1,5 @@
 #5.0.7 2016-??-??
+ - 2016-09-29 Added support to choose and sort FAQ attribute columns for linked FAQ items (complex table mode).
  - 2016-09-22 Added new SysConfig setting "FAQ::Frontend::AgentFAQExplorer###ShowInvalidFAQItems" to show or hide invalid FAQ items (disabled by default).
  - 2016-09-22 Fixed bug#[11498](http://bugs.otrs.org/show_bug.cgi?id=11498)(PR#48) - FAQ items disappear from AgentFAQExplorer when invalid, thanks to S7.
  - 2016-09-21 Fixed bug#[11158](http://bugs.otrs.org/show_bug.cgi?id=11158)(PR#49) - Only ro priviledge's user cannot search FAQ, thanks to S7.
--- Kernel/Output/HTML/LinkObject/FAQ.pm
+++ Kernel/Output/HTML/LinkObject/FAQ.pm
@@ -12,10 +12,16 @@ use strict;
 use warnings;
 
 use Kernel::Output::HTML::Layout;
+use Kernel::System::VariableCheck qw(:all);
 
 our @ObjectDependencies = (
     'Kernel::Config',
+    'Kernel::Language',
+    'Kernel::System::DynamicField',
+    'Kernel::System::DynamicField::Backend',
+    'Kernel::System::JSON',
     'Kernel::System::Log',
+    'Kernel::System::User',
     'Kernel::System::Web::Request',
 );
 
@@ -61,9 +67,16 @@ sub new {
 
     # define needed variables
     $Self->{ObjectData} = {
-        Object   => 'FAQ',
-        Realname => 'FAQ',
+        Object     => 'FAQ',
+        Realname   => 'FAQ',
+        ObjectName => 'SourceObjectID',
     };
+
+    # get the dynamic fields for this screen
+    $Self->{DynamicField} = $Kernel::OM->Get('Kernel::System::DynamicField')->DynamicFieldListGet(
+        Valid      => 0,
+        ObjectType => ['FAQ'],
+    );
 
     return $Self;
 }
@@ -80,6 +93,9 @@ a result could be
 
     %BlockData = (
         {
+            ObjectName  => 'FAQID',
+            ObjectID    => '14785',
+
             Object    => 'FAQ',
             Blockname => 'FAQ',
             Headline  => [
@@ -152,7 +168,6 @@ a result could be
 sub TableCreateComplex {
     my ( $Self, %Param ) = @_;
 
-    # check needed stuff
     if ( !$Param{ObjectLinkListWithData} || ref $Param{ObjectLinkListWithData} ne 'HASH' ) {
         $Kernel::OM->Get('Kernel::System::Log')->Log(
             Priority => 'error',
@@ -161,7 +176,7 @@ sub TableCreateComplex {
         return;
     }
 
-    # convert the list
+    # Convert the list.
     my %LinkList;
     for my $LinkType ( sort keys %{ $Param{ObjectLinkListWithData} } ) {
 
@@ -180,13 +195,167 @@ sub TableCreateComplex {
         }
     }
 
-    # create the item list
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
+    my $ComplexTableData = $ConfigObject->Get("LinkObject::ComplexTable");
+    my $DefaultColumns;
+    if (
+        $ComplexTableData
+        && IsHashRefWithData($ComplexTableData)
+        && $ComplexTableData->{FAQ}
+        && IsHashRefWithData( $ComplexTableData->{FAQ} )
+        )
+    {
+        $DefaultColumns = $ComplexTableData->{"FAQ"}->{"DefaultColumns"};
+    }
+
+    my @TimeLongTypes = (
+        "Created",
+        "Changed",
+    );
+
+    # Define the block data.
+    my $FAQHook = $ConfigObject->Get('FAQ::FAQHook');
+
+    my @Headline = (
+        {
+            Content => $FAQHook,
+        },
+    );
+
+    my $UserObject = $Kernel::OM->Get('Kernel::System::User');
+
+    # Load user preferences.
+    my %Preferences = $UserObject->GetPreferences(
+        UserID => $Self->{UserID},
+    );
+
+    if ( !$DefaultColumns || !IsHashRefWithData($DefaultColumns) ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'error',
+            Message  => 'Missing configuration for LinkObject::ComplexTable###FAQ!',
+        );
+        return;
+    }
+
+    # Get default column priority from SysConfig.
+    # Each column in table (Title, State,...) has defined Priority in SysConfig. System use this
+    #   priority to sort columns, if user doesn't have own settings.
+    my %SortOrder;
+    if (
+        $ComplexTableData->{"FAQ"}->{"Priority"}
+        && IsHashRefWithData( $ComplexTableData->{"FAQ"}->{"Priority"} )
+        )
+    {
+        %SortOrder = %{ $ComplexTableData->{"FAQ"}->{"Priority"} };
+    }
+
+    my %UserColumns = %{$DefaultColumns};
+
+    if ( $Preferences{'LinkObject::ComplexTable-FAQ'} ) {
+
+        my $ColumnsEnabled = $Kernel::OM->Get('Kernel::System::JSON')->Decode(
+            Data => $Preferences{'LinkObject::ComplexTable-FAQ'},
+        );
+
+        if (
+            $ColumnsEnabled
+            && IsHashRefWithData($ColumnsEnabled)
+            && $ColumnsEnabled->{Order}
+            && IsArrayRefWithData( $ColumnsEnabled->{Order} )
+            )
+        {
+            # Clear sort order.
+            %SortOrder = ();
+
+            DEFAULTCOLUMN:
+            for my $DefaultColumn ( sort keys %UserColumns ) {
+                my $Index = 0;
+
+                for my $UserSetting ( @{ $ColumnsEnabled->{Order} } ) {
+                    $Index++;
+                    if ( $DefaultColumn eq $UserSetting ) {
+                        $UserColumns{$DefaultColumn} = 2;
+                        $SortOrder{$DefaultColumn}   = $Index;
+
+                        next DEFAULTCOLUMN;
+                    }
+                }
+
+                # Not found, means user chose to hide this item.
+                if ( $UserColumns{$DefaultColumn} == 2 ) {
+                    $UserColumns{$DefaultColumn} = 1;
+                }
+
+                if ( !$SortOrder{$DefaultColumn} ) {
+                    $SortOrder{$DefaultColumn} = 0;    # Set 0, it system will hide this item anyways
+                }
+            }
+        }
+    }
+    else {
+
+        # User has no own settings.
+        for my $Column ( sort keys %UserColumns ) {
+            if ( !$SortOrder{$Column} ) {
+                $SortOrder{$Column} = 0;               # Set 0, it system will hide this item anyways
+            }
+        }
+    }
+
+    # Define Headline columns.
+    COLUMN:
+    for my $Column ( sort { $SortOrder{$a} <=> $SortOrder{$b} } keys %UserColumns ) {
+        next COLUMN if $Column eq 'FAQNumber';         # Always present, already added.
+
+        # Ff enabled by default.
+        if ( $UserColumns{$Column} == 2 ) {
+            my $ColumnName = '';
+
+            if ( $Column eq 'CategoryName' ) {
+                $ColumnName = 'Category';
+            }
+            elsif ( $Column eq 'ContentType' ) {
+                $ColumnName = 'Content Type'
+            }
+
+            # Other FAQ fields.
+            elsif ( $Column !~ m{\A DynamicField_}xms ) {
+                $ColumnName = $Column;
+            }
+
+            # Dynamic fields.
+            else {
+                my $DynamicFieldConfig;
+                my $DFColumn = $Column;
+                $DFColumn =~ s{DynamicField_}{}g;
+
+                DYNAMICFIELD:
+                for my $DFConfig ( @{ $Self->{DynamicField} } ) {
+                    next DYNAMICFIELD if !IsHashRefWithData($DFConfig);
+                    next DYNAMICFIELD if $DFConfig->{Name} ne $DFColumn;
+
+                    $DynamicFieldConfig = $DFConfig;
+                    last DYNAMICFIELD;
+                }
+                next COLUMN if !IsHashRefWithData($DynamicFieldConfig);
+
+                $ColumnName = $DynamicFieldConfig->{Label};
+            }
+            push @Headline, {
+                Content => $ColumnName,
+            };
+        }
+    }
+
+    # Create the item list (table content).
     my @ItemList;
     for my $FAQID ( sort { $a <=> $b } keys %LinkList ) {
 
-        # extract FAQ data
+        # Extract FAQ data.
         my $FAQ = $LinkList{$FAQID}->{Data};
 
+        # FAQ Number must be present (since it contains master link to the FAQ).
         my @ItemColumns = (
             {
                 Type    => 'Link',
@@ -194,50 +363,87 @@ sub TableCreateComplex {
                 Content => $FAQ->{Number},
                 Link    => $Self->{LayoutObject}->{Baselink} . 'Action=AgentFAQZoom;ItemID=' . $FAQID,
             },
-            {
-                Type      => 'Text',
-                Content   => $FAQ->{Title},
-                MaxLength => 50,
-            },
-            {
-                Type      => 'Text',
-                Content   => $FAQ->{State},
-                Translate => 1,
-            },
-            {
-                Type    => 'TimeLong',
-                Content => $FAQ->{Created},
-            },
         );
+
+        COLUMN:
+        for my $Column ( sort { $SortOrder{$a} <=> $SortOrder{$b} } keys %UserColumns ) {
+            next COLUMN if $Column eq 'FAQNumber';    # Always present, already added.
+
+            # if enabled by default
+            if ( $UserColumns{$Column} == 2 ) {
+
+                my %Hash;
+                if ( grep { $_ eq $Column } @TimeLongTypes ) {
+                    $Hash{'Type'} = 'TimeLong';
+                }
+                else {
+                    $Hash{'Type'} = 'Text';
+                }
+
+                if ( $Column eq 'Title' ) {
+                    $Hash{MaxLength} = 50;
+                }
+
+                # FAQ fields.
+                if ( $Column !~ m{\A DynamicField_}xms ) {
+                    if ( $Column eq 'Approved' ) {
+                        my $Value = $FAQ->{$Column} ? 'Yes' : 'No';
+                        $Hash{'Content'} = $Kernel::OM->Get('Kernel::Language')->Translate($Value);
+                    }
+                    else {
+                        $Hash{'Content'} = $FAQ->{$Column};
+                    }
+                }
+
+                # Dynamic fields.
+                else {
+                    my $DynamicFieldConfig;
+                    my $DFColumn = $Column;
+                    $DFColumn =~ s{DynamicField_}{}g;
+
+                    DYNAMICFIELD:
+                    for my $DFConfig ( @{ $Self->{DynamicField} } ) {
+                        next DYNAMICFIELD if !IsHashRefWithData($DFConfig);
+                        next DYNAMICFIELD if $DFConfig->{Name} ne $DFColumn;
+
+                        $DynamicFieldConfig = $DFConfig;
+                        last DYNAMICFIELD;
+                    }
+                    next COLUMN if !IsHashRefWithData($DynamicFieldConfig);
+
+                    # Get field value.
+                    my $Value = $Kernel::OM->Get('Kernel::System::DynamicField::Backend')->ValueGet(
+                        DynamicFieldConfig => $DynamicFieldConfig,
+                        ObjectID           => $FAQID,
+                    );
+
+                    my $ValueStrg = $Kernel::OM->Get('Kernel::System::DynamicField::Backend')->DisplayValueRender(
+                        DynamicFieldConfig => $DynamicFieldConfig,
+                        Value              => $Value,
+                        ValueMaxChars      => 20,
+                        LayoutObject       => $Self->{LayoutObject},
+                    );
+
+                    $Hash{'Content'} = $ValueStrg->{Title};
+                }
+
+                push @ItemColumns, \%Hash;
+            }
+        }
 
         push @ItemList, \@ItemColumns;
     }
 
     return if !@ItemList;
 
-    # define the block data
-    my $FAQHook = $Kernel::OM->Get('Kernel::Config')->Get('FAQ::FAQHook');
-    my %Block   = (
-        Object    => $Self->{ObjectData}->{Object},
-        Blockname => $Self->{ObjectData}->{Realname},
-        Headline  => [
-            {
-                Content => $FAQHook,
-                Width   => 130,
-            },
-            {
-                Content => 'Title',
-            },
-            {
-                Content => 'State',
-                Width   => 110,
-            },
-            {
-                Content => 'Created',
-                Width   => 130,
-            },
-        ],
-        ItemList => \@ItemList,
+    # Define the block data.
+    my %Block = (
+        Object     => $Self->{ObjectData}->{Object},
+        Blockname  => $Self->{ObjectData}->{Realname},
+        ObjectName => $Self->{ObjectData}->{ObjectName},
+        ObjectID   => $Param{ObjectID},
+        Headline   => \@Headline,
+        ItemList   => \@ItemList,
     );
 
     return ( \%Block );
