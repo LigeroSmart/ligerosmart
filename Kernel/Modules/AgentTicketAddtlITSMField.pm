6d32379cb9c295f7509d1ab6096df4d16bf7b934
Manuel Hecht <manuel.hecht@otrs.com>
2016-10-19 21:35:29 +0200

    Tidied.

--- Kernel/Modules/AgentTicketAddtlITSMField.pm
+++ Kernel/Modules/AgentTicketAddtlITSMField.pm
@@ -1,6 +1,8 @@
 # --
 # Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
 # --
+# $origin: https://github.com/OTRS/otrs/blob/297033c935169948bd3ea917f46b9c852e05b052/Kernel/Modules/AgentTicketFreeText.pm
+# --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
