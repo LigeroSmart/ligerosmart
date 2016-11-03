fefe3b97f38407505bb4dd3cb500b5dc3aebb1d4
Manuel Hecht <manuel.hecht@otrs.com>
2016-11-03 13:29:07 +0100

    Changed origin to new structure.

--- Kernel/Modules/AgentTicketMasterSlave.pm
+++ Kernel/Modules/AgentTicketMasterSlave.pm
@@ -1,7 +1,7 @@
 # --
 # Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
 # --
-# $origin: https://github.com/OTRS/otrs/blob/b37dd8b4130ece66185d0c61a040766a33ebaeb2/Kernel/Modules/AgentTicketActionCommon.pm
+# $origin: otrs - b37dd8b4130ece66185d0c61a040766a33ebaeb2 - Kernel/Modules/AgentTicketActionCommon.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
