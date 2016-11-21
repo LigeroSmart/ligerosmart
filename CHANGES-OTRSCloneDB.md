f21480361773acb69de2028429006688c32a15f8
Martin Gruner <martin.gruner@otrs.com>
2016-11-21 12:54:11 +0100

    Added Hungarian translation, thanks to Balázs Úr.

--- CHANGES-OTRSCloneDB.md
+++ CHANGES-OTRSCloneDB.md
@@ -1,2 +1,5 @@
+#5.0.2 2016-??-??
+ - 2016-11-21 Added Hungarian translation, thanks to Balázs Úr.
+
 #5.0.1 2015-10-15
  - 2015-10-15 Initial version for OTRS 5
---
+++ Kernel/Language/hu_OTRSCloneDB.pm
@@ -0,0 +1,31 @@
+# --
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::hu_OTRSCloneDB;
+
+use strict;
+use warnings;
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    # SysConfig
+    $Self->{Translation}->{'Settings for connecting with the target database.'} = 'Beállítások a céladatbázissal történő csatlakozáshoz.';
+    $Self->{Translation}->{'List of tables should be skipped, perhaps internal DB tables. Please use lowercase.'} = 'Azon táblák listája, amelyeket ki kell hagyni. Valószínűleg belső adatbázistáblák. Használjon kisbetűs neveket.';
+    $Self->{Translation}->{'This setting specifies which table columns contain blob data as these need special treatment.'} = 'Ez a beállítás határozza meg, hogy mely táblaoszlopok tartalmaznak blob adatokat, mivel ezek különleges bánásmódot igénylenek.';
+    $Self->{Translation}->{'Specifies which columns should be checked for valid UTF-8 source data.'} = 'Meghatározza, hogy mely oszlopokat kell ellenőrizni érvényes UTF-8 forrásadatokhoz.';
+    $Self->{Translation}->{'Log file for replacement of malformed UTF-8 data values.'} = 'Naplófájl a helytelenül formázott UTF-8 adatértékek helyettesítéséhez.';
+
+    #
+    # OBSOLETE ENTRIES FOR REFERENCE, DO NOT TRANSLATE!
+    #
+
+}
+
+1;
