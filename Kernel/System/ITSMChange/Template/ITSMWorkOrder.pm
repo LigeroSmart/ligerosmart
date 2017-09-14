09b320ae22fe49cbc0326a4f29f490e74796560c
Andre Bras <andre.bras@otrs.com>
2017-09-14 09:54:44 +0100

    Fixed return value (timestamp instead of epoch).

--- Kernel/System/ITSMChange/Template/ITSMWorkOrder.pm
+++ Kernel/System/ITSMChange/Template/ITSMWorkOrder.pm
@@ -472,7 +472,7 @@ sub _MoveTime {
         ObjectParams => {
             Epoch => $CurrentSystemTime + $Param{Difference},
             }
-    )->ToEpoch();
+    )->ToString();
 
     return $NewTime;
 }
