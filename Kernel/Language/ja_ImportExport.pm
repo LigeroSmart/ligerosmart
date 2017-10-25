5650c4ea0f0a8b119da14885e6375507ea2a491c
Udo Bretz <udo.bretz@otrs.com>
2017-10-25 16:09:57 +0200

    Updated translations.

--- Kernel/Language/ja_ImportExport.pm
+++ Kernel/Language/ja_ImportExport.pm
@@ -16,7 +16,7 @@ sub Data {
     my $Self = shift;
 
     # Template: AdminImportExport
-    $Self->{Translation}->{'Import/Export Management'} = 'Import/Export マネージャ';
+    $Self->{Translation}->{'Import/Export Management'} = 'インポート/エクスポートの管理';
     $Self->{Translation}->{'Create a template to import and export object information.'} = 'オブジェクトのインポート・エクスポート用のテンプレートを作成する';
     $Self->{Translation}->{'Start Import'} = 'インポート開始';
     $Self->{Translation}->{'Start Export'} = 'エクスポート開始';
@@ -32,9 +32,9 @@ sub Data {
     $Self->{Translation}->{'Add Mapping Element'} = 'マッピング要素の追加';
     $Self->{Translation}->{'Step 5 of 5 - Edit search information'} = 'Step 5 of 5 - 検索情報の設定';
     $Self->{Translation}->{'Restrict export per search'} = '検索あたりの出力を制限';
-    $Self->{Translation}->{'Import information'} = '情報の読み込み';
+    $Self->{Translation}->{'Import information'} = '情報をインポート';
     $Self->{Translation}->{'Source File'} = 'ソースファイル';
-    $Self->{Translation}->{'Import summary for %s'} = '';
+    $Self->{Translation}->{'Import summary for %s'} = '%sのインポート・サマリー';
     $Self->{Translation}->{'Records'} = 'レコード';
     $Self->{Translation}->{'Success'} = '成功';
     $Self->{Translation}->{'Duplicate names'} = '名前が重複しています';
@@ -49,7 +49,7 @@ sub Data {
     $Self->{Translation}->{'Needed TemplateID!'} = 'テンプレートIDの入力が必要です！';
     $Self->{Translation}->{'Error occurred. Import impossible! See Syslog for details.'} = 'インポートが出来ません（エラーが発生しました。詳細はシステムログをご確認ください）！';
     $Self->{Translation}->{'Error occurred. Export impossible! See Syslog for details.'} = 'エクスポートが出来ません（エラーが発生しました。詳細はシステムログをご確認ください）！';
-    $Self->{Translation}->{'Template List'} = 'テンプレート一覧';
+    $Self->{Translation}->{'Template List'} = 'テンプレート・リスト';
     $Self->{Translation}->{'number'} = '数値';
     $Self->{Translation}->{'number bigger than zero'} = '0以上の数値';
     $Self->{Translation}->{'integer'} = '整数値';
