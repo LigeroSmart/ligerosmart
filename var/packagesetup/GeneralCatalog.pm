aeb2fa09dde459b3ff7b249351ebfbd39b806663
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2018-03-27 16:38:44 -0600

    Code cleanup.

--- var/packagesetup/GeneralCatalog.pm
+++ var/packagesetup/GeneralCatalog.pm
@@ -21,7 +21,7 @@ our @ObjectDependencies = (
 
 =head1 NAME
 
-GeneralCatalog.pm - code to execute during package installation
+var::packagesetup::GeneralCatalog - code to execute during package installation
 
 =head1 PUBLIC INTERFACE
 
