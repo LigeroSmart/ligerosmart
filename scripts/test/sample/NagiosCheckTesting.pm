1189519bde0187121257bc36cc8f5441f46eef2b
Manuel Hecht <manuel.hecht@otrs.com>
2018-09-05 11:40:48 +0200

    Tidied.

--- scripts/test/sample/NagiosCheckTesting.pm
+++ scripts/test/sample/NagiosCheckTesting.pm
@@ -1,41 +1,40 @@
 # --
-# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2018 OTRS AG, https://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
 # --
 
 %Config = (
-   Search => {
+    Search => {
 
-       # tickets created in the last 120 minutes
-       TicketID => 1,
-   },
+        # tickets created in the last 120 minutes
+        TicketID => 1,
+    },
 
-# Declaration of thresholds
-# min_warn_treshold > Number of tickets -> WARNING
-# max_warn_treshold < Number of tickets -> WARNING
-# min_crit_treshold > Number of tickets -> ALARM
-# max_warn_treshold < Number of tickets -> ALARM
+    # Declaration of thresholds
+    # min_warn_treshold > Number of tickets -> WARNING
+    # max_warn_treshold < Number of tickets -> WARNING
+    # min_crit_treshold > Number of tickets -> ALARM
+    # max_warn_treshold < Number of tickets -> ALARM
 
-   min_warn_treshold => 0,
-   max_warn_treshold => 10,
-   min_crit_treshold => 0,
-   max_crit_treshold => 20,
+    min_warn_treshold => 0,
+    max_warn_treshold => 10,
+    min_crit_treshold => 0,
+    max_crit_treshold => 20,
 
-# Information used by Nagios
-# Name of check shown in Nagios Status Information
-   checkname => 'OTRS Checker',
+    # Information used by Nagios
+    # Name of check shown in Nagios Status Information
+    checkname => 'OTRS Checker',
 
-# Text shown in Status Information if everything is OK
-   OK_TXT    => 'enjoy   tickets:',
+    # Text shown in Status Information if everything is OK
+    OK_TXT => 'enjoy   tickets:',
 
-# Text shown in Status Information if warning threshold reached
-   WARN_TXT  => 'number of tickets:',
+    # Text shown in Status Information if warning threshold reached
+    WARN_TXT => 'number of tickets:',
 
-# Text shown in Status Information if critical threshold reached
-   CRIT_TXT  => 'critical number of tickets:',
+    # Text shown in Status Information if critical threshold reached
+    CRIT_TXT => 'critical number of tickets:',
 
 );
-
