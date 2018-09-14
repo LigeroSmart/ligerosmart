ae38bf36e0eb0303d8d5d288c4fd02dda13bcbc4
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2018-09-13 22:09:49 -0500

    Updated framework files to OTRS 6.0.11.

--- Kernel/Modules/AgentTicketMasterSlave.pm
+++ Kernel/Modules/AgentTicketMasterSlave.pm
@@ -1,7 +1,7 @@
 # --
 # Copyright (C) 2001-2018 OTRS AG, https://otrs.com/
 # --
-# $origin: otrs - e90ad7d84fe87c8397ffdf688d6792350ebef1f6 - Kernel/Modules/AgentTicketActionCommon.pm
+# $origin: otrs - 87629f00b8a02498bf28c802419865b3286ead2e - Kernel/Modules/AgentTicketActionCommon.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
