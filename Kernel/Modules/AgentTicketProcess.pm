f02d02d57f98554d987af5a00021cf6eef379868
Manuel Hecht <manuel.hecht@otrs.com>
2019-01-02 21:27:58 +0100

    Updated copyright.

--- Kernel/Modules/AgentTicketProcess.pm
+++ Kernel/Modules/AgentTicketProcess.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2001-2018 OTRS AG, https://otrs.com/
+# Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
 # --
 # $origin: otrs - cbbafc16c1bd8aa7f1651ed93e48a7977de738ae - Kernel/Modules/AgentTicketProcess.pm - rel-6_0_16
 # --
--- Kernel/Modules/CustomerTicketProcess.pm
+++ Kernel/Modules/CustomerTicketProcess.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2001-2018 OTRS AG, https://otrs.com/
+# Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
 # --
 # $origin: otrs - 8ce19805570da1d4442f32f839a33057131e6335 - Kernel/Modules/CustomerTicketProcess.pm
 # --
