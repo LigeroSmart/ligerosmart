718c294c61dae2d2ac427b055600ba35458b7fc7
Ronaldo Richieri <richieri@gmail.com>
2023-08-03 22:42:46 -0300

    Update Actions that uses LigeroForms to use morphdom

--- var/httpd/htdocs/js/Core.Agent.DynamicFieldByService.js
+++ var/httpd/htdocs/js/Core.Agent.DynamicFieldByService.js
@@ -8,9 +8,7 @@
 var Core = Core || {};
 Core.Agent = Core.Agent || {};
 Core.Agent.DynamicFieldByServicePreLoad = {};
-Core.Agent.DynamicFieldByServiceLastFocus = null;
 Core.Agent.DynamicFieldByServiceLastChangedElement = null;
-Core.Agent.DynamicFieldByServiceLastKey = null;
 Core.Agent.ClearFormIfEmptyConfig = null;
 Core.Agent.checkLigeroForms;
 Core.Agent.DynamicFieldByService = (function (TargetNS) {
@@ -114,6 +112,12 @@ Core.Agent.DynamicFieldByService = (function (TargetNS) {
 							// Parse ajax response
 							var res    = Response.split(':$$:Add:$$:');
 							Response   = res[0];
+							Response   = `<div id="ligerosmartforms">${Response}</div>`;
+							morphdom(document.getElementById('ligerosmartforms'), Response,
+							{
+								childrenOnly: true,
+							});
+
 							arrayJSON  = res[1].split('@%@%@');
 
 							// Define some local variables in order to insert the form fields inside de DOM
@@ -170,10 +174,10 @@ Core.Agent.DynamicFieldByService = (function (TargetNS) {
 								FieldConfigInsert = AgentFieldConfigInsert;
 							}
 							// Remove Form fields from last selected service
-							cleanOldLigeroFormFields();
+								// cleanOldLigeroFormFields();
 
 							// Insert fields on DOM and restore form fields
-							var $ElementToUpdate = $(Response).insertBefore(FieldConfigInsert);
+							// var $ElementToUpdate = $(Response).insertBefore(FieldConfigInsert);
 							Object.keys( uploadFields ).forEach( function(i,v) {
 								$("#"+i).replaceWith( uploadFields[i] );
 							});
@@ -191,112 +195,108 @@ Core.Agent.DynamicFieldByService = (function (TargetNS) {
 							// 5) Fill preLoaded values/attributes
 							var JavaScriptString = '';
 							var ErrorMessage;
-							if ($ElementToUpdate && isJQueryObject($ElementToUpdate) && $ElementToUpdate.length) {
+							// if ($ElementToUpdate && isJQueryObject($ElementToUpdate) && $ElementToUpdate.length) {
 
-								// JS Execution
-								$ElementToUpdate.find('script').each(function () {
-									JavaScriptString += $(this).html();
-									$(this).remove();
-								});
-								try {
-									/*eslint-disable no-eval */
-									eval(JavaScriptString);
-									/*eslint-enable no-eval */
-								} catch (Event) {
-									$.noop(Event);
-								}
+							// 	// JS Execution
+							// 	$ElementToUpdate.find('script').each(function () {
+							// 		JavaScriptString += $(this).html();
+							// 		$(this).remove();
+							// 	});
+							// 	try {
+							// 		/*eslint-disable no-eval */
+							// 		eval(JavaScriptString);
+							// 		/*eslint-enable no-eval */
+							// 	} catch (Event) {
+							// 		$.noop(Event);
+							// 	}
 
-								// Handle errors
-								if ($ElementToUpdate.children().first().hasClass('ServerError')) {
-									ErrorMessage = $ElementToUpdate.children().first().data('message');
-									// Add class ServerError to the process select element
-									$('#ServiceID').addClass('ServerError');
-									// Set a custom error message to the proccess select element
-									$('#ProcessEntityIDServerError').children().first().text(ErrorMessage);
-								}
-								Core.Form.Validate.Init();
+							// 	// Handle errors
+							// 	if ($ElementToUpdate.children().first().hasClass('ServerError')) {
+							// 		ErrorMessage = $ElementToUpdate.children().first().data('message');
+							// 		// Add class ServerError to the process select element
+							// 		$('#ServiceID').addClass('ServerError');
+							// 		// Set a custom error message to the proccess select element
+							// 		$('#ProcessEntityIDServerError').children().first().text(ErrorMessage);
+							// 	}
+							// 	Core.Form.Validate.Init();
 
-								// Bind additional control events
-								Core.UI.TreeSelection.InitTreeSelection();
+							// 	// Bind additional control events
+							// 	Core.UI.TreeSelection.InitTreeSelection();
 
-								// Move help triggers into field rows for dynamic fields
-								$('.Row > .FieldHelpContainer').each(function () {
-									if (!$(this).next('label').find('.Marker').length) {
-										$(this).prependTo($(this).next('label'));
-									} else {
-										$(this).insertAfter($(this).next('label').find('.Marker'));
-									}
-								});
+							// 	// Move help triggers into field rows for dynamic fields
+							// 	$('.Row > .FieldHelpContainer').each(function () {
+							// 		if (!$(this).next('label').find('.Marker').length) {
+							// 			$(this).prependTo($(this).next('label'));
+							// 		} else {
+							// 			$(this).insertAfter($(this).next('label').find('.Marker'));
+							// 		}
+							// 	});
 
-								// Bind Dynamic Fields control events and trigger responsive events
-								Core.UI.TreeSelection.InitDynamicFieldTreeViewRestore();
-								if (Core.App.Responsive.IsSmallerOrEqual(Core.App.Responsive.GetScreenSize(), 'ScreenL'))
-									Core.App.Publish('Event.App.Responsive.SmallerOrEqualScreenL');
+							// 	// Bind Dynamic Fields control events and trigger responsive events
+							// 	Core.UI.TreeSelection.InitDynamicFieldTreeViewRestore();
+							// 	if (Core.App.Responsive.IsSmallerOrEqual(Core.App.Responsive.GetScreenSize(), 'ScreenL'))
+							// 		Core.App.Publish('Event.App.Responsive.SmallerOrEqualScreenL');
 
-								// Fill preLoaded data
-								jQuery.each( Core.Agent.DynamicFieldByServicePreLoad, function( oKey, oObj ) {
-									if (oKey == 'Message' && oObj.value.length) {
-										window.CKEDITOR.instances['RichText'].on('instanceReady', function() { 
-											window.CKEDITOR.instances['RichText'].setData(oObj.value);
-										});
-										window.CKEDITOR.instances['RichText'].setData(oObj.value);
-									} else {
-										$( '#' + oKey ).val( oObj.value ).attr('class',oObj.attr);
-									}
-								});
-								Core.Agent.DynamicFieldByServicePreLoad = {};
+							// 	// Fill preLoaded data
+							// 	jQuery.each( Core.Agent.DynamicFieldByServicePreLoad, function( oKey, oObj ) {
+							// 		if (oKey == 'Message' && oObj.value.length) {
+							// 			window.CKEDITOR.instances['RichText'].on('instanceReady', function() { 
+							// 				window.CKEDITOR.instances['RichText'].setData(oObj.value);
+							// 			});
+							// 			window.CKEDITOR.instances['RichText'].setData(oObj.value);
+							// 		} else {
+							// 			$( '#' + oKey ).val( oObj.value ).attr('class',oObj.attr);
+							// 		}
+							// 	});
+							// 	Core.Agent.DynamicFieldByServicePreLoad = {};
 
-								// Bind change events for new fields in order to trigger form update
-								$(".AddDFS").not('.RemoveDFS').each(function () {
+							// 	// Bind change events for new fields in order to trigger form update
+							// 	$(".AddDFS").not('.RemoveDFS').each(function () {
 
-									// Ignore some fields
-									if ($(this).hasClass('DateSelection') || $(this).hasClass('Validate_MaxLength'))
-										return true;
+							// 		// Ignore some fields
+							// 		if ($(this).hasClass('DateSelection') || $(this).hasClass('Validate_MaxLength'))
+							// 			return true;
 
-									// Get formID
-									var formID = "";
-									$("form").each(function () {
-										if ($(this).attr("name") == "compose") {
-											formID = $(this).attr("id");
-										}
-									});
+							// 		// Get formID
+							// 		var formID = "";
+							// 		$("form").each(function () {
+							// 			if ($(this).attr("name") == "compose") {
+							// 				formID = $(this).attr("id");
+							// 			}
+							// 		});
 
-									// Get all inputs inside the given form
-									var $inputs = $('#' + formID + ' :input');
-									var ids = [];
-									$inputs.each(function (index) {
-										if (typeof $(this).attr('id') != 'undefined' &&
-											typeof $(this).attr('name') != 'undefined') {
-											ids.push($(this).attr('id'));
-										}
-									});
+							// 		// Get all inputs inside the given form
+							// 		var $inputs = $('#' + formID + ' :input');
+							// 		var ids = [];
+							// 		$inputs.each(function (index) {
+							// 			if (typeof $(this).attr('id') != 'undefined' &&
+							// 				typeof $(this).attr('name') != 'undefined') {
+							// 				ids.push($(this).attr('id'));
+							// 			}
+							// 		});
 
-									// Prevent fileUpload reset
-									var index = ids.indexOf('FileUpload');
-									if (index !== -1) ids.splice(index, 1);
+							// 		// Prevent fileUpload reset
+							// 		var index = ids.indexOf('FileUpload');
+							// 		if (index !== -1) ids.splice(index, 1);
 
-									// Trigger field update event
-									var id = $(this).attr('id');
-									$("#" + id).bind('change',function(e) {
-										Core.Agent.DynamicFieldByServiceLastChangedElement = id;
-									});
-								});
-							} else {
-								$('#AJAXLoader').addClass('Hidden');
-							}
-
-							// Restore last focused field
-							//setTimeout(restoreLastFocus(), 100);
+							// 		// Trigger field update event
+							// 		var id = $(this).attr('id');
+							// 		$("#" + id).bind('change',function(e) {
+							// 			Core.Agent.DynamicFieldByServiceLastChangedElement = id;
+							// 		});
+							// 	});
+							// } else {
+							// 	$('#AJAXLoader').addClass('Hidden');
+							// }
 
 							// Bind change events on dynamic fields in order to check ACLs
 							// Exclude Modernized fields search from our filter
 							$("[id^='DynamicField_'][data-ligeroform!='ok']:not(.InputField_Search)").each(function () {
-									$(this)
+									$(this).unbind('change')
 									.bind('change', checkLigeroForms('HideAndShowDynamicFields') )
 							});
 							$("[id^='DynamicField_'][data-ligeroform!='ok']").each(function () {
 								$(this)
-								.bind('focus.jstree',  storeLigeroFormLastFocus)
 								.attr('data-ligeroform', 'ok');
 						});
 					}, 'html');
@@ -337,70 +337,10 @@ Core.Agent.DynamicFieldByService = (function (TargetNS) {
 				window.CKEDITOR.instances['RichText'].setData('');
 		};
 
-		// Store last focused field
-		var storeLigeroFormLastFocus = function(e) {
-			if(typeof e !== 'undefined' && typeof e.target.id !== 'undefined'){
-				var ModernizeSubelement = /_anchor$/;
-				var NewFocus = ''
-				if(ModernizeSubelement.test(e.target.id)){
-					NewFocus = $('#'+e.target.id).parent().parent().parent().attr('id');
-					NewFocus = NewFocus.replace('_Select','_Search');
-				} else {
-					NewFocus = e.target.id
-				}
-				Core.Agent.DynamicFieldByServiceLastFocus = NewFocus;
-			}
-		};
-
-		var restoreLastFocus = function(e) {
-			// @TODO: tratar a tecla TAB para casos onde há carregamento de novos campos a partir de ACLs
-			//			do hide and show, para que o foco não pule o próximo campo
-			// if (Core.Agent.DynamicFieldByServiceLastKey !== null
-			// 	&& Core.Agent.DynamicFieldByServiceLastKey == 'TAB'){
-			// 		console.log('TAB Key')
-			// 	} else {
-					// RR: Coloquei com o setTimeout para deixar essa função assincrona. O resultado foi melhor
-					// pois chamando direto fazia com que o campo modernized ocultasse a caixa de seleção
-					if(Core.Agent.DynamicFieldByServiceLastFocus !== null){
-						$('.jstree').parent().parent().remove();
-						console.log('olha ai '+Core.Agent.DynamicFieldByServiceLastFocus);
-
-						var setFocus = function(element){
-							$('#'+element).focus();
-						}
-						setTimeout(setFocus(Core.Agent.DynamicFieldByServiceLastFocus), 400);
-					}
-				// }
-		}
-
-		var restoreLastFocusFromCallBack = function(id){
-			if(Core.Agent.DynamicFieldByServiceLastFocus !== null
-				&& Core.Agent.DynamicFieldByServiceLastFocus.replace('_Search','').replace('_Select','') == id){
-				// Restore last focused field
-				setTimeout(restoreLastFocus(), 0);
-			}
-		}
-
-		// Sotre the last elements
-		var FormsKey = function(Event){
-			if(
-			typeof Event !== 'undefined'
-			&& Event.which === $.ui.keyCode.TAB) {
-				Core.Agent.DynamicFieldByServiceLastKey = 'TAB';
-			} else {
-				Core.Agent.DynamicFieldByServiceLastKey = '';
-			}
-			storeLigeroFormLastFocus(Event);
-		};
-		document.addEventListener('keyup', FormsKey, true);
-		document.addEventListener('keydown', FormsKey, true);
-		document.addEventListener('keydpress', FormsKey, true);
-
 		// Bind event for Service fields
 		$("#ServiceID[data-ligeroform!='ok']").each(function () {
 			$(this)
 				.bind('change', checkLigeroForms('DisplayActivityDialogAJAX'))
-				.bind('focus',  storeLigeroFormLastFocus)
 				.attr('data-ligeroform', 'ok');
 		});
 
